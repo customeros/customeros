a3f32d9b9a005b61b0c318a54dd73d3678a0b372
Eduard Firut <edi@openline.ai>
2022-10-20 16:10:03 +0300

    Replace HTTP client with Gin. Add CORS

--- cutomer-os-api/server/customer-os-api-server.go
+++ cutomer-os-api/server/customer-os-api-server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -13,15 +13,40 @@ import (
 
 const customerOSApiPort = "1010"
 
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
+	// Setting up Gin
+	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	r.Use(cors.New(config))
+
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = customerOSApiPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	r.Run(":" + port)
 }
