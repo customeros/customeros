6c19d6f208de4c7debd91f5023cb583d2c17d14c
xvasi <88987042+xvasi@users.noreply.github.com>
2022-10-26 15:26:05 +0200

    remove consul from customerOS infrastructure

--- deployment/k8s/local-minikube/port-forwarding.sh
+++ deployment/k8s/local-minikube/port-forwarding.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 NAMESPACE_NAME=openline-development
-kubectl port-forward consul-server-0 --namespace $NAMESPACE_NAME 8500:8500 &
 kubectl port-forward --namespace $NAMESPACE_NAME svc/postgresql-customer-os-dev 5432:5432 &
