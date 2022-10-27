03363edadff3d0c27851986c91c3b753295cda38
tsearle <torrey@openline.ai>
2022-10-27 14:25:30 +0200

    104 create environment setup script for ubuntu (#105)
    * initial script attempt
    
    * fix shebang
    
    * fix file test
    
    * fix permissions
    
    * remove sudo that doesn't work
    
    * install snap
    
    * snap != snapd
    
    * snapd doesn't work in a container install kubectl manually
    
    * hardcode kubctl version
    
    * use wget instead of curl

---
+++ deployment/k8s/local-minikube/0-ubuntu-install-prerequisites.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install ca-certificates curl gnupg lsb-release
+if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+fi
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $(whoami)
+wget -O /tmp/minikube_latest.deb https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo apt install -y /tmp/minikube_latest.deb
+wget -O /tmp/kubectl "https://dl.k8s.io/release/v1.25.3/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
