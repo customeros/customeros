450f6c2fcafd5c0f64c962c22418b9219f92cf9f
tsearle <torrey@openline.ai>
2022-10-27 13:31:43 +0200

    fix shebang

---
+++ deployment/k8s/local-minikube/0-ubuntu-install-prerequisites.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install ca-certificates curl gnupg lsb-release
+if [ -f "/etc/apt/sources.list.d/docker.list" ]; then
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+fi
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $(whoami)
+exec su -l $USER
+wget -O /tmp/minikube_latest.deb https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo apt install -y /tmp/minikube_latest.deb
+sudo snap install kubectl --classic
