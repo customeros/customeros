9a0a31a636bccd5f6802549b2729ee682774aba6
tsearle <torrey@openline.ai>
2022-10-27 13:34:42 +0200

    fix file test

--- deployment/k8s/local-minikube/0-ubuntu-install-prerequisites.sh
+++ deployment/k8s/local-minikube/0-ubuntu-install-prerequisites.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install ca-certificates curl gnupg lsb-release
-if [ -f "/etc/apt/sources.list.d/docker.list" ]; then
+if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
