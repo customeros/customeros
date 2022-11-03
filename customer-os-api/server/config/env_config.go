09d3cbc760e6481b2a9058e597d287148cb25717
alexopenline <alex@openline.ai>
2022-11-03 20:13:38 +0200

    Add configuration option to enabled/disable graphql playground

--- customer-os-api/server/config/env_config.go
+++ customer-os-api/server/config/env_config.go
@@ -2,7 +2,10 @@ package config
 
 type Config struct {
 	ApiPort string `env:"PORT"`
-	Neo4j   struct {
+	GraphQL struct {
+		PlaygroundEnabled bool `env:"GRAPHQL_PLAYGROUND_ENABLED" envDefault:"false"`
+	}
+	Neo4j struct {
 		Target                string `env:"NEO4J_TARGET,required"`
 		User                  string `env:"NEO4J_AUTH_USER,required,unset"`
 		Pwd                   string `env:"NEO4J_AUTH_PWD,required,unset"`
