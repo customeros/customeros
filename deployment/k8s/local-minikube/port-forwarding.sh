a1034cc601ba64667cf79e72d7385a63397be39a
tsearle <torrey@openline.ai>
2022-11-10 12:19:19 +0100

    more fixes

--- deployment/k8s/local-minikube/port-forwarding.sh
+++ deployment/k8s/local-minikube/port-forwarding.sh
@@ -3,3 +3,4 @@ NAMESPACE_NAME=openline
 kubectl port-forward --namespace $NAMESPACE_NAME service/postgresql-customer-os-dev 5432:5432 &
 kubectl port-forward --namespace $NAMESPACE_NAME service/neo4j-customer-os 7474:7474 &
 kubectl port-forward --namespace $NAMESPACE_NAME service/neo4j-customer-os 7687:7687 &
+wait
