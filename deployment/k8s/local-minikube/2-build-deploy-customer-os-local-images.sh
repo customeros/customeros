8a79e995addb96984ed8f74175416d81c8ee0711
tsearle <torrey@openline.ai>
2022-11-10 12:19:19 +0100

    more fixes

--- deployment/k8s/local-minikube/2-build-deploy-customer-os-local-images.sh
+++ deployment/k8s/local-minikube/2-build-deploy-customer-os-local-images.sh
@@ -3,21 +3,44 @@ NAMESPACE_NAME="openline"
 CUSTOMER_OS_HOME="$(dirname $(readlink -f $0))/../../../"
 
 ## Build Images
-cd  $CUSTOMER_OS_HOME/packages/server/customer-os-api/
 
 ## clean up previous install
 kubectl delete service customer-os-api-service --namespace $NAMESPACE_NAME
 kubectl delete deployments customer-os-api --namespace $NAMESPACE_NAME
-docker image rm customer-os-api
+kubectl delete service message-store-service --namespace $NAMESPACE_NAME
+kubectl delete deployments message-store --namespace $NAMESPACE_NAME
+docker image rm ghcr.io/openline-ai/openline-customer-os/customer-os-api:otter
+docker image rm ghcr.io/openline-ai/openline-customer-os/message-store:otter
 
-minikube image unload customer-os-api
+minikube image unload ghcr.io/openline-ai/openline-customer-os/customer-os-api:otter
+minikube image unload ghcr.io/openline-ai/openline-customer-os/message-store:otter
 
-# build image
+# build or download image
 
-docker build --no-cache -t customer-os-api .
-minikube image load customer-os-api:latest
+if [ "x$1" == "xbuild" ]; then
+	echo "locally building docker images"
+	cd  $CUSTOMER_OS_HOME/customer-os-api/server/
+	docker build --no-cache -t ghcr.io/openline-ai/openline-customer-os/customer-os-api:otter .
+	minikube image load ghcr.io/openline-ai/openline-customer-os/customer-os-api:otter
+
+	cd  $CUSTOMER_OS_HOME
+docker build --no-cache -t ghcr.io/openline-ai/openline-customer-os/message-store:otter -f packages/server/message-store/Dockerfile packages/server/
+	minikube image load ghcr.io/openline-ai/openline-customer-os/message-store:otter
+else
+	echo "installing pre-build images"
+	docker pull ghcr.io/openline-ai/openline-customer-os/customer-os-api:otter .
+	minikube image load ghcr.io/openline-ai/openline-customer-os/customer-os-api:otter
+
+	docker pull ghcr.io/openline-ai/openline-customer-os/message-store:otter .
+	minikube image load ghcr.io/openline-ai/openline-customer-os/message-store:otter
+fi
 
 # Deploy Images
 cd $CUSTOMER_OS_HOME/deployment/k8s/local-minikube
 kubectl apply -f apps-config/customer-os-api.yaml --namespace $NAMESPACE_NAME
 kubectl apply -f apps-config/customer-os-api-k8s-service.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/message-store.yaml --namespace $NAMESPACE_NAME
+kubectl apply -f apps-config/message-store-k8s-service.yaml --namespace $NAMESPACE_NAME
+
+cd $CUSTOMER_OS_HOME/packages/server/message-store/sql
+SQL_USER=openline SQL_DATABABASE=openline SQL_PASSWORD=password ./build_db.sh local-kube
