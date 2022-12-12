0051535d30c46641d5381440f57ec8ab34d66999
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2022-12-12 18:47:26 +0000

    Update provision-neo4j.sh

--- deployment/scripts/neo4j/provision-neo4j.sh
+++ deployment/scripts/neo4j/provision-neo4j.sh
@@ -5,7 +5,7 @@ NAMESPACE_NAME="openline"
 NEO4J_CYPHER="./openline-setup/customer-os.cypher"
 #########################################################
 
-VERSION=$(kubectl describe pod neo4j-customer-os-0 -n openline | grep HELM_NEO4J_VERSION | cut -b 33-38)
+VERSION=$(kubectl describe pod customer-db-neo4j-0 -n openline | grep HELM_NEO4J_VERSION | cut -b 33-38)
 
 neo_output="not empty"
 while  [ ! -z "$neo_output" ]; do
