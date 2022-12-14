8ad7f3eb9676a8e36eb5df6a079c862ea30660f6
edifirut <108661145+edifirut@users.noreply.github.com>
2022-12-14 01:46:40 -0800

    Fix initial data for oasis (#405)

--- deployment/scripts/postgresql/setup.sql
+++ deployment/scripts/postgresql/setup.sql
@@ -1,2 +1,7 @@
-INSERT INTO message_feeds(contact_id, first_name, last_name) VALUES ('echotest', 'Echo', 'Test');
-INSERT INTO message_items(type, username, message, channel, direction, message_feed_message_item) (SELECT 'MESSAGE', 'echo@oasis.openline.ai', 'call me!', 'MAIL', 'INBOUND', id FROM message_feeds WHERE contact_id='echotest');
+INSERT INTO conversations(contact_id, created_on, updated_on, state, last_message, last_sender_id, last_sender_type) VALUES ('echotest', now(), now(), 'NEW', 'test message', '1', 'CONTACT');
+INSERT INTO conversation_items(type, sender_id, sender_type, message, channel, direction, "time", conversation_conversation_item)
+    (SELECT 'MESSAGE','1', 'CONTACT','test message', 'CHAT', 'INBOUND', now(), id FROM conversations WHERE contact_id='echotest');
+INSERT INTO conversation_items(type, sender_id, sender_type, message, channel, direction, "time", conversation_conversation_item)
+    (SELECT 'MESSAGE','1', 'CONTACT','email message', 'MAIL', 'INBOUND', now(), id FROM conversations WHERE contact_id='echotest');
+INSERT INTO conversation_items(type, sender_id, sender_type, message, channel, direction, "time", conversation_conversation_item)
+    (SELECT 'MESSAGE','1', 'CONTACT', 'here would be the transcription for the phone call', 'VOICE', 'INBOUND', now(), id FROM conversations WHERE contact_id='echotest');
