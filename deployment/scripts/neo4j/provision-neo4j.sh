080809209ef2f7ff4ab5e8d7bd1fc860ae9bd231
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2022-12-14 08:52:02 +0000

    Update provision-neo4j.sh (#404)

--- deployment/scripts/neo4j/provision-neo4j.sh
+++ deployment/scripts/neo4j/provision-neo4j.sh
@@ -9,7 +9,7 @@ VERSION=$(kubectl describe pod customer-db-neo4j-0 -n openline | grep HELM_NEO4J
 
 neo_output="not empty"
 while  [ ! -z "$neo_output" ]; do
-	neo_output=$(cat $NEO4J_CYPHER |kubectl run --rm -i --namespace $NAMESPACE_NAME --image "neo4j:$VERSION" cypher-shell  -- bash -c 'NEO4J_PASSWORD=StrongLocalPa\$\$ cypher-shell -a neo4j://neo4j-customer-os.openline.svc.cluster.local:7687 -u neo4j --non-interactive' 2>&1 |grep -v "see a command prompt" |grep -v "deleted")
+	neo_output=$(cat $NEO4J_CYPHER |kubectl run --rm -i --namespace $NAMESPACE_NAME --image "neo4j:$VERSION" cypher-shell  -- bash -c 'NEO4J_PASSWORD=StrongLocalPa\$\$ cypher-shell -a neo4j://customer-db-neo4j.openline.svc.cluster.local:7687 -u neo4j --non-interactive' 2>&1 |grep -v "see a command prompt" |grep -v "deleted")
 	if [ ! -z "$neo_output" ]; then
 		kubectl delete pod cypher-shell -n $NAMESPACE_NAME
 		sleep 2
