23174d027037fd3339f9cbe65381c94c04e14027
tsearle <torrey@openline.ai>
2022-12-14 14:57:05 +0100

    fix neo4j provisioning (#412)

--- deployment/scripts/neo4j/provision-neo4j.sh
+++ deployment/scripts/neo4j/provision-neo4j.sh
@@ -5,7 +5,7 @@ NAMESPACE_NAME="openline"
 NEO4J_CYPHER="./openline-setup/customer-os.cypher"
 #########################################################
 
-VERSION=$(kubectl describe pod customer-db-neo4j-0 -n openline | grep HELM_NEO4J_VERSION | cut -b 33-38)
+VERSION=$(kubectl describe pod customer-db-neo4j-0 -n openline | grep HELM_NEO4J_VERSION | tr -s ' ' |cut -d ' ' -f 3)
 
 neo_output="not empty"
 while  [ ! -z "$neo_output" ]; do
