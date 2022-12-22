ab99bcaed0fce32441081848cd076f3a33bc0a6d
Eduard Firut <edi@openline.ai>
2022-12-22 10:59:34 +0200

    Fix docker build

--- packages/server/customer-os-api/build-docker-image-in-local.sh
+++ packages/server/customer-os-api/build-docker-image-in-local.sh
@@ -2,9 +2,9 @@
 rm -rf tmp-sources
 mkdir tmp-sources
 mkdir tmp-sources/customer-os-common-module
-mkdir tmp-sources/file-storage-api
+mkdir tmp-sources/customer-os-api
 
-cp -rv `ls -A | grep -vE "tmp-sources|tmp-sources/file-storage-api"` tmp-sources/file-storage-api
+cp -rv `ls -A | grep -vE "tmp-sources|tmp-sources/customer-os-api"` tmp-sources/customer-os-api
 cp -r ../customer-os-common-module/* tmp-sources/customer-os-common-module
 
 cp Dockerfile tmp-sources/Dockerfile
