bfea28a8ab5c880c1e086a86c7be99c31ca95f2f
edifirut <108661145+edifirut@users.noreply.github.com>
2022-12-22 01:13:34 -0800

    Fix docker build for COS and FS (#472)

---
+++ packages/server/file-storage-api/build-docker-image-in-local.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm -rf tmp-sources
+mkdir tmp-sources
+mkdir tmp-sources/customer-os-common-module
+mkdir tmp-sources/file-storage-api
+
+cp -rv `ls -A | grep -vE "tmp-sources|tmp-sources/file-storage-api"` tmp-sources/file-storage-api
+cp -r ../customer-os-common-module/* tmp-sources/customer-os-common-module
+
+cp Dockerfile tmp-sources/Dockerfile
+
+docker build -t aa tmp-sources/.
+
+rm -rf tmp-sources
