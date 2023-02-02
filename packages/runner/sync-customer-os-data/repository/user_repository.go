437cf139965615d8e5724da6abd1eb899337ff3b
alexopenline <alex@openline.ai>
2023-02-02 11:02:46 +0200

    Sync user emails as separate node

--- packages/runner/sync-customer-os-data/repository/user_repository.go
+++ packages/runner/sync-customer-os-data/repository/user_repository.go
@@ -10,6 +10,7 @@ import (
 
 type UserRepository interface {
 	MergeUser(tenant string, syncDate time.Time, user entity.UserData) (string, error)
+	MergeEmail(tenant, userId, email, externalSystem string, createdAt time.Time) error
 	GetUserIdForExternalId(tenant, userExternalId, externalSystem string) (string, error)
 }
 
@@ -34,19 +35,18 @@ func (r *userRepository) MergeUser(tenant string, syncDate time.Time, user entit
 	query := "MATCH (t:Tenant {name:$tenant})<-[:EXTERNAL_SYSTEM_BELONGS_TO_TENANT]-(e:ExternalSystem {id:$externalSystem}) " +
 		" MERGE (u:User)-[r:IS_LINKED_WITH {externalId:$externalId}]->(e) " +
 		" ON CREATE SET r.externalId=$externalId, r.externalOwnerId=$externalOwnerId, r.syncDate=$syncDate, u.id=randomUUID(), u.createdAt=$createdAt, " +
-		"               u.firstName=$firstName, u.lastName=$lastName, u.email=$email, " +
+		"               u.firstName=$firstName, u.lastName=$lastName, " +
 		"               u.source=$source, u.sourceOfTruth=$sourceOfTruth, u.appSource=$appSource, " +
 		"               u:%s" +
 		" ON MATCH SET 	r.syncDate = CASE WHEN u.sourceOfTruth=$sourceOfTruth THEN $syncDate ELSE r.syncDate END, " +
 		"				u.firstName = CASE WHEN u.sourceOfTruth=$sourceOfTruth THEN $firstName ELSE u.firstName END, " +
-		"				u.lastName = CASE WHEN u.sourceOfTruth=$sourceOfTruth THEN $lastName ELSE u.lastName END, " +
-		"				u.email = CASE WHEN u.sourceOfTruth=$sourceOfTruth THEN $email ELSE u.email END " +
+		"				u.lastName = CASE WHEN u.sourceOfTruth=$sourceOfTruth THEN $lastName ELSE u.lastName END " +
 		" WITH u, t " +
 		" MERGE (u)-[:USER_BELONGS_TO_TENANT]->(t)" +
 		" WITH u " +
 		" FOREACH (x in CASE WHEN u.sourceOfTruth <> $sourceOfTruth THEN [u] ELSE [] END | " +
 		"  MERGE (x)-[:ALTERNATE]->(alt:AlternateUser {source:$source, id:x.id}) " +
-		"    SET alt.updatedAt=$now, alt.appSource=$appSource, alt.email=$email, alt.firstName=$firstName, alt.lastName=$lastName " +
+		"    SET alt.updatedAt=$now, alt.appSource=$appSource, alt.firstName=$firstName, alt.lastName=$lastName " +
 		") " +
 		" RETURN u.id"
 
@@ -80,6 +80,39 @@ func (r *userRepository) MergeUser(tenant string, syncDate time.Time, user entit
 		return "", err
 	}
 	return dbRecord.(string), nil
+}
+
+func (r *userRepository) MergeEmail(tenant, userId, email, externalSystem string, createdAt time.Time) error {
+	session := utils.NewNeo4jWriteSession(*r.driver)
+	defer session.Close()
+
+	query := "MATCH (u:User {id:$userId})-[:USER_BELONGS_TO_TENANT]->(:Tenant {name:$tenant}) " +
+		" MERGE (u)-[r:HAS]->(e:Email {email: $email}) " +
+		" ON CREATE SET r.primary=true, " +
+		"				e.id=randomUUID(), " +
+		"				e.label=$label, " +
+		"				e.createdAt=$now, " +
+		"				e.updatedAt=$now, " +
+		"				e.source=$source, " +
+		"				e.sourceOfTruth=$sourceOfTruth, " +
+		"				e.appSource=$appSource, " +
+		"				e:%s "
+	_, err := session.WriteTransaction(func(tx neo4j.Transaction) (any, error) {
+		_, err := tx.Run(fmt.Sprintf(query, "Email_"+tenant),
+			map[string]interface{}{
+				"tenant":        tenant,
+				"userId":        userId,
+				"email":         email,
+				"createdAt":     createdAt,
+				"label":         "WORK",
+				"source":        externalSystem,
+				"sourceOfTruth": externalSystem,
+				"appSource":     externalSystem,
+				"now":           time.Now().UTC(),
+			})
+		return nil, err
+	})
+	return err
 }
 
 func (r *userRepository) GetUserIdForExternalId(tenant, userExternalId, externalSystem string) (string, error) {
