dbe9c884aec72caf48252d6fbbb837c43ce82bd1
alexopenline <alex@openline.ai>
2023-02-15 11:14:05 +0200

    fix3

--- packages/server/message-store-api/build-docker-image-in-local.sh
+++ packages/server/message-store-api/build-docker-image-in-local.sh
@@ -2,11 +2,13 @@
 rm -rf tmp-sources
 mkdir tmp-sources
 mkdir tmp-sources/customer-os-common-module
+mkdir tmp-sources/customer-os-api
 mkdir tmp-sources/message-store-api
 
 rsync -av --progress --exclude="tmp-sources" * tmp-sources/message-store-api
 cp .env tmp-sources/message-store-api/.env
 cp -r ../customer-os-common-module/* tmp-sources/customer-os-common-module
+cp -r ../customer-os-api/* tmp-sources/customer-os-api
 
 cp Dockerfile tmp-sources/Dockerfile
 
