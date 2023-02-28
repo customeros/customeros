4a8c8fa542e35c2f7cf6b06fa612bf03d7ebeeb4
tsearle <torrey@openline.ai>
2023-02-28 11:43:07 +0100

    simplify things

--- packages/server/ory-tunnel/tunnel.sh
+++ packages/server/ory-tunnel/tunnel.sh
@@ -3,6 +3,6 @@
 read -p "Which ORY project to tunnel to? [kind-babbage-plrgzvk56q]" project
 project=${project:-kind-babbage-plrgzvk56q}
 read -p "Which app to redirect to? [http://localhost.openline.svc.cluster.local:3001]" redirect 
-redirect=${redirect:-http://localhost.openline.svc.cluster.local:3001}
+redirect=${redirect:-http://localhost:3001}
 
 ory tunnel --dev --project $project $redirect
