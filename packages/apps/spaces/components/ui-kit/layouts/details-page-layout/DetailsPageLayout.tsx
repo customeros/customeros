97c33e1a71c9f0b0475ce0c22954858933e82e96
kasia <kasia@openline.ai>
2023-03-06 13:03:49 +0100

    #1123:  - Decreased grid-gap size for details page layout  - moved back button to the side of the details-layout  - changed back button to icon button  - added title to contact history

--- packages/apps/spaces/components/ui-kit/layouts/details-page-layout/DetailsPageLayout.tsx
+++ packages/apps/spaces/components/ui-kit/layouts/details-page-layout/DetailsPageLayout.tsx
@@ -2,7 +2,7 @@ import '@openline-ai/openline-web-chat/dist/esm/index.css';
 import React, { FC, ReactNode } from 'react';
 import classNames from 'classnames';
 import styles from './details-page-layout.module.scss';
-import { ArrowLeft, Button } from '../../atoms';
+import { ArrowLeft, Button, IconButton } from '../../atoms';
 
 interface DetailsPageLayout {
   children: ReactNode;
@@ -15,9 +15,11 @@ export const DetailsPageLayout: FC<DetailsPageLayout> = ({
   return (
     <div className={classNames(styles.layout)}>
       <div className={styles.backButton}>
-        <Button mode='secondary' icon={<ArrowLeft />} onClick={onNavigateBack}>
-          Back
-        </Button>
+        <IconButton
+          mode='secondary'
+          icon={<ArrowLeft />}
+          onClick={onNavigateBack}
+        />
       </div>
 
       {children}
