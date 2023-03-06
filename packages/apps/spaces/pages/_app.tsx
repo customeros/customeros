e9b3bbefc9c291f3b1c2cf74b00db3111d8e0a8d
kasia <kasia@openline.ai>
2023-03-06 15:34:02 +0100

    fixed styles

--- packages/apps/spaces/pages/_app.tsx
+++ packages/apps/spaces/pages/_app.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
