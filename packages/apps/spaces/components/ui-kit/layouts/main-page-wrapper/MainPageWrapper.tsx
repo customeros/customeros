663117df15d9556df021845f9a24515448335b21
kasia <kasia@openline.ai>
2023-03-09 17:17:03 +0100

    feat: #1061 Track user sign in

--- packages/apps/spaces/components/ui-kit/layouts/main-page-wrapper/MainPageWrapper.tsx
+++ packages/apps/spaces/components/ui-kit/layouts/main-page-wrapper/MainPageWrapper.tsx
@@ -45,10 +45,15 @@ export const MainPageWrapper = ({ children }: any) => {
         // User has a session!
         setSession(data);
         setUserEmail({ identity: getUserName(data.identity) });
+
         // @ts-expect-error analytics is added to window object from script
         if (window?.analytics) {
           // @ts-expect-error analytics is added to window object from script
           window.analytics.identify(data.id, {
+            email: data.identity,
+          });
+          // @ts-expect-error analytics is added to window object from script
+          window.analytics.track('Signed in', {
             email: data.identity,
           });
         }
