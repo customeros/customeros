04d07ecdd7c92ef1f89525315bc3867b84e2d7a1
kasia <kasia@openline.ai>
2023-03-17 15:09:11 +0100

    Avatar fix

--- packages/apps/spaces/components/ui-kit/molecules/contact-avatar/ContactAvatar.tsx
+++ packages/apps/spaces/components/ui-kit/molecules/contact-avatar/ContactAvatar.tsx
@@ -16,6 +16,10 @@ export const ContactAvatar: React.FC<Props> = ({ contactId, size = 30 }) => {
   const name = getContactDisplayName(data).split(' ');
   console.log('🏷️ ----- name: ', name);
   return (
-    <Avatar name={name?.[0] || ''} surname={name?.[2] || ''} size={size} />
+    <Avatar
+      name={name?.[0] || ''}
+      surname={name.length === 2 ? name[1] : name[2]}
+      size={size}
+    />
   );
 };
