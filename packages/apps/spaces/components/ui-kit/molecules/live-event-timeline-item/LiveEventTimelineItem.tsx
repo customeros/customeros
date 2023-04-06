3a231f9308c1cb7304a5b98f73d3a7406d8fd7e6
Gabriel Gontariu <gabi@openline.ai>
2023-04-06 17:55:04 +0300

    1516-comms-api-websockets

--- packages/apps/spaces/components/ui-kit/molecules/live-event-timeline-item/LiveEventTimelineItem.tsx
+++ packages/apps/spaces/components/ui-kit/molecules/live-event-timeline-item/LiveEventTimelineItem.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Skeleton } from 'primereact/skeleton';
 import { EmailTimelineItem } from '../email-timeline-item';
 import { TimelineItem } from '../../atoms/timeline-item';
 import useWebSocket from 'react-use-websocket';
-import { ConversationTimelineItem } from '../conversation-timeline-item';
 
 interface Props {
   contactId?: string;
@@ -53,67 +51,48 @@ export const LiveEventTimelineItem: React.FC<Props> = ({
     });
   };
   return (
-    <div className='flex flex-column w-full'>
-      <div className='flex-grow-1 w-full'>
-        <div className='flex flex-column mb-2'>
-          <div className='mb-2 flex justify-content-end'>
-            <Skeleton height='40px' width='50%' />
-          </div>
-          <div className='mb-2 flex justify-content-start'>
-            <Skeleton height='50px' width='40%' />
-          </div>
-          <div className='flex justify-content-end mb-2'>
-            <Skeleton height='45px' width='50%' />
-          </div>
-          <div className='flex justify-content-start'>
-            <Skeleton height='40px' width='45%' />
-          </div>
-        </div>
-
-        <div className='flex flex-column'>
-          {
-            // email
-            getSortedItems(liveEvents).map((event: any, index: number) => {
-              switch (event.channel) {
-                case 'EMAIL': {
-                  const fl =
-                    first && (index === 0 || index === liveEvents.length - 1);
-                  return (
-                    <TimelineItem
-                      first={fl}
-                      createdAt={event.createdAt}
-                      key={event.id}
-                    >
-                      <EmailTimelineItem
-                        content={event.content as string}
-                        contentType={event.contentType as string}
-                        sentBy={event.sentBy}
-                        sentTo={event.sentTo}
-                        interactionSession={{
-                          name: event.interactionSession?.name,
-                        }}
-                      />
-                    </TimelineItem>
-                  );
-                }
-                case 'Voice': {
-                  // todo
-                  break;
-                }
-                default:
-                  return event.channel ? (
-                    <div>
-                      Sorry, looks like &apos;{event.channel}&apos; live
-                      activity type is not supported yet{' '}
-                    </div>
-                  ) : (
-                    ''
-                  );
-              }
-            })
+    <div className='flex flex-column'>
+      {
+        // email
+        getSortedItems(liveEvents).map((event: any, index: number) => {
+          switch (event.channel) {
+            case 'EMAIL': {
+              const fl =
+                first && (index === 0 || index === liveEvents.length - 1);
+              return (
+                <TimelineItem
+                  first={fl}
+                  createdAt={event.createdAt}
+                  key={event.id}
+                >
+                  <EmailTimelineItem
+                    content={event.content as string}
+                    contentType={event.contentType as string}
+                    sentBy={event.sentBy}
+                    sentTo={event.sentTo}
+                    interactionSession={{
+                      name: event.interactionSession?.name,
+                    }}
+                  />
+                </TimelineItem>
+              );
+            }
+            case 'Voice': {
+              // todo
+              break;
+            }
+            default:
+              return event.channel ? (
+                <div>
+                  Sorry, looks like &apos;{event.channel}&apos; live activity
+                  type is not supported yet{' '}
+                </div>
+              ) : (
+                ''
+              );
           }
-        </div>
-      </div>
+        })
+      }
     </div>
   );
 };
