45b20ce355e2bec4d4d71ee1baa3632a9b49100c
alexopenline <alex@openline.ai>
2023-04-10 09:45:32 +0300

    Add organization proto files

--- packages/server/events-processing-common/proto/email/email_grpc.pb.go
+++ packages/server/events-processing-common/proto/email/email_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.20.1
-// source: email/email.proto
+// source: proto/email/email.proto
 
 package emailGrpcService
 
@@ -101,5 +101,5 @@ var EmailGrpcService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "email/email.proto",
+	Metadata: "proto/email/email.proto",
 }
