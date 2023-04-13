04b9db07a2ea4d31b5860ca465f480d47bee2ed4
kasia <kasia@openline.ai>
2023-04-13 14:12:48 +0200

    Updated ribbon copy

--- packages/apps/spaces/components/ui-kit/layouts/details-page-layout/DetailsPageLayout.tsx
+++ packages/apps/spaces/components/ui-kit/layouts/details-page-layout/DetailsPageLayout.tsx
@@ -19,8 +19,9 @@ export const DetailsPageLayout: FC<DetailsPageLayout> = ({
     <div className={classNames(styles.layout)}>
       {tenant && (
         <Ribbon top={0}>
-          When sending the email, please include {tenant}@getopenline.com so
-          that the email can be viewed in our timeline.
+          When sending emails to your contacts, please BCC
+          {tenant}@getopenline.com so that the email can be viewed in your
+          Openline timeline.
         </Ribbon>
       )}
 
