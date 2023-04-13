5994cff20050bc199986e541569dd8e82f3b9435
Eduard Firut <edi@openline.ai>
2023-04-13 11:57:31 +0300

    fix build

---
+++ packages/apps/spaces/components/finder/finder-table/index.ts
@@ -0,0 +1,7 @@
+export { ActionColumn } from './ActionTableHeader';
+export { AddressTableCell } from './AddressTableCell';
+export { ContactTableCell } from './ContactTableCell';
+export { EmailTableCell } from './EmailTableCell';
+export { FinderMergeItemTableHeader } from './FinderMergeItemTableHeader';
+export { FinderCell } from './FinderTableCell';
+export { OrganizationTableCell } from './OrganizationTableCell';
---
+++ packages/apps/spaces/components/finder/index.ts
@@ -0,0 +1,9 @@
+export {
+  ContactTableCell,
+  EmailTableCell,
+  AddressTableCell,
+  OrganizationTableCell,
+  FinderCell,
+  FinderMergeItemTableHeader,
+  ActionColumn,
+} from './finder-table';
