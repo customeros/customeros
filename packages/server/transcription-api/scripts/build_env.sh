2dd9fcf4e37d43aa41b0ff41610a0f9c3315c019
tsearle <torrey@openline.ai>
2023-04-17 18:11:30 +0200

    update

--- packages/server/transcription-api/scripts/build_env.sh
+++ packages/server/transcription-api/scripts/build_env.sh
@@ -8,4 +8,6 @@ echo "CUSTOMER_OS_API_URL=$CUSTOMER_OS_API_URL" >> /etc/transcription/environmen
 echo "TRANSCRIPTION_KEY=$TRANSCRIPTION_KEY" >> /etc/transcription/environment
 echo "VCON_API_KEY=$VCON_API_KEY" >> /etc/transcription/environment
 echo "VCON_API_URL=$VCON_API_URL" >> /etc/transcription/environment
+echo "FILE_STORE_API_KEY=$FILE_STORE_API_KEY" >> /etc/transcription/environment
+echo "FILE_STORE_API_URL=$FILE_STORE_API_URL" >> /etc/transcription/environment
 echo "REPLICATE_API_TOKEN=$REPLICATE_API_TOKEN" >> /etc/transcription/environment
