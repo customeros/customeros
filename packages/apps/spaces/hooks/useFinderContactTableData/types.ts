c105221a1610d4bd7a56c390352109ece85fcb7e
edifirut <108661145+edifirut@users.noreply.github.com>
2023-04-18 12:59:08 +0300

    [Spaces] - Contacts finder ( hidden for now ) (#1690)

---
+++ packages/apps/spaces/hooks/useFinderContactTableData/types.ts
@@ -0,0 +1,7 @@
+export { useGetDashboardDataQuery } from '../../graphQL/__generated__/generated';
+
+export type {
+  DashboardViewItem,
+  GetDashboardDataQueryVariables,
+  Pagination,
+} from '../../graphQL/__generated__/generated';
---
+++ packages/apps/spaces/hooks/useFinderContactTableData/useFinderContactTableData.ts
@@ -0,0 +1,77 @@
+import { ApolloError, NetworkStatus } from 'apollo-client';
+import { Pagination } from './types';
+import {
+  Contact,
+  GetContactListQueryVariables,
+  useGetContactListQuery,
+} from '../../graphQL/__generated__/generated';
+
+interface Props {
+  pagination: Pagination;
+  searchTerm: string;
+}
+
+interface Result {
+  data: Array<Contact> | null;
+  loading: boolean;
+  error: ApolloError | null;
+  fetchMore: (data: { variables: GetContactListQueryVariables }) => void;
+  variables: GetContactListQueryVariables;
+  networkStatus?: NetworkStatus;
+  totalElements: null | number;
+}
+export const useFinderContactTableData = ({
+  pagination,
+  searchTerm,
+}: Props): Result => {
+  const initialVariables = {
+    pagination: {
+      page: 0,
+      limit: 10,
+    },
+    searchTerm,
+  };
+  const { data, loading, error, refetch, variables, fetchMore, networkStatus } =
+    useGetContactListQuery({
+      fetchPolicy: 'cache-first',
+      notifyOnNetworkStatusChange: true,
+      variables: initialVariables,
+    });
+
+  if (loading) {
+    return {
+      loading: true,
+      error: null,
+      //@ts-expect-error revisit later, not matching generated types
+      data: data?.contacts?.content || [],
+      totalElements: data?.contacts?.totalElements || null,
+      fetchMore,
+      variables: variables || initialVariables,
+      networkStatus,
+    };
+  }
+
+  if (error) {
+    return {
+      error,
+      loading: false,
+      variables: variables || initialVariables,
+      networkStatus,
+      data: null,
+      fetchMore,
+      totalElements: data?.contacts?.totalElements || null,
+    };
+  }
+
+  return {
+    //@ts-expect-error revisit later, not matching generated types
+    data: data?.contacts?.content,
+    totalElements: data?.contacts?.totalElements || null,
+    fetchMore,
+    loading,
+    error: null,
+    variables: variables || initialVariables,
+    refetch,
+    networkStatus,
+  };
+};
