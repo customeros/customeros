348f188974bde15bc6566e89763cedd839a7e519
edifirut <108661145+edifirut@users.noreply.github.com>
2023-04-27 15:25:47 +0300

    Remove luncher app (#1762)

--- packages/apps/launcher/launcher-webapp/node_modules/next-auth/client/__tests__/helpers/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export function getBroadcastEvents(): any;
-export function printFetchCalls(mockCalls: any): any;
--- packages/apps/launcher/launcher-webapp/node_modules/next-auth/providers/hubspot.d.ts
+++
@@ -1,9 +0,0 @@
-import type { OAuthConfig, OAuthUserConfig } from ".";
-interface HubSpotProfile extends Record<string, any> {
-    user: string;
-    user_id: string;
-    hub_domain: string;
-    hub_id: string;
-}
-export default function HubSpot<P extends HubSpotProfile>(options: OAuthUserConfig<P>): OAuthConfig<P>;
-export {};
