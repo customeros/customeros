97bc54665d068ee6db92870306c1b0e75654b05b
kasia <kasia@openline.ai>
2023-04-28 17:45:47 +0200

    Added note id

--- packages/apps/spaces/hooks/useMeeting/useCreateMeetingOrganization.ts
+++ packages/apps/spaces/hooks/useMeeting/useCreateMeetingOrganization.ts
@@ -85,6 +85,7 @@ export const useCreateMeetingFromOrganization = ({
             name: '',
             startedAt: new Date().toISOString(),
             endedAt: new Date().toISOString(),
+            note: {html: '<p>Notes:</p>', appSource: 'OPENLINE'}
           },
         },
         //@ts-expect-error fixme
