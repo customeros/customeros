949e6644fedebbc8c16f89b32da340bc5890b1e3
alexopenline <alex@openline.ai>
2023-05-01 14:56:12 +0300

    Support multiple tasks in the runner

--- packages/runner/sync-customer-os-data/main.go
+++ packages/runner/sync-customer-os-data/main.go
@@ -12,19 +12,20 @@ import (
 	"time"
 )
 
-type taskQueue struct {
+type TaskQueue struct {
+	name          string
 	taskFunctions []func()
 	mutex         sync.Mutex
 	waitGroup     sync.WaitGroup
 }
 
-func (t *taskQueue) AddTask(function func()) {
+func (t *TaskQueue) AddTask(function func()) {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 	t.taskFunctions = append(t.taskFunctions, function)
 }
 
-func (t *taskQueue) RunTasks() {
+func (t *TaskQueue) RunTasks() {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 	if len(t.taskFunctions) == 0 {
@@ -68,21 +69,40 @@ func main() {
 
 	services.InitService.Init()
 
-	var taskQueue = &taskQueue{}
-	for {
-		if errPostgres == nil && errNeo4j == nil {
-			taskQueue.AddTask(func() {
-				runId, _ := uuid.NewRandom()
-				logrus.Infof("run id: %s syncing data into customer-os at %v", runId.String(), time.Now().UTC())
-				services.SyncService.Sync(ctx, runId.String())
-				logrus.Infof("run id: %s sync completed at %v", runId.String(), time.Now().UTC())
+	var taskQueueSyncCustomerOsData = &TaskQueue{name: "Sync Customer OS Data"}
+	var taskQueueSyncToEventStore = &TaskQueue{name: "Sync Neo4j Data to EventStore"}
 
-				timeout := time.Second * time.Duration(cfg.TimeoutAfterTaskRun)
-				logrus.Infof("waiting %v seconds before next run", timeout.Seconds())
-				time.Sleep(timeout)
-			})
+	go runTaskQueue(taskQueueSyncCustomerOsData, cfg.SyncCustomerOsData.TimeoutAfterTaskRun, []func(){
+		func() {
+			runId, _ := uuid.NewRandom()
+			logrus.Infof("run id: %s syncing data into customer-os at %v", runId.String(), time.Now().UTC())
+			services.SyncCustomerOsDataService.Sync(ctx, runId.String())
+			logrus.Infof("run id: %s sync completed at %v", runId.String(), time.Now().UTC())
+		},
+	})
+	if cfg.SyncToEventStore.Enabled {
+		go runTaskQueue(taskQueueSyncToEventStore, cfg.SyncToEventStore.TimeoutAfterTaskRun, []func(){
+			func() {
+				services.SyncToEventStoreService.SyncEmails(ctx)
+			},
+		})
+	}
+
+	select {}
+}
+
+func runTaskQueue(taskQueue *TaskQueue, timeoutAfterTaskRun int, taskFuncs []func()) {
+	for {
+		for _, v := range taskFuncs {
+			taskQueue.AddTask(v)
 		}
+
 		taskQueue.RunTasks()
+
+		// Wait for a fixed amount of time before running the tasks again
+		timeout := time.Second * time.Duration(timeoutAfterTaskRun)
+		logrus.Infof("waiting %v seconds before next run for %s", timeout.Seconds(), taskQueue.name)
+		time.Sleep(timeout)
 	}
 }
 
