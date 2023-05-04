1fd7ad6ebe9da256221c5f238827447c37cb72bc
Gabriel Gontariu <gabi@openline.ai>
2023-05-04 15:08:37 +0300

    Fix compilation

--- packages/apps/spaces/hooks/useMeeting/index.ts
+++ packages/apps/spaces/hooks/useMeeting/index.ts
@@ -1,7 +1,6 @@
 export { useCreateMeetingFromContact } from './useCreateMeetingContact';
 export { useCreateMeetingFromOrganization } from './useCreateMeetingOrganization';
 export { useUpdateMeeting } from './useUpdateMeeting';
-export { useUpdateMeetingRecording } from './useUpdateMeetingRecording';
 export { useUnlinkMeetingAttachement } from './useUnlinkMeetingAttachement';
 export { useLinkMeetingAttachement } from './useLinkMeetingAttachement';
 export { useLinkMeetingAttendee } from './useLinkMeetingAttendees';
