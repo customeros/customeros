7e5d6a9c936bb74a2ab06066eaabbaa24a3807c2
alexopenline <alex@openline.ai>
2023-05-08 17:28:12 +0300

    CR changes

--- packages/server/events-processing-platform/eventstroredb/config.go
+++ packages/server/events-processing-platform/eventstroredb/config.go
@@ -7,10 +7,10 @@ type EventStoreConfig struct {
 	KeepAliveTimeout   int    `env:"EVENT_STORE_CONNECTION_KEEP_ALIVE_TIMEOUT_MS" envDefault:"10000"`
 	KeepAliveInterval  int    `env:"EVENT_STORE_CONNECTION_KEEP_ALIVE_INTERVAL_MS" envDefault:"10000"`
 	ConnectionUser     string `env:"EVENT_STORE_CONNECTION_USERNAME"`
-	ConnectionPassword string `env:"EVENT_STORE_CONNECTION_PASSWORD"`
+	ConnectionPassword string `env:"EVENT_STORE_CONNECTION_PASSWORD,unset"`
 
 	AdminUsername      string `env:"EVENT_STORE_ADMIN_USERNAME"`
-	AdminPassword      string `env:"EVENT_STORE_ADMIN_PASSWORD"`
+	AdminPassword      string `env:"EVENT_STORE_ADMIN_PASSWORD,unset"`
 	OperationsUsername string `env:"EVENT_STORE_OPS_USERNAME"`
-	OperationsPassword string `env:"EVENT_STORE_OPS_PASSWORD"`
+	OperationsPassword string `env:"EVENT_STORE_OPS_PASSWORD,unset"`
 }
