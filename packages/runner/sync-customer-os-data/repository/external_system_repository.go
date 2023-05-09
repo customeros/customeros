5334d32fc6b13755de27d23ac6a6663e00a8774e
alexopenline <alex@openline.ai>
2023-05-09 17:48:57 +0300

    Use neo4j helper session methods from common module

--- packages/runner/sync-customer-os-data/repository/external_system_repository.go
+++ packages/runner/sync-customer-os-data/repository/external_system_repository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
-	"github.com/openline-ai/openline-customer-os/packages/runner/sync-customer-os-data/utils"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
 	"time"
 )
 
