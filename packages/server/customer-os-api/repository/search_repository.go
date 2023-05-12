f5762a37a813d7550a9f30991c723719450a02b8
edifirut <108661145+edifirut@users.noreply.github.com>
2023-05-12 13:18:03 +0300

    [COS] - improve gCli search results (#1945)
    * [COS] - improve gCli search results

--- packages/server/customer-os-api/repository/search_repository.go
+++ packages/server/customer-os-api/repository/search_repository.go
@@ -28,15 +28,12 @@ func (r *searchRepository) GCliSearch(ctx context.Context, tenant, keyword strin
 
 	params := map[string]any{
 		"tenant":        tenant,
-		"fuzzyKeyword":  fmt.Sprintf("%s~", keyword),
-		"keyword":       keyword,
+		"keyword":       fmt.Sprintf("%s*", keyword),
 		"indexStandard": "basicSearchStandard_location_terms",
 		"limit":         limit,
 	}
 	query := "CALL { " +
-		" CALL db.index.fulltext.queryNodes($indexStandard, $keyword) YIELD node, score RETURN score, node, labels(node) as labels limit $limit " +
-		" union" +
-		" CALL db.index.fulltext.queryNodes($indexStandard, $fuzzyKeyword) YIELD node, score RETURN score, node, labels(node) as labels limit $limit " +
+		" CALL db.index.fulltext.queryNodes($indexStandard, $keyword) YIELD node, score WHERE score > 0 RETURN score, node, labels(node) as labels limit $limit " +
 		"} " +
 		" with labels, node, score order by score desc " +
 		" with labels, node, collect(score) as scores " +
