3af54f67d2f7a7223521d2f9cb641efbb7d82668
alexopenline <alex@openline.ai>
2023-05-19 08:36:48 +0300

    Fix comms-api dependency on customer-os-api in workflow

--- packages/server/comms-api/test/graph/resolver/workspace.resolvers.go
+++ packages/server/comms-api/test/graph/resolver/workspace.resolvers.go
@@ -10,7 +10,7 @@ import (
 	"github.com/openline-ai/openline-customer-os/packages/server/comms-api/test/graph/model"
 )
 
-// LocationUpdate is the resolver for the location_Update field.
-func (r *mutationResolver) LocationUpdate(ctx context.Context, input model.LocationUpdateInput) (*model.Location, error) {
-	panic(fmt.Errorf("not implemented: LocationUpdate - location_Update"))
+// WorkspaceMergeToTenant is the resolver for the workspace_MergeToTenant field.
+func (r *mutationResolver) WorkspaceMergeToTenant(ctx context.Context, workspace model.WorkspaceInput, tenant string) (*model.Result, error) {
+	panic(fmt.Errorf("not implemented: WorkspaceMergeToTenant - workspace_MergeToTenant"))
 }
