a7f2f708aabbe83ae7268319a447d1b29fe7a59c
tsearle <torrey@openline.ai>
2023-06-09 09:48:08 +0200

    made out of order even more robust

--- packages/server/events-processing-platform/subscriptions/graph/contact_event_handlers_it_test.go
+++ packages/server/events-processing-platform/subscriptions/graph/contact_event_handlers_it_test.go
@@ -48,18 +48,18 @@ func TestGraphContactEventHandler_OnContactCreate(t *testing.T) {
 	require.Equal(t, 1, neo4jt.GetCountOfNodes(ctx, testDatabase.Driver, "Contact_"+tenantName), "Incorrect number of Contact_%s nodes in Neo4j", tenantName)
 	require.Equal(t, 1, neo4jt.GetCountOfRelationships(ctx, testDatabase.Driver, "CONTACT_BELONGS_TO_TENANT"), "Incorrect number of CONTACT_BELONGS_TO_TENANT relationships in Neo4j")
 
-	dbNode, err := neo4jt.GetNodeById(ctx, testDatabase.Driver, "Contact_"+tenantName, myContactId.String())
+	dbContactNode, err := neo4jt.GetNodeById(ctx, testDatabase.Driver, "Contact_"+tenantName, myContactId.String())
 	require.Nil(t, err)
-	require.NotNil(t, dbNode)
-	props := utils.GetPropsFromNode(*dbNode)
+	require.NotNil(t, dbContactNode)
+	contactProps := utils.GetPropsFromNode(*dbContactNode)
 
-	require.Equal(t, myContactId.String(), utils.GetStringPropOrEmpty(props, "id"))
-	require.Equal(t, "Bob", utils.GetStringPropOrEmpty(props, "firstName"))
-	require.Equal(t, "Smith", utils.GetStringPropOrEmpty(props, "lastName"))
-	require.Equal(t, "Mr.", utils.GetStringPropOrEmpty(props, "prefix"))
-	require.Equal(t, "This is a test contact.", utils.GetStringPropOrEmpty(props, "description"))
+	require.Equal(t, myContactId.String(), utils.GetStringPropOrEmpty(contactProps, "id"))
+	require.Equal(t, "Bob", utils.GetStringPropOrEmpty(contactProps, "firstName"))
+	require.Equal(t, "Smith", utils.GetStringPropOrEmpty(contactProps, "lastName"))
+	require.Equal(t, "Mr.", utils.GetStringPropOrEmpty(contactProps, "prefix"))
+	require.Equal(t, "This is a test contact.", utils.GetStringPropOrEmpty(contactProps, "description"))
 
-	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(props, "appSource"))
+	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(contactProps, "appSource"))
 
 }
 
@@ -134,6 +134,19 @@ func TestGraphContactEventHandler_OnContactCreateWithEmail(t *testing.T) {
 	require.Equal(t, email, utils.GetStringPropOrEmpty(emailProps, "rawEmail"))
 	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(emailProps, "appSource"))
 
+	dbContactNode, err := neo4jt.GetNodeById(ctx, testDatabase.Driver, "Contact_"+tenantName, myContactId.String())
+	require.Nil(t, err)
+	require.NotNil(t, dbContactNode)
+	contactProps := utils.GetPropsFromNode(*dbContactNode)
+
+	require.Equal(t, myContactId.String(), utils.GetStringPropOrEmpty(contactProps, "id"))
+	require.Equal(t, "Bob", utils.GetStringPropOrEmpty(contactProps, "firstName"))
+	require.Equal(t, "Smith", utils.GetStringPropOrEmpty(contactProps, "lastName"))
+	require.Equal(t, "Mr.", utils.GetStringPropOrEmpty(contactProps, "prefix"))
+	require.Equal(t, "This is a test contact.", utils.GetStringPropOrEmpty(contactProps, "description"))
+
+	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(contactProps, "appSource"))
+
 }
 
 func TestGraphContactEventHandler_OnContactCreateWithEmailOutOfOrder(t *testing.T) {
@@ -206,5 +219,97 @@ func TestGraphContactEventHandler_OnContactCreateWithEmailOutOfOrder(t *testing.
 	require.Equal(t, myMailId.String(), utils.GetStringPropOrEmpty(emailProps, "id"))
 	require.Equal(t, email, utils.GetStringPropOrEmpty(emailProps, "rawEmail"))
 	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(emailProps, "appSource"))
+
+	dbContactNode, err := neo4jt.GetNodeById(ctx, testDatabase.Driver, "Contact_"+tenantName, myContactId.String())
+	require.Nil(t, err)
+	require.NotNil(t, dbContactNode)
+	contactProps := utils.GetPropsFromNode(*dbContactNode)
+
+	require.Equal(t, myContactId.String(), utils.GetStringPropOrEmpty(contactProps, "id"))
+	require.Equal(t, "Bob", utils.GetStringPropOrEmpty(contactProps, "firstName"))
+	require.Equal(t, "Smith", utils.GetStringPropOrEmpty(contactProps, "lastName"))
+	require.Equal(t, "Mr.", utils.GetStringPropOrEmpty(contactProps, "prefix"))
+	require.Equal(t, "This is a test contact.", utils.GetStringPropOrEmpty(contactProps, "description"))
+
+	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(contactProps, "appSource"))
+
+}
+
+func TestGraphContactEventHandler_OnContactCreateWithVeryEmailOutOfOrder(t *testing.T) {
+	ctx := context.TODO()
+	defer tearDownTestCase(ctx, testDatabase)(t)
+
+	neo4jt.CreateTenant(ctx, testDatabase.Driver, tenantName)
+	contactEventHandler := &GraphContactEventHandler{
+		Repositories: testDatabase.Repositories,
+	}
+	emailEventHandler := &GraphEmailEventHandler{
+		Repositories: testDatabase.Repositories,
+	}
+	myContactId, _ := uuid.NewUUID()
+	myMailId, _ := uuid.NewUUID()
+	curTime := time.Now().UTC()
+
+	contactAggregate := contactAggregate.NewContactAggregateWithTenantAndID(tenantName, myContactId.String())
+
+	event, err := contactEvents.NewContactLinkEmailEvent(contactAggregate, tenantName, myMailId.String(), "work", true, curTime)
+	require.Nil(t, err)
+	err = contactEventHandler.OnEmailLinkToContact(context.Background(), event)
+	require.Nil(t, err)
+
+	contactDto := &contactModels.ContactDto{
+		ID:          myContactId.String(),
+		Tenant:      tenantName,
+		FirstName:   "Bob",
+		LastName:    "Smith",
+		Prefix:      "Mr.",
+		Description: "This is a test contact.",
+		Source:      commonModels.Source{Source: "N/A", SourceOfTruth: "N/A", AppSource: "unit-test"},
+		CreatedAt:   &curTime,
+		UpdatedAt:   &curTime,
+	}
+	event, err = contactEvents.NewContactCreateEvent(contactAggregate, contactDto, curTime, curTime)
+	require.Nil(t, err)
+	err = contactEventHandler.OnContactCreate(context.Background(), event)
+	require.Nil(t, err)
+
+	emailAggregate := emailAggregate.NewEmailAggregateWithTenantAndID(tenantName, myMailId.String())
+	email := "test@test.com"
+
+	event, err = emailEvents.NewEmailCreateEvent(emailAggregate, tenantName, email, "N/A", "N/A", "unit-test", curTime, curTime)
+	require.Nil(t, err)
+	err = emailEventHandler.OnEmailCreate(context.Background(), event)
+	require.Nil(t, err)
+
+	require.Equal(t, 1, neo4jt.GetCountOfNodes(ctx, testDatabase.Driver, "Contact"))
+	require.Equal(t, 1, neo4jt.GetCountOfNodes(ctx, testDatabase.Driver, "Contact_"+tenantName), "Incorrect number of Contact_%s nodes in Neo4j", tenantName)
+	require.Equal(t, 1, neo4jt.GetCountOfRelationships(ctx, testDatabase.Driver, "CONTACT_BELONGS_TO_TENANT"), "Incorrect number of CONTACT_BELONGS_TO_TENANT relationships in Neo4j")
+
+	require.Equal(t, 1, neo4jt.GetCountOfNodes(ctx, testDatabase.Driver, "Email"))
+	require.Equal(t, 1, neo4jt.GetCountOfNodes(ctx, testDatabase.Driver, "Email_"+tenantName), "Incorrect number of Email_%s nodes in Neo4j", tenantName)
+	require.Equal(t, 1, neo4jt.GetCountOfRelationships(ctx, testDatabase.Driver, "EMAIL_ADDRESS_BELONGS_TO_TENANT"), "Incorrect number of EMAIL_ADDRESS_BELONGS_TO_TENANT relationships in Neo4j")
+	require.Equal(t, 1, neo4jt.GetCountOfRelationships(ctx, testDatabase.Driver, "HAS"), "Incorrect number of HAS relationships in Neo4j")
+
+	emailNode, err := neo4jt.GetNodeById(ctx, testDatabase.Driver, "Email_"+tenantName, myMailId.String())
+	require.Nil(t, err)
+	require.NotNil(t, emailNode)
+	emailProps := utils.GetPropsFromNode(*emailNode)
+
+	require.Equal(t, myMailId.String(), utils.GetStringPropOrEmpty(emailProps, "id"))
+	require.Equal(t, email, utils.GetStringPropOrEmpty(emailProps, "rawEmail"))
+	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(emailProps, "appSource"))
+
+	dbContactNode, err := neo4jt.GetNodeById(ctx, testDatabase.Driver, "Contact_"+tenantName, myContactId.String())
+	require.Nil(t, err)
+	require.NotNil(t, dbContactNode)
+	contactProps := utils.GetPropsFromNode(*dbContactNode)
+
+	require.Equal(t, myContactId.String(), utils.GetStringPropOrEmpty(contactProps, "id"))
+	require.Equal(t, "Bob", utils.GetStringPropOrEmpty(contactProps, "firstName"))
+	require.Equal(t, "Smith", utils.GetStringPropOrEmpty(contactProps, "lastName"))
+	require.Equal(t, "Mr.", utils.GetStringPropOrEmpty(contactProps, "prefix"))
+	require.Equal(t, "This is a test contact.", utils.GetStringPropOrEmpty(contactProps, "description"))
+
+	require.Equal(t, "unit-test", utils.GetStringPropOrEmpty(contactProps, "appSource"))
 
 }
