e43c10b6ac3f214a577b55ae3cc6ae814486dd8f
alexopenline <alex@openline.ai>
2023-06-13 19:11:20 +0300

    Improve logging on update phone number

--- packages/server/customer-os-api/repository/phone_number_repository.go
+++ packages/server/customer-os-api/repository/phone_number_repository.go
@@ -201,6 +201,7 @@ func (r *phoneNumberRepository) GetByIdAndRelatedEntity(ctx context.Context, ent
 	span, ctx := opentracing.StartSpanFromContext(ctx, "PhoneNumberRepository.GetByIdAndRelatedEntity")
 	defer span.Finish()
 	tracing.SetDefaultNeo4jRepositorySpanTags(ctx, span)
+	span.LogFields(log.String("phoneNumberId", phoneNumberId), log.String("entityId", entityId))
 
 	session := utils.NewNeo4jReadSession(ctx, *r.driver)
 	defer session.Close(ctx)
@@ -216,6 +217,7 @@ func (r *phoneNumberRepository) GetByIdAndRelatedEntity(ctx context.Context, ent
 	}
 	query += ` MATCH (entity)-[rel:HAS]->(p:PhoneNumber {id:$phoneNumberId})-[:PHONE_NUMBER_BELONGS_TO_TENANT]->(t)
 			RETURN p`
+	span.LogFields(log.String("query", query))
 
 	result, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
 		if queryResult, err := tx.Run(ctx, query,
