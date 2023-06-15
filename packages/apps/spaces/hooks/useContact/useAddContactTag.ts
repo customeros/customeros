369e158b7d4ef96287102b5148c3f8af8e4abf83
Alex Calinica <acalinica@openline.ai>
2023-06-15 20:42:54 +0300

    refactor: usage of apollo in useAddContactTag hook

--- packages/apps/spaces/hooks/useContact/useAddContactTag.ts
+++ packages/apps/spaces/hooks/useContact/useAddContactTag.ts
@@ -1,68 +1,57 @@
-import {
-  AddTagToContactMutation,
-  ContactTagInput,
-  useAddTagToContactMutation,
-} from './types';
+import { ContactTagInput, useAddTagToContactMutation } from './types';
 import { GetContactTagsDocument } from '@spaces/graphql';
-import { gql, useApolloClient } from '@apollo/client';
+import { gql } from '@apollo/client';
 import { toast } from 'react-toastify';
 
 interface Result {
-  onAddTagToContact: (
-    input: ContactTagInput,
-  ) => Promise<AddTagToContactMutation['contact_AddTagById'] | null>;
+  onAddTagToContact: (input: ContactTagInput) => void;
 }
 export const useAddTagToContact = ({
   contactId,
 }: {
   contactId: string;
 }): Result => {
-  const client = useApolloClient();
+  // const client = useApolloClient();
   const [addTagToContactMutation] = useAddTagToContactMutation();
 
-  const handleAddTagToContact: Result['onAddTagToContact'] = async (
-    contactTagInput,
-  ) => {
-    try {
-      const response = await addTagToContactMutation({
-        variables: { input: contactTagInput },
-      });
-      const data = client.cache.readQuery({
-        query: GetContactTagsDocument,
-        variables: { id: contactTagInput.contactId },
-      });
+  const onAddTagToContact: Result['onAddTagToContact'] = (contactTagInput) => {
+    addTagToContactMutation({
+      variables: { input: contactTagInput },
+      onError: () => {
+        toast.error('Something went wrong while adding tag', {
+          toastId: `contact-add-tag-error`,
+        });
+      },
+      update: (cache, response) => {
+        const data = cache.readQuery({
+          query: GetContactTagsDocument,
+          variables: { id: contactTagInput.contactId },
+        });
 
-      client.cache.writeFragment({
-        id: `Contact:${contactId}`,
-        fragment: gql`
-          fragment Tags on Contact {
-            id
-            tags
-          }
-        `,
-        data: {
-          // @ts-expect-error revisit
-          ...data.contact,
-          tags: [
+        cache.writeFragment({
+          id: `Contact:${contactId}`,
+          fragment: gql`
+            fragment Tags on Contact {
+              id
+              tags
+            }
+          `,
+          data: {
             // @ts-expect-error revisit
-            ...(data.tags ?? []),
-            // @ts-expect-error revisit
-            ...(response.data ? response.data.contact_AddTagById.tags : []),
-          ],
-        },
-      });
-      // Update the cache with the new object
-      return response.data?.contact_AddTagById ?? null;
-    } catch (err) {
-      toast.error('Something went wrong while adding tag', {
-        toastId: `contact-add-tag-error`,
-      });
-      console.error(err);
-      return null;
-    }
+            ...data.contact,
+            tags: [
+              // @ts-expect-error revisit
+              ...(data.tags ?? []),
+              // @ts-expect-error revisit
+              ...(response.data ? response.data.contact_AddTagById.tags : []),
+            ],
+          },
+        });
+      },
+    });
   };
 
   return {
-    onAddTagToContact: handleAddTagToContact,
+    onAddTagToContact,
   };
 };
