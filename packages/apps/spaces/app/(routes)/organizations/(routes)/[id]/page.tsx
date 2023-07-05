becf34254ab0ad21bde5923990ac736e1c1bbe56
Alex Calinica <acalinica@openline.ai>
2023-07-05 13:13:18 +0300

    chore: weird bug showcase

--- packages/apps/spaces/app/(routes)/organizations/(routes)/[id]/page.tsx
+++ packages/apps/spaces/app/(routes)/organizations/(routes)/[id]/page.tsx
@@ -1,3 +1,8 @@
+import { dehydrate, Hydrate } from '@tanstack/react-query';
+import getQueryClient from '@shared/util/getQueryClient';
+import { getGraphQLClient } from '@shared/util/getGraphQLClient';
+import { gql } from 'graphql-request';
+
 import { SideSection } from './components/SideSection';
 import { MainSection } from './components/MainSection';
 import {
@@ -7,7 +12,53 @@ import {
   OrganizationHeader,
 } from './components/OrganizationInfo';
 
-export default function OrganizationPage() {
+const ORGANIZATION_QUERY = gql`
+  query Organization($id: ID!) {
+    organization(id: $id) {
+      id
+      name
+      description
+      domains
+      subIndustry
+      website
+      industry
+      isPublic
+      market
+      employees
+      socials {
+        id
+        platformName
+        url
+      }
+      relationshipStages {
+        relationship
+        stage
+      }
+    }
+  }
+`;
+
+interface OrganizationPageProps {
+  params: { id: string };
+}
+
+export default async function OrganizationPage({
+  params,
+}: OrganizationPageProps) {
+  // const queryClient = getQueryClient();
+  // await queryClient.prefetchQuery(['organization', params.id]);
+  // const dehydratedState = dehydrate(queryClient);
+
+  const graphqlClient = getGraphQLClient();
+  try {
+    const req = await graphqlClient.request(ORGANIZATION_QUERY, {
+      id: params.id,
+    });
+    console.log(req);
+  } catch (e) {
+    console.log(e);
+  }
+
   return (
     <>
       <SideSection>
@@ -19,7 +70,7 @@ export default function OrganizationPage() {
           <OrganizationTabs />
         </OrganizationInfo>
       </SideSection>
-      <MainSection></MainSection>
+      <MainSection>{params.id}</MainSection>
     </>
   );
 }
--- packages/apps/spaces/app/util/getGraphQLClient.tsx
+++ packages/apps/spaces/app/util/getGraphQLClient.tsx
@@ -3,8 +3,11 @@ import { GraphQLClient } from 'graphql-request';
 export const getGraphQLClient = () => {
   return new GraphQLClient(`${process.env.CUSTOMER_OS_API_PATH}/query`, {
     headers: {
-      'X-Openline-API-KEY': process.env.CUSTOMER_OS_API_KEY as string,
-      'X-Openline-USERNAME': 'development@openline.ai',
+      // 'X-Openline-API-KEY': process.env.CUSTOMER_OS_API_KEY as string,
+      // 'X-Openline-USERNAME': 'development@openline.ai',
+
+      'X-Openline-API-KEY': 'dd9d2474-b4a9-4799-b96f-73cd0a2917e4',
+      'X-Openline-USERNAME': 'acalinica@openline.ai',
     },
   });
 };
