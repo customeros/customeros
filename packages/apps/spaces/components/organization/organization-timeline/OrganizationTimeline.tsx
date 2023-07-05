4a9e5d376cb61115dd99fe85f214b9e11e957b28
kasia <kasia@openline.ai>
2023-07-05 12:13:40 +0200

    wip

--- packages/apps/spaces/components/organization/organization-timeline/OrganizationTimeline.tsx
+++ packages/apps/spaces/components/organization/organization-timeline/OrganizationTimeline.tsx
@@ -1,41 +1,97 @@
-import React, { useState } from 'react';
-import { Timeline, TimelineStatus } from '@spaces/organisms/timeline';
-import { useOrganizationTimeline } from '@spaces/hooks/useOrganizationTimeline';
+import React, { useCallback, useRef, useState } from 'react';
+import {
+  buckets,
+  events,
+} from '@spaces/organization/organization-timeline/data';
+// @ts-expect-error
+import { FixedSizeList as List } from 'react-window';
+// @ts-expect-error
+import InfiniteLoader from 'react-window-infinite-loader';
+import styles from './organization-timeline.module.scss';
+import { DateTimeUtils } from '@spaces/utils/date';
+// Assuming all events have same height
+const itemSize = 32;
+
+function getEventByIndex(index) {
+  // for simplicity, return the single event we have for each item
+  // replace this with your actual logic to map an index to an event
+  return events[0];
+}
 
 export const OrganizationTimeline = ({ id }: { id: string }) => {
-  const { data, loading, error, fetchMore } = useOrganizationTimeline({
-    organizationId: id,
-  });
+  const listRef = useRef<any>(null);
+  const otherRef = useRef<any>(null);
+  const itemCount = events.length;
+  const handleScrollToBottom = useCallback(() => {
+    listRef?.current?.scrollTo({
+      top: listRef?.current?.scrollHeight,
+    });
+  }, [listRef]);
 
-  const [prevDate, setPrevDate] = useState(null);
-
-  if (error) {
-    return <TimelineStatus status='timeline-error' />;
-  }
+  const handleScrubberChange = (event) => {
+    const { value } = event.target;
+    if (listRef.current) {
+      listRef.current.scrollTo({
+        top: (value / 100) * itemCount * itemSize,
+      });
+      otherRef.current.scrollTo({
+        top: (value / 100) * itemCount * itemSize,
+      });
+    }
+  };
 
   return (
-    <Timeline
-      mode='ORGANIZATION'
-      loading={loading}
-      onLoadMore={(containerRef) => {
-        const newFromDate = data[0]?.createdAt || data[0]?.startedAt;
-        if (!data[0] || prevDate === newFromDate) {
-          return;
-        }
-        // todo remove me when switching to virtualized list
-        containerRef.current.scrollTop = 400;
-        setPrevDate(newFromDate);
-        fetchMore({
-          variables: {
-            contactId: id,
-            size: 10,
-            from: newFromDate,
-          },
-        });
+    <div
+      style={{
+        display: 'flex',
       }}
-      noActivity={!data.length && !loading}
-      loggedActivities={[...(data || [])]}
-      id={id}
-    />
+    >
+      <div
+        ref={otherRef}
+        style={{ maxHeight: '90vh', overflowX: 'hidden', overflowY: 'auto' }}
+      >
+        {buckets.map((e, i) => (
+          <div key={e.timeBucket} style={{ height: `${e.count * itemSize}px` }}>
+            <span
+              style={{
+                background: 'paleturquoise',
+                fontSize: '10px',
+                whiteSpace: 'nowrap',
+              }}
+            >
+              {DateTimeUtils.format(e.timeBucket)}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{ maxHeight: '90vh', overflowX: 'hidden', overflowY: 'auto' }}
+        ref={listRef}
+        className={styles.div}
+      >
+        {events
+          .map((e, i) => (
+            <div
+              key={e.date}
+              style={{ background: '#ccc', marginBottom: '8px', width: '300px' }}
+            >
+              <span style={{ marginRight: '8px' }}>{i}</span>
+
+              Events
+            </div>
+          ))
+          .reverse()}
+      </div>
+      <input
+        className={styles.input}
+        type='range'
+        defaultValue={100}
+        min={1}
+        max={1}
+        orient='vertical'
+        aria-orientation={'vertical'}
+        onChange={handleScrubberChange}
+      />
+    </div>
   );
 };
