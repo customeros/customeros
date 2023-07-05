5f7e35265ab1a55fa21f2a4bf5553873ab06da77
kasia <kasia@openline.ai>
2023-07-05 12:13:40 +0200

    Timeline:  - introduced timeline query and populating data  - created email preview

---
+++ packages/apps/spaces/app/organizations/[id]/components/Timeline/events/EmailTimelineItem/EmailTimelineItem.tsx
@@ -0,0 +1,78 @@
+'use client';
+import React, { FC } from 'react';
+import Image from 'next/image';
+import { Card, CardBody, CardFooter } from '@ui/layout/Card';
+import { Text } from '@ui/typography/Text';
+import { VStack } from '@chakra-ui/react';
+import { convert } from 'html-to-text';
+import {
+  getEmailParticipantsName,
+  getParticipant,
+} from '@spaces/utils/getParticipantsName';
+
+export const EmailTimelineItem: FC<any> = ({ content }) => {
+  const text = convert(content.content, {
+    // limits: {
+    //   maxInputLength: 200,
+    // },
+  });
+
+    console.log('🏷️ ----- content.repliesTo: '
+        , content.repliesTo);
+    return (
+    <Card
+      variant='outline'
+      size='md'
+      fontSize='14px'
+      background='#F9FAFB'
+      flexDirection='row'
+      maxWidth={549}
+      aspectRatio='9/2'
+    >
+      <CardBody
+        pt={5}
+        pb={5}
+        pl={5}
+        pr={0}
+        overflow={'hidden '}
+        flexDirection='row'
+      >
+        <VStack align='flex-start' spacing={0}>
+          <Text as='p' noOfLines={1}>
+            <Text as={'span'} fontWeight={500}>
+              {getEmailParticipantsName(content?.sentBy)}
+            </Text>{' '}
+            <Text as={'span'} color='#6C757D'>
+              emailed
+            </Text>{' '}
+            <Text as={'span'} fontWeight={500} marginRight={2}>
+              {getEmailParticipantsName(content?.sentTo)}
+            </Text>{' '}
+            <Text as={'span'} color='#6C757D'>
+              CC:
+            </Text>{' '}
+            <Text as={'span'}>{getEmailParticipantsName(content?.sentTo)}</Text>
+          </Text>
+
+          <Text fontWeight={500} noOfLines={1}>
+            {content.interactionSession?.name}
+          </Text>
+
+          <Text noOfLines={[1, 2]}>
+              {text}
+          </Text>
+        </VStack>
+      </CardBody>
+      <CardFooter pt={5} pb={5} pr={5} pl={0}>
+        <div>
+          <Image
+            src={'/backgrounds/organization/poststamp.webp'}
+            alt='Email'
+            width={54}
+            height={70}
+          />
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
