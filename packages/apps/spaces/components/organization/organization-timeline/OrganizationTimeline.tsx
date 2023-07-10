8be06bb3b732d1fdeadc88a5491d1de4022dadbf
kasia <kasia@openline.ai>
2023-07-10 18:31:56 +0200

    Restore old page .

--- packages/apps/spaces/components/organization/organization-timeline/OrganizationTimeline.tsx
+++ packages/apps/spaces/components/organization/organization-timeline/OrganizationTimeline.tsx
@@ -1,81 +1,41 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { buckets, events } from '@spaces/organization/organization-timeline/data';
-import styles from './organization-timeline.module.scss';
-import { DateTimeUtils } from '@spaces/utils/date';
-import { Virtuoso, VirtuosoHandle } from "react-virtuoso";
-import { Skeleton } from "@spaces/atoms/skeleton";
+import React, { useState } from 'react';
+import { Timeline, TimelineStatus } from '@spaces/organisms/timeline';
+import { useOrganizationTimeline } from '@spaces/hooks/useOrganizationTimeline';
 
-const itemSize = 102;
-const START_INDEX = events.length - 1
-const INITIAL_ITEM_COUNT = events.length
+export const OrganizationTimeline = ({ id }: { id: string }) => {
+  const { data, loading, error, fetchMore } = useOrganizationTimeline({
+    organizationId: id,
+  });
 
-const Bucket = ({ e }) => (
-    <div key={e.timeBucket} style={{ height: `${(e.count * itemSize)}px`,  }}>
-        <span
-            style={{ whiteSpace: 'nowrap', }}
-            className={styles.date}
-        >
-          {DateTimeUtils.format(e.timeBucket, DateTimeUtils.defaultFormatShortString)}
-        </span>
-        {Array(e.count).fill('').map((_, i) => (
-            <div className={styles.marker} key={`timeline-type-marker-${i}`} />
-        ))}
-    </div>
-)
+  const [prevDate, setPrevDate] = useState(null);
 
+  if (error) {
+    return <TimelineStatus status='timeline-error' />;
+  }
 
-const TimelineItem = ({ user }) => (
-    <div style={{ padding: '22px 0 0' }}>
-        <div style={{ padding: '1rem 0.5rem', background: '#f2fafa' }} className={styles.abc}>
-            <h4>
-                {DateTimeUtils.format(user.date, DateTimeUtils.defaultFormatShortString)}
-            </h4>
-            <div>
-                {user.content.from} -> {user.content.to}
-            </div>
-        </div>
-    </div>
-)
-
-export const OrganizationTimeline = ({ id }) => {
-    const otherRef = useRef<any>(null);
-    const ref = useRef<VirtuosoHandle>(null)
-    const [firstItemIndex, setFirstItemIndex] = useState(START_INDEX)
-    const [users, setUsers] = useState(events)
-
-
-    useEffect(() => {
-        if (otherRef.current) {
-            otherRef.current.scrollTop = otherRef.current.scrollHeight
+  return (
+    <Timeline
+      mode='ORGANIZATION'
+      loading={loading}
+      onLoadMore={(containerRef) => {
+        const newFromDate = data[0]?.createdAt || data[0]?.startedAt;
+        if (!data[0] || prevDate === newFromDate) {
+          return;
         }
-    }, [otherRef])
-
-    return (
-        <div style={{ display: 'flex' }}>
-            <div
-                ref={otherRef}
-                style={{ maxHeight: 800, overflowY: 'hidden' }}
-                className={styles.line}
-            >
-                {buckets.map((e, i) => <Bucket e={e} />)}
-            </div>
-            <Virtuoso
-                ref={ref}
-                onScroll={(e) => {
-                    if (otherRef?.current) {
-                        otherRef.current.scrollTop = e.target.scrollTop
-                    }
-                }}
-                style={{ height: 800, width: '100%' }}
-                firstItemIndex={firstItemIndex}
-                initialItemCount={events.length}
-                initialTopMostItemIndex={INITIAL_ITEM_COUNT - 1}
-                data={users}
-                increaseViewportBy={300}
-                overscan={10}
-                atTopThreshold={100}
-                itemContent={(index, user) => <TimelineItem user={user} />} // render TimelineItem
-            />
-        </div>
-    );
+        // todo remove me when switching to virtualized list
+        containerRef.current.scrollTop = 400;
+        setPrevDate(newFromDate);
+        fetchMore({
+          variables: {
+            contactId: id,
+            size: 10,
+            from: newFromDate,
+          },
+        });
+      }}
+      noActivity={!data.length && !loading}
+      loggedActivities={[...(data || [])]}
+      id={id}
+    />
+  );
 };
