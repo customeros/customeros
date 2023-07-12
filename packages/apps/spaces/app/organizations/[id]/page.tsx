03fc8b157bb9ab48574e5e67b9e889467e61b562
Alex Calinica <acalinica@openline.ai>
2023-07-12 16:11:55 +0300

    chore: uncomment components

--- packages/apps/spaces/app/organizations/[id]/page.tsx
+++ packages/apps/spaces/app/organizations/[id]/page.tsx
@@ -1,22 +1,22 @@
-// import { Hydrate } from '@tanstack/react-query';
-// import { getDehydratedState } from '@shared/util/getDehydratedState';
+import { Hydrate } from '@tanstack/react-query';
+import { getDehydratedState } from '@shared/util/getDehydratedState';
 
-// import { SideSection } from './components/SideSection';
-// import { MainSection } from './components/MainSection';
+import { SideSection } from './components/SideSection';
+import { MainSection } from './components/MainSection';
 
-// import {
-//   Tabs,
-//   TabsHeader,
-//   AboutPanel,
-//   PeoplePanel,
-//   UpNextPanel,
-//   AccountPanel,
-//   SuccessPanel,
-//   TabsContainer,
-//   OrganizationLogo,
-// } from './components/Tabs';
+import {
+  Tabs,
+  TabsHeader,
+  AboutPanel,
+  PeoplePanel,
+  UpNextPanel,
+  AccountPanel,
+  SuccessPanel,
+  TabsContainer,
+  OrganizationLogo,
+} from './components/Tabs';
 
-// import { useOrganizationQuery } from './graphql/organization.generated';
+import { useOrganizationQuery } from './graphql/organization.generated';
 
 interface OrganizationPageProps {
   params: { id: string };
@@ -25,32 +25,31 @@ interface OrganizationPageProps {
 export default async function OrganizationPage({
   params,
 }: OrganizationPageProps) {
-  // const variables = { id: params.id };
-  // const dehydratedState = await getDehydratedState(
-  //   useOrganizationQuery,
-  //   variables,
-  // );
+  const variables = { id: params.id };
+  const dehydratedState = await getDehydratedState(
+    useOrganizationQuery,
+    variables,
+  );
 
   return (
-    <p>{params.id}</p>
-    // <Hydrate state={dehydratedState}>
-    //   <SideSection>
-    //     <TabsContainer>
-    //       <TabsHeader>
-    //         <OrganizationLogo src='/logos/bigquery.svg' />
-    //       </TabsHeader>
+    <Hydrate state={dehydratedState}>
+      <SideSection>
+        <TabsContainer>
+          <TabsHeader>
+            <OrganizationLogo src='/logos/bigquery.svg' />
+          </TabsHeader>
 
-    //       <Tabs>
-    //         <UpNextPanel />
-    //         <AccountPanel />
-    //         <SuccessPanel />
-    //         <PeoplePanel />
-    //         <AboutPanel />
-    //       </Tabs>
-    //     </TabsContainer>
-    //   </SideSection>
+          <Tabs>
+            <UpNextPanel />
+            <AccountPanel />
+            <SuccessPanel />
+            <PeoplePanel />
+            <AboutPanel />
+          </Tabs>
+        </TabsContainer>
+      </SideSection>
 
-    //   <MainSection></MainSection>
-    // </Hydrate>
+      <MainSection></MainSection>
+    </Hydrate>
   );
 }
