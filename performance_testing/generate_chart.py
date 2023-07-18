d62e998fea53633d5419c2135f7904bab11a88a2
silviu <silviu@openline.ai>
2023-07-18 16:43:22 +0300

    2734 - altered legend in the chart

--- performance_testing/generate_chart.py
+++ performance_testing/generate_chart.py
@@ -6,13 +6,13 @@ import numpy as np
 data = pd.read_csv('average_response_times.csv')
 
 # Extract the last 10 rows from the data
-last_15_rows = data.iloc[-10:]
+last_10_rows = data.iloc[-10:]
 
 # Extract x-axis values from the first column
-x = last_15_rows.iloc[:, 0]
+x = last_10_rows.iloc[:, 0]
 
 # Extract y-axis values from all subsequent columns
-y = last_15_rows.iloc[:, 1:]
+y = last_10_rows.iloc[:, 1:]
 
 # Set the figure size and margins
 fig, ax = plt.subplots(figsize=(16, 12))
@@ -24,10 +24,10 @@ for i, column in enumerate(y.columns):
     plt.plot(x, y[column], marker='o', label=column)
 
 # Set x-axis tick labels to the values from the first column
-plt.xticks(np.arange(len(last_15_rows)), last_15_rows.iloc[:, 0], rotation='vertical')
+plt.xticks(np.arange(len(last_10_rows)), last_10_rows.iloc[:, 0], rotation='vertical')
 
 # Add legend to show the lines for each column
-plt.legend()
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
 
 # Add measurement units to the x-axis and y-axis labels
 plt.xlabel('Response time (ms)')
