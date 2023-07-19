cf97020330ca539f935bfa5232dee607f80e1c2b
SilviuOpenline <125381623+SilviuOpenline@users.noreply.github.com>
2023-07-19 10:16:26 +0300

    2734 dashboardview organizations prep and mutations (#2738)
    * 2668 - test build
    
    * 2734 - altered legend in the chart
    
    * 2734 - altered legend in the chart
    
    * 2734 - altered legend in the chart
    
    * 2734 - altered legend in the chart
    
    * 2734 - altered legend in the chart

--- performance_testing/generate_chart.py
+++ performance_testing/generate_chart.py
@@ -6,16 +6,16 @@ import numpy as np
 data = pd.read_csv('average_response_times.csv')
 
 # Extract the last 10 rows from the data
-last_15_rows = data.iloc[-10:]
+last_10_rows = data.iloc[-10:]
 
 # Extract x-axis values from the first column
-x = last_15_rows.iloc[:, 0]
+x = last_10_rows.iloc[:, 0]
 
 # Extract y-axis values from all subsequent columns
-y = last_15_rows.iloc[:, 1:]
+y = last_10_rows.iloc[:, 1:]
 
 # Set the figure size and margins
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots(figsize=(16, 12))
 fig.subplots_adjust(bottom=0.2)  # Adjust the bottom margin
 
 # Generate the chart
@@ -24,14 +24,14 @@ for i, column in enumerate(y.columns):
     plt.plot(x, y[column], marker='o', label=column)
 
 # Set x-axis tick labels to the values from the first column
-plt.xticks(np.arange(len(last_15_rows)), last_15_rows.iloc[:, 0], rotation='vertical')
+plt.xticks(np.arange(len(last_10_rows)), last_10_rows.iloc[:, 0], rotation='vertical')
 
 # Add legend to show the lines for each column
-plt.legend()
+plt.legend(bbox_to_anchor=(-0.3, 1), loc='upper left', borderaxespad=0.)
 
 # Add measurement units to the x-axis and y-axis labels
 plt.xlabel('Response time (ms)')
 plt.ylabel('Request')
 
 # Save the chart as an image
-plt.savefig('average_response_times.png')
+plt.savefig('average_response_times.png', bbox_inches='tight')
