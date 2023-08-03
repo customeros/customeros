4b453653efdcd08a3e2feecba11c3a89a13d0172
Eduard Firut <edi@openline.ai>
2023-08-03 11:53:01 +0300

    Ai prompt entity - enhance

--- packages/server/customer-os-common-module/repository/postgres/entity/ai_prompt_log.go
+++ packages/server/customer-os-common-module/repository/postgres/entity/ai_prompt_log.go
@@ -10,6 +10,7 @@ type AiPromptLog struct {
 	Model                   string    `gorm:"column:model;type:varchar(100);NOT NULL;" json:"model" binding:"required"`
 	PromptType              string    `gorm:"column:prompt_type;type:varchar(255);NOT NULL;" json:"promptType" binding:"required"`
 	PromptTemplate          *string   `gorm:"column:prompt_template;type:text;" json:"promptTemplate" binding:"required"`
+	ParentId                string    `gorm:"column:parent_id;type:text;NOT NULL;" json:"parentId" binding:"required"`
 	Prompt                  string    `gorm:"column:prompt;type:text;NOT NULL;" json:"prompt" binding:"required"`
 	RawResponse             string    `gorm:"column:raw_response;type:text;NOT NULL;" json:"rawResponse" binding:"required"`
 	PostProcessError        bool      `gorm:"column:post_process_error;" json:"postProcessError" binding:"required"`
