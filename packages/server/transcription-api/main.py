cc86d43912c194a0f65eaa1ca0ae793e17c8f7ec
tsearle <torrey@openline.ai>
2023-08-10 16:54:26 +0200

    nuke transcription-api (#3129)

--- packages/server/transcription-api/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import routes.routes as routes
-
-if __name__ == '__main__':
-    # Create an HTTP server and listen for POST requests
-    print("Starting server on port 8014")
-    if __name__ == '__main__':
-        routes.app.run(host='0.0.0.0', port=8014)
-
--- packages/server/transcription-api/routes/healthcheck.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-
-
-def handle_health_get_request():
-    return jsonify({
-        'status': 'ok',
-    }), 200
