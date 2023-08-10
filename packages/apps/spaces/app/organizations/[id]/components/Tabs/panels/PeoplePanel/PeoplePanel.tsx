9d2cb8e4610d4891ed9a40c61a921d16275075ae
kasia <kasia@openline.ai>
2023-08-10 14:20:51 +0200

    feat #3126: Unified panels wrapper

--- packages/apps/spaces/app/organizations/[id]/components/Tabs/panels/PeoplePanel/PeoplePanel.tsx
+++ packages/apps/spaces/app/organizations/[id]/components/Tabs/panels/PeoplePanel/PeoplePanel.tsx
@@ -5,7 +5,6 @@ import { useQueryClient } from '@tanstack/react-query';
 
 import { Flex } from '@ui/layout/Flex';
 import { Box } from '@ui/layout/Box';
-import { VStack } from '@ui/layout/Stack';
 import { Text } from '@ui/typography/Text';
 import { Button } from '@ui/form/Button';
 import { Icons } from '@ui/media/Icon';
@@ -18,7 +17,8 @@ import { useAddOrganizationToContactMutation } from '@organization/graphql/addCo
 import { ContactCard } from '@organization/components/Tabs/panels/PeoplePanel/ContactCard/ContactCard';
 import { useOrganizationPeoplePanelQuery } from '@organization/graphql/organizationPeoplePanel.generated';
 import { invalidateQuery } from '@organization/components/Tabs/panels/PeoplePanel/util';
-import {Contact} from "@graphql/types";
+import { Contact } from '@graphql/types';
+import { OrganizationPanel } from '@organization/components/Tabs/panels/OrganizationPanel/OrganizationPanel';
 
 export const PeoplePanel = () => {
   const id = useParams()?.id as string;
@@ -49,27 +49,16 @@ export const PeoplePanel = () => {
       },
     );
   };
-
   return (
-    <Box
-      p={0}
-      flex={1}
-      as={Flex}
-      flexDirection='column'
-      height='100%'
-      backgroundImage={
+    <OrganizationPanel
+      title='People'
+      bgImage={
         !contacts?.length
           ? '/backgrounds/organization/half-circle-pattern.svg'
           : ''
       }
-      backgroundRepeat='no-repeat'
-      backgroundSize='contain'
-    >
-      <Flex justify='space-between' pt='4' pb='4' px='6'>
-        <Text fontSize='lg' color='gray.700' fontWeight='semibold'>
-          People
-        </Text>
-        {!!contacts.length && (
+      actionItem={
+        !!contacts.length && (
           <Button
             size='sm'
             variant='outline'
@@ -81,57 +70,47 @@ export const PeoplePanel = () => {
           >
             Add
           </Button>
-        )}
-      </Flex>
-
-      <VStack
-        spacing='2'
-        w='full'
-        h='100%'
-        justify='stretch'
-        overflowY='auto'
-        px='6'
-        pb={8}
-      >
-        {!contacts.length && (
-          <Flex direction='column' alignItems='center' mt='4'>
-            <Box
-              border='1px solid'
-              borderColor='gray.200'
-              padding={3}
-              borderRadius='md'
-              mb={6}
+        )
+      }
+    >
+      {!contacts.length && (
+        <Flex direction='column' alignItems='center' mt='4'>
+          <Box
+            border='1px solid'
+            borderColor='gray.200'
+            padding={3}
+            borderRadius='md'
+            mb={6}
+          >
+            <Icons.Users2 color='gray.700' boxSize='6' />
+          </Box>
+          <Text color='gray.700' fontWeight={600}>
+            Let’s add some people
+          </Text>
+          <Text color='gray.500' mt={1} mb={6} textAlign='center'>
+            With the right people, you&apos;ll create meaningful interactions
+            and results. Start by adding yourself, your colleagues or anyone
+            from {data?.organization?.name}.
+          </Text>
+          <div>
+            <Button
+              variant='outline'
+              loadingText='Adding'
+              isLoading={isLoading}
+              onClick={handleAddContact}
             >
-              <Icons.Users2 color='gray.700' boxSize='6' />
-            </Box>
-            <Text color='gray.700' fontWeight={600}>
-              Let’s add some people
-            </Text>
-            <Text color='gray.500' mt={1} mb={6} textAlign='center'>
-              With the right people, you&apos;ll create meaningful interactions
-              and results. Start by adding yourself, your colleagues or anyone
-              from {data?.organization?.name}.
-            </Text>
-            <div>
-              <Button
-                variant='outline'
-                loadingText='Adding'
-                isLoading={isLoading}
-                onClick={handleAddContact}
-              >
-                Add someone
-              </Button>
-            </div>
-          </Flex>
-        )}
+              Add someone
+            </Button>
+          </div>
+        </Flex>
+      )}
 
-        {!!contacts.length &&
-          contacts.map((contact, index) => (
-            <Fade key={contact.id} in style={{ width: '100%' }}>
-              <ContactCard index={index} contact={contact as Contact} />
-            </Fade>
-          ))}
-      </VStack>
-    </Box>
+      {!!contacts.length &&
+        contacts.map((contact, index) => (
+          <Fade key={contact.id} in style={{ width: '100%' }}>
+            <ContactCard index={index} contact={contact as Contact} />
+          </Fade>
+        ))}
+    </OrganizationPanel>
   );
 };
