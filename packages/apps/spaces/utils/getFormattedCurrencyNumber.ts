8b4a2a296506c0faf432a8e6f3ca5117500703c8
Kasia <121807793+Kasia9090@users.noreply.github.com>
2023-08-17 13:16:27 +0200

    fix #3174 Improved formatting of large numbers in renewal forecast ui. (#3244)
    Added a new utility function formatCurrency in getFormattedCurrencyNumber.ts file to better handle currency formatting. It also handles large number abbreviations. This was applied to amount display in the RenewalForecast component

---
+++ packages/apps/spaces/utils/getFormattedCurrencyNumber.ts
@@ -0,0 +1,22 @@
+function abbreviateNumber(num: number): string {
+  const SI_SYMBOL = ['', 'K', 'M', 'G', 'T', 'P', 'E'];
+  const tier = (Math.log10(Math.abs(num)) / 3) | 0;
+  if (tier == 0) return num.toString();
+  const suffix = SI_SYMBOL[tier];
+  const scale = Math.pow(10, tier * 3);
+  const scaled = num / scale;
+  return scaled.toFixed(1) + suffix;
+}
+
+export function formatCurrency(amount: number): string {
+  if (`${amount}`.length >= 6) {
+    const abbreviatedNumber = abbreviateNumber(amount);
+    return `$${abbreviatedNumber}`;
+  } else {
+    return Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+    }).format(amount);
+  }
+}
