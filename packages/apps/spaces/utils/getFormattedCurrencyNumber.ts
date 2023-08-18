269b0e9503556bc71213587b45bb3d5568099b02
Kasia <121807793+Kasia9090@users.noreply.github.com>
2023-08-18 13:01:40 +0200

    fix #3268: Change G to B in monetary value abbreviation, remove value after dot if it's zero (#3271)

--- packages/apps/spaces/utils/getFormattedCurrencyNumber.ts
+++ packages/apps/spaces/utils/getFormattedCurrencyNumber.ts
@@ -1,13 +1,16 @@
 function abbreviateNumber(num: number): string {
-  const SI_SYMBOL = ['', 'K', 'M', 'G', 'T', 'P', 'E'];
+  const SI_SYMBOL = ['', 'K', 'M', 'B', 'T', 'Qa', 'Qi'];
   const tier = (Math.log10(Math.abs(num)) / 3) | 0;
   if (tier == 0) return num.toString();
   const suffix = SI_SYMBOL[tier];
   const scale = Math.pow(10, tier * 3);
   const scaled = num / scale;
-  return scaled.toFixed(1) + suffix;
+  if (Math.round(scaled) === scaled) {
+    return scaled + suffix;
+  } else {
+    return scaled.toFixed(1) + suffix;
+  }
 }
-
 export function formatCurrency(amount: number): string {
   if (`${amount}`.length >= 6) {
     const abbreviatedNumber = abbreviateNumber(amount);
