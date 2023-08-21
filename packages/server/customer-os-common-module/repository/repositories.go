3e24af4c9e510ff86afdbe6d8b82ac38f3925ffc
edifirut <108661145+edifirut@users.noreply.github.com>
2023-08-21 21:15:43 +0300

    [Common module] - Add table for allowing orgs import (#3339)

--- packages/server/customer-os-common-module/repository/repositories.go
+++ packages/server/customer-os-common-module/repository/repositories.go
@@ -10,22 +10,24 @@ import (
 )
 
 type Repositories struct {
-	AppKeyRepository      repository.AppKeyRepository
-	AiPromptLogRepository repository.AiPromptLogRepository
-	UserRepository        neo4jrepo.UserRepository
-	TenantRepository      neo4jrepo.TenantRepository
-	CountryRepository     neo4jrepo.CountryRepository
-	StateRepository       neo4jrepo.StateRepository
+	AppKeyRepository                    repository.AppKeyRepository
+	AiPromptLogRepository               repository.AiPromptLogRepository
+	ImportAllowedOrganizationRepository repository.ImportAllowedOrganizationRepository
+	UserRepository                      neo4jrepo.UserRepository
+	TenantRepository                    neo4jrepo.TenantRepository
+	CountryRepository                   neo4jrepo.CountryRepository
+	StateRepository                     neo4jrepo.StateRepository
 }
 
 func InitRepositories(db *gorm.DB, driver *neo4j.DriverWithContext) *Repositories {
 	repositories := &Repositories{
-		AppKeyRepository:      repository.NewAppKeyRepo(db),
-		AiPromptLogRepository: repository.NewAiPromptLogRepository(db),
-		UserRepository:        neo4jrepo.NewUserRepository(driver),
-		TenantRepository:      neo4jrepo.NewTenantRepository(driver),
-		CountryRepository:     neo4jrepo.NewCountryRepository(driver),
-		StateRepository:       neo4jrepo.NewStateRepository(driver),
+		AppKeyRepository:                    repository.NewAppKeyRepo(db),
+		AiPromptLogRepository:               repository.NewAiPromptLogRepository(db),
+		ImportAllowedOrganizationRepository: repository.NewImportAllowedOrganizationRepository(db),
+		UserRepository:                      neo4jrepo.NewUserRepository(driver),
+		TenantRepository:                    neo4jrepo.NewTenantRepository(driver),
+		CountryRepository:                   neo4jrepo.NewCountryRepository(driver),
+		StateRepository:                     neo4jrepo.NewStateRepository(driver),
 	}
 
 	var err error
@@ -37,6 +39,12 @@ func InitRepositories(db *gorm.DB, driver *neo4j.DriverWithContext) *Repositorie
 	}
 
 	err = db.AutoMigrate(&entity.AiPromptLog{})
+	if err != nil {
+		log.Print(err)
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&entity.ImportAllowedOrganization{})
 	if err != nil {
 		log.Print(err)
 		panic(err)
