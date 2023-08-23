ff5b7519f3c8eade87d40c9b7af4048c179cfe3b
Gabriel Gontariu <gabi@openline.ai>
2023-08-23 22:43:52 +0300

    remove message store api

--- packages/server/message-store-api/proto/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../proto/generated
-protoc --go_out=../proto/generated --go_opt=paths=source_relative --go-grpc_out=../proto/generated --go-grpc_opt=paths=source_relative ./*.proto
-sed -i "" -e "s/,omitempty//g" ../proto/generated/*.go
