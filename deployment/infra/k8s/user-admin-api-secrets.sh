5c5d2b5575aacc395432e70d6e23ff16edd5e65e
silviu <silviu@openline.ai>
2023-08-25 10:06:40 +0300

    [3365] - added secret creation

--- deployment/infra/k8s/user-admin-api-secrets.sh
+++ deployment/infra/k8s/user-admin-api-secrets.sh
@@ -4,8 +4,8 @@
 SECRET_NAME=user-admin-api-secret
 
 # Get secret data from env vars
-GOOGLE_OAUTH_CLIENT_ID_BASE64_ENCODED=$(echo "$GOOGLE_OAUTH_CLIENT_ID" | tr -d '\n' | base64)
-GOOGLE_OAUTH_CLIENT_SECRET_BASE64_ENCODED=$(echo "$GOOGLE_OAUTH_CLIENT_SECRET" | tr -d '\n' | base64)
+GOOGLE_OAUTH_CLIENT_ID_BASE64_ENCODED=$(echo "$GOOGLE_OAUTH_CLIENT_ID" | base64)
+GOOGLE_OAUTH_CLIENT_SECRET_BASE64_ENCODED=$(echo "$GOOGLE_OAUTH_CLIENT_SECRET" | base64)
 
 # Generate secret YAML 
 cat <<EOF > ${SECRET_NAME}.yaml
@@ -17,7 +17,4 @@ type: Opaque
 data:
   GOOGLE_OAUTH_CLIENT_ID: ${GOOGLE_OAUTH_CLIENT_ID_BASE64_ENCODED}
   GOOGLE_OAUTH_CLIENT_SECRET: ${GOOGLE_OAUTH_CLIENT_SECRET_BASE64_ENCODED}
----
 EOF
-cat user-admin-api.yaml >> user-admin-api-secret.yaml
-mv user-admin-api-secret.yaml user-admin-api.yaml
