2e9b29e193cc942e9b58b3de97ccefecb32af17f
alexopenline <alex@openline.ai>
2023-09-04 16:47:24 +0300

    Remove message-store left code

--- deployment/scripts/postgresql/setup.sql
+++ deployment/scripts/postgresql/setup.sql
@@ -1,7 +1,6 @@
 INSERT INTO app_keys (app_id, key, active) VALUES ('customer-os-api', 'dd9d2474-b4a9-4799-b96f-73cd0a2917e4', true) ON CONFLICT DO NOTHING;
 INSERT INTO app_keys (app_id, key, active) VALUES ('file-store-api', '9eb87aa2-75e7-45b2-a1e6-53ed297d0ba8', true) ON CONFLICT DO NOTHING;
 INSERT INTO app_keys (app_id, key, active) VALUES ('settings-api', '8b010f38-e5ca-4923-a62e-9f073c5c7dbf', true) ON CONFLICT DO NOTHING;
-INSERT INTO app_keys (app_id, key, active) VALUES ('message-store-api', 'f6e26f68-7e69-42fb-9aee-624becc29897', true) ON CONFLICT DO NOTHING;
 INSERT INTO app_keys (app_id, key, active) VALUES ('oasis-api', '10a6747a-97cd-4a6c-bcf5-e4ee89a12567', true) ON CONFLICT DO NOTHING;
 INSERT INTO app_keys (app_id, key, active) VALUES ('validation-api', 'c41192c4-08bc-4b4e-851c-10fc876b9ebb', true) ON CONFLICT DO NOTHING;
 INSERT INTO app_keys (app_id, key, active) VALUES ('anthropic-api', '01bfb7a3-917b-4af6-87f9-2bdfb73bea97', true) ON CONFLICT DO NOTHING;
