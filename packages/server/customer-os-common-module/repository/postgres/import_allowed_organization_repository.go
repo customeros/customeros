9e995c2e1c5b91d30b88f5615bd5b897a15c9630
Eduard Firut <edi@openline.ai>
2023-09-06 12:24:31 +0300

    [Sync Gmail] - Allow all orgs import + allowed orgs import control * row

--- packages/server/customer-os-common-module/repository/postgres/import_allowed_organization_repository.go
+++ packages/server/customer-os-common-module/repository/postgres/import_allowed_organization_repository.go
@@ -4,9 +4,14 @@ import (
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/repository/postgres/entity"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
+	"time"
 )
 
 type ImportAllowedOrganizationRepository interface {
+	GetAllOrgsImportRow(tenant string) (*entity.ImportAllowedOrganization, error)
+	SaveAllOrgsImportRow(tenant, source, appSource string) error
+	RemoveAllOrgsImportRow(tenant string) error
+
 	SaveOrganizationAllowedForImport(importAllowedOrganization entity.ImportAllowedOrganization) error
 	GetOrganizationsAllowedForImport(tenant string) ([]entity.ImportAllowedOrganization, error)
 }
@@ -17,6 +22,65 @@ type importAllowedOrganizationRepositoryImpl struct {
 
 func NewImportAllowedOrganizationRepository(gormDb *gorm.DB) ImportAllowedOrganizationRepository {
 	return &importAllowedOrganizationRepositoryImpl{gormDb: gormDb}
+}
+
+func (repo *importAllowedOrganizationRepositoryImpl) GetAllOrgsImportRow(tenant string) (*entity.ImportAllowedOrganization, error) {
+	var result entity.ImportAllowedOrganization
+	err := repo.gormDb.Model(&entity.ImportAllowedOrganization{}).Find(&result, "tenant = ? AND domain = ?", tenant, "*").Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		logrus.Errorf("error while getting import allowed organization: %v", err)
+		return nil, err
+	}
+	if result.Tenant == "" {
+		return nil, nil
+	}
+	return &result, nil
+}
+
+func (repo *importAllowedOrganizationRepositoryImpl) SaveAllOrgsImportRow(tenant, source, appSource string) error {
+	existing, err := repo.GetAllOrgsImportRow(tenant)
+	if err != nil {
+		return err
+	}
+	if existing != nil {
+		return nil
+	}
+
+	result := repo.gormDb.Create(&entity.ImportAllowedOrganization{
+		Tenant:    tenant,
+		Name:      "*",
+		Domain:    "*",
+		Allowed:   true,
+		AppSource: appSource,
+		CreatedAt: time.Now(),
+		Source:    source,
+	})
+	if result.Error != nil {
+		logrus.Errorf("error while saving import allowed organization: %v", err)
+		return result.Error
+	}
+	return nil
+}
+
+func (repo *importAllowedOrganizationRepositoryImpl) RemoveAllOrgsImportRow(tenant string) error {
+	existing, err := repo.GetAllOrgsImportRow(tenant)
+	if err != nil {
+		return err
+	}
+	if existing == nil {
+		return nil
+	}
+
+	result := repo.gormDb.Delete(existing)
+	if result.Error != nil {
+		logrus.Errorf("error while saving import allowed organization: %v", err)
+		return result.Error
+	}
+
+	return nil
 }
 
 func (repo *importAllowedOrganizationRepositoryImpl) SaveOrganizationAllowedForImport(importAllowedOrganization entity.ImportAllowedOrganization) error {
