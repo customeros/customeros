6fcfbd2a112dd190e45af4d113f66f9687b8db7b
Eduard Firut <edi@openline.ai>
2023-09-07 20:20:15 +0300

    asd

--- packages/runner/sync-gmail/entity/raw_email.go
+++ packages/runner/sync-gmail/entity/raw_email.go
@@ -1,14 +1,20 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type RawEmail struct {
 	ID uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 
-	ExternalSystem string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
-	TenantName     string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
-	UsernameSource string `gorm:"size:255;not null"`
-	MessageId      string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
+	CreatedAt time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
+
+	ExternalSystem    string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
+	TenantName        string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
+	UsernameSource    string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
+	ProviderMessageId string `gorm:"size:255;not null;"`
+	MessageId         string `gorm:"size:255;not null;index:idx_raw_email_external_system"`
 
 	SentToEventStoreState  string  `gorm:"size:50;not null"`
 	SentToEventStoreReason *string `gorm:"type:text"`
