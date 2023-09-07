31ca1d82a555b0ce54824e758f67c6ea6f137d09
Alex Calinica <acalinica@openline.ai>
2023-09-07 17:56:41 +0300

    fix: remove unused code

--- packages/apps/spaces/app/organizations/[id]/components/Tabs/panels/PeoplePanel/PeoplePanelSkeleton.tsx
+++ packages/apps/spaces/app/organizations/[id]/components/Tabs/panels/PeoplePanel/PeoplePanelSkeleton.tsx
@@ -5,8 +5,6 @@ import { Button } from '@ui/form/Button';
 import { Icons } from '@ui/media/Icon';
 import { OrganizationPanel } from '@organization/components/Tabs/panels/OrganizationPanel/OrganizationPanel';
 import { CardHeader } from '@ui/presentation/Card';
-import { Avatar } from '@ui/media/Avatar';
-import User from '@spaces/atoms/icons/User';
 import { Card } from '@ui/layout/Card';
 import { Skeleton, SkeletonCircle } from '@ui/presentation/Skeleton';
 
@@ -83,32 +81,6 @@ export const PeoplePanelSkeleton = () => {
           </CardHeader>
         </Card>
       ))}
-      {/* <Card
-        w='full'
-        boxShadow={'xs'}
-        cursor='pointer'
-        borderRadius='lg'
-        border='1px solid'
-        borderColor='gray.200'
-        _hover={{
-          boxShadow: 'md',
-          '& > div > #confirm-button': {
-            opacity: '1',
-            pointerEvents: 'auto',
-          },
-        }}
-        transition='all 0.2s ease-out'
-      >
-        <CardHeader as={Flex} p='4' pb={2} position='relative'>
-          <SkeletonCircle height={12} width={12} boxShadow='avatarRingGray' />
-
-          <Flex ml='4' flexDir='column' flex='1'>
-            <Skeleton h={3} w={100} mb={3} />
-            <Skeleton h={3} w={200} mb={4} />
-            <Skeleton h={3} w={250} mb={2} />
-          </Flex>
-        </CardHeader>
-      </Card> */}
     </OrganizationPanel>
   );
 };
