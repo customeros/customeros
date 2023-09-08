be5863c4340b2445a48d21a025f51ee71b90de7f
kasia <kasia@openline.ai>
2023-09-08 18:18:51 +0200

    refactor #3651: Update api to use organization_HideAll in place of organization_ArchiveAll
    Remove getContactTimeline and getOrganizationTimeline queries
    
    Deleted unnecessary GraphQL queries getContactTimeline and getOrganizationTimeline. These queries were not being used in the application and so had been deprecated, also removed their references from generated.ts. Improved the quality of the codebase by removing unused code.

--- packages/apps/spaces/hooks/useOrganization/index.ts
+++ packages/apps/spaces/hooks/useOrganization/index.ts
@@ -1,3 +1,2 @@
 export { useCreateOrganization } from './useCreateOrganization';
 export { useMergeOrganizations } from './useMergeOrganizations';
-export { useArchiveOrganizations } from './useArchiveOrganizations';
