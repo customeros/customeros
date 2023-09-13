36bb0d7e46831532cbbef132df7edef913d58dfe
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2023-09-13 13:56:23 +0200

    refactor: change note field implementation (#3713)

---
+++ packages/apps/spaces/app/organizations/[id]/components/Tabs/panels/AccountPanel/Notes/Notes.dto.ts
@@ -0,0 +1,32 @@
+import { OrganizationAccountDetailsQuery } from '@organization/graphql/getAccountPanelDetails.generated';
+import { UpdateOrganizationMutationVariables } from '@organization/graphql/updateOrganization.generated';
+
+export interface NotesForm {
+  id: string;
+  note: string;
+}
+
+export class NotesDTO implements NotesForm {
+  id: string;
+  note: string;
+
+  constructor(
+    data?: Partial<OrganizationAccountDetailsQuery['organization']> | null,
+  ) {
+    this.id = data?.id || '';
+    this.note = data?.note || '';
+  }
+
+  static toForm(data: OrganizationAccountDetailsQuery) {
+    return new NotesDTO(data.organization);
+  }
+
+  static toPayload(data: NotesForm) {
+    return {
+      id: data.id,
+      note: data.note,
+      name: '',
+      patch: true,
+    } as UpdateOrganizationMutationVariables['input'];
+  }
+}
