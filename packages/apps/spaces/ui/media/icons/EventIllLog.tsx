b1d6cdd94b427c1d443d08cebd2b31358a5c28d4
kasia <kasia@openline.ai>
2023-09-18 16:19:30 +0200

    Added new SVG icons
    This commit includes a set of newly added SVG icons (such as Calendar, Phone, and Mail01)

---
+++ packages/apps/spaces/ui/media/icons/EventIllLog.tsx
@@ -0,0 +1,128 @@
+import { Icon, IconProps } from '@ui/media/Icon';
+
+export const EventIllLog = (props: IconProps) => (
+  <Icon viewBox='0 0 174 123' fill='none' boxSize='4' {...props}>
+    <rect y='23' width='174' height='1' fill='url(#paint0_linear_6008_2052)' />
+    <rect y='48' width='174' height='1' fill='url(#paint1_linear_6008_2052)' />
+    <rect y='73' width='174' height='1' fill='url(#paint2_linear_6008_2052)' />
+    <rect y='98' width='174' height='1' fill='url(#paint3_linear_6008_2052)' />
+    <rect x='24' width='1' height='123' fill='url(#paint4_linear_6008_2052)' />
+    <rect x='49' width='1' height='123' fill='url(#paint5_linear_6008_2052)' />
+    <rect x='74' width='1' height='123' fill='url(#paint6_linear_6008_2052)' />
+    <rect x='99' width='1' height='123' fill='url(#paint7_linear_6008_2052)' />
+    <rect x='124' width='1' height='123' fill='url(#paint8_linear_6008_2052)' />
+    <rect x='149' width='1' height='123' fill='url(#paint9_linear_6008_2052)' />
+    <defs>
+      <linearGradient
+        id='paint0_linear_6008_2052'
+        x1='174'
+        y1='23.5002'
+        x2='1.22854e-06'
+        y2='23.5007'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint1_linear_6008_2052'
+        x1='174'
+        y1='48.5002'
+        x2='1.22854e-06'
+        y2='48.5007'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint2_linear_6008_2052'
+        x1='174'
+        y1='73.5002'
+        x2='1.22854e-06'
+        y2='73.5007'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint3_linear_6008_2052'
+        x1='174'
+        y1='98.5002'
+        x2='1.22854e-06'
+        y2='98.5007'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint4_linear_6008_2052'
+        x1='24.5'
+        y1='0'
+        x2='24.5'
+        y2='123'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint5_linear_6008_2052'
+        x1='49.5'
+        y1='0'
+        x2='49.5'
+        y2='123'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint6_linear_6008_2052'
+        x1='74.5'
+        y1='0'
+        x2='74.5'
+        y2='123'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint7_linear_6008_2052'
+        x1='99.5'
+        y1='0'
+        x2='99.5'
+        y2='123'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+      <linearGradient
+        id='paint8_linear_6008_2052'
+        x1='124.5'
+        y1='0'
+        x2='124.5'
+        y2='123'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#FFCECB' />
+        <stop offset='1' stopColor='#FFCECB' />
+      </linearGradient>
+      <linearGradient
+        id='paint9_linear_6008_2052'
+        x1='149.5'
+        y1='0'
+        x2='149.5'
+        y2='123'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#DCDFEA' />
+        <stop offset='1' stopColor='#DCDFEA' />
+      </linearGradient>
+    </defs>
+  </Icon>
+);
