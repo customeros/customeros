1665157e665871e1161fbb6513db7ab92b28688d
Alex Calinica <acalinica@openline.ai>
2023-09-21 12:20:17 +0300

    refactor: integration app credentials usage

--- packages/apps/spaces/app/api/integration-token/route.ts
+++ packages/apps/spaces/app/api/integration-token/route.ts
@@ -5,20 +5,25 @@ type ResponseData = {
   token: string;
 };
 
-const tokenData = {
-  id: '650aec2b46c5eb01406d56b1',
-  name: 'Alex Calinica',
-};
-
-const WORKSPACE_KEY = '39aefeb6-fdc3-4790-93c3-568e10b5e694';
-const WORKSPACE_SECRET =
-  '48184ecc1a1ff4aa6bd1b01b468d644ed58a84b2333ed99111f50e85d60a141a';
-const options = {
-  issuer: WORKSPACE_KEY,
-  expiresIn: 7200,
-};
-
 export async function GET(req: NextRequest, res: NextResponse<ResponseData>) {
+  const WORKSPACE_KEY = process.env.INTEGRATION_APP_WORKSPACE_KEY;
+  const WORKSPACE_SECRET = process.env.INTEGRATION_APP_WORKSPACE_SECRET;
+  const options = {
+    issuer: WORKSPACE_KEY,
+    expiresIn: 7200,
+  };
+
+  const tokenData = {
+    id: '650aec2b46c5eb01406d56b1',
+    name: 'Alex Calinica',
+  };
+
+  if (!WORKSPACE_KEY || !WORKSPACE_SECRET) {
+    return new Response('Missing integration app credentials', {
+      status: 500,
+    });
+  }
+
   const token = jwt.sign(tokenData, WORKSPACE_SECRET, options);
   return new Response(JSON.stringify(token));
 }
