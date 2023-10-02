096746bc47e587cc312eaf2ca41e93571beb44f3
kasia <kasia@openline.ai>
2023-10-02 16:36:12 +0200

    Cleanup hooks index file

--- packages/apps/spaces/hooks/index.ts
+++ packages/apps/spaces/hooks/index.ts
@@ -1,6 +1,1 @@
-export { useWindowSize } from './useWindowSize';
-export { useBoundingClientRect } from './useBoundingClientRect';
 export { useDetectClickOutside } from './useDetectClickOutside';
-
-export type { WindowSize } from './useWindowSize';
-export type { BoundingClientRect } from './useBoundingClientRect';
