4efd80f118a733d11d6aea4cd73e464f1268d440
kasia <kasia@openline.ai>
2023-10-03 16:25:03 +0200

    (fix|COS-293): Remove unnecessary conditional rendering in SlackStub
    Large parts of the SlackStub component were wrapped in a condition that only rendered if there was a defined slackSender. Whether a slackSender is defined or not, the component should render a SlackMessageCard. Now, in the absence of a defined slackSender, the name is set to 'Unknown'

--- packages/apps/spaces/app/organization/[id]/src/components/Timeline/events/slack/SlackStub.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Timeline/events/slack/SlackStub.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { FC } from 'react';
-import { Card, CardBody } from '@ui/presentation/Card';
 import {
   ContactParticipant,
   JobRoleParticipant,
@@ -9,9 +8,7 @@ import {
 import { useTimelineEventPreviewContext } from '@organization/src/components/Timeline/preview/context/TimelineEventPreviewContext';
 import { Avatar } from '@ui/media/Avatar';
 import { Flex } from '@ui/layout/Flex';
-import { Text } from '@ui/typography/Text';
 import { getName } from '@spaces/utils/getParticipantsName';
-import Slack from '@spaces/atoms/icons/Slack';
 import { Button } from '@ui/form/Button';
 import { SlackMessageCard } from '@organization/src/components/Timeline/events/slack/SlackMessageCard';
 import { DateTimeUtils } from '@spaces/utils/date';
@@ -29,92 +26,56 @@ export const SlackStub: FC<{ slackEvent: InteractionEventWithDate }> = ({
       ?.contact ||
     (slackEvent?.sentBy?.[0] as UserParticipant)?.userParticipant;
 
-  if (!slackSender) {
-    return (
-      <Card
-        variant='outline'
-        size='md'
-        fontSize='14px'
-        background='white'
-        flexDirection='row'
-        maxWidth={549}
-        position='unset'
-        cursor='pointer'
-        boxShadow='xs'
-        borderColor='gray.100'
-      >
-        <CardBody p={3} overflow={'hidden'}>
-          <Flex gap={3} flex={1} alignItems='center'>
-            <Avatar
-              variant='roundedSquare'
-              size='sm'
-              colorScheme='gray'
-              bg='white'
-              border='1px solid var(--chakra-colors-gray-200)'
-              icon={<Slack height='1.8rem' />}
-            />
-            <Text color='gray.700' as='span' fontWeight={600}>
-              {slackEvent?.content}
-            </Text>
-          </Flex>
-        </CardBody>
-      </Card>
-    );
-  }
-
   const slackEventReplies = slackEvent.interactionSession?.events?.filter(
     (e) => e?.id !== slackEvent?.id,
   );
   const uniqThreadParticipants = slackEventReplies
     ?.map((e) => {
-      const threadSender =
+      return (
         (e?.sentBy?.[0] as ContactParticipant)?.contactParticipant ||
         (e?.sentBy?.[0] as JobRoleParticipant)?.jobRoleParticipant?.contact ||
-        (e?.sentBy?.[0] as UserParticipant)?.userParticipant;
-
-      return threadSender;
+        (e?.sentBy?.[0] as UserParticipant)?.userParticipant
+      );
     })
     ?.filter((v, i, a) => a.findIndex((t) => !!t && t?.id === v?.id) === i);
 
   return (
-    <>
-      <SlackMessageCard
-        name={getName(slackSender)}
-        profilePhotoUrl={slackSender?.profilePhotoUrl}
-        sourceUrl={slackEvent?.externalLinks?.[0]?.externalUrl}
-        content={slackEvent?.content || ''}
-        onClick={() => openModal(slackEvent)}
-        date={DateTimeUtils.formatTime(slackEvent?.date)}
-        showDateOnHover
-      >
-        {!!slackEventReplies?.length && (
-          <Flex mt={1}>
-            <Flex columnGap={1} mr={1}>
-              {uniqThreadParticipants?.map(
-                ({ id, name, firstName, lastName, profilePhotoUrl }) => {
-                  const displayName =
-                    name ?? [firstName, lastName].filter(Boolean).join(' ');
+    <SlackMessageCard
+      name={slackSender ? getName(slackSender) : 'Unknown'}
+      profilePhotoUrl={slackSender?.profilePhotoUrl}
+      sourceUrl={slackEvent?.externalLinks?.[0]?.externalUrl}
+      content={slackEvent?.content || ''}
+      onClick={() => openModal(slackEvent)}
+      date={DateTimeUtils.formatTime(slackEvent?.date)}
+      showDateOnHover
+    >
+      {!!slackEventReplies?.length && (
+        <Flex mt={1}>
+          <Flex columnGap={1} mr={1}>
+            {uniqThreadParticipants?.map(
+              ({ id, name, firstName, lastName, profilePhotoUrl }) => {
+                const displayName =
+                  name ?? [firstName, lastName].filter(Boolean).join(' ');
 
-                  return (
-                    <Avatar
-                      size='xs'
-                      name={displayName}
-                      variant='roundedSquareSmall'
-                      icon={<User02 color='primary.700' />}
-                      src={profilePhotoUrl ? profilePhotoUrl : undefined}
-                      key={`uniq-slack-thread-participant-${slackEvent.id}-${id}`}
-                    />
-                  );
-                },
-              )}
-            </Flex>
-            <Button variant='link' fontSize='sm' size='sm'>
-              {slackEventReplies.length}{' '}
-              {slackEventReplies.length === 1 ? 'reply' : 'replies'}
-            </Button>
+                return (
+                  <Avatar
+                    size='xs'
+                    name={displayName}
+                    variant='roundedSquareSmall'
+                    icon={<User02 color='primary.700' />}
+                    src={profilePhotoUrl ? profilePhotoUrl : undefined}
+                    key={`uniq-slack-thread-participant-${slackEvent.id}-${id}`}
+                  />
+                );
+              },
+            )}
           </Flex>
-        )}
-      </SlackMessageCard>
-    </>
+          <Button variant='link' fontSize='sm' size='sm'>
+            {slackEventReplies.length}{' '}
+            {slackEventReplies.length === 1 ? 'reply' : 'replies'}
+          </Button>
+        </Flex>
+      )}
+    </SlackMessageCard>
   );
 };
