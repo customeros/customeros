efafd0bf72bfb606da217620abcbf54638e8c2b1
Kasia <121807793+Kasia9090@users.noreply.github.com>
2023-10-04 17:50:26 +0200

    (fix|COS-389): Fixed contact roles ui (#3942)

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/PeoplePanel/ContactCard/FormRoleSelect.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/PeoplePanel/ContactCard/FormRoleSelect.tsx
@@ -3,12 +3,15 @@ import { useRef } from 'react';
 import { FormSelect } from '@ui/form/SyncSelect';
 import { Text } from '@ui/typography/Text';
 import { useOutsideClick } from '@ui/utils';
+import { getTagColorScheme, RoleTag } from './RoleTag';
+import { Flex } from '@chakra-ui/react';
+import { SelectOption } from '@shared/types/SelectOptions';
 
 interface FormRoleSelectProps {
   name: string;
   formId: string;
   isFocused: boolean;
-  displayValue: string;
+  data: SelectOption<string>[];
   placeholder?: string;
   isCardOpen?: boolean;
   setIsFocused: (isFocused: boolean) => void;
@@ -20,7 +23,7 @@ export const FormRoleSelect = ({
   isFocused,
   isCardOpen,
   placeholder,
-  displayValue,
+  data,
   setIsFocused,
 }: FormRoleSelectProps) => {
   const ref = useRef<HTMLDivElement>(null);
@@ -49,28 +52,66 @@ export const FormRoleSelect = ({
           ]}
           formId={formId}
           placeholder='Role'
+          chakraStyles={{
+            multiValue: (props, data) => {
+              const colorScheme = (() => getTagColorScheme(data.data.label))();
+              return {
+                ...props,
+                fontSize: 'xs',
+                fontWeight: 'normal',
+                color: `${[colorScheme]}.700`,
+                border: '1px solid',
+                borderColor: `${[colorScheme]}.200`,
+                backgroundColor: `${[colorScheme]}.50`,
+
+                '& div[role="button"]': {
+                  display: 'none',
+                },
+              };
+            },
+          }}
         />
       </span>
     );
   }
 
+  if (!data.length) {
+    return (
+      <Text
+        cursor='text'
+        color={'gray.400'}
+        onClick={(e) => {
+          if (isCardOpen) {
+            e.stopPropagation();
+          }
+          setIsFocused(true);
+        }}
+        borderBottom='1px solid transparent'
+        transition='border-color 0.2s ease-in-out'
+        _hover={{
+          borderColor: 'gray.300',
+        }}
+      >
+        {placeholder}
+      </Text>
+    );
+  }
   return (
-    <Text
-      cursor='text'
-      color={displayValue ? 'gray.500' : 'gray.400'}
+    <Flex
+      gap={1}
+      mt={2}
+      pb={2}
+      flexWrap='wrap'
       onClick={(e) => {
         if (isCardOpen) {
           e.stopPropagation();
         }
         setIsFocused(true);
       }}
-      borderBottom='1px solid transparent'
-      transition='border-color 0.2s ease-in-out'
-      _hover={{
-        borderColor: 'gray.300',
-      }}
     >
-      {displayValue || placeholder}
-    </Text>
+      {data.map((e) => (
+        <RoleTag key={e.label} label={e.label} />
+      ))}
+    </Flex>
   );
 };
---
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/PeoplePanel/ContactCard/RoleTag.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Tag, TagLabel } from '@ui/presentation/Tag';
+
+interface RoleTagProps {
+  label: string;
+}
+
+export const getTagColorScheme = (label: string) => {
+  switch (label) {
+    case 'Decision Maker':
+      return 'primary';
+    case 'Influencer':
+      return 'green';
+    case 'User':
+      return 'blue';
+    case 'Stakeholder':
+      return 'pink';
+    case 'Gatekeeper':
+      return 'orange';
+    case 'Champion':
+      return 'error';
+    default:
+      return 'gray';
+  }
+};
+
+export const RoleTag: React.FC<RoleTagProps> = ({ label }) => {
+  const colorScheme = (() => getTagColorScheme(label))();
+  return (
+    <Tag
+      size='sm'
+      variant='outline'
+      colorScheme={colorScheme}
+      border='1px solid'
+      borderColor={`${[colorScheme]}.200`}
+      backgroundColor={`${[colorScheme]}.50`}
+      color={`${[colorScheme]}.700`}
+      boxShadow='none'
+      fontWeight='normal'
+      minHeight={6}
+    >
+      <TagLabel>{label}</TagLabel>
+    </Tag>
+  );
+};
