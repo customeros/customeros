026974355da8ba0c4abce617ddb715d6774b1658
silviu <silviu@openline.ai>
2023-10-05 18:10:08 +0300

    [3945] - first test

---
+++ packages/apps/spaces/app/src/graphql/tests/tenantName.test.ts
@@ -0,0 +1,14 @@
+import { TenantNameDocument } from '../tenantName.generated';
+import expected from './tenantName.expected.json';
+import { client } from '../../../../test.resources';
+
+describe('graphql suite', () => {
+  test('should match tenant name', async () => {
+    try {
+      const res = await client.request(TenantNameDocument);
+      expect(res).toEqual(expected);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+});
