a5af6434fd05ef6b7da086711ff8d6f1f366a1cb
SilviuOpenline <125381623+SilviuOpenline@users.noreply.github.com>
2023-10-06 17:50:42 +0300

    3976 extend fe test suite (#3981)
    * [3976] - test suite extension
    
    * [3976] - removed commented code

--- packages/apps/spaces/app/src/graphql/tests/tenantName.test.ts
+++ packages/apps/spaces/app/src/graphql/tests/tenantName.test.ts
@@ -1,14 +1,44 @@
 import { TenantNameDocument } from '../tenantName.generated';
-import expected from './tenantName.expected.json';
-import { client } from '../../../../test.resources';
+import expectedGoodTenantName from './tenantName_good.expected.json';
+import expectedBadTenantName from './tenantName_bad.expected.json';
+import expectedUnauthenticatedUser from './unauthenticatedUser.expected.json';
+import {
+  authenticated_client,
+  unauthenticated_client,
+} from '../../../../test.resources';
 
 describe('graphql suite', () => {
   test('should match tenant name', async () => {
     try {
-      const res = await client.request(TenantNameDocument);
-      expect(res).toEqual(expected);
+      const res = await authenticated_client.request(TenantNameDocument);
+      expect(res).toEqual(expectedGoodTenantName);
     } catch (e) {
       expect(e).toBeNull();
+    }
+  });
+});
+
+describe('graphql suite', () => {
+  test('should not match tenant name', async () => {
+    try {
+      const res = await authenticated_client.request(TenantNameDocument);
+      expect(res).not.toEqual(expectedBadTenantName);
+    } catch (e) {
+      expect(e).toBeNull();
+    }
+  });
+});
+
+describe('graphql suite', () => {
+  test('should return error for unauthenticated user', async () => {
+    try {
+      await unauthenticated_client.request(TenantNameDocument);
+    } catch (e: any) {
+      const filteredReceived = Object.fromEntries(
+        Object.entries(e.response).filter(([key]) => key !== 'headers'),
+      );
+      expect(filteredReceived).toEqual(expectedUnauthenticatedUser);
+
     }
   });
 });
--- packages/apps/spaces/test.resources.ts
+++ packages/apps/spaces/test.resources.ts
@@ -4,7 +4,17 @@ export const headers = {
   'X-Openline-API-KEY': 'dd9d2474-b4a9-4799-b96f-73cd0a2917e4',
   'X-Openline-TENANT': 'openline',
 };
-export const client = new GraphQLClient('http://127.0.0.1:10000/query', {
-  credentials: 'include',
-  headers,
-});
+export const authenticated_client = new GraphQLClient(
+  'http://127.0.0.1:10000/query',
+  {
+    credentials: 'include',
+    headers,
+  },
+);
+
+export const unauthenticated_client = new GraphQLClient(
+  'http://127.0.0.1:10000/query',
+  {
+    credentials: 'include',
+  },
+);
