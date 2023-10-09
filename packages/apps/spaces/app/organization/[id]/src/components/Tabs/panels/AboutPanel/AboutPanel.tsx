2414a0502911b0d660251ea36ce6dbda500bbfde
Alex Calinica <acalinica@openline.ai>
2023-10-09 17:52:17 +0300

    fix: customer-os-id display label

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AboutPanel/AboutPanel.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AboutPanel/AboutPanel.tsx
@@ -408,7 +408,7 @@ export const AboutPanel = () => {
               cursor='pointer'
               onClick={() => copyToClipboard('plm2', 'CustomerOS ID copied')}
             >
-              CustomerIS ID: {data?.organization?.customerOsId}
+              CustomerOS ID: {data?.organization?.customerOsId}
             </Text>
           </Tooltip>
         )}
