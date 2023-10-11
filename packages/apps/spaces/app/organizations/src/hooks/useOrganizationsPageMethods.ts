5f74efc2aa0725332c573233f511534dab84bd2a
Alex Calinica <acalinica@openline.ai>
2023-10-11 17:15:15 +0300

    fix: remove old code

--- packages/apps/spaces/app/organizations/src/hooks/useOrganizationsPageMethods.ts
+++ packages/apps/spaces/app/organizations/src/hooks/useOrganizationsPageMethods.ts
@@ -45,7 +45,6 @@ export const useOrganizationsPageMethods = () => {
               if (!draft) return;
               draft.id = Math.random().toString();
               draft.name = '';
-              draft.relationshipStages = [];
               draft.website = '';
               draft.owner = null;
               draft.lastTouchPointTimelineEvent = null;
