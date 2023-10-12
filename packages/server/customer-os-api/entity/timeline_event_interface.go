98a718bea7f2319ac1b911f760772c1e2ad3fd4f
edifirut <108661145+edifirut@users.noreply.github.com>
2023-10-12 11:31:39 +0300

    [COS-473] - Sync issues from airbyte (#4083)

--- packages/server/customer-os-api/entity/timeline_event_interface.go
+++ packages/server/customer-os-api/entity/timeline_event_interface.go
@@ -5,7 +5,7 @@ import "github.com/openline-ai/openline-customer-os/packages/server/customer-os-
 var NodeLabelsByTimelineEventType = map[string]string{
 	//model.TimelineEventTypePageView.String():           NodeLabel_PageView,
 	//model.TimelineEventTypeInteractionSession.String(): NodeLabel_InteractionSession,
-	//model.TimelineEventTypeIssue.String():              NodeLabel_Issue,
+	model.TimelineEventTypeIssue.String(): NodeLabel_Issue,
 	//model.TimelineEventTypeNote.String():               NodeLabel_Note,
 	model.TimelineEventTypeInteractionEvent.String(): NodeLabel_InteractionEvent,
 	model.TimelineEventTypeMeeting.String():          NodeLabel_Meeting,
