916a10f378aa38cb4ee71fae2d40de1816068731
alexopenline <alex@openline.ai>
2023-10-14 14:05:09 +0300

    cache tenants to reduce neo4j checks

--- packages/server/customer-os-webhooks/caches/caches.go
+++ packages/server/customer-os-webhooks/caches/caches.go
@@ -8,21 +8,24 @@ import (
 const (
 	KB       = 1024
 	cache5MB = 5 * 1024 * KB
+	cache1MB = 1 * 1024 * KB
 )
 const (
-	days30       = 30
-	expire30Days = days30 * 24 * 60 * 60 // 20 days
+	expire30Days = 30 * 24 * 60 * 60
+	expire1Day   = 24 * 60 * 60
 )
 const delimiter = "--"
 
 type Cache struct {
-	mu    sync.RWMutex
-	cache *freecache.Cache
+	mu                  sync.RWMutex
+	externalSystemCache *freecache.Cache
+	tenantCache         *freecache.Cache
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		cache: freecache.NewCache(cache5MB),
+		externalSystemCache: freecache.NewCache(cache5MB),
+		tenantCache:         freecache.NewCache(cache1MB),
 	}
 }
 
@@ -34,7 +37,7 @@ func (c *Cache) AddExternalSystem(tenant, externalSystem string) {
 	keyBytes := []byte(tenant + delimiter + externalSystem)
 	valueBytes := []byte("1") // A simple marker value
 
-	_ = c.cache.Set(keyBytes, valueBytes, expire30Days)
+	_ = c.externalSystemCache.Set(keyBytes, valueBytes, expire30Days)
 }
 
 func (c *Cache) CheckExternalSystem(tenant, externalSystem string) bool {
@@ -44,7 +47,33 @@ func (c *Cache) CheckExternalSystem(tenant, externalSystem string) bool {
 	// Convert strings to []byte
 	keyBytes := []byte(tenant + delimiter + externalSystem)
 
-	_, err := c.cache.Get(keyBytes)
+	_, err := c.externalSystemCache.Get(keyBytes)
+	if err != nil {
+		return false // Key not found in cache
+	}
+
+	return true // Key found in cache
+}
+
+func (c *Cache) AddTenant(tenant string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// Convert strings to []byte
+	keyBytes := []byte(tenant)
+	valueBytes := []byte("1") // A simple marker value
+
+	_ = c.tenantCache.Set(keyBytes, valueBytes, expire1Day)
+}
+
+func (c *Cache) CheckTenant(tenant string) bool {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	// Convert strings to []byte
+	keyBytes := []byte(tenant)
+
+	_, err := c.tenantCache.Get(keyBytes)
 	if err != nil {
 		return false // Key not found in cache
 	}
