0c0bb76eff620361a27c35373f5f286881a61c3a
Alex Calinica <acalinica@openline.ai>
2023-10-17 16:37:55 +0300

    fix: Social Input icons and handle http links

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/shared/util.ts
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/shared/util.ts
@@ -8,6 +8,9 @@ export function formatSocialUrl(value = '') {
   if (url.startsWith('http')) {
     url = url.replace('https://', '');
   }
+  if (url.startsWith('http://')) {
+    url = url.replace('http://', '');
+  }
   if (url.startsWith('www')) {
     url = url.replace('www.', '');
   }
