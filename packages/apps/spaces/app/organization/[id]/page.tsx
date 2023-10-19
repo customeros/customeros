74d476561a827033a6aa7201491f56178693b81e
kasia <kasia@openline.ai>
2023-10-19 04:42:16 +0200

    (fix|COS-564): Remove access check in Organization Page.
    When user creates organization sometimes request for this org id results in 404. This is major problem for the users. Therefore code related to showing not found message for orgs that are actually not existing or parts of different tenant was removed as this is minor issue for now

--- packages/apps/spaces/app/organization/[id]/page.tsx
+++ packages/apps/spaces/app/organization/[id]/page.tsx
@@ -2,12 +2,6 @@ import { SideSection } from './src/components/SideSection';
 import { MainSection } from './src/components/MainSection';
 import { Panels, TabsContainer } from './src/components/Tabs';
 import { OrganizationTimelineWithActionsContext } from './src/components/Timeline/OrganizationTimelineWithActionsContext';
-import {
-  GetCanAccessOrganizationDocument,
-  GetCanAccessOrganizationQuery,
-} from '@organization/src/graphql/getCanAccessOrganization.generated';
-import { getServerGraphQLClient } from '@shared/util/getServerGraphQLClient';
-import NotFound from './src/components/NotFound/NotFound';
 import { TimelineContextsProvider } from '@organization/src/components/TimelineContextsProvider';
 
 interface OrganizationPageProps {
@@ -19,19 +13,6 @@ export default async function OrganizationPage({
   searchParams,
   params,
 }: OrganizationPageProps) {
-  const client = getServerGraphQLClient();
-
-  try {
-    await client.request<GetCanAccessOrganizationQuery>(
-      GetCanAccessOrganizationDocument,
-      {
-        id: params.id,
-      },
-    );
-  } catch (error) {
-    return <NotFound />;
-  }
-
   return (
     <TimelineContextsProvider id={params.id}>
       <SideSection>
