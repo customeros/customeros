fe835fe8aa5387fd41ebe6d7e7eaff0ffc913c75
alexopenline <alex@openline.ai>
2023-10-19 10:38:48 +0300

    hierarchical sorting of organizations

--- packages/server/customer-os-api/service/sort_builder.go
+++ packages/server/customer-os-api/service/sort_builder.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/graph/model"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
+	"github.com/pkg/errors"
 	"reflect"
 )
 
@@ -10,9 +11,9 @@ func buildSort(sortBy []*model.SortBy, T reflect.Type) (*utils.CypherSort, error
 	transformedSorting := new(utils.CypherSort)
 	if sortBy != nil {
 		for _, v := range sortBy {
-			err := transformedSorting.NewSortRule(v.By, v.Direction.String(), *v.CaseSensitive, T)
-			if err != nil {
-				return nil, err
+			orderBy := transformedSorting.NewSortRule(v.By, v.Direction.String(), *v.CaseSensitive, T)
+			if !orderBy.IsValid() {
+				return nil, errors.New("Invalid sorting rule")
 			}
 		}
 	}
