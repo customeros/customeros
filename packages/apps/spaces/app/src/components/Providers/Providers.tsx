e906e175facb269b77d7c39ee31f225b970ee4a1
kasia <kasia@openline.ai>
2023-10-20 16:18:07 +0200

    (fix|COS-617): Fix my portfolio page results
    Restore persisted query client as it was not causing the issue
    
     Added an additional check under the "enabled" option: the query fires only if the preset equals 'portfolio' and a valid user ID exists, or if the preset doesn't equal 'portfolio'. This ensures that query won't be called until async params data is available

--- packages/apps/spaces/app/src/components/Providers/Providers.tsx
+++ packages/apps/spaces/app/src/components/Providers/Providers.tsx
@@ -2,26 +2,49 @@
 
 import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+
+import { createIDBPersister } from '@shared/util/indexedDBPersister';
 import { AnalyticsProvider } from '@shared/components/Providers/AnalyticsProvider';
+
 import { NextAuthProvider } from './SessionProvider';
 
 interface ProvidersProps {
+  sessionEmail?: string | null;
   children: React.ReactNode;
 }
 
-export const Providers = ({ children }: ProvidersProps) => {
-  const [queryClient] = useState(() => new QueryClient());
+const hostname =
+  typeof window !== 'undefined' ? window?.location?.hostname : 'platform';
+
+export const Providers = ({ children, sessionEmail }: ProvidersProps) => {
+  const [persister] = useState(() =>
+    createIDBPersister(`${sessionEmail ?? 'cos'}-${hostname}`),
+  );
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+          },
+        },
+      }),
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
       <RecoilRoot>
         <NextAuthProvider>
           <AnalyticsProvider>{children}</AnalyticsProvider>
         </NextAuthProvider>
       </RecoilRoot>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 };
