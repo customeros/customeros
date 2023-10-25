893c83f7771e56556026b75427c701df761e9ce0
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2023-10-25 17:16:03 +0200

    feat: add intercom scripts (#4246)

--- packages/apps/spaces/app/layout.tsx
+++ packages/apps/spaces/app/layout.tsx
@@ -76,6 +76,36 @@ export default async function RootLayout({
         />
       )}
 
+      <Script
+        async
+        strategy='afterInteractive'
+        id='openline-customer-os-intercom-user-data-script'
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.intercomSettings = {
+          api_base: "https://api-iam.intercom.io",
+          app_id: "pqerb2dx",
+          alignment: "left",
+          vertical_padding: 132,
+          name: "${session?.user.name}",
+          email: "${session?.user.email}",
+          created_at: ${new Date().valueOf()} // Signup date as a Unix timestamp
+        };
+        `,
+        }}
+      />
+
+      <Script
+        async
+        strategy='afterInteractive'
+        id='openline-customer-os-intercom-script'
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/pqerb2dx';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+        `,
+        }}
+      />
+
       <body className='scrollbar'>
         <ThemeProvider>
           <Providers>
