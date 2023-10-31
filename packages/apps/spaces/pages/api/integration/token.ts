d5c33c0ccd42ff99fe954f3d73b6105c42297b91
Eduard Firut <edi@openline.ai>
2023-10-31 11:43:44 +0200

    [COS-738] - Sign algorithm CWE

--- packages/apps/spaces/pages/api/integration/token.ts
+++ packages/apps/spaces/pages/api/integration/token.ts
@@ -33,7 +33,7 @@ export default async function handler(
 
   const token = jwt.sign(tokenData, WORKSPACE_SECRET, {
     issuer: WORKSPACE_KEY,
-    expiresIn: 7200,
+    expiresIn: 3600,
     algorithm: 'HS256'
   });
 
