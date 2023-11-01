85936364b824619899c2cc6f69a159063eb84172
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2023-11-01 05:28:13 +0100

    chore: configure env for highlight (#4344)

--- packages/apps/spaces/app/src/components/Providers/AnalyticsProvider.tsx
+++ packages/apps/spaces/app/src/components/Providers/AnalyticsProvider.tsx
@@ -2,33 +2,38 @@
 
 import { useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-// import { H } from '@highlight-run/next/client';
+import { H } from '@highlight-run/next/client';
 
-declare const heap: any;
+declare const heap: {
+  identify: (email: string | null | undefined) => void;
+  addUserProperties: (
+    properties: Record<string, string | null | undefined>,
+  ) => void;
+};
 
 export const AnalyticsProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  // const { data: session } = useSession();
-  //
-  // useEffect(() => {
-  //   if (!session) return;
-  //   if (heap) {
-  //     heap.identify(session.user?.email);
-  //     heap.addUserProperties({
-  //       name: session?.user?.name,
-  //     });
-  //   }
-  //
-  //   if (session?.user?.email) {
-  //     H.identify(session?.user?.email, {
-  //       name: session?.user?.name ?? 'Unknown',
-  //       playerIdentityId: session.user.playerIdentityId,
-  //     });
-  //   }
-  // }, [session]);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (!session) return;
+    if (heap) {
+      heap.identify(session.user?.email);
+      heap.addUserProperties({
+        name: session?.user?.name,
+      });
+    }
+
+    if (session?.user?.email) {
+      H.identify(session?.user?.email, {
+        name: session?.user?.name ?? 'Unknown',
+        playerIdentityId: session.user.playerIdentityId,
+      });
+    }
+  }, [session]);
 
   return <>{children}</>;
 };
