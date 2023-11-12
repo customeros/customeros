f188ff3f420c5ee48f1162dfcd5490c10a7cf7e5
kasia <kasia@openline.ai>
2023-11-12 18:09:58 +0100

    (feat|COS-856): Added new SubscriptionServiceModal component & enhance FormNumberInput
    Add a new SubscriptionServiceModal component to allow users to add subscription services in the app UI. The interface was enhanced with a billing frequency selector, license selector, currency input, and description input. The FormNumberInput component was also improved to allow optional labels and extra elements for better UI. These changes aim to improve the user experience when managing subscription services.

--- packages/apps/spaces/ui/form/NumberInput/FormNumberInput.tsx
+++ packages/apps/spaces/ui/form/NumberInput/FormNumberInput.tsx
@@ -1,26 +1,82 @@
 'use client';
 
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 import { useField } from 'react-inverted-form';
+
+import { FormLabel, FormControl, VisuallyHidden } from '@chakra-ui/react';
+
+import { NumberInputField } from '@ui/form/NumberInput/NumberInput';
+import {
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@ui/form/InputGroup/InputGroup';
 
 import { NumberInput, NumberInputProps } from './NumberInput';
 
 interface FormNumberInputProps extends NumberInputProps {
   name: string;
   formId: string;
+  label?: string;
+  isLabelVisible?: boolean;
+  leftElement?: React.ReactNode;
+  rightElement?: React.ReactNode;
 }
 
-export const FormNumberInput = forwardRef(
-  ({ name, formId, ...props }: FormNumberInputProps, ref) => {
+export const FormNumberInput = forwardRef<
+  HTMLInputElement,
+  FormNumberInputProps
+>(
+  (
+    {
+      name,
+      formId,
+      isLabelVisible,
+      label = '',
+      leftElement,
+      rightElement,
+      ...props
+    },
+    ref,
+  ) => {
     const { getInputProps } = useField(name, formId);
 
     return (
-      <NumberInput
-        ref={ref}
-        {...getInputProps()}
-        {...props}
-        autoComplete='off'
-      />
+      <FormControl>
+        {isLabelVisible ? (
+          <FormLabel
+            fontWeight={600}
+            color={props?.color}
+            fontSize='sm'
+            mb={-1}
+          >
+            {label}
+          </FormLabel>
+        ) : (
+          <VisuallyHidden>
+            <FormLabel>{label}</FormLabel>
+          </VisuallyHidden>
+        )}
+        <InputGroup>
+          {leftElement && (
+            <InputLeftElement w='4'>{leftElement}</InputLeftElement>
+          )}
+
+          <NumberInput {...getInputProps()} {...props} autoComplete='off'>
+            <NumberInputField
+              ref={ref}
+              pl={leftElement ? '30px' : '0'}
+              pr={0}
+              autoComplete='off'
+              placeholder={props?.placeholder || ''}
+            />
+          </NumberInput>
+
+          {rightElement && (
+            <InputRightElement>{rightElement}</InputRightElement>
+          )}
+        </InputGroup>
+      </FormControl>
     );
   },
 );
