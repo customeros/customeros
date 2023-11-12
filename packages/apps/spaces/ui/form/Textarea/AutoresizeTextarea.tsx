b5c0c496dc99f60f1193dfcd3b23d686c9f79c9e
kasia <kasia@openline.ai>
2023-11-12 16:59:10 +0100

    Modified AutoresizeTextarea to support labels Updated SyncSelect styling for readOnly state Fixed color issue on DotSingle Icon
    This commit introduces several UI improvements. Implemented the ClockCheck icon for use in time-related features and updates the color of DotSingle to improve icon consistency across the app. Improved AutoresizeTextarea by adding support for labels, providing better context for form fields. Finally, updates the styling of SyncSelect in a readOnly state for better user experience.

--- packages/apps/spaces/ui/form/Textarea/AutoresizeTextarea.tsx
+++ packages/apps/spaces/ui/form/Textarea/AutoresizeTextarea.tsx
@@ -1,7 +1,13 @@
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 import ResizeTextarea, { TextareaAutosizeProps } from 'react-textarea-autosize';
 
-import { Textarea, TextareaProps } from '@chakra-ui/react';
+import {
+  Textarea,
+  FormLabel,
+  FormControl,
+  TextareaProps,
+  VisuallyHidden,
+} from '@chakra-ui/react';
 
 import { InputGroup, InputLeftElement } from '../InputGroup';
 
@@ -11,35 +17,57 @@ export interface AutoresizeTextareaProps
       TextareaAutosizeProps,
       'maxRows' | 'minRows' | 'onHeightChange' | 'cacheMeasurements'
     > {
+  label?: string;
+  isLabelVisible?: boolean;
   leftElement?: React.ReactNode;
 }
 
 export const AutoresizeTextarea = forwardRef<
   HTMLTextAreaElement,
   AutoresizeTextareaProps
->(({ leftElement, ...props }, ref) => {
+>(({ leftElement, isLabelVisible, label = '', ...props }, ref) => {
   return (
-    <InputGroup>
-      {leftElement && <InputLeftElement w='4'>{leftElement}</InputLeftElement>}
-      <Textarea
-        w='100%'
-        ref={ref}
-        minRows={1}
-        minH='unset'
-        resize='none'
-        overflow='hidden'
-        as={ResizeTextarea}
-        borderColor='transparent'
-        color='gray.700'
-        _hover={{
-          borderColor: 'gray.300',
-        }}
-        _focusVisible={{
-          borderColor: 'primary.500',
-          boxShadow: 'unset',
-        }}
-        {...props}
-      />
-    </InputGroup>
+    <FormControl>
+      {isLabelVisible ? (
+        <FormLabel
+          fontWeight={600}
+          color={props?.color}
+          fontSize='sm'
+          mb={0}
+          mt={2}
+        >
+          {label}
+        </FormLabel>
+      ) : (
+        <VisuallyHidden>
+          <FormLabel>{label}</FormLabel>
+        </VisuallyHidden>
+      )}
+      <InputGroup>
+        {leftElement && (
+          <InputLeftElement w='4'>{leftElement}</InputLeftElement>
+        )}
+        <Textarea
+          w='100%'
+          ref={ref}
+          minRows={1}
+          minH='unset'
+          resize='none'
+          aria-label={label}
+          overflow='hidden'
+          as={ResizeTextarea}
+          borderColor='transparent'
+          color='gray.700'
+          _hover={{
+            borderColor: 'gray.300',
+          }}
+          _focusVisible={{
+            borderColor: 'primary.500',
+            boxShadow: 'unset',
+          }}
+          {...props}
+        />
+      </InputGroup>
+    </FormControl>
   );
 });
