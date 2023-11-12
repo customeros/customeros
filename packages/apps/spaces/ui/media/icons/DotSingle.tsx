963c561f6a33987401065218664abaa6b0d127e5
kasia <kasia@openline.ai>
2023-11-12 16:59:10 +0100

    Modified AutoresizeTextarea to support labels Updated SyncSelect styling for readOnly state Fixed color issue on DotSingle Icon
    This commit introduces several UI improvements. Implemented the ClockCheck icon for use in time-related features and updates the color of DotSingle to improve icon consistency across the app. Improved AutoresizeTextarea by adding support for labels, providing better context for form fields. Finally, updates the styling of SyncSelect in a readOnly state for better user experience.

--- packages/apps/spaces/ui/media/icons/DotSingle.tsx
+++ packages/apps/spaces/ui/media/icons/DotSingle.tsx
@@ -2,6 +2,6 @@ import { Icon, IconProps } from '@ui/media/Icon';
 
 export const DotSingle = (props: IconProps) => (
   <Icon viewBox='0 0 24 24' fill='none' boxSize='4' {...props}>
-    <circle cx='12' cy='12' r='4' fill='#0C111D' />
+    <circle cx='12' cy='12' r='4' fill='currentColor' />
   </Icon>
 );
