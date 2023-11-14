ac4928351351d872c574c2c66b046a45651cd73d
Kasia <121807793+Kasia9090@users.noreply.github.com>
2023-11-14 10:50:32 +0100

    (feat|COS-855): Draft contract UI (#4437)
    * WIP: (feat|COS-855): Update AccountPanel component, add Contract DTO and ContractCard
    
    Updated AccountPanel component to include ContractCard and ARRForecast components instead of RenewalForecast, TimeToRenewal and BillingDetailsCard. The change also includes the addition of Contract.dto.ts and ContractCard.tsx files corresponding to the new Contract module. The modals related to hooks 'useDisclosure' are commented out for future modification and connection. This change is part of the requirement to transition the organization's accounts to a contract-based system.
    
    * WIP: Added EmptyContracts view for accounts without contracts
    
    This commit introduces a new EmptyContracts component that will be displayed within the AccountPanel when there are no contracts associated with the account. The component includes a button to prompt the creation of a new contract.
    
    * Add ARRForecast component to AccountPanel
    
    Added a new component named ARRForecast to the AccountPanel. This component displays Annual Recurring Revenue (ARR) forecast.
    
    * Changed icon in EmptyContracts component
    
    * Updated components to manage Contracts and Services
    
    In this commit, a ServicesList component was added to allow the listing of services. Modifications were also made on the SubscriptionServiceModal, ContractCard, and ARRCost components to enable adding and updating of services in a Contract.
    
    * Additionally, a one-time service component has been added, the SubscriptionServiceModal has been renamed to OneTimeServiceModal and components to handle one-time and subscription services were extracted out from the old SubscriptionServiceModal into independent forms. This was done to offer better structured, more reusable and more testable components. Also, FormInput component was modified to allow for custom FormLabelProps, providing more flexibility in label customization.
    
    SubscriptionServiceModal was earlier tightly coupled, and handled both one-time and subscription services. By having separate forms, it provides a clear distinction between the two types of service and allows more flexibility and control. Also, renaming SubscriptionServiceModal to ServiceModal makes it clear that it can encompass any kind of service, and not just subscription ones.

---
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/ARRForecast/ARRForecast.tsx
@@ -0,0 +1,136 @@
+'use client';
+import { useIsMutating, useIsRestoring } from '@tanstack/react-query';
+
+import { Flex } from '@ui/layout/Flex';
+import { Text } from '@ui/typography/Text';
+import { UseDisclosureReturn } from '@ui/utils';
+import { IconButton } from '@ui/form/IconButton';
+import { Heading } from '@ui/typography/Heading';
+import { Icons, FeaturedIcon } from '@ui/media/Icon';
+import { Card, CardBody } from '@ui/presentation/Card';
+import { RenewalLikelihoodProbability } from '@graphql/types';
+import { InfoDialog } from '@ui/overlay/AlertDialog/InfoDialog';
+import { CurrencyDollar } from '@ui/media/icons/CurrencyDollar';
+import { formatCurrency } from '@spaces/utils/getFormattedCurrencyNumber';
+import { getFeatureIconColor } from '@organization/src/components/Tabs/panels/AccountPanel/utils';
+import { RenewalForecastType } from '@organization/src/components/Tabs/panels/AccountPanel/RenewalForecast';
+import { useUpdateRenewalLikelihoodMutation } from '@organization/src/graphql/updateRenewalLikelyhood.generated';
+
+interface ARRForecastProps {
+  name: string;
+  isInitialLoading?: boolean;
+  infoModal: UseDisclosureReturn;
+  aRRForecast?: RenewalForecastType;
+  renewalProbability?: RenewalLikelihoodProbability | null;
+}
+
+export const ARRForecast = ({
+  isInitialLoading,
+  aRRForecast,
+  renewalProbability,
+  infoModal,
+  name,
+}: ARRForecastProps) => {
+  const isRestoring = useIsRestoring();
+
+  const isMutating = useIsMutating({
+    mutationKey: useUpdateRenewalLikelihoodMutation.getKey(),
+  });
+
+  return (
+    <>
+      <Card
+        p='4'
+        w='full'
+        size='lg'
+        variant='ghost'
+        bg='transparent'
+        cursor='default'
+        boxShadow='none'
+        sx={{
+          '& button': {
+            opacity: 0,
+            transition: 'opacity 0.2s linear',
+          },
+        }}
+        _hover={{
+          '& button': {
+            opacity: 1,
+          },
+        }}
+      >
+        <CardBody as={Flex} p='0' align='center'>
+          <FeaturedIcon
+            size='md'
+            minW='10'
+            colorScheme={
+              aRRForecast?.amount && !aRRForecast?.updatedBy
+                ? getFeatureIconColor(renewalProbability)
+                : 'gray'
+            }
+          >
+            <CurrencyDollar />
+          </FeaturedIcon>
+          <Flex
+            ml='5'
+            w='full'
+            align='center'
+            columnGap={4}
+            justify='space-between'
+          >
+            <Flex flexDir='column'>
+              <Flex align='center'>
+                <Heading
+                  size='sm'
+                  whiteSpace='nowrap'
+                  fontWeight='semibold'
+                  color='gray.700'
+                  mr={2}
+                >
+                  ARR forecast
+                </Heading>
+                <IconButton
+                  size='xs'
+                  variant='ghost'
+                  aria-label='Help'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    infoModal.onOpen();
+                  }}
+                  icon={<Icons.HelpCircle color='gray.400' />}
+                />
+              </Flex>
+            </Flex>
+
+            <Heading fontSize='2xl' color='gray.700'>
+              {isMutating && (!isInitialLoading || !isRestoring)
+                ? 'Calculating...'
+                : formatCurrency(aRRForecast?.amount ?? 0)}
+            </Heading>
+          </Flex>
+        </CardBody>
+      </Card>
+
+      <InfoDialog
+        isOpen={infoModal.isOpen}
+        onClose={infoModal.onClose}
+        onConfirm={infoModal.onClose}
+        confirmButtonLabel='Got it'
+        label='ARR forecast'
+      >
+        <Text fontSize='sm' fontWeight='normal' mb={4}>
+          Annual Recurring Revenue (ARR) is the total amount of money you can
+          expect to receive from{' '}
+          <Text as='span' fontWeight='medium'>
+            {name}
+          </Text>{' '}
+          for the next 12 months.
+        </Text>
+        <Text fontSize='sm' fontWeight='normal'>
+          It includes all renewals but excludes one-time services. Renewals are
+          discounted based on the renewal likelihood.
+        </Text>
+      </InfoDialog>
+    </>
+  );
+};
