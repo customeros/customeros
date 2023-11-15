b707e477809e3dcae2a3d21d68b3ffa9f64e6ec1
kasia <kasia@openline.ai>
2023-11-15 12:54:31 +0100

    Added Contract Renewal card and introduce it in contract UI

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/Contract/ContractCard.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/Contract/ContractCard.tsx
@@ -1,21 +1,25 @@
 import { useForm } from 'react-inverted-form';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import { Flex } from '@ui/layout/Flex';
+import { Button } from '@ui/form/Button';
 import { Text } from '@ui/typography/Text';
 import { Plus } from '@ui/media/icons/Plus';
+import { Edit03 } from '@ui/media/icons/Edit03';
 import { UseDisclosureReturn } from '@ui/utils';
 import { File02 } from '@ui/media/icons/File02';
 import { FormSelect } from '@ui/form/SyncSelect';
 import { IconButton } from '@ui/form/IconButton';
 import { Heading } from '@ui/typography/Heading';
-import { Divider } from '@ui/presentation/Divider';
+import { Collapse } from '@ui/transitions/Collapse';
+import { ChevronUp } from '@ui/media/icons/ChevronUp';
 import { DatePicker } from '@ui/form/DatePicker/DatePicker';
 import { Card, CardBody, CardFooter, CardHeader } from '@ui/presentation/Card';
 import { billingFrequencyOptions } from '@organization/src/components/Tabs/panels/AccountPanel/utils';
 import { ContractDTO } from '@organization/src/components/Tabs/panels/AccountPanel/Contract/Contract.dto';
 import { ServiceModal } from '@organization/src/components/Tabs/panels/AccountPanel/Services/ServiceModal';
 import { ServicesList } from '@organization/src/components/Tabs/panels/AccountPanel/Contract/ServicesList';
+import { RenewalARRCard } from '@organization/src/components/Tabs/panels/AccountPanel/Contract/RenewalARRCard';
 import { ContractStatusSelect } from '@organization/src/components/Tabs/panels/AccountPanel/Contract/contractStatuses/ContractStatusSelect';
 
 interface ContractCardProps {
@@ -29,6 +33,7 @@ export const ContractCard = ({
   name = '',
 }: ContractCardProps) => {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const formId = 'contractForm';
 
@@ -75,63 +80,102 @@ export const ContractCard = ({
       borderColor='gray.200'
       bg='gray.50'
     >
-      <CardHeader
-        as={Flex}
-        p='0'
-        pb={3}
-        w='full'
-        justifyContent='space-between'
-      >
-        <Heading size='sm' color='gray.700' noOfLines={1}>
-          {name} ContractCard
-        </Heading>
-        <Flex>
-          <File02 color='gray.400' mr={4} />
-          <ContractStatusSelect />
+      <CardHeader as={Flex} p='0' pb={2} w='full' flexDir='column'>
+        <Flex justifyContent='space-between' w='full'>
+          <Heading size='sm' color='gray.700' noOfLines={1}>
+            {name} ContractCard
+          </Heading>
+          <Flex alignItems='center' gap={2}>
+            <File02 color='gray.400' />
+            <ContractStatusSelect />
+
+            {isExpanded && (
+              <IconButton
+                size='xs'
+                variant='ghost'
+                aria-label='Show validationresults'
+                icon={<ChevronUp />}
+                onClick={() => setIsExpanded(false)}
+              />
+            )}
+          </Flex>
         </Flex>
+
+        {!isExpanded && (
+          <Button
+            bg='transparent'
+            _hover={{
+              bg: 'transparent',
+              svg: { opacity: 1, transition: 'opacity 0.2s linear' },
+            }}
+            sx={{ svg: { opacity: 0, transition: 'opacity 0.2s linear' } }}
+            size='xs'
+            fontSize='sm'
+            fontWeight='normal'
+            color='gray.500'
+            p={0}
+            alignItems='center'
+            justifyContent='flex-start'
+            onClick={() => setIsExpanded(true)}
+          >
+            Service starts 1 Now 2023
+            <Edit03 ml={1} color='gray.400' boxSize='3' />
+          </Button>
+        )}
       </CardHeader>
+      {isExpanded && (
+        <Collapse
+          in={isExpanded}
+          style={{ overflow: 'unset' }}
+          delay={{
+            exit: 2,
+          }}
+        >
+          <CardBody as={Flex} p='0' flexDir='column' w='full'>
+            <Flex gap='4' mb={2}>
+              <DatePicker
+                label='Contract signed'
+                placeholder='Signed date'
+                formId={formId}
+                name='contractSigned'
+                calendarIconHidden
+                inset='120% auto auto 0px'
+              />
+              <DatePicker
+                label='Contract ends'
+                placeholder='End date'
+                formId={formId}
+                name='contractEnds'
+                calendarIconHidden
+              />
+            </Flex>
+            <Flex gap='4'>
+              <DatePicker
+                label='Service starts'
+                placeholder='Start date'
+                formId={formId}
+                name='serviceStarts'
+                calendarIconHidden
+                inset='120% auto auto 0px'
+              />
 
-      <CardBody as={Flex} p='0' flexDir='column' w='full'>
-        <Flex gap='4' mb={2}>
-          <DatePicker
-            label='Contract signed'
-            placeholder='Signed date'
-            formId={formId}
-            name='contractSigned'
-            calendarIconHidden
-            inset='120% auto auto 0px'
-          />
-          <DatePicker
-            label='Contract ends'
-            placeholder='End date'
-            formId={formId}
-            name='contractEnds'
-            calendarIconHidden
-          />
-        </Flex>
-        <Flex gap='4'>
-          <DatePicker
-            label='Service starts'
-            placeholder='Start date'
-            formId={formId}
-            name='serviceStarts'
-            calendarIconHidden
-            inset='120% auto auto 0px'
-          />
+              <FormSelect
+                label='Contract renews'
+                placeholder='Contract renews'
+                isLabelVisible
+                name='renewalCycle'
+                formId={formId}
+                options={billingFrequencyOptions}
+              />
+            </Flex>
+            {/*<Divider  />*/}
+          </CardBody>
+        </Collapse>
+      )}
 
-          <FormSelect
-            label='Contract renews'
-            placeholder='Contract renews'
-            isLabelVisible
-            name='renewalCycle'
-            formId={formId}
-            options={billingFrequencyOptions}
-          />
-        </Flex>
-        <Divider my='2' />
-      </CardBody>
+      <CardFooter p='0' w='full' flexDir='column'>
+        <RenewalARRCard />
 
-      <CardFooter p='0' w='full'>
         <Flex w='full' alignItems='center' justifyContent='space-between'>
           <Text fontWeight='semibold' fontSize='sm'>
             No services
---
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/Contract/RenewalARRCard.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+import { Flex } from '@ui/layout/Flex';
+import { Text } from '@ui/typography/Text';
+import { FeaturedIcon } from '@ui/media/Icon';
+import { Heading } from '@ui/typography/Heading';
+import { RenewalLikelihoodProbability } from '@graphql/types';
+import { Card, CardBody, CardHeader } from '@ui/presentation/Card';
+import { ClockFastForward } from '@ui/media/icons/ClockFastForward';
+import { getFeatureIconColor } from '@organization/src/components/Tabs/panels/AccountPanel/utils';
+
+interface RenewalARRCardProps {
+  data?: null; // todo when BE contract is available
+}
+export const RenewalARRCard = ({ data }: RenewalARRCardProps) => {
+  const renewalForecast = '10000'; // remove todo when BE contract is available
+  const renewalProbability = 'HIGH'; // remove todo when BE contract is available
+
+  return (
+    <Card
+      px='4'
+      py='3'
+      w='full'
+      my={2}
+      size='lg'
+      variant='outline'
+      cursor='default'
+      border='1px solid'
+      borderColor='gray.200'
+      position='relative'
+      sx={{
+        '&:after': {
+          // todo after integrating with BE this should be visible only for contracts that renewed at least once
+          content: "''",
+          width: 2,
+          height: '80%',
+          left: -2,
+          top: '7px',
+          bg: 'white',
+          position: 'absolute',
+          borderTopLeftRadius: 'md',
+          borderBottomLeftRadius: 'md',
+          border: '1px solid',
+          borderColor: 'gray.200',
+        },
+      }}
+    >
+      <CardHeader as={Flex} p='0' w='full' alignItems='center' gap={4}>
+        <FeaturedIcon
+          size='md'
+          minW='10'
+          colorScheme={
+            renewalForecast
+              ? getFeatureIconColor(
+                  renewalProbability as
+                    | RenewalLikelihoodProbability
+                    | undefined,
+                )
+              : 'gray'
+          }
+        >
+          <ClockFastForward />
+        </FeaturedIcon>
+        <Flex
+          alignItems='center'
+          justifyContent='space-between'
+          w='full'
+          mt={-4}
+        >
+          <Flex flex={1} alignItems='center'>
+            <Heading size='sm' color='gray.700' noOfLines={1}>
+              Renewal ARR
+            </Heading>
+            <Text color='gray.500' ml={1} fontSize='sm'>
+              in 1 yr
+            </Text>
+          </Flex>
+          {/*TODO swap with real data after integrating with BE*/}
+          <Text fontWeight='semibold'>$24,000</Text>
+        </Flex>
+      </CardHeader>
+
+      <CardBody
+        as={Text}
+        w='full'
+        color='gray.500'
+        fontSize='sm'
+        p={0}
+        pl={14}
+        mt={-5}
+      >
+        Likelihood{' '}
+        <Text as='span' color='success.500'>
+          High
+        </Text>
+      </CardBody>
+    </Card>
+  );
+};
