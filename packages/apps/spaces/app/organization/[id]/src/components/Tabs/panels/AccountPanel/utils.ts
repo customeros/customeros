49c8ad4f0e0d694438ff50e7374705c692e7e985
kasia <kasia@openline.ai>
2023-11-21 15:39:11 +0100

    Revert "(feat|COS-981): Integrate with opportunities (#4537) (#4538)"
    This reverts commit f5c28324f0130fe330a9bd8d9644df63b7ac4865.

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/utils.ts
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/utils.ts
@@ -16,7 +16,7 @@ export const invalidateAccountDetailsQuery = (
     useOrganizationAccountDetailsQuery.getKey({ id }),
   );
 
-export function getARRColor(
+export function getFeatureIconColor(
   renewalLikelihood?: Maybe<RenewalLikelihoodProbability> | undefined,
 ) {
   switch (renewalLikelihood) {
