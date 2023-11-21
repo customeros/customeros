ac41ac5310f96258183cdda47cc60ac39ce5725f
alexopenline <alex@openline.ai>
2023-11-21 09:22:38 +0200

    Update go version

--- packages/server/comms-api/go.mod
+++ packages/server/comms-api/go.mod
@@ -2,8 +2,6 @@ module github.com/openline-ai/openline-customer-os/packages/server/comms-api
 
 go 1.21
 
-toolchain go1.21.3
-
 replace github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module => ./../customer-os-common-module
 
 replace github.com/openline-ai/openline-customer-os/packages/server/customer-os-api => ./../customer-os-api
@@ -23,7 +21,7 @@ require (
 	github.com/openline-ai/openline-customer-os/packages/server/customer-os-api v0.0.0-00010101000000-000000000000
 	github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-auth v0.0.0-20230928134243-77b704310d37
 	github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module v0.0.0-20231104122705-6b76a1af1686
-	github.com/openline-ai/openline-customer-os/packages/server/file-store-api v0.0.0-20231109125649-25f5840406d6
+	github.com/openline-ai/openline-customer-os/packages/server/file-store-api v0.0.0-20231121071452-bc2751469f9f
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/redis/go-redis/v9 v9.3.0
 	github.com/sirupsen/logrus v1.9.3
--- packages/server/comms-api/test/graph/resolver/dashboard.resolvers.go
+++ packages/server/comms-api/test/graph/resolver/dashboard.resolvers.go
@@ -8,15 +8,58 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/openline-ai/openline-customer-os/packages/server/comms-api/test/graph/generated"
 	"github.com/openline-ai/openline-customer-os/packages/server/comms-api/test/graph/model"
 )
 
-// DashboardViewContacts is the resolver for the dashboardView_Contacts field.
-func (r *queryResolver) DashboardViewContacts(ctx context.Context, pagination model.Pagination, where *model.Filter, sort *model.SortBy) (*model.ContactsPage, error) {
-	panic(fmt.Errorf("not implemented: DashboardViewContacts - dashboardView_Contacts"))
+// Organization is the resolver for the organization field.
+func (r *dashboardCustomerMapResolver) Organization(ctx context.Context, obj *model.DashboardCustomerMap) (*model.Organization, error) {
+	panic(fmt.Errorf("not implemented: Organization - organization"))
 }
 
 // DashboardViewOrganizations is the resolver for the dashboardView_Organizations field.
 func (r *queryResolver) DashboardViewOrganizations(ctx context.Context, pagination model.Pagination, where *model.Filter, sort *model.SortBy) (*model.OrganizationPage, error) {
 	panic(fmt.Errorf("not implemented: DashboardViewOrganizations - dashboardView_Organizations"))
 }
+
+// DashboardCustomerMap is the resolver for the dashboard_CustomerMap field.
+func (r *queryResolver) DashboardCustomerMap(ctx context.Context) ([]*model.DashboardCustomerMap, error) {
+	panic(fmt.Errorf("not implemented: DashboardCustomerMap - dashboard_CustomerMap"))
+}
+
+// DashboardMRRPerCustomer is the resolver for the dashboard_MRRPerCustomer field.
+func (r *queryResolver) DashboardMRRPerCustomer(ctx context.Context, year int) (*model.DashboardMRRPerCustomer, error) {
+	panic(fmt.Errorf("not implemented: DashboardMRRPerCustomer - dashboard_MRRPerCustomer"))
+}
+
+// DashboardGrossRevenueRetention is the resolver for the dashboard_GrossRevenueRetention field.
+func (r *queryResolver) DashboardGrossRevenueRetention(ctx context.Context, year int) (*model.DashboardGrossRevenueRetention, error) {
+	panic(fmt.Errorf("not implemented: DashboardGrossRevenueRetention - dashboard_GrossRevenueRetention"))
+}
+
+// DashboardARRBreakdown is the resolver for the dashboard_ARRBreakdown field.
+func (r *queryResolver) DashboardARRBreakdown(ctx context.Context, year int) (*model.DashboardARRBreakdown, error) {
+	panic(fmt.Errorf("not implemented: DashboardARRBreakdown - dashboard_ARRBreakdown"))
+}
+
+// DashboardRevenueAtRisk is the resolver for the dashboard_RevenueAtRisk field.
+func (r *queryResolver) DashboardRevenueAtRisk(ctx context.Context, year int) (*model.DashboardRevenueAtRisk, error) {
+	panic(fmt.Errorf("not implemented: DashboardRevenueAtRisk - dashboard_RevenueAtRisk"))
+}
+
+// DashboardRetentionRate is the resolver for the dashboard_RetentionRate field.
+func (r *queryResolver) DashboardRetentionRate(ctx context.Context, year int) (*model.DashboardRetentionRate, error) {
+	panic(fmt.Errorf("not implemented: DashboardRetentionRate - dashboard_RetentionRate"))
+}
+
+// DashboardNewCustomers is the resolver for the dashboard_NewCustomers field.
+func (r *queryResolver) DashboardNewCustomers(ctx context.Context, year int) (*model.DashboardNewCustomers, error) {
+	panic(fmt.Errorf("not implemented: DashboardNewCustomers - dashboard_NewCustomers"))
+}
+
+// DashboardCustomerMap returns generated.DashboardCustomerMapResolver implementation.
+func (r *Resolver) DashboardCustomerMap() generated.DashboardCustomerMapResolver {
+	return &dashboardCustomerMapResolver{r}
+}
+
+type dashboardCustomerMapResolver struct{ *Resolver }
