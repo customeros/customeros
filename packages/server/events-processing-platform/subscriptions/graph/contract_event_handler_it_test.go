5d503067b859999fcb19704c833483b85cb7262e
alexopenline <alex@openline.ai>
2023-11-22 15:42:14 +0200

    Update next cycle date on service start date change

--- packages/server/events-processing-platform/subscriptions/graph/contract_event_handler_it_test.go
+++ packages/server/events-processing-platform/subscriptions/graph/contract_event_handler_it_test.go
@@ -314,6 +314,64 @@ func TestContractEventHandler_OnUpdate_FrequencyChanged(t *testing.T) {
 	require.Equal(t, opportunityevent.OpportunityUpdateNextCycleDateV1, generatedEvent2.EventType)
 }
 
+func TestContractEventHandler_OnUpdate_ServiceStartDateChanged(t *testing.T) {
+	ctx := context.Background()
+	defer tearDownTestCase(ctx, testDatabase)(t)
+
+	aggregateStore := eventstoret.NewTestAggregateStore()
+
+	now := utils.Now()
+	yesterday := now.AddDate(0, 0, -1)
+
+	// prepare neo4j data
+	neo4jt.CreateTenant(ctx, testDatabase.Driver, tenantName)
+	contractId := neo4jt.CreateContract(ctx, testDatabase.Driver, tenantName, entity.ContractEntity{
+		RenewalCycle:     string(model.MonthlyRenewalCycleString),
+		ServiceStartedAt: &yesterday,
+	})
+	opportunityId := neo4jt.CreateOpportunity(ctx, testDatabase.Driver, tenantName, entity.OpportunityEntity{
+		InternalType:  string(opportunitymodel.OpportunityInternalTypeStringRenewal),
+		InternalStage: string(opportunitymodel.OpportunityInternalStageStringOpen),
+	})
+	neo4jt.LinkContractWithOpportunity(ctx, testDatabase.Driver, contractId, opportunityId, true)
+
+	prepareRenewalOpportunity(t, tenantName, opportunityId, aggregateStore)
+
+	// prepare event handler
+	contractEventHandler := &ContractEventHandler{
+		log:                 testLogger,
+		repositories:        testDatabase.Repositories,
+		opportunityCommands: opportunitycmdhandler.NewCommandHandlers(testLogger, &config.Config{}, aggregateStore),
+	}
+	contractAggregate := aggregate.NewContractAggregateWithTenantAndID(tenantName, contractId)
+	updateEvent, err := event.NewContractUpdateEvent(contractAggregate,
+		model.ContractDataFields{
+			Name:             "test contract updated",
+			RenewalCycle:     model.MonthlyRenewal,
+			ServiceStartedAt: &now,
+		},
+		commonmodel.ExternalSystem{},
+		constants.SourceOpenline,
+		utils.Now())
+	require.Nil(t, err)
+
+	// EXECUTE
+	err = contractEventHandler.OnUpdate(context.Background(), updateEvent)
+	require.Nil(t, err)
+
+	eventsMap := aggregateStore.GetEventMap()
+	require.Equal(t, 1, len(eventsMap))
+	var eventList []eventstore.Event
+	for _, value := range eventsMap {
+		eventList = value
+	}
+	require.Equal(t, 3, len(eventList))
+	generatedEvent1 := eventList[1]
+	require.Equal(t, opportunityevent.OpportunityUpdateV1, generatedEvent1.EventType)
+	generatedEvent2 := eventList[2]
+	require.Equal(t, opportunityevent.OpportunityUpdateNextCycleDateV1, generatedEvent2.EventType)
+}
+
 func TestContractEventHandler_OnUpdate_CurrentSourceOpenline_UpdateSourceNonOpenline(t *testing.T) {
 	ctx := context.Background()
 	defer tearDownTestCase(ctx, testDatabase)(t)
