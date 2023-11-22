553461f9d1baf309a995356d20cd3644eeaf4e47
kasia <kasia@openline.ai>
2023-11-22 15:28:15 +0100

    Added updateOpportunity and updateOpportunityRenewal GraphQL mutations
    - Generated TypeScript types for "updateOpportunityMutation" and "updateOpportunityRenewalMutation" GraphQL queries, and added them to the application.
    - Created a new React Query mutation for updating an opportunity data. This mutation will be triggered when user updates the renewal details. Optimistically updated the local cache and rolled back in case of error.
    - Updated "RenewalDetailsModal.tsx" and "RenewalARRCard.tsx" components to consume the newly added mutation. Users can now interact with modal that opens when they click on a renewal item from AccountPanel and updates are properly reflected on the UI.
    - This provides a way for users to update opportunity details directly from their dashboard.

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/Contract/RenewalDetailsModal.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/AccountPanel/Contract/RenewalDetailsModal.tsx
@@ -1,28 +1,36 @@
 'use client';
 // TODO uncomment when forecast
-// import { useParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import { useRef, useMemo, useState, useEffect } from 'react';
 
-// import { Dot } from '@ui/media/Dot';
-// import { Box } from '@ui/layout/Box';
-// import { Flex } from '@ui/layout/Flex';
-// import { Text } from '@ui/typography/Text';
-// import { Select } from '@ui/form/SyncSelect';
+import { produce } from 'immer';
+import { useQueryClient } from '@tanstack/react-query';
+
+import { Dot } from '@ui/media/Dot';
+import { Box } from '@ui/layout/Box';
+import { Flex } from '@ui/layout/Flex';
+import { Text } from '@ui/typography/Text';
 import { FeaturedIcon } from '@ui/media/Icon';
-// import { User02 } from '@ui/media/icons/User02';
 import { Heading } from '@ui/typography/Heading';
-// import { Button, ButtonGroup } from '@ui/form/Button';
-// import { AutoresizeTextarea } from '@ui/form/Textarea';
-// import { FormCurrencyInput } from '@ui/form/CurrencyInput';
-// import { RenewalLikelihoodProbability } from '@graphql/types';
-// import { CurrencyDollar } from '@ui/media/icons/CurrencyDollar';
-// import { getGraphQLClient } from '@shared/util/getGraphQLClient';
+import { FormSelect } from '@ui/form/SyncSelect';
+import { toastError } from '@ui/presentation/Toast';
+import { Button, ButtonGroup } from '@ui/form/Button';
+import { AutoresizeTextarea } from '@ui/form/Textarea';
+import { CurrencyInput } from '@ui/form/CurrencyInput';
+import { CurrencyDollar } from '@ui/media/icons/CurrencyDollar';
+import { getGraphQLClient } from '@shared/util/getGraphQLClient';
 import { ClockFastForward } from '@ui/media/icons/ClockFastForward';
-// import { useGetUsersQuery } from '@organizations/graphql/getUsers.generated';
+import { Opportunity, OpportunityRenewalLikelihood } from '@graphql/types';
+import { useGetUsersQuery } from '@organizations/graphql/getUsers.generated';
+import { useUpdateOpportunityMutation } from '@organization/src/graphql/updateOpportunity.generated';
+import {
+  GetContractsQuery,
+  useGetContractsQuery,
+} from '@organization/src/graphql/getContracts.generated';
 import {
   Modal,
-  // ModalBody,
-  // ModalFooter,
+  ModalBody,
+  ModalFooter,
   ModalHeader,
   ModalContent,
   ModalOverlay,
@@ -30,158 +38,121 @@ import {
 } from '@ui/overlay/Modal';
 
 interface RenewalDetailsProps {
-  // data: any;
   isOpen: boolean;
+  data: Opportunity;
   onClose: () => void;
 }
 
 export const RenewalDetailsModal = ({
-  // data,
+  data,
   isOpen,
   onClose,
 }: RenewalDetailsProps) => {
-  // const client = getGraphQLClient();
-  //
-  // const formId = 'renewalDetailsForm';
-  // const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  // // const id = useParams()?.id as string;
-  // const [probability, setLikelihood] = useState<
-  //   RenewalLikelihoodProbability | undefined | null
-  // >(data?.probability);
-  // const [reason, setReason] = useState<string>(data?.comment || '');
-  // const { data: usersData } = useGetUsersQuery(client, {
-  //   pagination: {
-  //     limit: 100,
-  //     page: 1,
-  //   },
-  // });
-  //
-  // const options = useMemo(() => {
-  //   return usersData?.users?.content
-  //     ?.filter((e) => Boolean(e.firstName) || Boolean(e.lastName))
-  //     ?.map((o) => ({
-  //       value: o.id,
-  //       label: `${o.firstName} ${o.lastName}`.trim(),
-  //     }));
-  // }, [data]);
+  const orgId = useParams()?.id as string;
 
-  // const { data: session } = useSession();
-  //
-  // const client = getGraphQLClient();
-  // const queryClient = useQueryClient();
-  //
-  // const getOrganizationQueryKey = useOrganizationAccountDetailsQuery.getKey({
-  //   id,
-  // });
+  const client = getGraphQLClient();
+  const queryClient = useQueryClient();
 
-  // const updateData = useUpdateDataMutation(client, {
-  //   onSuccess: () => {
-  //     queryClient.setQueryData<OrganizationAccountDetailsQuery>(
-  //       getOrganizationQueryKey,
-  //       (oldData) => {
-  //         if (!oldData || !oldData?.organization) return;
-  //
-  //         return {
-  //           organization: {
-  //             ...(oldData?.organization ?? {}),
-  //             accountDetails: {
-  //               ...(oldData?.organization?.accountDetails ?? {}),
-  //               data: {
-  //                 comment: reason,
-  //                 previousProbability: data?.probability,
-  //                 probability: probability,
-  //                 updatedAt: new Date(),
-  //                 updatedBy: [session?.user] as unknown as User,
-  //               },
-  //             },
-  //           },
-  //         };
-  //       },
-  //     );
-  //
-  //     queryClient.setQueryData(
-  //       useInfiniteGetTimelineQuery.getKey({
-  //         organizationId: id,
-  //         from: NEW_DATE,
-  //         size: 50,
-  //       }),
-  //       (oldData) => {
-  //         const newEvent = {
-  //           __typename: 'Action',
-  //           id: `timeline-event-action-new-id-${new Date()}`,
-  //           actionType: 'RENEWAL_LIKELIHOOD_UPDATED',
-  //           appSource: 'customer-os-api',
-  //           createdAt: new Date(),
-  //           metadata: JSON.stringify({
-  //             likelihood: probability,
-  //             reason,
-  //           }),
-  //           actionCreatedBy: null,
-  //           content: `Renewal likelihood set to ${probability} by ${session?.user?.name}`,
-  //         };
-  //
-  //         // @ts-expect-error TODO: queryClient.setQueryClient should be typed in order to fix this line
-  //         if (!oldData || !oldData.pages?.length) {
-  //           return {
-  //             pages: [
-  //               {
-  //                 organization: {
-  //                   id,
-  //                   timelineEventsTotalCount: 1,
-  //                   timelineEvents: [newEvent],
-  //                 },
-  //               },
-  //             ],
-  //           };
-  //         }
-  //
-  //         // @ts-expect-error TODO: queryClient.setQueryClient should be typed in order to fix this line
-  //         const firstPage = oldData.pages[0] ?? {};
-  //         // @ts-expect-error TODO: queryClient.setQueryClient should be typed in order to fix this line
-  //         const pages = oldData.pages?.slice(1);
-  //
-  //         const firstPageWithEvent = {
-  //           ...firstPage,
-  //           organization: {
-  //             ...firstPage?.organization,
-  //             timelineEvents: [
-  //               ...(firstPage?.organization?.timelineEvents ?? []),
-  //               newEvent,
-  //             ],
-  //             timelineEventsTotalCount:
-  //               (firstPage?.organization?.timelineEventsTotalCount ?? 0) + 1,
-  //           },
-  //         };
-  //
-  //         return {
-  //           ...oldData,
-  //           pages: [firstPageWithEvent, ...pages],
-  //         };
-  //       },
-  //     );
-  //   },
-  //   onSettled: () => {
-  //     if (timeoutRef.current) {
-  //       clearTimeout(timeoutRef.current);
-  //     }
-  //     timeoutRef.current = setTimeout(() => {
-  //       queryClient.invalidateQueries(getOrganizationQueryKey);
-  //     }, 1000);
-  //   },
-  // });
+  const formId = 'renewalDetailsForm';
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [probability, setLikelihood] = useState<
+    OpportunityRenewalLikelihood | undefined | null
+  >((data?.renewalLikelihood as OpportunityRenewalLikelihood) ?? null);
+  const [amount, setAmount] = useState<string>(data?.amount?.toString() || '');
+  const [reason, setReason] = useState<string>(data?.generalNotes || '');
+  const [owner, setOwner] = useState<null | { value: string; label: string }>(
+    null,
+  );
+  const { data: usersData } = useGetUsersQuery(client, {
+    pagination: {
+      limit: 50,
+      page: 1,
+    },
+  });
 
-  // const handleSet = () => {
-  // updateData.mutate({
-  //   input: { id, probability: probability, comment: reason },
-  // });
-  onClose();
-  // };
+  const options = useMemo(() => {
+    return usersData?.users?.content
+      ?.filter((e) => Boolean(e.firstName) || Boolean(e.lastName))
+      ?.map((o) => ({
+        value: o.id,
+        label: `${o.firstName} ${o.lastName}`.trim(),
+      }));
+  }, [usersData?.users?.content?.length]);
+
+  const getContractsQueryKey = useGetContractsQuery.getKey({
+    id: orgId,
+  });
+
+  const updateOpportunityMutation = useUpdateOpportunityMutation(client, {
+    onMutate: ({ input }) => {
+      queryClient.cancelQueries(getContractsQueryKey);
+
+      queryClient.setQueryData<GetContractsQuery>(
+        getContractsQueryKey,
+        (currentCache) => {
+          if (!currentCache || !currentCache?.organization) return;
+
+          return produce(currentCache, (draft) => {
+            if (draft?.['organization']?.['contracts']) {
+              draft['organization']['contracts']?.map((contractData, index) => {
+                return (contractData.opportunities ?? []).map((opportunity) => {
+                  if ((opportunity as Opportunity).id === input.opportunityId) {
+                    return {
+                      ...opportunity,
+                      amount: input.amount,
+                      renewalUpdatedByUserAt: new Date().toISOString(),
+                    };
+                  }
+
+                  return opportunity;
+                });
+              });
+            }
+          });
+        },
+      );
+      const previousEntries =
+        queryClient.getQueryData<GetContractsQuery>(getContractsQueryKey);
+
+      return { previousEntries };
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData<GetContractsQuery>(
+        getContractsQueryKey,
+        context?.previousEntries,
+      );
+      toastError(
+        'Failed to update renewal details',
+        'update-renewal-details-error',
+      );
+    },
+    onSettled: () => {
+      onClose();
+
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      timeoutRef.current = setTimeout(() => {
+        queryClient.invalidateQueries(getContractsQueryKey);
+      }, 1000);
+    },
+  });
+
+  const handleSet = () => {
+    updateOpportunityMutation.mutate({
+      input: {
+        opportunityId: data.id,
+        generalNotes: reason,
+        amount: parseFloat(amount),
+      },
+    });
+  };
 
   useEffect(() => {
     return () => {
-      // if (timeoutRef.current) {
-      //   clearTimeout(timeoutRef.current);
-      // }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
   }, []);
 
@@ -206,104 +177,151 @@ export const RenewalDetailsModal = ({
             Renewal details
           </Heading>
         </ModalHeader>
-        {/*<ModalBody as={Flex} flexDir='column' pb='0'>*/}
-        {/*  <Select*/}
-        {/*    isClearable*/}
-        {/*    // value={value}*/}
-        {/*    isLoading={false}*/}
-        {/*    placeholder='Owner'*/}
-        {/*    backspaceRemovesValue*/}
-        {/*    onChange={() => null}*/}
-        {/*    options={options}*/}
-        {/*    leftElement={<User02 color='gray.500' mr={3} />}*/}
-        {/*  />*/}
+        <ModalBody as={Flex} flexDir='column' pb='0' gap={4}>
+          <FormSelect
+            formId={formId}
+            name='arrForecast'
+            isClearable
+            isDisabled
+            value={owner}
+            isLoading={false}
+            placeholder='Owner'
+            backspaceRemovesValue
+            onChange={setOwner}
+            options={options}
+            label='Owner'
+            isLabelVisible
+          />
 
-        {/*  <ButtonGroup w='full' isAttached>*/}
-        {/*    <Button*/}
-        {/*      w='full'*/}
-        {/*      variant='outline'*/}
-        {/*      leftIcon={<Dot colorScheme='success' />}*/}
-        {/*      onClick={() => setLikelihood(RenewalLikelihoodProbability.High)}*/}
-        {/*      bg={probability === 'HIGH' ? 'gray.100' : 'white'}*/}
-        {/*    >*/}
-        {/*      High*/}
-        {/*    </Button>*/}
-        {/*    <Button*/}
-        {/*      w='full'*/}
-        {/*      variant='outline'*/}
-        {/*      leftIcon={<Dot colorScheme='warning' />}*/}
-        {/*      onClick={() => setLikelihood(RenewalLikelihoodProbability.Medium)}*/}
-        {/*      bg={probability === 'MEDIUM' ? 'gray.100' : 'white'}*/}
-        {/*    >*/}
-        {/*      Medium*/}
-        {/*    </Button>*/}
-        {/*    <Button*/}
-        {/*      w='full'*/}
-        {/*      variant='outline'*/}
-        {/*      leftIcon={<Dot colorScheme='error' />}*/}
-        {/*      onClick={() => setLikelihood(RenewalLikelihoodProbability.Low)}*/}
-        {/*      bg={probability === 'LOW' ? 'gray.100' : 'white'}*/}
-        {/*    >*/}
-        {/*      Low*/}
-        {/*    </Button>*/}
-        {/*    <Button*/}
-        {/*      variant='outline'*/}
-        {/*      w='full'*/}
-        {/*      leftIcon={<Dot />}*/}
-        {/*      onClick={() => setLikelihood(RenewalLikelihoodProbability.Zero)}*/}
-        {/*      bg={probability === 'ZERO' ? 'gray.100' : 'white'}*/}
-        {/*    >*/}
-        {/*      Zero*/}
-        {/*    </Button>*/}
-        {/*  </ButtonGroup>*/}
-        {/*  <Text>Last updated by </Text>*/}
+          <div>
+            <Text
+              fontWeight='semibold'
+              fontSize='sm'
+              mb={2}
+              id='likelihood-options-button'
+            >
+              Likelihood
+            </Text>
+            <ButtonGroup
+              w='full'
+              isAttached
+              aria-describedby='likelihood-oprions-button'
+            >
+              <Button
+                variant='outline'
+                w='full'
+                leftIcon={<Dot />}
+                onClick={() =>
+                  setLikelihood(OpportunityRenewalLikelihood.ZeroRenewal)
+                }
+                bg={
+                  probability === OpportunityRenewalLikelihood.ZeroRenewal
+                    ? 'gray.100'
+                    : 'white'
+                }
+              >
+                Zero
+              </Button>
+              <Button
+                w='full'
+                variant='outline'
+                leftIcon={<Dot colorScheme='error' />}
+                onClick={() =>
+                  setLikelihood(OpportunityRenewalLikelihood.LowRenewal)
+                }
+                bg={
+                  probability === OpportunityRenewalLikelihood.LowRenewal
+                    ? 'gray.100'
+                    : 'white'
+                }
+              >
+                Low
+              </Button>
+              <Button
+                w='full'
+                variant='outline'
+                leftIcon={<Dot colorScheme='warning' />}
+                onClick={() =>
+                  setLikelihood(OpportunityRenewalLikelihood.MediumRenewal)
+                }
+                bg={
+                  probability === OpportunityRenewalLikelihood.MediumRenewal
+                    ? 'gray.100'
+                    : 'white'
+                }
+              >
+                Medium
+              </Button>
+              <Button
+                w='full'
+                variant='outline'
+                leftIcon={<Dot colorScheme='success' />}
+                onClick={() =>
+                  setLikelihood(OpportunityRenewalLikelihood.HighRenewal)
+                }
+                bg={
+                  probability === OpportunityRenewalLikelihood.HighRenewal
+                    ? 'gray.100'
+                    : 'white'
+                }
+              >
+                High
+              </Button>
+            </ButtonGroup>
+            {data?.renewalUpdatedByUserId && (
+              <Text color='gray.500' fontSize='xs' mt={2}>
+                Last updated by{' '}
+              </Text>
+            )}
+          </div>
 
-        {/*  <FormCurrencyInput*/}
-        {/*    formId={formId}*/}
-        {/*    name='arrForecast'*/}
-        {/*    w='full'*/}
-        {/*    placeholder='Amount'*/}
-        {/*    label='ARR forecast'*/}
-        {/*    min={0}*/}
-        {/*    leftElement={*/}
-        {/*      <Box color='gray.500'>*/}
-        {/*        <CurrencyDollar height='16px' />*/}
-        {/*      </Box>*/}
-        {/*    }*/}
-        {/*  />*/}
+          {data?.amount && (
+            <CurrencyInput
+              w='full'
+              placeholder='Amount'
+              label='ARR forecast'
+              isLabelVisible
+              value={amount}
+              onChange={(value) => setAmount(value)}
+              min={0}
+              leftElement={
+                <Box color='gray.500'>
+                  <CurrencyDollar height='16px' />
+                </Box>
+              }
+            />
+          )}
 
-        {/*  {!!probability && (*/}
-        {/*    <>*/}
-        {/*      <Text as='label' htmlFor='reason' mt='5' fontSize='sm'>*/}
-        {/*        <b>Reason for change</b> (optional)*/}
-        {/*      </Text>*/}
-        {/*      <AutoresizeTextarea*/}
-        {/*        pt='0'*/}
-        {/*        id='reason'*/}
-        {/*        value={reason}*/}
-        {/*        spellCheck='false'*/}
-        {/*        onChange={(e) => setReason(e.target.value)}*/}
-        {/*        placeholder={`What is the reason for ${*/}
-        {/*          !data?.probability ? 'setting' : 'updating'*/}
-        {/*        } these details`}*/}
-        {/*      />*/}
-        {/*    </>*/}
-        {/*  )}*/}
-        {/*</ModalBody>*/}
-        {/*<ModalFooter p='6'>*/}
-        {/*  <Button variant='outline' w='full' onClick={onClose}>*/}
-        {/*    Cancel*/}
-        {/*  </Button>*/}
-        {/*  <Button*/}
-        {/*    ml='3'*/}
-        {/*    w='full'*/}
-        {/*    variant='outline'*/}
-        {/*    colorScheme='primary'*/}
-        {/*    onClick={handleSet}*/}
-        {/*  >*/}
-        {/*    Update*/}
-        {/*  </Button>*/}
-        {/*</ModalFooter>*/}
+          {!!probability && (
+            <div>
+              <Text as='label' htmlFor='reason' fontSize='sm'>
+                <b>Reason for change</b> (optional)
+              </Text>
+              <AutoresizeTextarea
+                pt='0'
+                id='reason'
+                value={reason}
+                spellCheck='false'
+                onChange={(e) => setReason(e.target.value)}
+                placeholder={`What is the reason for updating these details`}
+              />
+            </div>
+          )}
+        </ModalBody>
+        <ModalFooter p='6'>
+          <Button variant='outline' w='full' onClick={onClose}>
+            Cancel
+          </Button>
+          <Button
+            ml='3'
+            w='full'
+            variant='outline'
+            colorScheme='primary'
+            onClick={handleSet}
+          >
+            Update
+          </Button>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   );
