cd47dc29e1344d164a44cee6ae21f8c053bf8f54
Alex Calinica <acalinica@openline.ai>
2023-12-06 17:47:30 +0200

    feat: handle ChartCard subline when percentage is 0

---
+++ packages/apps/spaces/ui/media/icons/ArrowNarrowRight.tsx
@@ -0,0 +1,16 @@
+import { Icon, IconProps } from '@ui/media/Icon';
+
+export const ArrowNarrowRight = (props: IconProps) => (
+  <Icon viewBox='0 0 24 24' fill='none' boxSize='4' {...props}>
+    <g id='arrow-narrow-right'>
+      <path
+        id='Icon'
+        d='M4 12H20M20 12L14 6M20 12L14 18'
+        stroke='#0C111D'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </g>
+  </Icon>
+);
