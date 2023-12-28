60a347a5812385568df67a2cd2c988fdc6bcd9a7
alexopenline <alex@openline.ai>
2023-12-28 23:27:07 +0200

    move country repository to new module

---
+++ packages/server/customer-os-neo4j-repository/repository/country_read_repository.go
@@ -0,0 +1,62 @@
+package repository
+
+import (
+	"context"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/openline-ai/customer-os-neo4j-repository/tracing"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
+)
+
+type CountryReadRepository interface {
+	GetDefaultCountryCodeA3(ctx context.Context, tenant string) (string, error)
+}
+
+type countryReadRepository struct {
+	driver   *neo4j.DriverWithContext
+	database string
+}
+
+func NewCountryReadRepository(driver *neo4j.DriverWithContext, database string) CountryReadRepository {
+	return &countryReadRepository{
+		driver:   driver,
+		database: database,
+	}
+}
+
+func (r *countryReadRepository) prepareReadSession(ctx context.Context) neo4j.SessionWithContext {
+	return utils.NewNeo4jReadSession(ctx, *r.driver, utils.WithDatabaseName(r.database))
+}
+
+func (r *countryReadRepository) GetDefaultCountryCodeA3(ctx context.Context, tenant string) (string, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "CountryReadRepository.GetDefaultCountryCodeA3")
+	defer span.Finish()
+	tracing.SetNeo4jRepositorySpanTags(span, tenant)
+
+	cypher := `MATCH (t:Tenant {name:$tenant})
+				OPTIONAL MATCH (tenant)-[:DEFAULT_COUNTRY]->(dc:Country)
+				RETURN COALESCE(dc.codeA3, "") AS countryCodeA3`
+	params := map[string]any{
+		"tenant": tenant,
+	}
+	span.LogFields(log.String("cypher", cypher))
+	tracing.LogObjectAsJson(span, "params", params)
+
+	session := r.prepareReadSession(ctx)
+	defer session.Close(ctx)
+
+	result, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		if queryResult, err := tx.Run(ctx, cypher, params); err != nil {
+			return nil, err
+		} else {
+			return utils.ExtractSingleRecordFirstValueAsString(ctx, queryResult, err)
+		}
+	})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return "", err
+	}
+	span.LogFields(log.String("result", result.(string)))
+	return result.(string), nil
+}
