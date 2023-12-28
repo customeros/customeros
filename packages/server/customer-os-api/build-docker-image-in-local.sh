b4ec342d303a0f09c9c950395bf1bb2f42e6d205
alexopenline <alex@openline.ai>
2023-12-28 15:02:35 +0200

    fix

--- packages/server/customer-os-api/build-docker-image-in-local.sh
+++ packages/server/customer-os-api/build-docker-image-in-local.sh
@@ -2,11 +2,13 @@
 rm -rf tmp-sources
 mkdir tmp-sources
 mkdir tmp-sources/customer-os-common-module
+mkdir tmp-sources/customer-os-neo4j-repository
 mkdir tmp-sources/customer-os-api
 
 rsync -av --progress --exclude="tmp-sources" * tmp-sources/customer-os-api
 cp .env tmp-sources/customer-os-api/.env
 cp -r ../customer-os-common-module/* tmp-sources/customer-os-common-module
+cp -r ../customer-os-neo4j-repository/* tmp-sources/customer-os-neo4j-repository
 
 cp Dockerfile tmp-sources/Dockerfile
 
--- packages/server/events-processing-platform/build-docker-image-in-local.sh
+++ packages/server/events-processing-platform/build-docker-image-in-local.sh
@@ -2,12 +2,16 @@
 rm -rf tmp-sources
 mkdir tmp-sources
 mkdir tmp-sources/customer-os-common-module
+mkdir tmp-sources/customer-os-common-ai
+mkdir tmp-sources/customer-os-neo4j-repository
 mkdir tmp-sources/events-processing-proto
 mkdir tmp-sources/events-processing-platform
 
 rsync -av --progress --exclude="tmp-sources" * tmp-sources/events-processing-platform
 cp .env tmp-sources/events-processing-platform/.env
+cp -r ../customer-os-common-ai/* tmp-sources/customer-os-common-ai
 cp -r ../customer-os-common-module/* tmp-sources/customer-os-common-module
+cp -r ../customer-os-neo4j-repository/* tmp-sources/customer-os-neo4j-repository
 cp -r ../events-processing-proto/* tmp-sources/events-processing-proto
 
 cp Dockerfile tmp-sources/Dockerfile
