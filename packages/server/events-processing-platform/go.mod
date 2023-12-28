a58ee1e63b43cff5aa5d150e7faed7c019a8a98f
alexopenline <alex@openline.ai>
2023-12-28 13:50:43 +0200

    More changes

--- packages/server/events-processing-platform/go.mod
+++ packages/server/events-processing-platform/go.mod
@@ -1,6 +1,6 @@
 module github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform
 
-go 1.21.4
+go 1.21
 
 replace github.com/openline-ai/openline-customer-os/packages/server/events-processing-proto => ../events-processing-proto
 
--- packages/server/events-processing-platform/repository/contract_repository.go
+++ packages/server/events-processing-platform/repository/contract_repository.go
@@ -18,8 +18,6 @@ import (
 type ContractRepository interface {
 	CreateForOrganization(ctx context.Context, tenant, contractId string, evt event.ContractCreateEvent) error
 	UpdateAndReturn(ctx context.Context, tenant, contractId string, evt event.ContractUpdateEvent) (*dbtype.Node, error)
-	GetContractByServiceLineItemId(ctx context.Context, tenant string, serviceLineItemId string) (*dbtype.Node, error)
-	GetContractByOpportunityId(ctx context.Context, tenant string, opportunityId string) (*dbtype.Node, error)
 	UpdateStatus(ctx context.Context, tenant, contractId string, evt event.ContractUpdateStatusEvent) error
 	SuspendActiveRenewalOpportunity(ctx context.Context, tenant, contractId string) error
 	ActivateSuspendedRenewalOpportunity(ctx context.Context, tenant, contractId string) error
@@ -139,72 +137,6 @@ func (r *contractRepository) UpdateAndReturn(ctx context.Context, tenant, contra
 		return nil, err
 	}
 	return result.(*dbtype.Node), nil
-}
-
-func (r *contractRepository) GetContractByServiceLineItemId(ctx context.Context, tenant string, serviceLineItemId string) (*dbtype.Node, error) {
-	span, ctx := opentracing.StartSpanFromContext(ctx, "ContractRepository.GetContractByServiceLineItemId")
-	defer span.Finish()
-	tracing.SetNeo4jRepositorySpanTags(ctx, span, tenant)
-	span.LogFields(log.String("serviceLineItemId", serviceLineItemId))
-
-	cypher := fmt.Sprintf(`MATCH (sli:ServiceLineItem {id:$id})<-[:HAS_SERVICE]-(c:Contract:Contract_%s) RETURN c limit 1`, tenant)
-	params := map[string]any{
-		"id": serviceLineItemId,
-	}
-	span.LogFields(log.String("query", cypher), log.Object("params", params))
-
-	session := utils.NewNeo4jReadSession(ctx, *r.driver, utils.WithDatabaseName(r.database))
-	defer session.Close(ctx)
-
-	result, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-		if queryResult, err := tx.Run(ctx, cypher, params); err != nil {
-			return nil, err
-		} else {
-			return utils.ExtractAllRecordsFirstValueAsDbNodePtrs(ctx, queryResult, err)
-		}
-	})
-	if err != nil {
-		return nil, err
-	}
-	records := result.([]*dbtype.Node)
-	if len(records) == 0 {
-		return nil, nil
-	} else {
-		return records[0], nil
-	}
-}
-
-func (r *contractRepository) GetContractByOpportunityId(ctx context.Context, tenant string, opportunityId string) (*dbtype.Node, error) {
-	span, ctx := opentracing.StartSpanFromContext(ctx, "ContractRepository.GetContractByServiceLineItemId")
-	defer span.Finish()
-	tracing.SetNeo4jRepositorySpanTags(ctx, span, tenant)
-	span.LogFields(log.String("opportunityId", opportunityId))
-
-	cypher := fmt.Sprintf(`MATCH (:Opportunity {id:$id})<-[:HAS_OPPORTUNITY]-(c:Contract:Contract_%s) RETURN c limit 1`, tenant)
-	params := map[string]any{
-		"id": opportunityId,
-	}
-	span.LogFields(log.String("query", cypher), log.Object("params", params))
-
-	session := utils.NewNeo4jReadSession(ctx, *r.driver, utils.WithDatabaseName(r.database))
-	defer session.Close(ctx)
-
-	result, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-		if queryResult, err := tx.Run(ctx, cypher, params); err != nil {
-			return nil, err
-		} else {
-			return utils.ExtractAllRecordsFirstValueAsDbNodePtrs(ctx, queryResult, err)
-		}
-	})
-	if err != nil {
-		return nil, err
-	}
-	records := result.([]*dbtype.Node)
-	if len(records) == 0 {
-		return nil, nil
-	} else {
-		return records[0], nil
-	}
 }
 
 func (r *contractRepository) UpdateStatus(ctx context.Context, tenant, contractId string, evt event.ContractUpdateStatusEvent) error {
