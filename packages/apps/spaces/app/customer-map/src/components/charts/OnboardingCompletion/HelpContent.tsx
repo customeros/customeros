951964e3bf65591e7f5bb0786f222733d67082bf
Alex Calinica <acalinica@openline.ai>
2024-01-03 20:06:50 +0200

    fix: clear copy of help content

--- packages/apps/spaces/app/customer-map/src/components/charts/OnboardingCompletion/HelpContent.tsx
+++ packages/apps/spaces/app/customer-map/src/components/charts/OnboardingCompletion/HelpContent.tsx
@@ -6,10 +6,7 @@ import { Text } from '@ui/typography/Text';
 export const HelpContent = () => {
   return (
     <Box mt='1'>
-      <Text fontSize='md' fontWeight='normal'>
-        This is the number of new customers you’ve added, meaning that their
-        contract’s start date falls in the given period.
-      </Text>
+      <Text fontSize='md' fontWeight='normal'></Text>
     </Box>
   );
 };
