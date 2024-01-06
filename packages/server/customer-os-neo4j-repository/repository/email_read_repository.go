b70154d696ef08dc1836bbf4d8148c7f492e9815
alexopenline <alex@openline.ai>
2024-01-06 10:27:23 +0200

    Move last read email method

--- packages/server/customer-os-neo4j-repository/repository/email_read_repository.go
+++ packages/server/customer-os-neo4j-repository/repository/email_read_repository.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j/db"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j/dbtype"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/tracing"
 	"github.com/opentracing/opentracing-go"
@@ -13,6 +14,7 @@ import (
 
 type EmailReadRepository interface {
 	GetEmailIdIfExists(ctx context.Context, tenant, email string) (string, error)
+	GetEmailForUser(ctx context.Context, tenant string, userId string) (*dbtype.Node, error)
 }
 
 type emailReadRepository struct {
@@ -64,4 +66,42 @@ func (r *emailReadRepository) GetEmailIdIfExists(ctx context.Context, tenant, em
 	}
 	span.LogFields(log.String("result", result.([]*db.Record)[0].Values[0].(string)))
 	return result.([]*db.Record)[0].Values[0].(string), err
+}
+
+func (r *emailReadRepository) GetEmailForUser(ctx context.Context, tenant string, userId string) (*dbtype.Node, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailReadRepository.GetEmailForUser")
+	defer span.Finish()
+	tracing.SetNeo4jRepositorySpanTags(span, tenant)
+	span.LogFields(log.String("userId", userId))
+
+	cypher := fmt.Sprintf("MATCH (e:Email_%s)<-[:HAS]-(u:User {id:$userId})-[:USER_BELONGS_TO_TENANT]->(:Tenant {name:$tenant}) WHERE u:User_%s return e", tenant, tenant)
+	params := map[string]any{
+		"userId": userId,
+		"tenant": tenant,
+	}
+	span.LogFields(log.String("cypher", cypher))
+	tracing.LogObjectAsJson(span, "params", params)
+
+	session := r.prepareReadSession(ctx)
+	defer session.Close(ctx)
+
+	result, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		if queryResult, err := tx.Run(ctx, cypher, params); err != nil {
+			return nil, err
+		} else {
+			return utils.ExtractFirstRecordFirstValueAsDbNodePtr(ctx, queryResult, err)
+		}
+	})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return nil, err
+	}
+
+	if result == nil {
+		span.LogFields(log.Bool("result.found", false))
+		return nil, nil
+	}
+
+	span.LogFields(log.Bool("result.found", true))
+	return result.(*dbtype.Node), nil
 }
