5925f3e54ff6248b314ed83dcb15243817ead839
max-openline <max@openline.ai>
2024-01-11 15:20:14 +0100

    [WebScraper] - fix linkedin scraping get logos

--- packages/server/events-processing-platform/config/config.go
+++ packages/server/events-processing-platform/config/config.go
@@ -118,7 +118,7 @@ type Services struct {
 	EventsProcessingPlatformUrl    string `env:"EVENTS_PROCESSING_PLATFORM_URL" validate:"required"`
 	EventsProcessingPlatformApiKey string `env:"EVENTS_PROCESSING_PLATFORM_API_KEY" validate:"required"`
 	ScrapingBeeApiKey              string `env:"SCRAPING_BEE_API_KEY" validate:"required"`
-	ScrapingDogApiKey              string `env:"SCRAPING_DOG_API_KEY" validate:"required"`
+	CoreSignalApiKey               string `env:"CORE_SIGNAL_API_KEY" validate:"required"`
 	PromptJsonSchema               string `env:"PROMPT_JSON_SCHEMA" validate:"required" envDefault:"{
 		"$schema": "http://json-schema.org/draft-07/schema#",
 		"type": "object",
