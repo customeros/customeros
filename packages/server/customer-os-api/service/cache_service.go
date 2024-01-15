0f2a20daabd32a7304bb492877ced4a4f48c4dec
Eduard Firut <edi@openline.ai>
2024-01-15 11:09:35 +0200

    Migrate country entity to events

--- packages/server/customer-os-api/service/cache_service.go
+++ packages/server/customer-os-api/service/cache_service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/graph/model"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/mapper"
-	commonEntity "github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/repository/neo4j/entity"
 )
 
 type CacheService interface {
@@ -29,19 +28,15 @@ func (s *cacheService) InitCache() {
 	//cache US states for the gCliCache
 	gCliStatesCache := make([]*model.GCliItem, 0)
 
-	countries := []*commonEntity.CountryEntity{}
-	countries = append(countries, &commonEntity.CountryEntity{Id: "1", CodeA3: "USA"})
+	//read states from db for USA
+	states, err := s.services.CommonServices.StateService.GetStatesByCountryId(context.Background(), "1")
+	if err != nil {
+		//todo: log error
+	}
 
-	for _, country := range countries {
-		states, err := s.services.CommonServices.StateService.GetStatesByCountryId(context.Background(), country.Id)
-		if err != nil {
-			//todo: log error
-		}
-
-		for _, v := range states {
-			item := mapper.MapStateToGCliItem(*v)
-			gCliStatesCache = append(gCliStatesCache, &item)
-		}
+	for _, v := range states {
+		item := mapper.MapStateToGCliItem(*v)
+		gCliStatesCache = append(gCliStatesCache, &item)
 	}
 
 	s.States = gCliStatesCache
--- packages/server/customer-os-neo4j-repository/repository/country_read_repository.go
+++ packages/server/customer-os-neo4j-repository/repository/country_read_repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j/db"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j/dbtype"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/tracing"
 	"github.com/opentracing/opentracing-go"
@@ -11,6 +13,9 @@ import (
 
 type CountryReadRepository interface {
 	GetDefaultCountryCodeA3(ctx context.Context, tenant string) (string, error)
+	GetCountryByCodeA3(ctx context.Context, codeA3 string) (*dbtype.Node, error)
+	GetCountriesPaginated(ctx context.Context, skip, limit int) (*utils.DbNodesWithTotalCount, error)
+	GetCountries(ctx context.Context) ([]*dbtype.Node, error)
 }
 
 type countryReadRepository struct {
@@ -59,4 +64,103 @@ func (r *countryReadRepository) GetDefaultCountryCodeA3(ctx context.Context, ten
 	}
 	span.LogFields(log.String("result", result.(string)))
 	return result.(string), nil
+}
+
+func (r *countryReadRepository) GetCountryByCodeA3(ctx context.Context, codeA3 string) (*dbtype.Node, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "CountryReadRepository.GetCountryByCodeA3")
+	defer span.Finish()
+	tracing.LogObjectAsJson(span, "codeA3", codeA3)
+
+	cypher := `MATCH (c:Country {codeA3:$codeA3} ) RETURN c`
+	params := map[string]any{
+		"codeA3": codeA3,
+	}
+	span.LogFields(log.String("cypher", cypher))
+	tracing.LogObjectAsJson(span, "params", params)
+
+	session := r.prepareReadSession(ctx)
+	defer session.Close(ctx)
+
+	dbRecord, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		queryResult, err := tx.Run(ctx, cypher, params)
+		if err != nil {
+			return nil, err
+		}
+		return queryResult.Single(ctx)
+	})
+	return utils.NodePtr(dbRecord.(*db.Record).Values[0].(dbtype.Node)), err
+}
+
+func (r *countryReadRepository) GetCountriesPaginated(ctx context.Context, skip, limit int) (*utils.DbNodesWithTotalCount, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "CountryReadRepository.GetCountryByCodeA3")
+	defer span.Finish()
+	tracing.LogObjectAsJson(span, "skip", skip)
+	tracing.LogObjectAsJson(span, "limit", limit)
+
+	cypherCount := `MATCH (c:Country) RETURN count(c) as count`
+	paramsCount := map[string]any{}
+	span.LogFields(log.String("cypherCount", cypherCount))
+	tracing.LogObjectAsJson(span, "paramsCount", paramsCount)
+
+	cypher := `MATCH (c:Country) RETURN c ORDER BY c.name SKIP $skip LIMIT $limit`
+	params := map[string]any{
+		"skip":  skip,
+		"limit": limit,
+	}
+	span.LogFields(log.String("cypher", cypher))
+	tracing.LogObjectAsJson(span, "params", params)
+
+	session := r.prepareReadSession(ctx)
+	defer session.Close(ctx)
+
+	dbNodesWithTotalCount := new(utils.DbNodesWithTotalCount)
+
+	dbRecords, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		queryResult, err := tx.Run(ctx, cypherCount, paramsCount)
+		if err != nil {
+			return nil, err
+		}
+		count, _ := queryResult.Single(ctx)
+		dbNodesWithTotalCount.Count = count.Values[0].(int64)
+
+		queryResult, err = tx.Run(ctx, cypher, params)
+		return queryResult.Collect(ctx)
+	})
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range dbRecords.([]*neo4j.Record) {
+		dbNodesWithTotalCount.Nodes = append(dbNodesWithTotalCount.Nodes, utils.NodePtr(v.Values[0].(neo4j.Node)))
+	}
+	return dbNodesWithTotalCount, nil
+}
+
+func (r *countryReadRepository) GetCountries(ctx context.Context) ([]*dbtype.Node, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "CountryReadRepository.GetCountryByCodeA3")
+	defer span.Finish()
+
+	cypher := `MATCH (c:Country) RETURN c ORDER BY c.name`
+	span.LogFields(log.String("cypher", cypher))
+
+	session := r.prepareReadSession(ctx)
+	defer session.Close(ctx)
+
+	dbRecords, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		queryResult, err := tx.Run(ctx, cypher, map[string]any{})
+		if err != nil {
+			return nil, err
+		}
+		return queryResult.Collect(ctx)
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	dbNodes := []*dbtype.Node{}
+	for _, v := range dbRecords.([]*neo4j.Record) {
+		if v.Values[0] != nil {
+			dbNodes = append(dbNodes, utils.NodePtr(v.Values[0].(dbtype.Node)))
+		}
+	}
+	return dbNodes, err
 }
