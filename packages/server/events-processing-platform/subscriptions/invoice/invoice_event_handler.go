b599645be2fad61aa71b5da2226bf6bc0e20e359
alexopenline <alex@openline.ai>
2024-01-17 15:24:37 +0200

    Invoke pay with stripe

--- packages/server/events-processing-platform/subscriptions/invoice/invoice_event_handler.go
+++ packages/server/events-processing-platform/subscriptions/invoice/invoice_event_handler.go
@@ -1,8 +1,13 @@
 package invoice
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/data"
 	neo4jentity "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/entity"
 	neo4jmapper "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/mapper"
+	"github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/config"
 	"github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/domain/invoice"
 	"github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/eventstore"
 	"github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/logger"
@@ -11,17 +16,29 @@ import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
+	"net/http"
 )
+
+type RequestBodyInvoiceReady struct {
+	Tenant                       string `json:"tenant"`
+	Currency                     string `json:"currency"`
+	AmountInSmallestCurrencyUnit int64  `json:"amountInSmallestCurrencyUnit"`
+	StripeCustomerId             string `json:"stripeCustomerId"`
+	InvoiceId                    string `json:"invoiceId"`
+	InvoiceDescription           string `json:"invoiceDescription"`
+}
 
 type InvoiceEventHandler struct {
 	log          logger.Logger
 	repositories *repository.Repositories
+	cfg          *config.EventNotifications
 }
 
-func NewInvoiceEventHandler(log logger.Logger, repositories *repository.Repositories) *InvoiceEventHandler {
+func NewInvoiceEventHandler(log logger.Logger, repositories *repository.Repositories, cfg *config.EventNotifications) *InvoiceEventHandler {
 	return &InvoiceEventHandler{
 		log:          log,
 		repositories: repositories,
+		cfg:          cfg,
 	}
 }
 
@@ -70,14 +87,64 @@ func (h *InvoiceEventHandler) invokeInvoiceReadyWebhook(ctx context.Context, ten
 	span.SetTag(tracing.SpanTagTenant, tenant)
 	tracing.LogObjectAsJson(span, "invoice", invoice)
 
-	// TODO CALL STRIPE
-	//invcoice - contract - org
-	//get
-	//stripe
-	//id
-	//for org
-	//
-	//// webhook
-	//	invoice- > techPaymentRequested(bool)
+	if invoice.DryRun {
+		return nil
+	}
+	if h.cfg.EndPoints.InvoiceReady == "" {
+		return nil
+	}
+
+	currency := "USD"         // TODO implement currency in invoice
+	stripeCustomerId := "123" // TODO fetch stripe customer id from customer-os
+	amountInSmallestCurrencyUnit, err := data.InSmallestCurrencyUnit(currency, invoice.Amount)
+	if err != nil {
+		return fmt.Errorf("error converting amount to smallest currency unit: %v", err.Error())
+	}
+	requestBody := RequestBodyInvoiceReady{
+		Tenant:                       tenant,
+		Currency:                     currency,
+		AmountInSmallestCurrencyUnit: amountInSmallestCurrencyUnit,
+		StripeCustomerId:             stripeCustomerId,
+		InvoiceId:                    invoice.Id,
+		InvoiceDescription:           fmt.Sprintf("Invoice %s", invoice.Number),
+	}
+
+	// Convert the request body to JSON
+	requestBodyJSON, err := json.Marshal(requestBody)
+	if err != nil {
+		return fmt.Errorf("error encoding JSON: %v", err)
+	}
+
+	// Create an HTTP client
+	client := &http.Client{}
+
+	// Create a POST request with headers and body
+	req, err := http.NewRequest("POST", h.cfg.EndPoints.InvoiceReady, bytes.NewBuffer(requestBodyJSON))
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	// Set the content type header
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the POST request
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("request failed with status code: %s", resp.Status)
+	}
+
+	// Request was successful
+	err = h.repositories.Neo4jRepositories.InvoiceWriteRepository.SetInvoicePaymentRequested(ctx, tenant, invoice.Id)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		h.log.Errorf("Error setting invoice payment requested for invoice %s: %s", invoice.Id, err.Error())
+	}
+
 	return nil
 }
