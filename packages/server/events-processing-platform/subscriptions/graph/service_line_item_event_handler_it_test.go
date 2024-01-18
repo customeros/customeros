fc951fe87f5b0d0dd95573b9a0babe5bc74ed0f2
Eduard Firut <edi@openline.ai>
2024-01-18 17:40:12 +0200

    fix

--- packages/server/events-processing-platform/subscriptions/graph/service_line_item_event_handler_it_test.go
+++ packages/server/events-processing-platform/subscriptions/graph/service_line_item_event_handler_it_test.go
@@ -480,6 +480,7 @@ func TestServiceLineItemEventHandler_OnUpdatePriceIncreaseRetroactively_Timeline
 	updateEvent, err := event.NewServiceLineItemUpdateEvent(
 		aggregate.NewServiceLineItemAggregateWithTenantAndID(tenantName, serviceLineItemId),
 		model.ServiceLineItemDataFields{
+			Billed:   model.MonthlyBilled,
 			Price:    200.0,
 			Comments: "this is the reason for change",
 		},
@@ -513,7 +514,7 @@ func TestServiceLineItemEventHandler_OnUpdatePriceIncreaseRetroactively_Timeline
 
 	serviceLineItem := graph_db.MapDbNodeToServiceLineItemEntity(*serviceLineItemDbNode)
 	require.Equal(t, serviceLineItemId, serviceLineItem.Id)
-	require.Equal(t, model.NoneBilled.String(), serviceLineItem.Billed)
+	require.Equal(t, model.MonthlyBilled.String(), serviceLineItem.Billed)
 	require.Equal(t, float64(200.0), serviceLineItem.Price)
 
 	// verify action
