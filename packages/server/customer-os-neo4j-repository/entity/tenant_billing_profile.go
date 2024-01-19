7a589b493adfa65329ded9b4f92ba9c8b9f4e13a
Eduard Firut <edi@openline.ai>
2024-01-19 21:34:20 +0200

    [COS-1546] - Enhance tenant billing profile with more fields needed in Invoice PDF

--- packages/server/customer-os-neo4j-repository/entity/tenant_billing_profile.go
+++ packages/server/customer-os-neo4j-repository/entity/tenant_billing_profile.go
@@ -5,18 +5,25 @@ import (
 )
 
 type TenantBillingProfile struct {
-	Id            string
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-	LegalName     string
-	Email         string
-	Phone         string
-	AddressLine1  string
-	AddressLine2  string
-	AddressLine3  string
-	Source        DataSource
-	SourceOfTruth DataSource
-	AppSource     string
+	Id                                string
+	CreatedAt                         time.Time
+	UpdatedAt                         time.Time
+	LegalName                         string
+	Email                             string
+	Phone                             string
+	AddressLine1                      string
+	AddressLine2                      string
+	AddressLine3                      string
+	DomesticPaymentsBankName          string
+	DomesticPaymentsAccountNumber     string
+	DomesticPaymentsSortCode          string
+	InternationalPaymentsSwiftBic     string
+	InternationalPaymentsBankName     string
+	InternationalPaymentsBankAddress  string
+	InternationalPaymentsInstructions string
+	Source                            DataSource
+	SourceOfTruth                     DataSource
+	AppSource                         string
 }
 
 type TenantBillingProfiles []TenantBillingProfile
--- packages/server/events-processing-platform/domain/tenant/event/add_billing_profile_event.go
+++ packages/server/events-processing-platform/domain/tenant/event/add_billing_profile_event.go
@@ -4,35 +4,50 @@ import (
 	commonmodel "github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/domain/common/model"
 	"github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/eventstore"
 	"github.com/openline-ai/openline-customer-os/packages/server/events-processing-platform/validator"
+	tenantpb "github.com/openline-ai/openline-customer-os/packages/server/events-processing-proto/gen/proto/go/api/grpc/v1/tenant"
 	"github.com/pkg/errors"
 	"time"
 )
 
 type CreateTenantBillingProfileEvent struct {
-	Tenant       string             `json:"tenant" validate:"required"`
-	Id           string             `json:"id" validate:"required"`
-	CreatedAt    time.Time          `json:"createdAt"`
-	SourceFields commonmodel.Source `json:"sourceFields"`
-	Email        string             `json:"email"`
-	Phone        string             `json:"phone"`
-	AddressLine1 string             `json:"addressLine1"`
-	AddressLine2 string             `json:"addressLine2"`
-	AddressLine3 string             `json:"addressLine3"`
-	LegalName    string             `json:"legalName"`
+	Tenant                            string             `json:"tenant" validate:"required"`
+	Id                                string             `json:"id" validate:"required"`
+	CreatedAt                         time.Time          `json:"createdAt"`
+	SourceFields                      commonmodel.Source `json:"sourceFields"`
+	Email                             string             `json:"email"`
+	Phone                             string             `json:"phone"`
+	AddressLine1                      string             `json:"addressLine1"`
+	AddressLine2                      string             `json:"addressLine2"`
+	AddressLine3                      string             `json:"addressLine3"`
+	LegalName                         string             `json:"legalName"`
+	DomesticPaymentsBankName          string             `json:"domesticPaymentsBankName"`
+	DomesticPaymentsAccountNumber     string             `json:"domesticPaymentsAccountNumber"`
+	DomesticPaymentsSortCode          string             `json:"domesticPaymentsSortCode"`
+	InternationalPaymentsSwiftBic     string             `json:"internationalPaymentsSwiftBic"`
+	InternationalPaymentsBankName     string             `json:"internationalPaymentsBankName"`
+	InternationalPaymentsBankAddress  string             `json:"internationalPaymentsBankAddress"`
+	InternationalPaymentsInstructions string             `json:"internationalPaymentsInstructions"`
 }
 
-func NewCreateTenantBillingProfileEvent(aggregate eventstore.Aggregate, sourceFields commonmodel.Source, id, email, phone, addressLine1, addressLine2, addressLine3, legalName string, createdAt time.Time) (eventstore.Event, error) {
+func NewCreateTenantBillingProfileEvent(aggregate eventstore.Aggregate, sourceFields commonmodel.Source, id string, request *tenantpb.AddBillingProfileRequest, createdAt time.Time) (eventstore.Event, error) {
 	eventData := CreateTenantBillingProfileEvent{
-		Tenant:       aggregate.GetTenant(),
-		Id:           id,
-		CreatedAt:    createdAt,
-		SourceFields: sourceFields,
-		Email:        email,
-		Phone:        phone,
-		AddressLine1: addressLine1,
-		AddressLine2: addressLine2,
-		AddressLine3: addressLine3,
-		LegalName:    legalName,
+		Tenant:                            aggregate.GetTenant(),
+		Id:                                id,
+		CreatedAt:                         createdAt,
+		SourceFields:                      sourceFields,
+		Email:                             request.Email,
+		Phone:                             request.Phone,
+		AddressLine1:                      request.AddressLine1,
+		AddressLine2:                      request.AddressLine2,
+		AddressLine3:                      request.AddressLine3,
+		LegalName:                         request.LegalName,
+		DomesticPaymentsBankName:          request.DomesticPaymentsBankName,
+		DomesticPaymentsAccountNumber:     request.DomesticPaymentsAccountNumber,
+		DomesticPaymentsSortCode:          request.DomesticPaymentsSortCode,
+		InternationalPaymentsSwiftBic:     request.InternationalPaymentsSwiftBic,
+		InternationalPaymentsBankName:     request.InternationalPaymentsBankName,
+		InternationalPaymentsBankAddress:  request.InternationalPaymentsBankAddress,
+		InternationalPaymentsInstructions: request.InternationalPaymentsInstructions,
 	}
 
 	if err := validator.GetValidator().Struct(eventData); err != nil {
