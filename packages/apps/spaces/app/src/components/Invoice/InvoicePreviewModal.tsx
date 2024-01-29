4e2e4d3df0e79f5bc1cbe974ef56bc1cec8f5bde
kasia <kasia@openline.ai>
2024-01-29 11:07:55 +0100

    COS-1764: Added address information to invoice preview

--- packages/apps/spaces/app/src/components/Invoice/InvoicePreviewModal.tsx
+++ packages/apps/spaces/app/src/components/Invoice/InvoicePreviewModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Text } from '@ui/typography/Text';
 import { Invoice } from '@shared/components/Invoice/Invoice';
+import { InvoiceCustomer, InvoiceProvider } from '@graphql/types';
 import { GetInvoiceQuery } from '@shared/graphql/getInvoice.generated';
 import { InvoiceSkeleton } from '@shared/components/Invoice/InvoiceSkeleton';
 interface InvoicePreviewModalProps {
@@ -10,6 +11,18 @@ interface InvoicePreviewModalProps {
   isFetching: boolean;
   data: GetInvoiceQuery | undefined;
 }
+
+const extractAddressData = (invoiceData: InvoiceCustomer | InvoiceProvider) => {
+  return {
+    zip: invoiceData?.addressZip ?? '',
+    email: (invoiceData as InvoiceCustomer)?.email ?? '',
+    name: invoiceData?.name ?? '',
+    country: invoiceData?.addressCountry ?? '',
+    locality: invoiceData?.addressLocality ?? '',
+    addressLine: invoiceData?.addressLine1 ?? '',
+    addressLine2: invoiceData?.addressLine2 ?? '',
+  };
+};
 
 export const InvoicePreviewModalContent: React.FC<InvoicePreviewModalProps> = ({
   id,
@@ -26,32 +39,19 @@ export const InvoicePreviewModalContent: React.FC<InvoicePreviewModalProps> = ({
     return <Text> Sorry, we couldn't find this invoice</Text>;
   }
 
+  const customerAddressData = extractAddressData(data?.invoice?.customer);
+  const providerAddressData = extractAddressData(data?.invoice?.provider);
+
   return (
     <Invoice
       tax={data?.invoice?.vat}
       note={''}
-      from={{
-        zip: '',
-        email: '',
-        name: '',
-        country: '',
-        locality: '',
-        addressLine: '',
-        addressLine2: '',
-      }}
+      from={providerAddressData}
       total={data?.invoice.totalAmount}
       dueDate={data?.invoice.dueDate}
       subtotal={data?.invoice.amount}
       issueDate={data?.invoice?.createdAt}
-      billedTo={{
-        zip: '',
-        email: '',
-        name: '',
-        country: '',
-        locality: '',
-        addressLine: '',
-        addressLine2: '',
-      }}
+      billedTo={customerAddressData}
       invoiceNumber={data?.invoice?.number ?? ''}
       lines={data?.invoice?.invoiceLines ?? []}
       currency={data?.invoice?.currency ?? 'USD'}
