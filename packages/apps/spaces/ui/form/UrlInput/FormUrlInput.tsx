9742a6205a0e03ff680babddb1cb7832001cf294
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-02-07 16:13:28 +0100

    (fix|COS-1952): Make contract link a hyperlink (#5585)

--- packages/apps/spaces/ui/form/UrlInput/FormUrlInput.tsx
+++ packages/apps/spaces/ui/form/UrlInput/FormUrlInput.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import { useField } from 'react-inverted-form';
+
+import { FormLabelProps } from '@chakra-ui/react';
 
 import { InputProps } from '@ui/form/Input';
 
@@ -7,6 +10,11 @@ import { UrlInput } from './UrlInput';
 interface FormUrlInputProps extends InputProps {
   name: string;
   formId: string;
+  label?: string;
+  isLabelVisible?: boolean;
+  labelProps?: FormLabelProps;
+  leftElement?: React.ReactNode;
+  rightElement?: React.ReactNode;
 }
 
 export const FormUrlInput = ({ name, formId, ...props }: FormUrlInputProps) => {
