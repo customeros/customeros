9134fa0fa6474f79357fc04d88141bf71a2f402e
max-openline <max@openline.ai>
2024-02-08 18:16:48 +0100

    [COS-1989] - phx presence for finder channel

--- packages/server/customer_os_realtime/lib/customer_os_realtime_web/channels/finder_view_channel.ex
+++ packages/server/customer_os_realtime/lib/customer_os_realtime_web/channels/finder_view_channel.ex
@@ -1,10 +1,13 @@
 defmodule CustomerOsRealtimeWeb.FinderChannel do
   require Logger
   use CustomerOsRealtimeWeb, :channel
+  alias CustomerOsRealtimeWeb.Presence
 
   @impl true
   def join("finder:lobby", _payload, socket) do
     Logger.debug("Reached join handler in finder_view_channel.ex")
+    send(self(), :after_join)
+    # {:ok, assign(socket, :user_id)}
     {:ok, socket}
 
     # if authorized?(payload) do
@@ -12,6 +15,17 @@ defmodule CustomerOsRealtimeWeb.FinderChannel do
     # else
     #   {:error, %{reason: "unauthorized"}}
     # end
+  end
+
+  @impl true
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:second))
+    })
+
+    push(socket, "presence_state", Presence.list(socket))
+    # push(socket, "feed", %{list: feed_items(socket)}) # TODO: push CRDT state to the client
+    {:noreply, socket}
   end
 
   # Channels can be used in a request/response fashion
