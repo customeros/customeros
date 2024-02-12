4916e27c21b9dd2570a05ecc53a483c022c84d76
max-openline <max@openline.ai>
2024-02-12 12:59:02 +0100

    formatter config

--- packages/server/customer_os_realtime/.formatter.exs
+++ packages/server/customer_os_realtime/.formatter.exs
@@ -1,5 +1,5 @@
 [
   import_deps: [:phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
+  inputs: ["mix.exs", "*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
