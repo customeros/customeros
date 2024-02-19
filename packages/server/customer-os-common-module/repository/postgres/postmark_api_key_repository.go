a44ddea2ee3abc209e3b53e2292361b4840e0314
max-openline <max@openline.ai>
2024-02-19 15:39:35 +0100

    [COS-2091] - create postmark api key table

---
+++ packages/server/customer-os-common-module/repository/postgres/postmark_api_key_repository.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/repository/postgres/entity"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/repository/postgres/helper"
+	"gorm.io/gorm"
+)
+
+type PostmarkApiKeyRepo struct {
+	db *gorm.DB
+}
+
+type PostmarkApiKeyRepository interface {
+	GetPostmarkApiKey(tenant string) helper.QueryResult
+	SaveApiKey(integration entity.PostmarkApiKey) helper.QueryResult
+}
+
+func NewPostmarkApiKeyRepo(db *gorm.DB) *PostmarkApiKeyRepo {
+	return &PostmarkApiKeyRepo{db: db}
+}
+
+func (r *PostmarkApiKeyRepo) GetPostmarkApiKey(tenant string) helper.QueryResult {
+	var postmarkApiKeyEntity entity.PostmarkApiKey
+	err := r.db.
+		Where("tenant_name = ?", tenant).
+		First(&postmarkApiKeyEntity).Error
+
+	if err != nil {
+		return helper.QueryResult{Error: err}
+	}
+
+	return helper.QueryResult{Result: &postmarkApiKeyEntity}
+}
+
+func (r *PostmarkApiKeyRepo) SaveApiKey(apiKey entity.PostmarkApiKey) helper.QueryResult {
+	postmarkApiKeyEntity := entity.PostmarkApiKey{
+		TenantName: apiKey.TenantName,
+		Key:        apiKey.Key,
+	}
+
+	err := r.db.Create(&postmarkApiKeyEntity).Error
+	if err != nil {
+		return helper.QueryResult{Error: err}
+	}
+
+	return helper.QueryResult{Result: &postmarkApiKeyEntity}
+}
