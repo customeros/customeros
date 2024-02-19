eafca2988c867eb576f71de6447347bc12ca92e8
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-02-19 17:02:44 +0100

    (fix|COS-2163): Fix fraction digits for monetary values (#5798)

--- packages/apps/spaces/utils/getFormattedCurrencyNumber.ts
+++ packages/apps/spaces/utils/getFormattedCurrencyNumber.ts
@@ -3,10 +3,13 @@ export function formatCurrency(
   maximumFractionDigits: number = 2,
   currency: string = 'USD',
 ): string {
+  const hasFraction = amount % 1 !== 0;
+  const minimumFractionDigits = hasFraction ? 2 : 0;
+
   return Intl.NumberFormat('en-US', {
     style: 'currency',
     currency,
-    minimumFractionDigits: 0,
+    minimumFractionDigits,
     maximumFractionDigits: maximumFractionDigits,
   }).format(amount);
 }
