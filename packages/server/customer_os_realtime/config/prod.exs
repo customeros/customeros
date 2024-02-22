0703f0acc36ec6a6859a716476eac2fb1ce42fae
Eduard Firut <edi@openline.ai>
2024-02-22 17:23:51 +0200

    Fix CORS for phoenix

--- packages/server/customer_os_realtime/config/prod.exs
+++ packages/server/customer_os_realtime/config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :customer_os_realtime, CustomerOsRealtimeWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: [
         "https://app.openline.dev",
         "https://app.customeros.ai",
