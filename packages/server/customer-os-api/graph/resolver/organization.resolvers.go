62cb07d9456c7bdf856e81be909484f28b077e33
alexopenline <alex@openline.ai>
2024-02-23 12:12:39 +0200

    fix

--- packages/server/customer-os-api/graph/resolver/organization.resolvers.go
+++ packages/server/customer-os-api/graph/resolver/organization.resolvers.go
@@ -7,7 +7,6 @@ package resolver
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/99designs/gqlgen/graphql"
@@ -1183,13 +1182,3 @@ func (r *Resolver) Organization() generated.OrganizationResolver { return &organ
 
 type lastTouchpointResolver struct{ *Resolver }
 type organizationResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *organizationResolver) Notes(ctx context.Context, obj *model.Organization, pagination *model.Pagination) (*model.NotePage, error) {
-	panic(fmt.Errorf("not implemented: Notes - notes"))
-}
