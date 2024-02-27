3a81c175dada895c7f2cadbf5fc20dbf42ac35a1
max-openline <max@openline.ai>
2024-02-27 20:53:19 +0100

    fix test syntax for heartbeat and add timeout

--- packages/server/events-processing-platform/events-processing-platform_test.go
+++ packages/server/events-processing-platform/events-processing-platform_test.go
@@ -23,8 +23,8 @@ func TestTemporalWorkerDoesNotKillServer(t *testing.T) {
 	// Start a heartbeat
 	done := make(chan interface{})
 	defer close(done)
-	const timeout = time.Second
-	heartbeat := Heartbeat(done, timeout)
+	const timeout = 2 * time.Second
+	heartbeat := Heartbeat(done, timeout/2)
 
 	// test we get a heartbeat even if temporal worker fails
 	intSlice := []int{0, 1, 2, 3, 5}
@@ -33,11 +33,18 @@ func TestTemporalWorkerDoesNotKillServer(t *testing.T) {
 
 	i := 0
 	for i < len(intSlice) {
-		if r, ok := <-heartbeat; !ok {
-			t.Error("heartbeat channel closed")
-		} else if r != 1 {
-			t.Errorf("heartbeat sent wrong value: got %d, expected %d", r, 1)
+		select {
+		case r, ok := <-heartbeat:
+			if ok == false {
+				t.Error("heartbeat channel closed")
+			} else if r != 1 {
+				t.Errorf("heartbeat sent wrong value: got %d, expected %d", r, 1)
+			}
+			i++
+		case <-time.After(timeout):
+			t.Fatal("test timed out")
 		}
+
 		logger.Debug("pulse")
 		i++
 	}
@@ -68,8 +75,8 @@ func TestHeartbeat(t *testing.T) {
 			} else if r != 1 {
 				t.Errorf(
 					"heartbeat sent wrong value: got %d, expected %d",
-					1,
 					r,
+					1,
 				)
 			}
 			i++
