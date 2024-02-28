25a86b0ccb0a4d3bb08461fdfa3ae7b2aa47a397
max-openline <max@openline.ai>
2024-02-28 14:07:12 +0100

    test fix

--- packages/server/customer-os-common-module/temporal/test/workflow_test.go
+++ packages/server/customer-os-common-module/temporal/test/workflow_test.go
@@ -5,14 +5,11 @@ import (
 	"context"
 	"errors"
 	"testing"
-	"time"
 
-	"github.com/google/uuid"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/temporal/activity"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/temporal/workflows"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
-	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/testsuite"
 )
@@ -37,9 +34,18 @@ func TestUnitTestSuite(t *testing.T) {
 }
 
 func (s *UnitTestSuite) Test_WebhookWorkflow_ActivityFails() {
-	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything).Return(
-		"", errors.New("WebhookActivityFailure"))
-	s.env.ExecuteWorkflow(workflows.WebhookWorkflow, "test_failure")
+	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, targetUrl, authHeaderName, authHeaderValue string, reqBody *bytes.Buffer) error {
+			return errors.New("WebhookActivityFailure")
+		})
+	workflowParams := workflows.WHWorkflowParam{
+		TargetUrl:       "test_URL",
+		RequestBody:     bytes.NewBuffer([]byte("test_body")),
+		AuthHeaderName:  "test_auth_header",
+		AuthHeaderValue: "test_auth_value",
+		RetryPolicy:     nil,
+	}
+	s.env.ExecuteWorkflow(workflows.WebhookWorkflow, workflowParams)
 
 	s.True(s.env.IsWorkflowCompleted())
 
@@ -51,30 +57,24 @@ func (s *UnitTestSuite) Test_WebhookWorkflow_ActivityFails() {
 }
 
 func (s *UnitTestSuite) Test_WebhookWorkflow_ActivityParamCorrect() {
-	workflowOptions := client.StartWorkflowOptions{
-		ID:                       "test-webhook-calls_" + uuid.New().String(),
-		WorkflowExecutionTimeout: time.Hour * 24 * 365 * 10,
-		TaskQueue:                "test_task_queue",
-	}
-
+	reqBod := bytes.NewBuffer([]byte{})
 	workflowParams := workflows.WHWorkflowParam{
 		TargetUrl:       "test_URL",
-		RequestBody:     bytes.NewBuffer([]byte("test_body")),
+		RequestBody:     reqBod,
 		AuthHeaderName:  "test_auth_header",
 		AuthHeaderValue: "test_auth_value",
 		RetryPolicy:     nil,
 	}
-	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything).Return(
+	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(
 		func(ctx context.Context, targetUrl, authHeaderName, authHeaderValue string, reqBody *bytes.Buffer) error {
-			//s.Equal("test_success", value)
 			s.Equal("test_URL", targetUrl)
 			s.Equal("test_auth_header", authHeaderName)
 			s.Equal("test_auth_value", authHeaderValue)
-			s.Equal("test_body", reqBody.String())
+			s.Equal("", reqBody.String())
 			return nil
 		})
 
-	s.env.ExecuteWorkflow(context.Background(), workflowOptions, workflows.WebhookWorkflow, workflowParams)
+	s.env.ExecuteWorkflow(workflows.WebhookWorkflow, workflowParams)
 
 	s.True(s.env.IsWorkflowCompleted())
 	s.NoError(s.env.GetWorkflowError())
