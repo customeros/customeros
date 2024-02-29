250693dd3f091d2f1f4aca98f00d71124b94be62
Alex Calinica <acalinica@openline.ai>
2024-02-29 08:24:04 +0200

    chore: add localstorage persister

--- packages/apps/spaces/app/src/components/Providers/Providers.tsx
+++ packages/apps/spaces/app/src/components/Providers/Providers.tsx
@@ -3,15 +3,15 @@
 import React, { useState } from 'react';
 
 import { RecoilRoot } from 'recoil';
+import { compress, decompress } from 'lz-string';
 import { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
-
-import { createIDBPersister } from '@shared/util/indexedDBPersister';
-import { AnalyticsProvider } from '@shared/components/Providers/AnalyticsProvider';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 import { Env, EnvProvider } from './EnvProvider';
 import { NextAuthProvider } from './SessionProvider';
+import { AnalyticsProvider } from './AnalyticsProvider';
 import { PhoenixSocketProvider } from './SocketProvider';
 import { GrowthbookProvider } from './GrowthbookProvider';
 import { IntegrationsProvider } from './IntegrationsProvider';
@@ -20,20 +20,15 @@ interface ProvidersProps {
   env: Env;
   isProduction?: boolean;
   children: React.ReactNode;
-  sessionEmail?: string | null;
 }
 
-const hostname =
-  typeof window !== 'undefined' ? window?.location?.hostname : 'platform';
-
-export const Providers = ({
-  env,
-  children,
-  sessionEmail,
-  isProduction,
-}: ProvidersProps) => {
+export const Providers = ({ env, children, isProduction }: ProvidersProps) => {
   const [persister] = useState(() =>
-    createIDBPersister(`${sessionEmail ?? 'cos'}-${hostname}`),
+    createSyncStoragePersister({
+      storage: typeof window !== 'undefined' ? window?.localStorage : null,
+      serialize: (data) => compress(JSON.stringify(data)),
+      deserialize: (data) => JSON.parse(decompress(data)),
+    }),
   );
 
   const [queryClient] = useState(
@@ -51,7 +46,7 @@ export const Providers = ({
     <EnvProvider env={env}>
       <PersistQueryClientProvider
         client={queryClient}
-        persistOptions={{ persister }}
+        persistOptions={{ persister, maxAge: 1000 * 60 * 60 * 12 }}
       >
         <ReactQueryDevtools initialIsOpen={false} position='bottom' />
         <PhoenixSocketProvider>
