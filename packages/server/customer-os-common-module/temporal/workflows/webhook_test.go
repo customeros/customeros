c3f4b8751cd2c580eb02c5e7bc3b2cccf9327673
max-openline <max@openline.ai>
2024-03-01 10:23:43 +0100

    modify tests accordingly

--- packages/server/customer-os-common-module/temporal/workflows/webhook_test.go
+++ packages/server/customer-os-common-module/temporal/workflows/webhook_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/notifications"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/temporal/activity"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
@@ -32,16 +33,28 @@ func TestUnitTestSuite(t *testing.T) {
 }
 
 func (s *UnitTestSuite) Test_WebhookWorkflow_ActivityFails() {
-	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(
-		func(ctx context.Context, targetUrl, authHeaderName, authHeaderValue string, reqBody string) error {
+	s.env.OnActivity(activity.WebhookActivity,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+	).Return(
+		func(ctx context.Context, targetUrl, authHeaderName, authHeaderValue string, reqBody string, notifyFailure bool, notification *notifications.NovuNotification, provider notifications.NotificationProvider) error {
 			return errors.New("WebhookActivityFailure")
 		})
 	workflowParams := WHWorkflowParam{
-		TargetUrl:       "test_URL",
-		RequestBody:     `{"data":{"amountDue":28.16,"amountDueInSmallestUnit":2816,"amountPaid":0,"amountRemaining":28.16,"currency":"GBP","due":"2024-02-29T19:59:12.032628504Z","invoiceNumber":"GVD-88528","invoicePeriodEnd":"2024-01-31T00:00:00Z","invoicePeriodStart":"2024-01-01T00:00:00Z","invoiceUrl":"https://fs.customeros.ai/file//download","note":"invoiceNote","paid":false,"status":"DUE","subtotal":28.16,"taxDue":0,"contract":{"contractName":"BCC Contract Name 002","contractStatus":"LIVE","metadata":{"id":"172c636c-bb9c-4abe-9a93-ff4cb6dc466a"}},"invoiceLineItems":[{"description":"Service line 002 for BCC Contract Name 001","metadata":{"id":"94163956-2af9-402f-b4f1-df73c25a6454"}},{"description":"Service line 003 for BCC Contract Name 001","metadata":{"id":"b0f36efc-c8c3-4a7c-981f-2523aeceacec"}},{"description":"Service line 004 for BCC Contract Name 001","metadata":{"id":"dd72fce7-b724-4ba4-aab9-afd5d8650c77"}}],"metadata":{"created":"2024-02-29T19:59:12.032628504Z","id":"980f37be-04c7-42f6-9fd4-852456fd450e"},"organization":{"customerOsId":"C-PPK-7RN","metadata":{"id":"efb6a64a-3222-4b46-acaa-c09cc0749752"},"name":"BCC"}},"event":"invoice.finalized"}`,
-		AuthHeaderName:  "test_auth_header",
-		AuthHeaderValue: "test_auth_value",
-		RetryPolicy:     nil,
+		TargetUrl:            "test_URL",
+		RequestBody:          `{"data":{"amountDue":28.16,"amountDueInSmallestUnit":2816,"amountPaid":0,"amountRemaining":28.16,"currency":"GBP","due":"2024-02-29T19:59:12.032628504Z","invoiceNumber":"GVD-88528","invoicePeriodEnd":"2024-01-31T00:00:00Z","invoicePeriodStart":"2024-01-01T00:00:00Z","invoiceUrl":"https://fs.customeros.ai/file//download","note":"invoiceNote","paid":false,"status":"DUE","subtotal":28.16,"taxDue":0,"contract":{"contractName":"BCC Contract Name 002","contractStatus":"LIVE","metadata":{"id":"172c636c-bb9c-4abe-9a93-ff4cb6dc466a"}},"invoiceLineItems":[{"description":"Service line 002 for BCC Contract Name 001","metadata":{"id":"94163956-2af9-402f-b4f1-df73c25a6454"}},{"description":"Service line 003 for BCC Contract Name 001","metadata":{"id":"b0f36efc-c8c3-4a7c-981f-2523aeceacec"}},{"description":"Service line 004 for BCC Contract Name 001","metadata":{"id":"dd72fce7-b724-4ba4-aab9-afd5d8650c77"}}],"metadata":{"created":"2024-02-29T19:59:12.032628504Z","id":"980f37be-04c7-42f6-9fd4-852456fd450e"},"organization":{"customerOsId":"C-PPK-7RN","metadata":{"id":"efb6a64a-3222-4b46-acaa-c09cc0749752"},"name":"BCC"}},"event":"invoice.finalized"}`,
+		AuthHeaderName:       "test_auth_header",
+		AuthHeaderValue:      "test_auth_value",
+		RetryPolicy:          nil,
+		Notification:         nil,
+		NotificationProvider: nil,
+		NotifyFailure:        false,
 	}
 	s.env.ExecuteWorkflow(WebhookWorkflow, workflowParams)
 
@@ -57,14 +70,17 @@ func (s *UnitTestSuite) Test_WebhookWorkflow_ActivityFails() {
 func (s *UnitTestSuite) Test_WebhookWorkflow_ActivityParamCorrect() {
 	payload := `{"data":{"amountDue":28.16,"amountDueInSmallestUnit":2816,"amountPaid":0,"amountRemaining":28.16,"currency":"GBP","due":"2024-02-29T19:59:12.032628504Z","invoiceNumber":"GVD-88528","invoicePeriodEnd":"2024-01-31T00:00:00Z","invoicePeriodStart":"2024-01-01T00:00:00Z","invoiceUrl":"https://fs.customeros.ai/file//download","note":"invoiceNote","paid":false,"status":"DUE","subtotal":28.16,"taxDue":0,"contract":{"contractName":"BCC Contract Name 002","contractStatus":"LIVE","metadata":{"id":"172c636c-bb9c-4abe-9a93-ff4cb6dc466a"}},"invoiceLineItems":[{"description":"Service line 002 for BCC Contract Name 001","metadata":{"id":"94163956-2af9-402f-b4f1-df73c25a6454"}},{"description":"Service line 003 for BCC Contract Name 001","metadata":{"id":"b0f36efc-c8c3-4a7c-981f-2523aeceacec"}},{"description":"Service line 004 for BCC Contract Name 001","metadata":{"id":"dd72fce7-b724-4ba4-aab9-afd5d8650c77"}}],"metadata":{"created":"2024-02-29T19:59:12.032628504Z","id":"980f37be-04c7-42f6-9fd4-852456fd450e"},"organization":{"customerOsId":"C-PPK-7RN","metadata":{"id":"efb6a64a-3222-4b46-acaa-c09cc0749752"},"name":"BCC"}},"event":"invoice.finalized"}`
 	workflowParams := WHWorkflowParam{
-		TargetUrl:       "test_URL",
-		RequestBody:     payload,
-		AuthHeaderName:  "test_auth_header",
-		AuthHeaderValue: "test_auth_value",
-		RetryPolicy:     nil,
+		TargetUrl:            "test_URL",
+		RequestBody:          payload,
+		AuthHeaderName:       "test_auth_header",
+		AuthHeaderValue:      "test_auth_value",
+		RetryPolicy:          nil,
+		Notification:         nil,
+		NotificationProvider: nil,
+		NotifyFailure:        false,
 	}
-	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(
-		func(ctx context.Context, targetUrl, authHeaderName, authHeaderValue string, reqBody string) error {
+	s.env.OnActivity(activity.WebhookActivity, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, targetUrl, authHeaderName, authHeaderValue string, reqBody string, notifyFailure bool, notification *notifications.NovuNotification, provider notifications.NotificationProvider) error {
 			s.Equal("test_URL", targetUrl)
 			s.Equal("test_auth_header", authHeaderName)
 			s.Equal("test_auth_value", authHeaderValue)
