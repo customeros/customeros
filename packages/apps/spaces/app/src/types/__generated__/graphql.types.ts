1d39e8cebcd322c480aa3dff21829897062676f2
kasia <kasia@openline.ai>
2024-03-14 14:45:52 +0100

    Regenerate gql

--- packages/apps/spaces/app/src/types/__generated__/graphql.types.ts
+++ packages/apps/spaces/app/src/types/__generated__/graphql.types.ts
@@ -1179,7 +1179,7 @@ export type ExternalSystem = {
 
 export type ExternalSystemInstance = {
   __typename?: 'ExternalSystemInstance';
-  paymentMethods: Array<Scalars['String']['output']>;
+  stripeDetails?: Maybe<ExternalSystemStripeDetails>;
   type: ExternalSystemType;
 };
 
@@ -1189,6 +1189,11 @@ export type ExternalSystemReferenceInput = {
   externalUrl?: InputMaybe<Scalars['String']['input']>;
   syncDate?: InputMaybe<Scalars['Time']['input']>;
   type: ExternalSystemType;
+};
+
+export type ExternalSystemStripeDetails = {
+  __typename?: 'ExternalSystemStripeDetails';
+  paymentMethodTypes: Array<Scalars['String']['output']>;
 };
 
 export enum ExternalSystemType {
@@ -3572,6 +3577,7 @@ export type Query = {
   email: Email;
   entityTemplates: Array<EntityTemplate>;
   externalMeetings: MeetingsPage;
+  externalSystemInstances: Array<ExternalSystemInstance>;
   gcli_Search: Array<GCliItem>;
   global_Cache: GlobalCache;
   interactionEvent: InteractionEvent;
@@ -3800,6 +3806,7 @@ export type QueryReminderArgs = {
 };
 
 export type QueryRemindersForOrganizationArgs = {
+  dismissed?: InputMaybe<Scalars['Boolean']['input']>;
   organizationId: Scalars['ID']['input'];
 };
 
