c9ce3b39f3c4ffaa233297d218c9f60d70175b31
mihai u <mihai.ungureanu@adservio.ro>
2024-03-18 20:23:15 +0200

    fix and to be fixed

--- packages/apps/spaces/app/src/components/Notifications/NotificationCenter.tsx
+++ packages/apps/spaces/app/src/components/Notifications/NotificationCenter.tsx
@@ -5,7 +5,7 @@ import { IMessage, PopoverNotificationCenter } from '@novu/notification-center';
 
 import { cn } from '@ui/utils/cn';
 import { Tooltip } from '@ui/overlay/Tooltip/';
-import { DateTimeUtils } from '@spaces/utils/date';
+// import { DateTimeUtils } from '@spaces/utils/date';
 // import { Avatar, AvatarBadge } from '@ui/media/Avatar';
 import { Avatar, AvatarBadge } from '@ui/media/Avatar/Avatar';
 import { CountButton } from '@shared/components/Notifications/CountButton';
@@ -54,10 +54,10 @@ export const NotificationCenter: React.FC<NotificationCenterProps> = () => {
         const content: false | string[] =
           typeof parsedMessage === 'string' &&
           parsedMessage?.split('owner of ');
-        const dynamicTextClass = cn(message.read ? 'gray.400' : 'gray.500');
-        const dynamicCursor = cn(
-          message.payload.isArchived ? 'cursor-default' : 'cursor-pointer',
-        );
+        // const dynamicTextClass = cn(message.read ? 'gray.400' : 'gray.500');
+        // const dynamicCursor = cn(
+        //   message.payload.isArchived ? 'cursor-default' : 'cursor-pointer',
+        // );
 
         return (
           <Tooltip
@@ -70,7 +70,7 @@ export const NotificationCenter: React.FC<NotificationCenterProps> = () => {
             }
           >
             <div
-              className={`flex px-4 mb-5 ${dynamicCursor}`}
+              // className={`flex px-4 mb-5 ${dynamicCursor}`}
               role='button'
               tabIndex={message.payload.isArchived ? -1 : 0}
               onClick={
@@ -103,12 +103,12 @@ export const NotificationCenter: React.FC<NotificationCenterProps> = () => {
                     (content[1]?.trim()?.length ? content[1] : 'Unnamed')}
                 </span>
               </p>
-              <p className={`text-xs leading-4 ${dynamicTextClass}`}>
+              {/* <p className={`text-xs leading-4 ${dynamicTextClass}`}>
                 {DateTimeUtils.timeAgo(message?.createdAt as string, {
                   includeMin: true,
                   addSuffix: true,
                 })}
-              </p>
+              </p> */}
             </div>
           </Tooltip>
         );
