0d077eecccc528428a90fa2f88d8ecbed6f2e9fa
mihai u <mihai.ungureanu@adservio.ro>
2024-03-26 15:53:54 +0200

    type fix

--- packages/apps/spaces/ui/form/Input/FormResizableInput2.tsx
+++ packages/apps/spaces/ui/form/Input/FormResizableInput2.tsx
@@ -1,71 +1,54 @@
 'use client';
 
-import { forwardRef } from 'react';
 import { useField } from 'react-inverted-form';
-
-import {
-  Flex,
-  FormLabel,
-  FormControl,
-  VisuallyHidden,
-  FormLabelProps,
-} from '@chakra-ui/react';
-
-import { Text } from '@ui/typography/Text';
+import { forwardRef, ForwardedRef } from 'react';
 
 import { InputProps } from './Input';
-import { ResizableInput } from './ResizableInput';
+import { ResizableInput } from './ResizableInput2';
 
 interface FormInputProps extends InputProps {
   name: string;
   formId: string;
   label?: string;
   error?: string | null;
-  isLabelVisible?: boolean;
-  labelProps?: FormLabelProps;
   rightElement?: React.ReactNode;
+  labelProps?: React.LabelHTMLAttributes<HTMLLabelElement>;
 }
 
 export const FormResizableInput = forwardRef<HTMLInputElement, FormInputProps>(
   (
     {
       name,
+      size = 'md',
       formId,
       label,
-      isLabelVisible,
       labelProps,
       rightElement,
       ...props
     }: FormInputProps,
-    ref,
+    ref: ForwardedRef<HTMLInputElement>,
   ) => {
     const { getInputProps, renderError, state } = useField(name, formId);
 
     return (
-      <FormControl>
-        {isLabelVisible ? (
-          <FormLabel {...labelProps}>{label}</FormLabel>
-        ) : (
-          <VisuallyHidden>
-            <FormLabel>{label}</FormLabel>
-          </VisuallyHidden>
-        )}
-        <Flex alignItems='center'>
+      <div>
+        <label {...labelProps}>{label}</label>
+
+        <div className='flex items-center'>
+          {/*@ts-expect-error ignore this*/}
           <ResizableInput
             ref={ref}
             {...getInputProps()}
             {...props}
-            isInvalid={state.meta?.meta?.hasError}
+            onInvalid={() => state.meta?.meta?.hasError}
             autoComplete='off'
           />
           {rightElement && rightElement}
-        </Flex>
+        </div>
         {renderError((error) => (
-          <Text fontSize='xs' color='error.500'>
-            {error}
-          </Text>
+          <span className='text-xs text-error-500'>{error}</span>
         ))}
-      </FormControl>
+      </div>
     );
   },
 );
