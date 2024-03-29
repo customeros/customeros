683de98e908e9fe4dc218309444e4111a85bb074
kasia <kasia@openline.ai>
2024-03-29 09:46:11 +0100

    FIX

--- packages/apps/spaces/app/src/util/currencyOptions.ts
+++ packages/apps/spaces/app/src/util/currencyOptions.ts
@@ -3,14 +3,14 @@ import { Currency } from '@graphql/types';
 export const currencyOptions = [
   {
     label: 'Euro - EUR',
-    value: Currency.Usd,
-  },
-  {
-    label: 'Pound sterling - GBP',
     value: Currency.Eur,
   },
   {
-    label: 'United States dollar - USD',
+    label: 'Pound sterling - GBP',
     value: Currency.Gbp,
+  },
+  {
+    label: 'United States dollar - USD',
+    value: Currency.Usd,
   },
 ];
