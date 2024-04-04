310f44d29a950636638c21f177de77e8ccf50ed0
edifirut <108661145+edifirut@users.noreply.github.com>
2024-04-04 19:57:26 +0300

    Filter out dry run invoices (#6568)

--- packages/apps/spaces/app/src/components/Invoice/utils.ts
+++ packages/apps/spaces/app/src/components/Invoice/utils.ts
@@ -1,9 +1,13 @@
 import { ComparisonOperator } from '@graphql/types';
 
 export const filterOutDryRunInvoices = {
-  filter: {
-    property: 'DRY_RUN',
-    operation: ComparisonOperator.Eq,
-    value: false,
-  },
+  AND: [
+    {
+      filter: {
+        property: 'DRY_RUN',
+        operation: ComparisonOperator.Eq,
+        value: false,
+      },
+    },
+  ],
 };
