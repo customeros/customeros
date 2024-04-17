48b2535503910f2f48ff1bfee90e9abf10e96c67
Mihai <163020214+umihai29@users.noreply.github.com>
2024-04-17 13:53:22 +0300

    fix PageLayout (#6718)

--- packages/apps/spaces/app/src/components/PageLayout/PageLayout.tsx
+++ packages/apps/spaces/app/src/components/PageLayout/PageLayout.tsx
@@ -5,7 +5,7 @@ interface PageLayoutProps {
 export const PageLayout = ({ children }: PageLayoutProps) => {
   return (
     <div
-      className='h-screen grid bg-gray-25 grid-cols-[200px_minmax(100px,_1fr)] grid-rows-1fr gap-4 transition-all ease-in-out duration-250'
+      className='h-screen grid bg-gray-25 grid-cols-[200px_minmax(100px,_1fr)] grid-rows-1fr transition-all ease-in-out duration-250'
       style={{ gridTemplateAreas: `"sidebar content"` }}
     >
       {children}
