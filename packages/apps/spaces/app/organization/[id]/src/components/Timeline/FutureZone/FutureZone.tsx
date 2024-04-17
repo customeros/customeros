56ae1e8350ac071b75bf4096ca99f8bdd84a4d0a
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2024-04-17 13:52:45 +0300

    chore: remove reminders feature flag (#6717)

--- packages/apps/spaces/app/organization/[id]/src/components/Timeline/FutureZone/FutureZone.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Timeline/FutureZone/FutureZone.tsx
@@ -1,11 +1,5 @@
-import { useFeatureIsOn } from '@growthbook/growthbook-react';
-
 import { Reminders } from './reminders';
 
 export const FutureZone = () => {
-  const isRemindersEnabled = useFeatureIsOn('reminders');
-
-  if (!isRemindersEnabled) return null;
-
   return <Reminders />;
 };
