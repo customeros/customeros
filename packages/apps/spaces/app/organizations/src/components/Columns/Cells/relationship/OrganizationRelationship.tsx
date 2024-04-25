35ed9634d00e19fbfd43d221279b466fa57bf5a2
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2024-04-25 16:54:59 +0300

    Fix invoices page initial load error (#6835)
    * refactor: replace select with menu
    
    * fix: json parse default value if tableViewDefs are not available

--- packages/apps/spaces/app/organizations/src/components/Columns/Cells/relationship/OrganizationRelationship.tsx
+++ packages/apps/spaces/app/organizations/src/components/Columns/Cells/relationship/OrganizationRelationship.tsx
@@ -3,15 +3,14 @@ import { useRef, useState, useEffect, useCallback } from 'react';
 import { produce } from 'immer';
 import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 
+import { cn } from '@ui/utils/cn';
 import { Edit03 } from '@ui/media/icons/Edit03';
-import { Select } from '@ui/form/Select/Select';
-import { getContainerClassNames } from '@ui/form/Select';
 import { SelectOption } from '@shared/types/SelectOptions';
 import { IconButton } from '@ui/form/IconButton/IconButton';
 import { getGraphQLClient } from '@shared/util/getGraphQLClient';
 import { useOrganizationsMeta } from '@shared/state/OrganizationsMeta.atom';
+import { Menu, MenuList, MenuItem, MenuButton } from '@ui/overlay/Menu/Menu';
 import { useOrganizationQuery } from '@organization/src/graphql/organization.generated';
-import { getMenuListClassNames } from '@ui/form/MultiCreatableSelect/MultiCreatableSelect2';
 import { useUpdateOrganizationMutation } from '@shared/graphql/updateOrganization.generated';
 import {
   OrganizationRowDTO,
@@ -114,51 +113,41 @@ export const OrganizationRelationship = ({
     };
   }, []);
 
-  if (!isEditing) {
-    return (
-      <div className='flex w-full gap-1 items-center [&_.edit-button]:hover:opacity-100'>
-        <p
-          className='cursor-default text-gray-700 group'
-          onDoubleClick={() => setIsEditing(true)}
-        >
-          {value?.value ? 'Customer' : 'Prospect'}
-        </p>
-        <IconButton
-          className='edit-button rounded-md opacity-0'
-          aria-label='erc'
-          size='sm'
-          variant='ghost'
-          id='edit-button'
-          onClick={() => setIsEditing(true)}
-          icon={<Edit03 className='text-gray-500 size-3' />}
-        />
-      </div>
-    );
-  }
-
   return (
-    <Select
-      size='sm'
-      isClearable
-      defaultValue={value}
-      autoFocus
-      onKeyDown={(e) => {
-        if (e.key === 'Escape') {
-          setIsEditing(false);
-        }
-      }}
-      defaultMenuIsOpen
-      onBlur={() => setIsEditing(false)}
-      isLoading={updateOrganization.isPending}
-      backspaceRemovesValue
-      onChange={handleSelect}
-      openMenuOnClick={false}
-      placeholder='Relationship'
-      options={relationshipOptions}
-      classNames={{
-        container: () => getContainerClassNames('hover:border-transparent'),
-        menuList: () => getMenuListClassNames('w-[262px]'),
-      }}
-    />
+    <div className='flex gap-1 items-center group'>
+      <p
+        className='cursor-default text-gray-700'
+        onDoubleClick={() => setIsEditing(true)}
+      >
+        {value?.value ? 'Customer' : 'Prospect'}
+      </p>
+      <Menu open={isEditing} onOpenChange={setIsEditing}>
+        <MenuButton>
+          <IconButton
+            className={cn(
+              'rounded-md opacity-0 group-hover:opacity-100',
+              isEditing && 'opacity-100',
+            )}
+            aria-label='edit relationship'
+            size='xxs'
+            variant='ghost'
+            id='edit-button'
+            onClick={() => setIsEditing(true)}
+            isLoading={updateOrganization.isPending}
+            icon={<Edit03 className='text-gray-500' />}
+          />
+        </MenuButton>
+        <MenuList>
+          {relationshipOptions.map((option) => (
+            <MenuItem
+              key={option.value.toString()}
+              onClick={() => handleSelect(option)}
+            >
+              {option.label}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </div>
   );
 };
