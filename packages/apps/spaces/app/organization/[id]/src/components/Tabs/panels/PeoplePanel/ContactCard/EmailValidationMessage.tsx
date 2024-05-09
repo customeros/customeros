c5658a00e27d0f7c40eb8ccff45dfbf836d27f2c
kasia <kasia@openline.ai>
2024-05-09 16:01:10 +0200

    (fix|COS-3125): Email validation bug (contacts)

--- packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/PeoplePanel/ContactCard/EmailValidationMessage.tsx
+++ packages/apps/spaces/app/organization/[id]/src/components/Tabs/panels/PeoplePanel/ContactCard/EmailValidationMessage.tsx
@@ -40,31 +40,23 @@ export const EmailValidationMessage = ({ email, validationDetails }: Props) => {
     return null;
   }
   const getMessages = () => {
-    const messages: Array<string> = [];
-    if (!validationData) return messages;
-    const { validated, isReachable, ...input } = validationData;
+    if (!validationData) return [];
+    const { validated, isReachable, isValidSyntax } = validationData;
 
-    for (const key in input) {
-      if (
-        //@ts-expect-error improve type
-        input[key] !== null &&
-        Object.prototype.hasOwnProperty.call(VALIDATION_MESSAGES, key) &&
-        //@ts-expect-error improve type
-        VALIDATION_MESSAGES[key]?.condition === input[key]
-      ) {
-        messages.push(VALIDATION_MESSAGES[key].message);
-      }
+    if (validated && isValidSyntax === false) {
+      return [VALIDATION_MESSAGES.isValidSyntax.message];
     }
     if (
+      validated &&
       isReachable &&
       (VALIDATION_MESSAGES.isReachable.condition as Array<string>).includes(
         isReachable,
       )
     ) {
-      messages.push(VALIDATION_MESSAGES.isReachable.message);
+      return [VALIDATION_MESSAGES.isReachable.message];
     }
 
-    return messages;
+    return [];
   };
 
   return (
