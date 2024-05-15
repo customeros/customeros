06bc3ec540a34c4cbd5bf272a8d9d5c950392672
Alex Calinica <acalinica@openline.ai>
2024-05-15 08:29:04 +0300

    feat: add count of customers to customer-map

--- packages/apps/spaces/app/customer-map/src/components/Legend/Legend.tsx
+++ packages/apps/spaces/app/customer-map/src/components/Legend/Legend.tsx
@@ -1,9 +1,8 @@
-'use client';
-
 import { scaleOrdinal } from '@visx/scale';
 import { LegendItem, LegendLabel, LegendOrdinal } from '@visx/legend';
 
 interface LegendProps {
+  leftElement?: React.ReactNode;
   data: {
     label: string;
     color: string;
@@ -12,7 +11,7 @@ interface LegendProps {
   }[];
 }
 
-export const Legend = ({ data }: LegendProps) => {
+export const Legend = ({ data, leftElement }: LegendProps) => {
   const scale = scaleOrdinal({
     domain: data.map((d) => d.label),
     range: data.map((d) => d.color),
@@ -21,31 +20,34 @@ export const Legend = ({ data }: LegendProps) => {
   return (
     <LegendOrdinal scale={scale}>
       {(labels) => (
-        <div className='flex justify-end'>
-          {labels.map((label, i) => (
-            <LegendItem key={`legend-quantile-${i}`} margin='0 0.5rem'>
-              <svg
-                width={data[i].borderColor ? 9 : 8}
-                height={data[i].borderColor ? 9 : 8}
-                style={{ marginRight: '0.25rem' }}
-              >
-                <circle
-                  fill={label.value}
-                  cx={data[i].borderColor ? 4.5 : 4}
-                  cy={data[i].borderColor ? 4.5 : 4}
-                  r={data[i].borderColor ? 4 : 4}
-                  strokeWidth={data[i].borderColor ? 1 : 0}
-                  stroke={data[i].borderColor ?? 'transparent'}
-                />
-              </svg>
-              <LegendLabel align='left' margin='0 0 0 4px'>
-                <p className='text-sm'>
-                  {label.text}
-                  {data[i].isMissingData && <span>*</span>}
-                </p>
-              </LegendLabel>
-            </LegendItem>
-          ))}
+        <div className='flex justify-between'>
+          {leftElement}
+          <div className='flex'>
+            {labels.map((label, i) => (
+              <LegendItem key={`legend-quantile-${i}`} margin='0 0.5rem'>
+                <svg
+                  width={data[i].borderColor ? 9 : 8}
+                  height={data[i].borderColor ? 9 : 8}
+                  style={{ marginRight: '0.25rem' }}
+                >
+                  <circle
+                    fill={label.value}
+                    cx={data[i].borderColor ? 4.5 : 4}
+                    cy={data[i].borderColor ? 4.5 : 4}
+                    r={data[i].borderColor ? 4 : 4}
+                    strokeWidth={data[i].borderColor ? 1 : 0}
+                    stroke={data[i].borderColor ?? 'transparent'}
+                  />
+                </svg>
+                <LegendLabel align='left' margin='0 0 0 4px'>
+                  <p className='text-sm'>
+                    {label.text}
+                    {data[i].isMissingData && <span>*</span>}
+                  </p>
+                </LegendLabel>
+              </LegendItem>
+            ))}
+          </div>
         </div>
       )}
     </LegendOrdinal>
