5832672c058086d84cf5dc797a47f7a44239dcff
umihai29 <mihai@custeomeros.ai>
2024-05-15 10:03:46 +0300

    fix: add click redirect link

--- packages/apps/frontera/src/routes/src/components/Notifications/NotificationCenter.tsx
+++ packages/apps/frontera/src/routes/src/components/Notifications/NotificationCenter.tsx
@@ -92,30 +92,31 @@ const CustomListItem: ListItem = (message, _, onNotificationClick) => {
               !message.seen ? <AvatarBadge className='bg-[#0BA5EC]' /> : <> </>
             }
           />
-        </div>
-        <div className='flex flex-col text-gray-700'>
-          <p
-            className={cn(
-              'text-sm leading-4 truncate text-inherit',
-              cursorClass,
-            )}
-          >
-            {content && `${content[0]} owner of `}
-            <span className='font-medium text-inherit'>
-              {content && (content[1]?.trim()?.length ? content[1] : 'Unnamed')}
-            </span>
-          </p>
-          <p
-            className={cn(
-              'text-xs leading-4 cursor-default',
-              message.read ? 'text-gray-400' : 'text-gray-500',
-            )}
-          >
-            {DateTimeUtils.timeAgo(message?.createdAt as string, {
-              includeMin: true,
-              addSuffix: true,
-            })}
-          </p>
+          <div className='flex flex-col text-gray-700'>
+            <p
+              className={cn(
+                'text-sm leading-4 truncate text-inherit',
+                cursorClass,
+              )}
+            >
+              {content && `${content[0]} owner of `}
+              <span className='font-medium text-inherit'>
+                {content &&
+                  (content[1]?.trim()?.length ? content[1] : 'Unnamed')}
+              </span>
+            </p>
+            <p
+              className={cn(
+                'text-xs leading-4 cursor-default',
+                message.read ? 'text-gray-400' : 'text-gray-500',
+              )}
+            >
+              {DateTimeUtils.timeAgo(message?.createdAt as string, {
+                includeMin: true,
+                addSuffix: true,
+              })}
+            </p>
+          </div>
         </div>
       </div>
     </Tooltip>
