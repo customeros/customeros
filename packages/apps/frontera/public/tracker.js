c2c64ade86e76755c387d21dc5a02b03b8f90f2a
umihai29 <mihai@custeomeros.ai>
2024-05-16 15:49:16 +0300

    added tracker

---
+++ packages/apps/frontera/public/tracker.js
@@ -0,0 +1,41 @@
+(() => {
+  console.info('Hello from tracker.js');
+  const loader = document.querySelector('[data-customeros]');
+  loader.parentNode.removeChild(loader);
+
+  let trackerData = JSON.parse(
+    localStorage.getItem('customeros-tracker') ??
+      `{ "identity": { "sessionId": "${crypto.randomUUID()}" }, "activity": "" }`,
+  );
+
+  function persist() {
+    localStorage.setItem('customeros-tracker', JSON.stringify(trackerData));
+  }
+
+  function identify(identifier) {
+    trackerData.identity = { ...trackerData.identity, identifier };
+    persist();
+
+    const payload = JSON.stringify(trackerData);
+
+    fetch(`https://user-admin-api.customeros.ai/track'`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Tracker-Payload': payload,
+      },
+    });
+  }
+
+  function track(event) {
+    trackerData.activity = trackerData.activity.concat(
+      `${new Date().valueOf()},${event}`,
+    );
+    persist();
+  }
+
+  window.CustomerOS = {
+    identify,
+    track,
+  };
+})();
