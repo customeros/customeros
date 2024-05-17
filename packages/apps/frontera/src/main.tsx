2132330fcb45964fae956904840895b213588818
Alex Calinica <acalinica@openline.ai>
2024-05-17 14:10:07 +0300

    chore: remove filepond and adjust dockerfile for middleware

--- packages/apps/frontera/src/main.tsx
+++ packages/apps/frontera/src/main.tsx
@@ -8,13 +8,10 @@ import { Providers } from '@shared/components/Providers/Providers';
 import { router } from './routes/router';
 
 import './styles/globals.scss';
-import './styles/filepond.scss';
 import './styles/date-picker.scss';
 import './styles/normalization.scss';
 import './styles/remirror-editor.scss';
 import 'react-toastify/dist/ReactToastify.css';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
-import 'filepond/dist/filepond.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Providers>
