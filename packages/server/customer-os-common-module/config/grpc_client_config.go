c572a921d39d8284711b58648c346f0f5f5007f4
edifirut <108661145+edifirut@users.noreply.github.com>
2024-05-30 22:47:21 +0300

    EPP client - access it via ingress (#7360)

--- packages/server/customer-os-common-module/config/grpc_client_config.go
+++ packages/server/customer-os-common-module/config/grpc_client_config.go
@@ -1,7 +1,9 @@
 package config
 
 type GrpcClientConfig struct {
-	EventsProcessingPlatformEnabled bool   `env:"EVENTS_PROCESSING_PLATFORM_ENABLED" envDefault:"true"`
-	EventsProcessingPlatformUrl     string `env:"EVENTS_PROCESSING_PLATFORM_URL" validate:"required"`
-	EventsProcessingPlatformApiKey  string `env:"EVENTS_PROCESSING_PLATFORM_API_KEY" validate:"required"`
+	EventsProcessingPlatformEnabled     bool   `env:"EVENTS_PROCESSING_PLATFORM_ENABLED" envDefault:"true"`
+	EventsProcessingPlatformUrl         string `env:"EVENTS_PROCESSING_PLATFORM_URL" validate:"required"`
+	EventsProcessingPlatformApiKey      string `env:"EVENTS_PROCESSING_PLATFORM_API_KEY" validate:"required"`
+	EventsProcessingPlatformServername  string `env:"EVENTS_PROCESSING_PLATFORM_SERVER_NAME" envDefault:""`
+	EventsProcessingPlatformCertificate string `env:"EVENTS_PROCESSING_PLATFORM_CERTIFICATE" envDefault:""`
 }
