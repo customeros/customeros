71bd8388978b1acff9d2316fc5caadafbcb958d2
Mihai <163020214+umihai29@users.noreply.github.com>
2024-06-07 14:15:48 +0300

    Edit website and linkedin fields in table (#7460)

--- packages/apps/frontera/src/routes/organizations/src/components/Columns/Cells/website/WebsiteCell.tsx
+++ packages/apps/frontera/src/routes/organizations/src/components/Columns/Cells/website/WebsiteCell.tsx
@@ -1,17 +1,97 @@
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
+import { observer } from 'mobx-react-lite';
+
+import { Input } from '@ui/form/Input';
+import { Edit03 } from '@ui/media/icons/Edit03';
+import { useStore } from '@shared/hooks/useStore';
 import { IconButton } from '@ui/form/IconButton/IconButton';
 import { LinkExternal02 } from '@ui/media/icons/LinkExternal02';
 import { getExternalUrl, getFormattedLink } from '@utils/getExternalLink';
 
 interface WebsiteCellProps {
-  website?: string | null;
+  organizationId: string;
 }
 
-export const WebsiteCell = ({ website }: WebsiteCellProps) => {
+export const WebsiteCell = observer(({ organizationId }: WebsiteCellProps) => {
+  const store = useStore();
   const [isHovered, setIsHovered] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [metaKey, setMetaKey] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const organization = store.organizations.value.get(organizationId);
 
-  if (!website) return <p className='text-gray-400'>Unknown</p>;
+  useEffect(() => {
+    if (isHovered && isEdit) {
+      inputRef.current?.focus();
+    }
+  }, [isHovered, isEdit]);
+
+  useEffect(() => {
+    store.ui.setIsEditingTableCell(isEdit);
+  }, [isEdit]);
+
+  if (!organization?.value.website?.length)
+    return (
+      <div
+        className='flex items-center'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onDoubleClick={() => setIsEdit(true)}
+        onKeyDown={(e) => {
+          if (e.metaKey) {
+            setMetaKey(true);
+          }
+        }}
+        onKeyUp={() => metaKey && setMetaKey(false)}
+        onClick={(e) => {
+          if (e.metaKey) setIsEdit(true);
+        }}
+        onBlur={() => setIsEdit(false)}
+      >
+        {!isEdit ? (
+          <p className='text-gray-400'>Unkown</p>
+        ) : (
+          <Input
+            size='xs'
+            ref={inputRef}
+            variant='unstyled'
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                inputRef.current?.blur();
+              }
+            }}
+            onBlur={(e) => {
+              const value = e.target.value;
+              if (!organization || value === 'Unknown' || value === '') return;
+              organization.update((org) => {
+                if (value.includes('https://www')) {
+                  const newUrl = getFormattedLink(value);
+                  org.website = newUrl;
+                }
+                org.website = value;
+
+                return org;
+              });
+              setIsEdit(false);
+            }}
+          />
+        )}
+        {isHovered && !isEdit && (
+          <IconButton
+            className='ml-3 rounded-[5px]'
+            variant='ghost'
+            size='xxs'
+            onClick={() => setIsEdit(!isEdit)}
+            aria-label='edit'
+            icon={<Edit03 className='text-gray-500' />}
+          />
+        )}
+      </div>
+    );
+  const website = organization?.value.website;
+
+  const formattedLink = getFormattedLink(website);
 
   return (
     <div
@@ -19,21 +99,70 @@ export const WebsiteCell = ({ website }: WebsiteCellProps) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <p className='text-gray-700 cursor-default truncate'>
-        {getFormattedLink(website)}
-      </p>
-      {isHovered && (
-        <IconButton
-          className='ml-1 rounded-[5px]'
-          variant='ghost'
+      {isEdit ? (
+        <Input
           size='xs'
-          onClick={() =>
-            window.open(getExternalUrl(website ?? '/'), '_blank', 'noopener')
-          }
-          aria-label='organization website'
-          icon={<LinkExternal02 className='text-gray-500' />}
+          ref={inputRef}
+          variant='unstyled'
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              inputRef.current?.blur();
+            }
+          }}
+          value={formattedLink}
+          onChange={(e) => {
+            const value = e.target.value;
+            organization.update((org) => {
+              if (value.includes('https://www')) {
+                const newUrl = getFormattedLink(value);
+                org.website = newUrl;
+              }
+              org.website = value;
+
+              return org;
+            });
+          }}
+          onBlur={() => setIsEdit(false)}
         />
+      ) : (
+        <p
+          className='text-gray-700 cursor-default truncate'
+          onDoubleClick={() => setIsEdit(true)}
+          onKeyDown={(e) => {
+            if (e.metaKey) {
+              setMetaKey(true);
+            }
+          }}
+          onKeyUp={() => metaKey && setMetaKey(false)}
+          onClick={(e) => {
+            if (e.metaKey) setIsEdit(true);
+          }}
+        >
+          {formattedLink || 'Unknown'}
+        </p>
+      )}
+      {isHovered && !isEdit && (
+        <>
+          <IconButton
+            className='ml-3 rounded-[5px]'
+            variant='ghost'
+            size='xs'
+            onClick={() => setIsEdit(!isEdit)}
+            aria-label='edit'
+            icon={<Edit03 className='text-gray-500' />}
+          />
+          <IconButton
+            className='ml-1 rounded-[5px]'
+            variant='ghost'
+            size='xxs'
+            onClick={() =>
+              window.open(getExternalUrl(website ?? '/'), '_blank', 'noopener')
+            }
+            aria-label='organization website'
+            icon={<LinkExternal02 className='text-gray-500' />}
+          />
+        </>
       )}
     </div>
   );
-};
+});
--- packages/apps/frontera/src/routes/organizations/src/components/Columns/columnsDictionary.tsx
+++ packages/apps/frontera/src/routes/organizations/src/components/Columns/columnsDictionary.tsx
@@ -120,7 +120,11 @@ const columns: Record<string, Column> = {
     minSize: 150,
     enableSorting: false,
     filterFn: filterWebsiteFn,
-    cell: (props) => <WebsiteCell website={props.getValue()} />,
+    cell: (props) => {
+      const organizationId = props.row.original.value.metadata.id;
+
+      return <WebsiteCell organizationId={organizationId} />;
+    },
     header: (props) => (
       <THead<HTMLInputElement>
         id='website'
@@ -403,9 +407,11 @@ const columns: Record<string, Column> = {
   ORGANIZATIONS_SOCIALS: columnHelper.accessor('value.socialMedia', {
     id: 'ORGANIZATIONS_SOCIALS',
     minSize: 200,
-    cell: (props) => (
-      <LinkedInCell socials={props.row.original.value.socialMedia} />
-    ),
+    cell: (props) => {
+      const organizationId = props.row.original.value.metadata.id;
+
+      return <LinkedInCell organizationId={organizationId} />;
+    },
     header: (props) => (
       <THead<HTMLInputElement>
         id='socials'
