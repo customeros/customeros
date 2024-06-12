414109d41a782bbc60b801810d9d69694cf06465
edifirut <108661145+edifirut@users.noreply.github.com>
2024-06-12 11:53:28 +0300

    [COS-3426] - Clean up Source column data (#7542)

--- packages/runner/sync-gmail/service/sync_service.go
+++ packages/runner/sync-gmail/service/sync_service.go
@@ -153,7 +153,7 @@ func (s *syncService) GetEmailIdForEmail(ctx context.Context, tx neo4j.ManagedTr
 		hide := false
 		relationship := neo4jenum.Prospect.String()
 		stage := neo4jenum.Lead.String()
-		leadSource := "gmail"
+		leadSource := "Gmail"
 		organizationNode, err = s.repositories.OrganizationRepository.CreateOrganization(ctx, tx, tenant, organizationName, relationship, stage, leadSource, source, "openline", AppSource, now, hide)
 		if err != nil {
 			return "", fmt.Errorf("unable to create organization for tenant: %v", err)
