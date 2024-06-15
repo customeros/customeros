7e822ec73ec045bcd7f877666e8533ff9954752f
alexopenline <alex@openline.ai>
2024-06-15 11:43:37 +0300

    Prevent tenant creation without name

--- packages/server/customer-os-api/mapper/tenant_mapper.go
+++ packages/server/customer-os-api/mapper/tenant_mapper.go
@@ -5,11 +5,12 @@ import (
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/graph/model"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
 	neo4jentity "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/entity"
+	"strings"
 )
 
 func MapTenantInputToEntity(input model.TenantInput) neo4jentity.TenantEntity {
 	tenantEntity := neo4jentity.TenantEntity{
-		Name:      input.Name,
+		Name:      strings.TrimSpace(input.Name),
 		Source:    neo4jentity.DataSourceOpenline,
 		AppSource: utils.IfNotNilString(input.AppSource),
 	}
