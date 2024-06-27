c6ea75d48f629ffc910bd8c389dded31e6d19ca2
alexopenline <alex@openline.ai>
2024-06-27 15:31:34 +0300

    add more params

--- packages/server/customer-os-postgres-repository/entity/enrich_details_scrapin.go
+++ packages/server/customer-os-postgres-repository/entity/enrich_details_scrapin.go
@@ -13,6 +13,9 @@ type EnrichDetailsScrapIn struct {
 	ID            uint64      `gorm:"primary_key;autoIncrement:true" json:"id"`
 	Flow          ScrapInFlow `gorm:"column:flow;type:varchar(255);NOT NULL" json:"flow"`
 	Param1        string      `gorm:"column:param1;type:varchar(1000);DEFAULT:'';NOT NULL;index" json:"param1"`
+	Param2        string      `gorm:"column:param2;type:varchar(1000);" json:"param2"`
+	Param3        string      `gorm:"column:param3;type:varchar(1000);" json:"param3"`
+	Param4        string      `gorm:"column:param4;type:varchar(1000);" json:"param4"`
 	AllParamsJson string      `gorm:"column:all_params_json;type:text;DEFAULT:'';NOT NULL" json:"allParams"`
 	Data          string      `gorm:"column:data;type:text;DEFAULT:'';NOT NULL" json:"data"`
 	CreatedAt     time.Time   `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
