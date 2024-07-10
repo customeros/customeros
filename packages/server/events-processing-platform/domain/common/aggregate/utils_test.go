09554993805cf76cf0068b334aea3520907e0b18
Eduard Firut <edi@openline.ai>
2024-07-10 23:39:15 +0300

    fix

--- packages/server/events-processing-platform/domain/common/aggregate/utils_test.go
+++ packages/server/events-processing-platform/domain/common/aggregate/utils_test.go
@@ -1,13 +1,14 @@
 package aggregate
 
 import (
+	"github.com/openline-ai/openline-customer-os/packages/server/events/eventstore"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_GetAggregateTenant(t *testing.T) {
-	invoiceAggregate := NewCommonAggregateWithTenantAndId("invoice", "tenantName", "invoiceId")
-	organizationAggregate := NewCommonAggregateWithTenantAndId("organization", "tenantName", "invoiceId")
+	invoiceAggregate := eventstore.NewCommonAggregateWithTenantAndId("invoice", "tenantName", "invoiceId")
+	organizationAggregate := eventstore.NewCommonAggregateWithTenantAndId("organization", "tenantName", "invoiceId")
 
 	invoiceTenant := GetTenantFromAggregate(invoiceAggregate.GetID(), "invoice")
 	organizationTenant := GetTenantFromAggregate(organizationAggregate.GetID(), "organization")
