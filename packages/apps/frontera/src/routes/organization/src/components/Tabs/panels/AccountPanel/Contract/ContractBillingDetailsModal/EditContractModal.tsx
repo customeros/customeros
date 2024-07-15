5156473a95e6647194073e9c097768c013d19e4b
kasia <kasia@openline.ai>
2024-07-15 16:25:46 +0200

    (fix|COS-3948): Make button section on contract edit modal sticky

--- packages/apps/frontera/src/routes/organization/src/components/Tabs/panels/AccountPanel/Contract/ContractBillingDetailsModal/EditContractModal.tsx
+++ packages/apps/frontera/src/routes/organization/src/components/Tabs/panels/AccountPanel/Contract/ContractBillingDetailsModal/EditContractModal.tsx
@@ -362,7 +362,7 @@ export const EditContractModal = ({
                   onChangeModalMode(EditModalMode.BillingDetails)
                 }
               />
-              <ModalFooter className='p-0 flex'>
+              <ModalFooter className='p-0 flex sticky -bottom-5 -mb-6 pb-6 pt-3 bg-gray-25'>
                 <Button
                   variant='outline'
                   colorScheme='gray'
