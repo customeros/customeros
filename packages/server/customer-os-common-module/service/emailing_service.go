3388713edacffdf4a81f2b436a87eda9ac6c2869
alexopenline <alex@openline.ai>
2024-07-25 10:02:40 +0300

    Generate spy pixel and email link urls

--- packages/server/customer-os-common-module/service/emailing_service.go
+++ packages/server/customer-os-common-module/service/emailing_service.go
@@ -3,11 +3,16 @@ package service
 import (
 	"context"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/logger"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
+	postgresentity "github.com/openline-ai/openline-customer-os/packages/server/customer-os-postgres-repository/entity"
+	"github.com/opentracing/opentracing-go"
+	tracingLog "github.com/opentracing/opentracing-go/log"
 )
 
 type EmailingService interface {
-	GenerateSpyPixelUrl(ctx context.Context, tenant string) (string, error)
-	GenerateLinkUrl(ctx context.Context, tenant string, socialId string) (string, string, error)
+	GenerateEmailSpyPixelUrl(ctx context.Context, tenant, publicUrl, uniqueMessageId, campaign string) (url string, mid string, err error)
+	GenerateEmailLinkUrl(ctx context.Context, tenant, publicUrl, redirectUrl, uniqueMessageId, campaign string) (url string, mid string, lid string, err error)
 }
 
 type emailingService struct {
@@ -22,12 +27,58 @@ func NewEmailingService(log logger.Logger, services *Services) EmailingService {
 	}
 }
 
-func (e emailingService) GenerateSpyPixelUrl(ctx context.Context, tenant string) (string, error) {
-	//TODO implement me
-	panic("implement me")
+func (s emailingService) GenerateEmailSpyPixelUrl(ctx context.Context, tenant, publicUrl, uniqueMessageId, campaign string) (url string, mid string, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailingService.GenerateEmailSpyPixelUrl")
+	defer span.Finish()
+	span.SetTag("tenant", tenant)
+	span.LogFields(tracingLog.String("publicUrl", publicUrl), tracingLog.String("uniqueMessageId", uniqueMessageId), tracingLog.String("campaign", campaign))
+
+	mid = uniqueMessageId
+	if mid == "" {
+		mid = utils.GenerateRandomString(64)
+	}
+
+	emailLookup, err := s.services.PostgresRepositories.EmailLookupRepository.Create(ctx, postgresentity.EmailLookup{
+		Tenant:    tenant,
+		MessageId: mid,
+		Campaign:  campaign,
+		Type:      postgresentity.EmailLookupTypeSpyPixel,
+	})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		s.log.Errorf("Error creating email lookup: %v", err)
+		return "", "", err
+	}
+
+	return publicUrl + "/v1/s?c=" + emailLookup.ID, mid, nil
 }
 
-func (e emailingService) GenerateLinkUrl(ctx context.Context, tenant string, socialId string) (string, string, error) {
-	//TODO implement me
-	panic("implement me")
+func (s emailingService) GenerateEmailLinkUrl(ctx context.Context, tenant, publicUrl, redirectUrl, uniqueMessageId, campaign string) (url string, mid string, lid string, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailingService.GenerateEmailLinkUrl")
+	defer span.Finish()
+	span.SetTag("tenant", tenant)
+	span.LogFields(tracingLog.String("publicUrl", publicUrl), tracingLog.String("redirectUrl", redirectUrl), tracingLog.String("uniqueMessageId", uniqueMessageId), tracingLog.String("campaign", campaign))
+
+	mid = uniqueMessageId
+	if mid == "" {
+		mid = utils.GenerateRandomString(64)
+	}
+
+	lid = utils.GenerateRandomString(64)
+
+	emailLookup, err := s.services.PostgresRepositories.EmailLookupRepository.Create(ctx, postgresentity.EmailLookup{
+		Tenant:      tenant,
+		MessageId:   mid,
+		LinkId:      lid,
+		RedirectUrl: redirectUrl,
+		Campaign:    campaign,
+		Type:        postgresentity.EmailLookupTypeLink,
+	})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		s.log.Errorf("Error creating email lookup: %v", err)
+		return "", "", "", err
+	}
+
+	return publicUrl + "/v1/l?c=" + emailLookup.ID, mid, lid, nil
 }
