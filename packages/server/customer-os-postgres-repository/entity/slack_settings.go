f217a8a624556d554b383da15de1512d594b5d25
alexopenline <alex@openline.ai>
2024-08-05 16:08:05 +0300

    Fix nil access in trackservices

--- packages/server/customer-os-postgres-repository/entity/slack_settings.go
+++ packages/server/customer-os-postgres-repository/entity/slack_settings.go
@@ -2,11 +2,14 @@ package entity
 
 import (
 	"github.com/google/uuid"
+	"time"
 )
 
 type SlackSettingsEntity struct {
 	Id           uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	TenantName   string    `gorm:"index:idx_tenant_uk;size:255;not null"`
+	CreatedAt    time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
+	UpdatedAt    time.Time `gorm:"column:updated_at;type:timestamp;DEFAULT:current_timestamp" json:"updatedAt"`
 	AppId        string    `gorm:"size:255;"`
 	AuthedUserId string    `gorm:"size:255;"`
 	Scope        string    `gorm:"size:255;"`
--- packages/server/customer-os-postgres-repository/repository/slack_settings_repository.go
+++ packages/server/customer-os-postgres-repository/repository/slack_settings_repository.go
@@ -2,15 +2,18 @@ package repository
 
 import (
 	"fmt"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-postgres-repository/entity"
+	"github.com/opentracing/opentracing-go"
+	tracingLog "github.com/opentracing/opentracing-go/log"
+	"golang.org/x/net/context"
 	"gorm.io/gorm"
 )
 
 type SlackSettingsRepository interface {
-	Get(tenant string) (*entity.SlackSettingsEntity, error)
-	Save(slackSettings entity.SlackSettingsEntity) (*entity.SlackSettingsEntity, error)
-
-	Delete(tenant string) error
+	Get(ctx context.Context, tenant string) (*entity.SlackSettingsEntity, error)
+	Save(ctx context.Context, slackSettings entity.SlackSettingsEntity) (*entity.SlackSettingsEntity, error)
+	Delete(ctx context.Context, tenant string) error
 }
 
 type slackSettingsRepository struct {
@@ -23,26 +26,38 @@ func NewSlackSettingsRepository(db *gorm.DB) SlackSettingsRepository {
 	}
 }
 
-func (repo *slackSettingsRepository) Get(tenant string) (*entity.SlackSettingsEntity, error) {
+func (repo *slackSettingsRepository) Get(ctx context.Context, tenant string) (*entity.SlackSettingsEntity, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "SlackSettingsRepository.Get")
+	defer span.Finish()
+	span.SetTag(tracing.SpanTagTenant, tenant)
+
 	var existing *entity.SlackSettingsEntity
 	err := repo.db.Find(&existing, "tenant_name = ?", tenant).Error
 
 	if err != nil {
+		span.LogFields(tracingLog.Bool("result.found", false))
 		if err == gorm.ErrRecordNotFound {
 			return nil, nil
 		}
+		tracing.TraceErr(span, err)
 		return nil, err
 	}
 
 	if existing == nil {
+		span.LogFields(tracingLog.Bool("result.found", false))
 		return nil, nil
 	} else if existing.TenantName == "" {
+		span.LogFields(tracingLog.Bool("result.found", false))
 		return nil, nil
 	}
+	span.LogFields(tracingLog.Bool("result.found", true))
 	return existing, nil
 }
 
-func (repo *slackSettingsRepository) Save(slackSettings entity.SlackSettingsEntity) (*entity.SlackSettingsEntity, error) {
+func (repo *slackSettingsRepository) Save(ctx context.Context, slackSettings entity.SlackSettingsEntity) (*entity.SlackSettingsEntity, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "SlackSettingsRepository.Save")
+	defer span.Finish()
+
 	result := repo.db.Save(&slackSettings)
 	if result.Error != nil {
 		return nil, fmt.Errorf("saving slack settings failed: %w", result.Error)
@@ -50,8 +65,12 @@ func (repo *slackSettingsRepository) Save(slackSettings entity.SlackSettingsEnti
 	return &slackSettings, nil
 }
 
-func (repo *slackSettingsRepository) Delete(tenant string) error {
-	existing, err := repo.Get(tenant)
+func (repo *slackSettingsRepository) Delete(ctx context.Context, tenant string) error {
+	span, _ := opentracing.StartSpanFromContext(ctx, "SlackSettingsRepository.Delete")
+	defer span.Finish()
+	span.SetTag(tracing.SpanTagTenant, tenant)
+
+	existing, err := repo.Get(ctx, tenant)
 	if err != nil {
 		return err
 	}
