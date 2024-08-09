46a46afa8cd32e728fedd500ae7bd273926087f3
Eduard Firut <108661145+edifirut@users.noreply.github.com>
2024-08-09 10:44:30 +0300

    Fix local infra (#8352)

--- deployment/up.sh
+++ deployment/up.sh
@@ -1,2 +1,5 @@
+#docker build -t ghcr.io/customeros/customeros/neo4j-sidecar:latest -f neo4j-sidecar/Dockerfile .
+#docker push ghcr.io/customeros/customeros/neo4j-sidecar:latest
+
 docker compose -f docker-compose.yaml pull
 docker compose -f docker-compose.yaml up -d
