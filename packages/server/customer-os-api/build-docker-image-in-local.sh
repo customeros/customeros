67fb9a1dcd05a79eec3ee0e81334dffec8a8e057
alexopenline <alex@openline.ai>
2024-08-10 08:31:38 +0300

    Fix building customer-os-api docker image

--- packages/server/customer-os-api/build-docker-image-in-local.sh
+++ packages/server/customer-os-api/build-docker-image-in-local.sh
@@ -4,6 +4,7 @@ mkdir tmp-sources
 mkdir tmp-sources/customer-os-common-module
 mkdir tmp-sources/customer-os-neo4j-repository
 mkdir tmp-sources/validation-api
+mkdir tmp-sources/enrichment-api
 mkdir tmp-sources/customer-os-api
 
 rsync -av --progress --exclude="tmp-sources" * tmp-sources/customer-os-api
@@ -11,6 +12,7 @@ cp .env tmp-sources/customer-os-api/.env
 cp -r ../customer-os-common-module/* tmp-sources/customer-os-common-module
 cp -r ../customer-os-neo4j-repository/* tmp-sources/customer-os-neo4j-repository
 cp -r ../validation-api/* tmp-sources/validation-api
+cp -r ../enrichment-api/* tmp-sources/enrichment-api
 
 cp Dockerfile tmp-sources/Dockerfile
 
