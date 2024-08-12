80dad7e174fc87a2d77606e0a24029df5505352d
SilviuOpenline <125381623+SilviuOpenline@users.noreply.github.com>
2024-08-12 15:47:38 +0300

    [4206] workflow for FE tests (#8375)

--- packages/apps/frontera/playwright.config.ts
+++ packages/apps/frontera/playwright.config.ts
@@ -17,16 +17,17 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Update the reporter configuration
+  reporter: [['html'], ['list'], ['./playwrightCustomReporter.ts']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: process.env.PROD_FE_TEST_USER_URL || 'http://127.0.0.1:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
