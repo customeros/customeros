dc562b57a4badd8406fdf2cc769ece95f88cfaf5
silviu <silviu@openline.ai>
2024-08-12 16:17:36 +0300

    [4206] workflow for FE tests

--- packages/apps/frontera/tests/pages/loginPage.ts
+++ packages/apps/frontera/tests/pages/loginPage.ts
@@ -1,42 +1,27 @@
-import { Page } from '@playwright/test';
-
-import config from '../../tests/config.js';
+import { Page, expect } from '@playwright/test';
 
 export class LoginPage {
   constructor(private page: Page) {}
 
-  async loginWithMS(email: string, password: string) {
-    await this.page.goto('https://app.customeros.ai/');
-
-    const googleLoginButtonSelector = 'text=Sign in with Microsoft';
-
-    await this.page.waitForSelector(googleLoginButtonSelector, {
-      state: 'visible',
-    });
-    await this.page.locator(googleLoginButtonSelector).click();
-
-    await this.page.fill(
-      'input[aria-label="Enter your email or phone"]',
-      email,
-    );
-    await this.page.click('#idSIButton9');
-
-    await this.page.fill(
-      'input[aria-label="Enter the password for silviu@openline.dev"]',
-      password,
-    );
-    await this.page.click('#idSIButton9');
-
-    //   // Complete additional steps
-    await this.page.click('#idSubmit_ProofUp_Redirect');
-    await this.page.click('a.L0g5CbGcDigQv3yxT1b_:has-text("Skip setup")', {
-      timeout: 60000,
-    });
-    await this.page.click('#upgradeConsentCheckbox');
-    await this.page.click('#idSIButton9');
-  }
-
   async login() {
-    await this.page.goto(config.LOCAL_LOGIN_URL);
+    const loginUrl = process.env.PROD_FE_TEST_USER_URL;
+
+    // Listen to all responses from the page
+    this.page.on('response', async (response) => {
+      const url = response.url();
+      const status = response.status();
+
+      if (
+        url.includes('https://mid.customeros.ai/customer-os-api') &&
+        status === 401
+      ) {
+        expect(
+          status,
+          'Test failed: Expected not to receive a 401 Unauthorized response from the API endpoint',
+        ).not.toBe(401);
+      }
+    });
+
+    await this.page.goto(loginUrl);
   }
 }
