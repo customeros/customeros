7997d65769e0ccc37f0710ba3883e004825de2b7
alexopenline <alex@openline.ai>
2024-08-17 09:17:54 +0300

    Improve calling scrapin

--- packages/server/customer-os-postgres-repository/repository/enrich_details_scrapin_repository.go
+++ packages/server/customer-os-postgres-repository/repository/enrich_details_scrapin_repository.go
@@ -19,7 +19,9 @@ type EnrichDetailsScrapInRepository interface {
 	Create(ctx context.Context, data entity.EnrichDetailsScrapIn) (*entity.EnrichDetailsScrapIn, error)
 	GetAllByParam1AndFlow(ctx context.Context, param string, flow entity.ScrapInFlow) ([]entity.EnrichDetailsScrapIn, error)
 	GetLatestByParam1AndFlow(ctx context.Context, param string, flow entity.ScrapInFlow) (*entity.EnrichDetailsScrapIn, error)
+	GetLatestByParam1AndFlowWithPersonFound(ctx context.Context, param string, flow entity.ScrapInFlow) (*entity.EnrichDetailsScrapIn, error)
 	GetLatestByAllParamsAndFlow(ctx context.Context, param1, param2, param3, param4 string, flow entity.ScrapInFlow) (*entity.EnrichDetailsScrapIn, error)
+	GetLatestByAllParamsAndFlowWithPersonFound(ctx context.Context, param1, param2, param3, param4 string, flow entity.ScrapInFlow) (*entity.EnrichDetailsScrapIn, error)
 	GetById(ctx context.Context, id uint64) (*entity.EnrichDetailsScrapIn, error)
 }
 
@@ -95,6 +97,38 @@ func (r enrichDetailsScrapInRepository) GetLatestByAllParamsAndFlow(ctx context.
 
 	var data entity.EnrichDetailsScrapIn
 	err := r.db.Where("param1 = ? AND param2 = ? AND param3 = ? AND param4 = ? AND flow = ?", param1, param2, param3, param4, flow).Order("created_at desc").First(&data).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil // Return nil if no record found
+		}
+		return nil, err // Return other errors as usual
+	}
+
+	return &data, nil
+}
+
+func (r enrichDetailsScrapInRepository) GetLatestByParam1AndFlowWithPersonFound(ctx context.Context, param string, flow entity.ScrapInFlow) (*entity.EnrichDetailsScrapIn, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "EnrichDetailsScrapInRepository.GetLatestByParam1AndFlowWithPersonFound")
+	defer span.Finish()
+
+	var data entity.EnrichDetailsScrapIn
+	err := r.db.Where("param1 = ? AND flow = ? AND person_found = ?", param, flow, true).Order("created_at desc").First(&data).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil // Return nil if no record found
+		}
+		return nil, err // Return other errors as usual
+	}
+
+	return &data, nil
+}
+
+func (r enrichDetailsScrapInRepository) GetLatestByAllParamsAndFlowWithPersonFound(ctx context.Context, param1, param2, param3, param4 string, flow entity.ScrapInFlow) (*entity.EnrichDetailsScrapIn, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "EnrichDetailsScrapInRepository.GetLatestByAllParamsAndFlowWithPersonFound")
+	defer span.Finish()
+
+	var data entity.EnrichDetailsScrapIn
+	err := r.db.Where("param1 = ? AND param2 = ? AND param3 = ? AND param4 = ? AND flow = ? AND person_found = ?", param1, param2, param3, param4, flow, true).Order("created_at desc").First(&data).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, nil // Return nil if no record found
