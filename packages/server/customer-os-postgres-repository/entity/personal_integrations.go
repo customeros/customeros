ee3f4bbb2d46f18fa68c9eec9a4232c9efd44f03
alexopenline <alex@openline.ai>
2024-08-22 08:12:37 +0300

    Allow deactivating personal connections

--- packages/server/customer-os-postgres-repository/entity/personal_integrations.go
+++ packages/server/customer-os-postgres-repository/entity/personal_integrations.go
@@ -8,6 +8,7 @@ type PersonalIntegration struct {
 	Name       string    `gorm:"column:name;type:varchar(255);NOT NULL" json:"name" binding:"required"`
 	Email      string    `gorm:"column:email;type:varchar(255);NOT NULL" json:"email" binding:"required"`
 	Secret     string    `gorm:"column:key;type:varchar(255);NOT NULL;index:idx_key,unique" json:"key" binding:"required"`
+	Active     bool      `gorm:"column:active;type:boolean;DEFAULT:true" json:"active"`
 	CreatedAt  time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
 	UpdatedAt  time.Time `gorm:"column:updated_at;type:timestamp" json:"updatedAt"`
 }
