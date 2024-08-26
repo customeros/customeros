d354e738c930d12fcda954ae12f5941970414c3a
Mihai <163020214+umihai29@users.noreply.github.com>
2024-08-26 17:31:08 +0300

    archive contact from cmdK (#8533)

--- packages/apps/frontera/src/routes/src/components/CommandMenu/commands/ContactBulkCommands.tsx
+++ packages/apps/frontera/src/routes/src/components/CommandMenu/commands/ContactBulkCommands.tsx
@@ -2,9 +2,11 @@ import { observer } from 'mobx-react-lite';
 
 import { Tag01 } from '@ui/media/icons/Tag01';
 import { Clock } from '@ui/media/icons/Clock';
+import { Delete } from '@ui/media/icons/Delete';
 import { useStore } from '@shared/hooks/useStore';
-import { CommandItem } from '@ui/overlay/CommandMenu';
+import { Archive } from '@ui/media/icons/Archive';
 import { Certificate02 } from '@ui/media/icons/Certificate02';
+import { Kbd, CommandKbd, CommandItem } from '@ui/overlay/CommandMenu';
 
 import { CommandsContainer } from './shared';
 
@@ -18,7 +20,7 @@ export const ContactBulkCommands = observer(() => {
       <>
         <CommandItem
           leftAccessory={<Tag01 />}
-          keywords={['change', 'eidt', 'update', 'tag', 'label', 'profile']}
+          keywords={contactKeywords.edit_persona_tag}
           onSelect={() => {
             store.ui.commandMenu.setType('EditPersonaTag');
           }}
@@ -28,18 +30,10 @@ export const ContactBulkCommands = observer(() => {
 
         <CommandItem
           leftAccessory={<Certificate02 />}
+          keywords={contactKeywords.edit_job_title}
           onSelect={() => {
             store.ui.commandMenu.setType('EditJobTitle');
           }}
-          keywords={[
-            'change',
-            'edit',
-            'update',
-            'job',
-            'title',
-            'position',
-            'designation',
-          ]}
         >
           Edit job title
         </CommandItem>
@@ -48,28 +42,72 @@ export const ContactBulkCommands = observer(() => {
           onSelect={() => {
             store.ui.commandMenu.setType('ChangeOrAddJobRoles');
           }}
-          keywords={[
-            'change',
-            'edit',
-            'update',
-            'job',
-            'roles',
-            'position',
-            'function',
-          ]}
         >
           Edit job roles...
         </CommandItem>
         <CommandItem
           leftAccessory={<Clock />}
-          keywords={['change', 'edit', 'update', 'timezone', 'location']}
+          keywords={contactKeywords.edit_time_zone}
           onSelect={() => {
             store.ui.commandMenu.setType('EditTimeZone');
           }}
         >
           Edit time zone...
         </CommandItem>
+
+        <CommandItem
+          leftAccessory={<Archive />}
+          keywords={contactKeywords.archive_contact}
+          onSelect={() => {
+            store.ui.commandMenu.setType('DeleteConfirmationModal');
+          }}
+          rightAccessory={
+            <>
+              <CommandKbd />
+              <Kbd>
+                <Delete className='size-3' />
+              </Kbd>
+            </>
+          }
+        >
+          Archive contacts
+        </CommandItem>
       </>
     </CommandsContainer>
   );
 });
+
+const contactKeywords = {
+  archive_contact: ['archive', 'contact', 'delete', 'remove', 'hide'],
+  edit_persona_tag: [
+    'edit',
+    'persona',
+    'tag',
+    'change',
+    'update',
+    'label',
+    'profile',
+  ],
+  edit_job_title: [
+    'edit',
+    'job',
+    'title',
+    'change',
+    'update',
+    'position',
+    'designation',
+  ],
+  edit_job_roles: ['edit', 'job', 'roles', 'update', 'position', 'function'],
+  edit_time_zone: ['edit', 'timezone', 'change', 'update', 'location'],
+  edit_email: ['edit', 'email', 'change', 'update', 'address', '@'],
+  edit_name: ['edit', 'name', 'change', 'update', 'rename', 'contact'],
+  edit_phone_number: [
+    'edit',
+    'phone',
+    'number',
+    'change',
+    'update',
+    'mobile',
+    'telephone',
+  ],
+};
