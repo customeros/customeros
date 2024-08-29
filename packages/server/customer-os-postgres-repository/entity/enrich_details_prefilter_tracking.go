b4404cb6e6e5c51c0e404b8b72a2cee6871502fb
alexopenline <alex@openline.ai>
2024-08-29 16:41:24 +0300

    store skip IP verify reason

--- packages/server/customer-os-postgres-repository/entity/enrich_details_prefilter_tracking.go
+++ packages/server/customer-os-postgres-repository/entity/enrich_details_prefilter_tracking.go
@@ -5,12 +5,13 @@ import (
 )
 
 type EnrichDetailsPreFilterTracking struct {
-	ID             string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	CreatedAt      time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
-	UpdatedAt      time.Time `gorm:"column:updated_at;type:timestamp;" json:"updatedAt"`
-	IP             string    `gorm:"column:ip;uniqueIndex:ip_unique;type:varchar(255);" json:"ip"`
-	ShouldIdentify *bool     `gorm:"column:should_identify;type:boolean;" json:"shouldIdentify"`
-	Response       *string   `gorm:"column:response;type:text;" json:"response"`
+	ID                 string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	CreatedAt          time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
+	UpdatedAt          time.Time `gorm:"column:updated_at;type:timestamp;" json:"updatedAt"`
+	IP                 string    `gorm:"column:ip;uniqueIndex:ip_unique;type:varchar(255);" json:"ip"`
+	ShouldIdentify     *bool     `gorm:"column:should_identify;type:boolean;" json:"shouldIdentify"`
+	SkipIdentifyReason string    `gorm:"column:skip_identify_reason;type:varchar(255);" json:"skipIdentifyReason"`
+	Response           *string   `gorm:"column:response;type:text;" json:"response"`
 }
 
 func (EnrichDetailsPreFilterTracking) TableName() string {
