a10fdc3bd3c39d542bbfbc664f772f5c4f1c3cec
kasia <kasia@openline.ai>
2024-09-03 15:34:31 +0200

    (fix|COS-4502): Add shortcut to archive tooltip + padding fix

--- packages/apps/frontera/src/routes/organizations/src/components/Actions/components/ActionItem.tsx
+++ packages/apps/frontera/src/routes/organizations/src/components/Actions/components/ActionItem.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import { cn } from '@ui/utils/cn';
 import { Button } from '@ui/form/Button/Button';
 import { CommandKbd } from '@ui/overlay/CommandMenu';
 import { Tooltip } from '@ui/overlay/Tooltip/Tooltip';
 
 interface ActionItemProps {
-  tooltip?: string;
   dataTest?: string;
   onClick: () => void;
-  shortcutKey?: string;
   icon: React.ReactElement;
+  tooltip?: React.ReactNode;
   children: React.ReactNode;
 }
 
@@ -19,31 +17,20 @@ export const ActionItem = ({
   onClick,
   dataTest,
   tooltip,
-  shortcutKey,
   children,
 }: ActionItemProps) => {
   return (
     <Tooltip
-      className=''
+      className='p-1.5'
       label={
         tooltip ? (
-          <div className='flex items-center text-sm'>
-            {tooltip}
-            <span
-              className={cn(
-                'bg-gray-600 text-xs rounded-sm leading-[1.125rem]',
-                shortcutKey && 'pl-2',
-              )}
-            >
-              {shortcutKey}
-            </span>
-          </div>
+          tooltip
         ) : (
           <>
             <div className='flex items-center text-sm'>
               Open command menu
               <CommandKbd className='bg-gray-600 text-gray-25 mx-1' />
-              <div className='bg-gray-600 text-xs h-5 w-5 rounded-sm flex justify-center items-center'>
+              <div className='bg-gray-600 text-xs min-h-5 min-w-5 rounded flex justify-center items-center'>
                 K
               </div>
             </div>
