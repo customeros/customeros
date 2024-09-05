63ad11d5fe53f9eaa22b755d4b35443178a0b04a
Alex Calinica <acalinica@openline.ai>
2024-09-05 11:00:51 +0300

    fix: add ssl flag to pg driver

--- packages/server/browser-automation-api/src/infrastructure/persistance/postgresql/db.ts
+++ packages/server/browser-automation-api/src/infrastructure/persistance/postgresql/db.ts
@@ -3,5 +3,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 import { connectionUrl } from "./config";
 
-const pg = postgres(connectionUrl);
+const pg = postgres(connectionUrl, {
+  ssl: process.env.NODE_ENV !== "development",
+});
 export const db = drizzle(pg);
