efb431cc71edb9fcafae6950f813559e8f9e7bbb
kasia <kasia@openline.ai>
2024-09-12 10:13:06 +0200

    (feat|COS-4589): Added keywords for sequences command menu

--- packages/apps/frontera/src/routes/src/components/CommandMenu/commands/sequences/keywords.ts
+++ packages/apps/frontera/src/routes/src/components/CommandMenu/commands/sequences/keywords.ts
@@ -1,23 +1,6 @@
 export const sequenceKeywords = {
-  rename_sequence: [
-    'rename',
-    'org',
-    'sequence',
-    'company',
-    'update',
-    'edit',
-    'change',
-  ],
+  rename_sequence: ['rename', 'sequence', 'name', 'update', 'edit', 'change'],
 
-  archive_sequence: ['archive', 'org', 'delete', 'remove', 'hide', 'sequence'],
-  status_update: [
-    'change',
-    'status',
-    'update',
-    'edit',
-    'active',
-    'inactive',
-    'paused',
-    'archived',
-  ],
+  archive_sequence: ['archive', 'sequence', 'delete', 'remove', 'hide'],
+  status_update: ['change', 'sequence', 'status', 'stage', 'edit', 'update'],
 };
