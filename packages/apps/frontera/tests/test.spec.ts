66eaccec6f98b6c97967f5612da7db93b328c35a
silviu <silviu@openline.ai>
2024-09-13 15:31:18 +0300

    [4613] caching

--- packages/apps/frontera/tests/test.spec.ts
+++ packages/apps/frontera/tests/test.spec.ts
@@ -189,5 +189,4 @@ test('CmdK global menu', async ({ page }, testInfo) => {
   await organizationsCmdKPage.verifyNavigationToSequences(page);
   await organizationsCmdKPage.verifyNavigationToSettings(page);
   await organizationsCmdKPage.verifyNavigationToCustomerMap(page);
-  await organizationsCmdKPage.verifyNavigationToCustomerMap(page);
 });
