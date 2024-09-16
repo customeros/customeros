e5252f58784b5d6eeb040593ad3fde9a006f1def
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2024-09-16 09:36:54 +0300

    fix: missing tableId prop (#8858)

--- packages/apps/frontera/src/routes/prospects/page.tsx
+++ packages/apps/frontera/src/routes/prospects/page.tsx
@@ -1,4 +1,4 @@
-import { TableViewType } from '@graphql/types';
+import { TableIdType, TableViewType } from '@graphql/types';
 import { ViewSettings } from '@shared/components/ViewSettings';
 
 import { Search } from './src/components/Search';
@@ -9,7 +9,10 @@ export const ProspectsBoardPage = () => {
     <div className='flex flex-col text-gray-700 overflow-auto bg-white'>
       <div className='flex justify-between pr-4 border-b border-b-gray-200 bg-gray-25'>
         <Search />
-        <ViewSettings type={TableViewType.Opportunities} />
+        <ViewSettings
+          type={TableViewType.Opportunities}
+          tableId={TableIdType.Opportunities}
+        />
       </div>
       <ProspectsBoard />
     </div>
