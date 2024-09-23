2376c0ac2f167b3bb0281bc06e744913c19c2d6f
kasia <kasia@openline.ai>
2024-09-23 17:47:34 +0200

    Add feature flag

--- packages/apps/frontera/src/routes/flowEditor/page.tsx
+++ packages/apps/frontera/src/routes/flowEditor/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ReactFlowProvider } from '@xyflow/react';
+import { useFeatureIsOn } from '@growthbook/growthbook-react';
 
 import { Header } from './src/Header.tsx';
 import { MarketingFlowBuilder } from './src/FlowManager.tsx';
@@ -8,6 +9,12 @@ import { MarketingFlowBuilder } from './src/FlowManager.tsx';
 import '@xyflow/react/dist/style.css';
 
 export const FlowEditor = () => {
+  const allowExploration = useFeatureIsOn('flow-editor-poc');
+
+  if (!allowExploration) {
+    return null;
+  }
+
   return (
     <ReactFlowProvider>
       <div className='flex h-full flex-col'>
