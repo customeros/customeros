21da3c2047b846762899013602343a1827f319cc
silviu <silviu@openline.ai>
2024-10-04 13:09:04 +0300

    4923 - customize video names

--- packages/apps/frontera/tests/videoFixture.ts
+++ packages/apps/frontera/tests/videoFixture.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { chromium, test as base, BrowserContext } from '@playwright/test';
 
 type VideoFixture = {
@@ -7,10 +9,7 @@ type VideoFixture = {
 interface TestFixtures {}
 
 export const test = base.extend<TestFixtures & VideoFixture>({
-  context: async (
-    { context: _ },
-    use: (context: BrowserContext) => Promise<void>,
-  ) => {
+  context: async ({ context: _ }, use) => {
     const browser = await chromium.launch();
     const context = await browser.newContext({
       recordVideo: {
@@ -24,10 +23,33 @@ export const test = base.extend<TestFixtures & VideoFixture>({
     await context.close();
     await browser.close();
   },
-  page: async ({ context }, use) => {
+  page: async ({ context }, use, testInfo) => {
     const page = await context.newPage();
 
     await use(page);
+
+    await page.close();
+
+    const video = page.video();
+
+    if (video) {
+      const videoPath = await video.path();
+      const customVideoName = `${testInfo.title.replace(/\s+/g, '_')}.webm`;
+      const newVideoPath = path.join('videos', customVideoName);
+
+      testInfo.attachments.push({
+        name: 'video',
+        path: newVideoPath,
+        contentType: 'video/webm',
+      });
+
+      await new Promise<void>((resolve) => {
+        fs.rename(videoPath, newVideoPath, (err) => {
+          if (err) console.error('Error renaming video:', err);
+          resolve();
+        });
+      });
+    }
   },
 });
 
