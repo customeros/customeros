d9d0bac2c0503950851f01bed059953784864140
Eduard Firut <edi@customeros.ai>
2024-10-07 13:29:42 +0300

    fix

--- packages/server/customer-os-common-module/service/flow_execution_service_it_test.go
+++ packages/server/customer-os-common-module/service/flow_execution_service_it_test.go
@@ -53,12 +53,12 @@ func TestFlowExecutionService_FlowExecution_1(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	_, err = CommonServices.FlowService.FlowSenderMerge(ctx, "internal-email", &neo4jentity.FlowSenderEntity{
+	_, err = CommonServices.FlowService.FlowSenderMerge(ctx, flow.Id, &neo4jentity.FlowSenderEntity{
 		UserId: &mailbox1,
 	})
 	require.NoError(t, err)
 
-	_, err = CommonServices.FlowService.FlowSenderMerge(ctx, "internal-email", &neo4jentity.FlowSenderEntity{
+	_, err = CommonServices.FlowService.FlowSenderMerge(ctx, flow.Id, &neo4jentity.FlowSenderEntity{
 		UserId: &mailbox2,
 	})
 	require.NoError(t, err)
@@ -86,8 +86,8 @@ func TestFlowExecutionService_FlowExecution_1(t *testing.T) {
 	require.Equal(t, 2, neo4jtest.GetCountOfNodes(ctx, driver, model.NodeLabelFlowParticipant))
 
 	//activate flow
-	_, err = CommonServices.FlowService.FlowChangeStatus(ctx, flow.Id, neo4jentity.FlowStatusActive)
-	require.NoError(t, err)
+	//_, err = CommonServices.FlowService.FlowChangeStatus(ctx, flow.Id, neo4jentity.FlowStatusActive)
+	//require.NoError(t, err)
 
 	//require.Equal(t, 2, neo4jtest.GetCountOfNodes(ctx, driver, model.NodeLabelFlowActionExecution))
 	//
