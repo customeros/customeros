3ab5b905d8b10ffa387fb3aa53a3421215d6a059
Mihai <163020214+umihai29@users.noreply.github.com>
2024-10-09 14:12:38 +0300

    fix stages for trevi (#9223)

--- packages/apps/frontera/src/routes/finder/src/components/Columns/opportunities/Cells/stage/Stage.tsx
+++ packages/apps/frontera/src/routes/finder/src/components/Columns/opportunities/Cells/stage/Stage.tsx
@@ -1,3 +1,4 @@
+import { RootStore } from '@store/root';
 import { observer } from 'mobx-react-lite';
 
 import { InternalStage } from '@graphql/types';
@@ -13,11 +14,38 @@ const internalStageOptions: Record<string, string> = {
   [InternalStage.ClosedWon]: 'Closed Won',
 };
 
+const makeStageLabels = (store: RootStore, preset?: string) => {
+  const cacheKey = 'cache' as keyof typeof makeStageLabels;
+
+  if (makeStageLabels?.[cacheKey]) {
+    return makeStageLabels[cacheKey] as Record<string, string>;
+  } else {
+    const tableViewDefOpportunity = store.tableViewDefs.getById(preset ?? '1');
+
+    const columns = tableViewDefOpportunity?.value?.columns;
+    const opportunityStages = columns?.reduce((acc, curr) => {
+      if (!curr.filter?.includes('STAGE')) return acc;
+
+      return {
+        ...acc,
+        ['STAGE' + curr.filter.split('STAGE')[1][0]]: curr.name,
+      };
+    }, {} as Record<string, string>);
+
+    // @ts-expect-error ignore
+    makeStageLabels[cacheKey] = opportunityStages as Record<string, string>;
+
+    return opportunityStages;
+  }
+};
+
 export const StageCell = observer(({ stage, id }: StageCellProps) => {
   const store = useStore();
 
-  const stages = store.settings.tenant.value?.opportunityStages;
+  const opportunityDef = store.tableViewDefs.opportunitiesPreset;
+
   const internalStage = store.opportunities.value.get(id)?.value?.internalStage;
+
   const isClosed = internalStage
     ? [InternalStage.ClosedWon, InternalStage.ClosedLost].includes(
         internalStage,
@@ -28,11 +56,12 @@ export const StageCell = observer(({ stage, id }: StageCellProps) => {
     return <p>{internalStageOptions[internalStage]}</p>;
   }
 
-  const stageObj = stages?.find((s) => s.value === stage);
+  const stageLabel = makeStageLabels(store, opportunityDef);
+  const stageObj = stageLabel?.[stage];
 
   if (!stageObj) {
     return <p className='text-gray-400'>Unknown</p>;
   }
 
-  return <p>{stageObj?.label}</p>;
+  return <p>{stageObj}</p>;
 });
