e7859668e64087ca59610de7bb5c6dd29dc7d15c
alexopenline <alex@openline.ai>
2024-10-16 14:35:32 +0300

    Set expiration of presigned urls for invoice PDFs

--- packages/server/customer-os-api/rest/billing/model.go
+++ packages/server/customer-os-api/rest/billing/model.go
@@ -5,56 +5,44 @@ import "time"
 // Invoice represents the structure of an invoice
 // @Description Invoice details
 type InvoiceResponse struct {
-	// Status indicates the result of the action
-	// Example: success
+	// Status indicates the result of the action.
 	Status string `json:"status,omitempty" example:"success"`
 
-	// Message provides additional information about the action
-	// Example: Invoices retrieved successfully
+	// Message provides additional information about the action.
 	Message string `json:"message,omitempty" example:"Invoices retrieved successfully"`
 
-	// ID is the unique identifier for the invoice, uuid format
-	// Example: 123e4567-e89b-12d3-a456-426614174000
+	// ID is the unique identifier for the invoice, uuid format.
 	ID string `json:"id" example:"123e4567-e89b-12d3-a456-426614174000"`
 
-	// Number represents the invoice number
-	// Example: ABC-12345
+	// Number represents the invoice number.
 	Number string `json:"number" example:"ABC-12345"`
 
-	// DueDate represents the date the invoice is due
-	// Example: 2024-12-01T00:00:00Z
+	// DueDate represents the date the invoice is due.
 	DueDate time.Time `json:"dueDate" example:"2024-12-01T00:00:00Z"`
 
-	// Status represents the payment status of the invoice
-	// Example: PAID
+	// Status represents the payment status of the invoice.
 	InvoiceStatus string `json:"invoiceStatus" example:"PAID"`
 
-	// Amount represents the total amount due for the invoice
-	// Example: 1500.50
+	// Amount represents the total amount due for the invoice.
 	Amount float64 `json:"amount" example:"1500.50"`
 
-	// Currency represents the currency used for the invoice
-	// Example: USD
+	// Currency represents the currency used for the invoice, e.g. USD, EUR, etc.
 	Currency string `json:"currency" example:"USD"`
 
-	// PaymentLink represents the URL where the invoice can be paid
-	// Example: https://example.com/payments/12345
+	// PaymentLink represents the URL where the invoice can be paid.
 	PaymentLink string `json:"paymentLink" example:"https://example.com/payments/12345"`
 
-	// PublicUrl represents the public URL where the PDF version of the invoice can be accessed
-	// Example: https://example.com/invoices/12345.pdf
+	// PublicUrl represents the public URL where the PDF version of the invoice can be accessed.
 	PublicUrl string `json:"publicUrl" example:"https://example.com/invoices/12345.pdf"`
 }
 
-// InvoicesResponse defines the response structure for multiple invoices in the response
+// InvoicesResponse defines the response structure for multiple invoices in the response.
 // @Description Response body for all invoices details
 type InvoicesResponse struct {
 	// Status indicates the result of the action
-	// Example: success
 	Status string `json:"status,omitempty" example:"success"`
 
 	// Message provides additional information about the action
-	// Example: Invoices retrieved successfully
 	Message string `json:"message,omitempty" example:"Invoices retrieved successfully"`
 
 	Invoices []InvoiceResponse `json:"invoices"`
