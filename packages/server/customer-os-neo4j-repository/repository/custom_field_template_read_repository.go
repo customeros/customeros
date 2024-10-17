9cc6d2e21df142d7f187ec92ed8956080ceada64
alexopenline <alex@openline.ai>
2024-10-17 15:29:29 +0300

    APIs to save and delete custom field template

--- packages/server/customer-os-neo4j-repository/repository/custom_field_template_read_repository.go
+++ packages/server/customer-os-neo4j-repository/repository/custom_field_template_read_repository.go
@@ -12,6 +12,7 @@ import (
 
 type CustomFieldTemplateReadRepository interface {
 	GetAllForTenant(ctx context.Context, tenant string) ([]*dbtype.Node, error)
+	GetById(ctx context.Context, tenant, id string) (*dbtype.Node, error)
 }
 
 type customFieldTemplateReadRepository struct {
@@ -59,5 +60,37 @@ func (r *customFieldTemplateReadRepository) GetAllForTenant(ctx context.Context,
 	}
 	span.LogFields(log.Int("result.count", len(result.([]*dbtype.Node))))
 	return result.([]*dbtype.Node), err
+}
 
+func (r *customFieldTemplateReadRepository) GetById(ctx context.Context, tenant, id string) (*dbtype.Node, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "CustomFieldTemplateReadRepository.GetById")
+	defer span.Finish()
+	tracing.TagComponentNeo4jRepository(span)
+	tracing.TagTenant(span, tenant)
+	tracing.TagEntity(span, id)
+
+	cypher := `MATCH (:Tenant {name:$tenant})<-[:CUSTOM_FIELD_TEMPLATE_BELONGS_TO_TENANT]-(cft:CustomFieldTemplate {id:$id}) RETURN cft`
+	params := map[string]any{
+		"tenant": tenant,
+		"id":     id,
+	}
+	span.LogFields(log.String("cypher", cypher))
+	tracing.LogObjectAsJson(span, "params", params)
+
+	session := r.prepareReadSession(ctx)
+	defer session.Close(ctx)
+
+	result, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		if queryResult, err := tx.Run(ctx, cypher, params); err != nil {
+			return nil, err
+		} else {
+			return utils.ExtractSingleRecordFirstValueAsNode(ctx, queryResult, err)
+		}
+	})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return nil, err
+	}
+	span.LogFields(log.Object("result", result))
+	return result.(*dbtype.Node), err
 }
