90767e4b85c15ee9aebed743ff4d90482e19b1f4
kasia <kasia@openline.ai>
2024-10-22 18:33:17 +0200

    COS-5309: Show scheduling status in table

--- packages/apps/frontera/src/routes/finder/src/components/Columns/flows/Cells/status/FlowStatusCell.tsx
+++ packages/apps/frontera/src/routes/finder/src/components/Columns/flows/Cells/status/FlowStatusCell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { observer } from 'mobx-react-lite';
 import { flowOptions } from '@finder/components/Columns/flows/utils.ts';
@@ -7,6 +7,7 @@ import { cn } from '@ui/utils/cn';
 import { FlowStatus } from '@graphql/types';
 import { Edit03 } from '@ui/media/icons/Edit03';
 import { useStore } from '@shared/hooks/useStore';
+import { TableCellTooltip } from '@ui/presentation/Table';
 import { SelectOption } from '@shared/types/SelectOptions';
 import { IconButton } from '@ui/form/IconButton/IconButton';
 import { Menu, MenuItem, MenuList, MenuButton } from '@ui/overlay/Menu/Menu';
@@ -20,6 +21,7 @@ export const FlowStatusCell = observer(
   ({ id, dataTest }: FlowStatusCellProps) => {
     const store = useStore();
     const [isEditing, setIsEditing] = useState(false);
+    const itemRef = useRef<HTMLDivElement>(null);
 
     const flowSequence = store.flows.value.get(id);
 
@@ -35,6 +37,27 @@ export const FlowStatusCell = observer(
       });
       setIsEditing(false);
     };
+
+    if (flowSequence?.value.status === FlowStatus.Scheduling) {
+      return (
+        <TableCellTooltip
+          hasArrow
+          align='start'
+          side='bottom'
+          targetRef={itemRef}
+          label={'Scheduling'}
+        >
+          <div ref={itemRef} className='flex overflow-hidden'>
+            <div
+              data-test={`${dataTest}-text-in-flows-table`}
+              className=' overflow-x-hidden overflow-ellipsis'
+            >
+              Scheduling
+            </div>
+          </div>
+        </TableCellTooltip>
+      );
+    }
 
     return (
       <div className='flex gap-1 items-center group/relationship'>
