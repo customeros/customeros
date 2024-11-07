8f995c1787055540ee67e81ddce94bcb6cfa7dde
alexopenline <alex@openline.ai>
2024-11-07 17:16:22 +0200

    Register mailbox in opensrs on tenant registration

--- packages/server/customer-os-common-module/service/registration_service.go
+++ packages/server/customer-os-common-module/service/registration_service.go
@@ -2,9 +2,13 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/common"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/model"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
 	"github.com/opentracing/opentracing-go"
+	"strings"
 )
 
 type RegistrationService interface {
@@ -33,14 +37,40 @@ func (s *registrationService) PrepareDefaultTenantSetup(ctx context.Context, log
 		tracing.TraceErr(span, err)
 		return err
 	}
-	//tenant := common.GetTenantFromContext(ctx)
+	tenant := common.GetTenantFromContext(ctx)
 
 	// Step 1 - Create default user
-	_, err = s.services.UserService.CreateTestUser(ctx, "Test", "Sender")
+	testUserId, err := s.services.UserService.CreateTestUser(ctx, "Test", "Sender")
+	span.LogKV("result.testUserId", testUserId)
 	if err != nil {
 		tracing.TraceErr(span, err)
 		return err
 	}
+
+	// Step 2 - Create test email node for the user
+	mailboxAddress := strings.ToLower(fmt.Sprintf("%s@%s", tenant, TEST_MAILBOX_DOMAIN))
+	testEmailId, err := s.services.EmailService.Merge(ctx, tenant, EmailFields{
+		Email: mailboxAddress,
+	}, &LinkWith{
+		Type: model.USER,
+		Id:   testUserId,
+	})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return err
+	}
+	span.LogKV("result.testEmailId", testEmailId)
+
+	// Step 3 - Register mailbox
+	password := utils.GenerateLowerAlpha(1) + utils.GenerateKey(11, false)
+	username := strings.ToLower(tenant)
+	forwardingTo := []string{fmt.Sprintf("bcc@%s.customeros.ai", strings.ToLower(tenant))}
+	err = s.services.MailboxService.AddMailbox(ctx, TEST_MAILBOX_DOMAIN, username, password, mailboxAddress, true, true, forwardingTo)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return err
+	}
+	span.LogKV("result.mailbox", mailboxAddress)
 
 	return nil
 }
