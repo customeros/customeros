ee178d95a9dfd60ec3f7b8dc1fc46872ff0d75e5
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-11-18 15:29:48 +0100

    COS-5948: fix: prevent hover state from affecting all duplicate contact names in flow command menu (#9831)

--- packages/apps/frontera/src/routes/src/components/CommandMenu/commands/flows/AddContactsToFlow.tsx
+++ packages/apps/frontera/src/routes/src/components/CommandMenu/commands/flows/AddContactsToFlow.tsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import Fuse from 'fuse.js';
 import { observer } from 'mobx-react-lite';
 import { FlowStore } from '@store/Flows/Flow.store';
-import { ContactStore } from '@store/Contacts/Contact.store.ts';
+import { ContactStore } from '@store/Contacts/Contact.store';
 
+import { Avatar } from '@ui/media/Avatar';
 import { Check } from '@ui/media/icons/Check';
+import { User03 } from '@ui/media/icons/User03';
 import { useStore } from '@shared/hooks/useStore';
 import { useModKey } from '@shared/hooks/useModKey';
-import { Command, CommandItem, CommandInput } from '@ui/overlay/CommandMenu';
+import { Command, CommandInput } from '@ui/overlay/CommandMenu';
 
 export const AddContactsToFlow = observer(() => {
-  const { contacts, ui, flows } = useStore();
+  const { contacts, ui, flows, organizations } = useStore();
   const [search, setSearch] = useState('');
 
   const context = ui.commandMenu.context;
@@ -49,7 +51,7 @@ export const AddContactsToFlow = observer(() => {
       <CommandInput
         value={search}
         onValueChange={setSearch}
-        placeholder='Search contacts...'
+        placeholder='Add contacts to flow...'
         label={`Flow - ${selectedFlow.value.name}`}
         onKeyDownCapture={(e) => {
           if (e.key === ' ') {
@@ -63,17 +65,43 @@ export const AddContactsToFlow = observer(() => {
           const isSelected = contactStore?.flowsIds?.includes(selectedFlowId);
 
           return (
-            <CommandItem
-              key={contactStore.id}
-              rightAccessory={isSelected ? <Check /> : undefined}
+            <Command.Item
+              key={contactStore.value.metadata.id}
               onSelect={() => {
                 handleSelect(contactStore as ContactStore);
               }}
+              value={
+                `${contactStore.name} ${contactStore.value.metadata.id}` ?? ''
+              }
             >
-              {contactStore.name?.trim()?.length
-                ? contactStore.name
-                : 'Unnamed'}
-            </CommandItem>
+              <div className='flex justify-between w-full'>
+                <div className='flex items-center'>
+                  <Avatar
+                    size='xxs'
+                    textSize='xxs'
+                    name={contactStore.name}
+                    icon={<User03 className='text-primary-700  ' />}
+                    src={
+                      contactStore?.value?.profilePhotoUrl
+                        ? contactStore.value.profilePhotoUrl
+                        : undefined
+                    }
+                  />
+                  <span className='ml-2 capitalize line-clamp-1 '>
+                    {contactStore.name?.length ? contactStore.name : 'Unnamed'}
+                  </span>
+
+                  {contactStore.organizationId && (
+                    <span className='ml-1.5 text-gray-500 line-clamp-1 max-w-[250px]'>
+                      ·{' '}
+                      {organizations.value.get(contactStore.organizationId)
+                        ?.value.name ?? ''}
+                    </span>
+                  )}
+                </div>
+                {isSelected && <Check />}
+              </div>
+            </Command.Item>
           );
         })}
       </Command.List>
