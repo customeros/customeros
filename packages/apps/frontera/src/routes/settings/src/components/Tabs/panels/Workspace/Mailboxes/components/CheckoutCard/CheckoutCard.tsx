a248007dc21d8742c15ae051c9ec5388ba79ce7a
mihai@customeros.ai <mihai@customeros.ai>
2024-11-18 12:51:20 +0200

    wip mailstack

--- packages/apps/frontera/src/routes/settings/src/components/Tabs/panels/Workspace/Mailboxes/components/CheckoutCard/CheckoutCard.tsx
+++ packages/apps/frontera/src/routes/settings/src/components/Tabs/panels/Workspace/Mailboxes/components/CheckoutCard/CheckoutCard.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
 import { useLocalStorage } from 'usehooks-ts';
 
+import { cn } from '@ui/utils/cn';
 import { IconButton } from '@ui/form/IconButton';
 import { MinusCircle } from '@ui/media/icons/MinusCircle';
+import { ChevronExpand } from '@ui/media/icons/ChevronExpand';
 import { Card, CardHeader, CardContent } from '@ui/presentation/Card/Card';
 
 export const CheckoutCard = () => {
+  const [expanded, setExpanded] = useState(false);
   const [storedBrandName, _setStoredBrandName] = useLocalStorage<string[]>(
     'brandName',
     [],
@@ -20,7 +25,7 @@ export const CheckoutCard = () => {
         <span>Base Boundle</span>
         <span>$199.99</span>
       </CardHeader>
-      <CardContent>
+      <CardContent className='p-0'>
         <span>{storedBrandName.length} of 5 domains</span>
 
         {storedBrandName.map((name, index) => (
@@ -53,22 +58,43 @@ export const CheckoutCard = () => {
           </div>
         ))}
         {storeUserName.length > 0 && (
-          <CardHeader>
+          <CardHeader className='mt-2 flex justify-between items-center'>
             <span className='font-medium'>
-              {storeUserName.length} of 10 mailboxes
+              {storeUserName.length * storedBrandName.length} of 10 mailboxes
             </span>
+            <IconButton
+              size='xxs'
+              variant='ghost'
+              aria-label='Expand'
+              icon={<ChevronExpand />}
+              onClick={() => setExpanded(!expanded)}
+            />
           </CardHeader>
         )}
-        {storeUserName.map((user, userIndex) =>
-          storedBrandName.map((brand, brandIndex) => (
-            <div
-              key={`${user}-${brand}-${userIndex}-${brandIndex}`}
-              className='flex items-center justify-between mt-1 bg-gray-100 rounded-[4px] py-1 px-2'
-            >
-              <span className='text-sm'>{`${user}@${brand}`}</span>
-            </div>
-          )),
-        )}
+        {expanded &&
+          storeUserName.map((user, userIndex) =>
+            storedBrandName.map((brand, brandIndex) => {
+              const isLastTwo =
+                userIndex === storeUserName.length - 1 &&
+                brandIndex >= storedBrandName.length - 2;
+
+              return (
+                <div
+                  key={`${user}-${brand}-${userIndex}-${brandIndex}`}
+                  className={cn(
+                    'flex items-center justify-between mt-1',
+                    !isLastTwo &&
+                      (userIndex * storedBrandName.length + brandIndex) % 2 ===
+                        1
+                      ? 'mb-3'
+                      : '',
+                  )}
+                >
+                  <span className='text-sm'>{`${user}@${brand}`}</span>
+                </div>
+              );
+            }),
+          )}
       </CardContent>
     </Card>
   );
