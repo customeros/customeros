a57097d4f3db67fa6973532c42b6a864dc33ba7a
alexopenline <alex@openline.ai>
2024-11-20 18:19:24 +0200

    fix raw gmail

--- packages/runner/sync-gmail-raw/repository/repositories.go
+++ packages/runner/sync-gmail-raw/repository/repositories.go
@@ -2,40 +2,24 @@ package repository
 
 import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
-	"github.com/openline-ai/openline-customer-os/packages/runner/sync-gmail-raw/entity"
 	"gorm.io/gorm"
 )
 
 type Repositories struct {
-	TenantRepository TenantRepository
-	UserRepository   UserRepository
-	EmailRepository  EmailRepository
-
+	TenantRepository              TenantRepository
+	UserRepository                UserRepository
+	EmailRepository               EmailRepository
 	UserGCalImportStateRepository UserGCalImportStateRepository
-
-	RawCalendarEventRepository RawCalendarEventRepository
+	RawCalendarEventRepository    RawCalendarEventRepository
 }
 
 func InitRepos(driver *neo4j.DriverWithContext, gormDb *gorm.DB) *Repositories {
 	repositories := Repositories{
-		TenantRepository: NewTenantRepository(driver),
-		UserRepository:   NewUserRepository(driver),
-		EmailRepository:  NewEmailRepository(driver),
-
+		TenantRepository:              NewTenantRepository(driver),
+		UserRepository:                NewUserRepository(driver),
+		EmailRepository:               NewEmailRepository(driver),
 		UserGCalImportStateRepository: NewUserGCalImportStateRepository(gormDb),
-
-		RawCalendarEventRepository: NewRawCalendarEventRepository(gormDb),
-	}
-
-	var err error
-
-	err = gormDb.AutoMigrate(&entity.UserGCalImportState{})
-	if err != nil {
-		panic(err)
-	}
-	err = gormDb.AutoMigrate(&entity.RawCalendarEvent{})
-	if err != nil {
-		panic(err)
+		RawCalendarEventRepository:    NewRawCalendarEventRepository(gormDb),
 	}
 
 	return &repositories
