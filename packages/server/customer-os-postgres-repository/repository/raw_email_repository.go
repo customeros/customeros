dc5f7b21d600cbd49e0f31b6eefd875d1d9b0c39
mattbr0wn <matt@openline.ai>
2024-11-21 17:46:51 +0000

    fix db nulls

--- packages/server/customer-os-postgres-repository/repository/raw_email_repository.go
+++ packages/server/customer-os-postgres-repository/repository/raw_email_repository.go
@@ -157,10 +157,14 @@ func (repo *rawEmailRepositoryImpl) UpdateRawEmailTable(id uuid.UUID, dbUpdateRe
 
 	tx.Update("status", dbUpdateRecord.EmailProcessingStatus)
 	tx.Update("reason", dbUpdateRecord.Reason)
-	tx.Update("error", fmt.Sprintf("%v", dbUpdateRecord.Error))
-	jsonBytes, _ := json.Marshal(dbUpdateRecord.BouncedEmails)
-	bounced := string(jsonBytes)
-	tx.Update("bounced_emails", bounced)
+	if dbUpdateRecord.Error != nil {
+		tx.Update("error", fmt.Sprintf("%v", dbUpdateRecord.Error))
+	}
+	if len(*dbUpdateRecord.BouncedEmails) > 0 {
+		jsonBytes, _ := json.Marshal(dbUpdateRecord.BouncedEmails)
+		bounced := string(jsonBytes)
+		tx.Update("bounced_emails", bounced)
+	}
 
 	err := tx.Error
 	if err != nil {
