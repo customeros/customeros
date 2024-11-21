2b45eb60ee91e3f4ed631ceffdef20daaa6b163f
kasia <kasia@openline.ai>
2024-11-21 10:17:18 +0100

    COS-5993: Select flow name on focus

--- packages/apps/frontera/src/routes/flow-editor/src/components/header/HeaderInputName.tsx
+++ packages/apps/frontera/src/routes/flow-editor/src/components/header/HeaderInputName.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 
 import { observer } from 'mobx-react-lite';
@@ -16,24 +16,43 @@ export const HeaderInputName = observer(() => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const [name, setName] = useState(flow?.value?.name ?? '');
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const showFinder = searchParams.get('show') === 'finder';
+
+  useEffect(() => {
+    if (showFinder && inputRef.current) {
+      inputRef.current?.blur();
+      window.getSelection()?.removeAllRanges(); // needed to remove the highlighted text
+    }
+  }, [showFinder]);
 
   return (
     <>
       <ResizableInput
-        autoFocus
+        ref={inputRef}
         variant='unstyled'
         readOnly={showFinder}
         data-test='flows-flow-name-input'
-        onClick={() => (showFinder ? navigate(-1) : null)}
         value={store.flows.isLoading ? 'Loading flow…' : name}
         onChange={(e) => {
           setName(e.target.value);
         }}
+        onFocus={(e) => {
+          if (!showFinder) {
+            e.target.select();
+          }
+        }}
         className={cn({
           'text-gray-500 cursor-pointer hover:text-gray-700': showFinder,
         })}
+        onClick={(e) => {
+          if (showFinder) {
+            e.preventDefault();
+
+            navigate(-1);
+          }
+        }}
         onBlur={() => {
           flow?.update((value) => {
             value.name = name;
