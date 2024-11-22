08834ec59fed783d30666041172610947a1ea8fe
mattbr0wn <matt@openline.ai>
2024-11-22 17:31:40 +0000

    refactor bulk contact

--- packages/server/customer-os-api/rest/contact.go
+++ packages/server/customer-os-api/rest/contact.go
@@ -1,10 +1,14 @@
 package rest
 
 import (
+	"context"
 	"encoding/csv"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
-	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/constants"
-	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/service"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/common"
 	commonModel "github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/model"
 	commonservice "github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/service"
@@ -12,130 +16,174 @@ import (
 	neo4jentity "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/entity"
 	neo4jmodel "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/model"
 	neo4jrepo "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/repository"
-	tracingLog "github.com/opentracing/opentracing-go/log"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
 	"github.com/pkg/errors"
-	"io"
-	"net/http"
-	"strings"
+
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/constants"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/service"
 )
+
+type ContactRecord struct {
+	Email       string
+	LinkedInURL string
+}
 
 func CreateContactsFromCsvUpload(services *service.Services) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, span := tracing.StartHttpServerTracerSpanWithHeader(c.Request.Context(), "CreateContactsFromCsvUpload", c.Request.Header)
 		defer span.Finish()
 		tracing.TagComponentRest(span)
-		tracing.TagTenant(span, common.GetTenantFromContext(ctx))
 
-		tenant := common.GetTenantFromContext(ctx)
-		// if tenant missing return auth error
+		tenant := validateTenant(c, ctx, span)
 		if tenant == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "error", "message": "API key invalid or expired"})
-			span.LogFields(tracingLog.String("result", "Missing tenant in context"))
 			return
 		}
 
-		// Parse the uploaded CSV file
-		file, header, err := c.Request.FormFile("file")
+		file, err := validateAndOpenFile(c)
 		if err != nil {
-			tracing.TraceErr(span, errors.Wrap(err, "failed to insert records"))
-			c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to parse file"})
+			tracing.TraceErr(span, err)
 			return
 		}
 		defer file.Close()
 
-		// Validate file type
-		if header.Header.Get("Content-Type") != "text/csv" && !strings.HasSuffix(header.Filename, ".csv") {
-			c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Invalid file type"})
+		reader := csv.NewReader(file)
+		if err := validateHeaders(c, reader); err != nil {
 			return
 		}
 
-		// Parse the CSV file
-		reader := csv.NewReader(file)
-		headers, err := reader.Read()
+		processRecords(c, ctx, span, reader, services, tenant)
+	}
+}
+
+func validateTenant(c *gin.Context, ctx context.Context, span opentracing.Span) string {
+	tenant := common.GetTenantFromContext(ctx)
+	tracing.TagTenant(span, tenant)
+
+	if tenant == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "error", "message": "API key invalid or expired"})
+		return ""
+	}
+	return tenant
+}
+
+func validateAndOpenFile(c *gin.Context) (multipart.File, error) {
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to parse file"})
+		return nil, err
+	}
+
+	if header.Header.Get("Content-Type") != "text/csv" && !strings.HasSuffix(header.Filename, ".csv") {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Invalid file type"})
+		return nil, errors.New("invalid file type")
+	}
+
+	return file, nil
+}
+
+func validateHeaders(c *gin.Context, reader *csv.Reader) error {
+	headers, err := reader.Read()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to read file"})
+		return err
+	}
+
+	if headers[0] != "email" && headers[1] != "linkedin_url" {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Invalid headers, must be 'email' and 'linkedin_url'"})
+		return errors.New("invalid headers")
+	}
+	return nil
+}
+
+func processRecords(c *gin.Context, ctx context.Context, span opentracing.Span, reader *csv.Reader, services *service.Services, tenant string) {
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to read file"})
 			return
 		}
 
-		if headers[0] != "email" && headers[1] != "linkedin_url" {
-			c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Invalid headers, must be 'email' and 'linkedin_url'"})
-			return
+		contactRecord := ContactRecord{
+			Email:       record[0],
+			LinkedInURL: record[1],
 		}
 
-		// Read and validate each email
-		for {
-			record, err := reader.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Failed to read file"})
-				return
-			}
-
-			inputEmail := record[0]
-			inputSocialUrl := record[1]
-
-			if inputEmail == "" && inputSocialUrl == "" {
-				continue
-			}
-
-			// if email provided, check if email exists in db
-			var emailEntity *neo4jentity.EmailEntity
-			if inputEmail != "" {
-				emailEntity, err = services.EmailService.GetByEmailAddress(ctx, inputEmail)
-				if err != nil {
-					span.LogFields(tracingLog.String("result", "Failed to get email entity"))
-					c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to get email entity"})
-					return
-				}
-			}
-
-			emailId := ""
-			contactId := ""
-
-			// if email exists, get contact id associated with email
-			if emailEntity != nil {
-				emailId = emailEntity.Id
-				contactsWithEmail, err := services.ContactService.GetContactsForEmails(ctx, []string{emailEntity.Id})
-				if err != nil {
-					span.LogFields(tracingLog.String("result", "Failed to get contacts for email"))
-					c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to get contacts for email"})
-					return
-				}
-
-				if contactsWithEmail != nil && len(*contactsWithEmail) > 0 {
-					contactId = (*contactsWithEmail)[0].Id
-				}
-			}
-
-			// if contact not exists, create new contact
-			if contactId == "" {
-				contactId, err = services.CommonServices.ContactService.Save(ctx, nil, neo4jrepo.ContactFields{}, inputSocialUrl, neo4jmodel.ExternalSystem{})
-				if err != nil {
-					tracing.TraceErr(span, errors.Wrap(err, "failed to save contact"))
-					continue
-				}
-			}
-
-			// associate email with created contact if email address provided
-			if emailId == "" && inputEmail != "" {
-				_, err := services.CommonServices.EmailService.Merge(ctx, tenant,
-					commonservice.EmailFields{
-						Email:     inputEmail,
-						Source:    neo4jentity.DataSourceOpenline,
-						AppSource: constants.AppSourceCustomerOsApiRest,
-					}, &commonservice.LinkWith{
-						Type: commonModel.CONTACT,
-						Id:   contactId,
-					})
-				if err != nil {
-					tracing.TraceErr(span, err)
-					span.LogFields(tracingLog.String("result", "Failed to upsert email"))
-					c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to upsert email"})
-					return
-				}
-			}
+		if contactRecord.Email == "" && contactRecord.LinkedInURL == "" {
+			continue
 		}
+
+		processContact(c, ctx, span, services, tenant, contactRecord)
+	}
+}
+
+func processContact(c *gin.Context, ctx context.Context, span opentracing.Span, services *service.Services, tenant string, record ContactRecord) {
+	emailEntity, contactId := findExistingContact(c, ctx, span, services, record.Email)
+	if emailEntity == nil && contactId == "" {
+		contactId = createNewContact(c, ctx, span, services, record.LinkedInURL)
+	}
+
+	if emailEntity == nil && record.Email != "" {
+		associateEmail(c, ctx, span, services, tenant, record.Email, contactId)
+	}
+}
+
+func findExistingContact(c *gin.Context, ctx context.Context, span opentracing.Span, services *service.Services, email string) (*neo4jentity.EmailEntity, string) {
+	if email == "" {
+		return nil, ""
+	}
+
+	emailEntity, err := services.EmailService.GetByEmailAddress(ctx, email)
+	if err != nil {
+		span.LogFields(log.String("result", "Failed to get email entity"))
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to get email entity"})
+		return nil, ""
+	}
+
+	if emailEntity == nil {
+		return nil, ""
+	}
+
+	contacts, err := services.ContactService.GetContactsForEmails(ctx, []string{emailEntity.Id})
+	if err != nil {
+		span.LogFields(log.String("result", "Failed to get contacts for email"))
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to get contacts for email"})
+		return nil, ""
+	}
+
+	if contacts != nil && len(*contacts) > 0 {
+		return emailEntity, (*contacts)[0].Id
+	}
+
+	return emailEntity, ""
+}
+
+func createNewContact(c *gin.Context, ctx context.Context, span opentracing.Span, services *service.Services, linkedInURL string) string {
+	contactId, err := services.CommonServices.ContactService.Save(ctx, nil, neo4jrepo.ContactFields{}, linkedInURL, neo4jmodel.ExternalSystem{})
+	if err != nil {
+		tracing.TraceErr(span, errors.Wrap(err, "failed to save contact"))
+		return ""
+	}
+	return contactId
+}
+
+func associateEmail(c *gin.Context, ctx context.Context, span opentracing.Span, services *service.Services, tenant, email, contactId string) {
+	_, err := services.CommonServices.EmailService.Merge(ctx, tenant,
+		commonservice.EmailFields{
+			Email:     email,
+			Source:    neo4jentity.DataSourceOpenline,
+			AppSource: constants.AppSourceCustomerOsApiRest,
+		}, &commonservice.LinkWith{
+			Type: commonModel.CONTACT,
+			Id:   contactId,
+		})
+	if err != nil {
+		tracing.TraceErr(span, err)
+		span.LogFields(log.String("result", "Failed to upsert email"))
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to upsert email"})
+		return
 	}
 }
