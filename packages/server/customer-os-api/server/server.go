eaa1bfe5b7556a219dc4e30a8d5fba0beb515388
alexopenline <alex@openline.ai>
2024-11-26 20:44:04 +0200

    fix mailstask config

--- packages/server/customer-os-api/server/server.go
+++ packages/server/customer-os-api/server/server.go
@@ -105,8 +105,10 @@ func (server *server) Run(parentCtx context.Context) error {
 	commonServices := commonservice.InitServices(&commonConfig.GlobalConfig{
 		RabbitMQConfig: &server.cfg.RabbitMQConfig,
 		ExternalServices: commonConfig.ExternalServices{
-			OpenSRSConfig: server.cfg.ExternalServices.OpenSRSConfig,
-			StripeConfig:  server.cfg.ExternalServices.StripeConfig,
+			OpenSRSConfig:   server.cfg.ExternalServices.OpenSRSConfig,
+			StripeConfig:    server.cfg.ExternalServices.StripeConfig,
+			NamecheapConfig: server.cfg.ExternalServices.NamecheapConfig,
+			PostmarkConfig:  server.cfg.ExternalServices.PostmarkConfig,
 		},
 	}, db.GormDB, &neo4jDriver, server.cfg.Neo4j.Database, grpcContainer, server.log)
 
