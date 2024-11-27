e962d89a35cc3ecf8855c7764ab7e4f9ecb2e495
kasia <kasia@openline.ai>
2024-11-27 16:10:56 +0100

    Comment copy fix

--- packages/server/realtime/lib/realtime_web/channels/markdown_events_channel.ex
+++ packages/server/realtime/lib/realtime_web/channels/markdown_events_channel.ex
@@ -1,6 +1,6 @@
 defmodule RealtimeWeb.MarkdownEventsChannel do
   @moduledoc """
-  This Channel broadcasts sync events to all LogEntry entity subscribers.
+  This Channel broadcasts sync events to all MarkdownEvents entity subscribers.
   """
   use RealtimeWeb.EntityChannelMacro, "MarkdownEvents"
 end
