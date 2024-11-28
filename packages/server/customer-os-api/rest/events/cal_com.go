008ff52b60a4a30f51bdadece7136e197e871547
mattbr0wn <matt@openline.ai>
2024-11-28 13:14:02 +0000

    stash

--- packages/server/customer-os-api/rest/events/cal_com.go
+++ packages/server/customer-os-api/rest/events/cal_com.go
@@ -1,11 +1,15 @@
 package events
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
 	commontracing "github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
+	"github.com/pkg/errors"
 
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/rest"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/service"
@@ -17,14 +21,38 @@ func CalDotCom(services *service.Services) gin.HandlerFunc {
 		defer span.Finish()
 		commontracing.TagComponentRest(span)
 
-		tenant := rest.ValidateTenant(c, ctx, span)
-		if tenant == "" {
-			rest.SendError(c, span, http.StatusForbidden, rest.ErrForbidden)
+		if !strings.HasPrefix(c.ContentType(), "application/json") {
+			rest.SendError(c, span, http.StatusBadRequest, rest.ErrUnsupportedContentType)
 			return
 		}
 
-		if !strings.HasPrefix(c.ContentType(), "application/json") {
-			rest.SendError(c, span, http.StatusBadRequest, rest.ErrUnsupportedContentType)
+		// Read the raw body
+		body, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			rest.SendError(c, span, http.StatusBadRequest, rest.ErrBadRequest.WithMessage("Unable to read message body"))
+			return
+		}
+		// Important: Restore the body for later use
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+
+		// Get the signature from header
+		signature := c.GetHeader("X-Cal-Signature-256")
+		if signature == "" {
+			rest.SendError(c, span, http.StatusBadRequest, rest.ErrBadRequest.WithMessage("Missing signature header"))
+			return
+		}
+
+		// determine tenant, lookup api key
+		secretKey := "CAL_WEBHOOK_SECRET"
+
+		valid, err := VerifyCalWebhookSignature(body, signature, secretKey)
+		if err != nil {
+			rest.SendError(c, span, http.StatusInternalServerError, rest.ErrInternalServer.WithMessage("Unable to verify message payloar"))
+			return
+		}
+
+		if !valid {
+			rest.SendError(c, span, http.StatusUnauthorized, rest.ErrUnauthorized)
 			return
 		}
 
@@ -33,13 +61,51 @@ func CalDotCom(services *service.Services) gin.HandlerFunc {
 			ServiceContext: &ctx,
 			Span:           span,
 			Services:       services,
-			Tenant:         tenant,
 		}
 
 		handleCalDotComEvent(httpContext)
 	}
 }
 
+func VerifyCalWebhookSignature(payload []byte, signature string, secretKey string) (bool, error) {
+	if signature == "" || secretKey == "" {
+		return false, fmt.Errorf("signature or secret key is empty")
+	}
+
+	h := hmac.New(sha256.New, []byte(secretKey))
+	_, err := h.Write(payload)
+	if err != nil {
+		return false, fmt.Errorf("failed to write payload to HMAC: %w", err)
+	}
+
+	calculatedHash := hex.EncodeToString(h.Sum(nil))
+	expectedSignature := "sha256=" + calculatedHash
+	return hmac.Equal([]byte(expectedSignature), []byte(signature)), nil
+}
+
 func handleCalDotComEvent(ctx rest.HTTPContext) {
+	var webhook CalDotComPayload
+	err := ctx.GinContext.BindJSON(&webhook)
+	if err != nil {
+		rest.SendError(ctx.GinContext, ctx.Span, http.StatusBadRequest, rest.ErrBadRequest.WithMessage("Unable to parse cal.com payload"))
+		tracing.TraceErr(ctx.Span, errors.Wrap(err, "Unable to parse cal.com payload"))
+		return
+
+	}
+
+	switch webhook.TriggerEvent {
+	case "BOOKING_CREATED":
+		err := processBookingCreatedEvent(ctx, webhook)
+		// create contacts
+	case "BOOKING_RESCHEDULED":
+		// todo -- Handle reschedule
+	case "BOOKING_CANCELLED":
+		// todo -- Handle cancellation
+	}
+
 	return
+}
+
+func processBookingCreatedEvent(ctx rest.HTTPContext, payload CalDotComPayload) error {
+	return nil
 }
---
+++ packages/server/customer-os-api/rest/events/cal_com_model.go
@@ -0,0 +1,122 @@
+package events
+
+import "time"
+
+// WebhookPayload represents the root structure of all Cal.com webhooks
+type CalDotComPayload struct {
+	TriggerEvent string    `json:"triggerEvent"`
+	CreatedAt    time.Time `json:"createdAt"`
+	Payload      Booking   `json:"payload"`
+}
+
+// Booking represents the main booking data structure
+type Booking struct {
+	BookerURL           string              `json:"bookerUrl"`
+	Type                string              `json:"type"`
+	Title               string              `json:"title"`
+	Description         string              `json:"description"`
+	AdditionalNotes     string              `json:"additionalNotes,omitempty"`
+	CustomInputs        map[string]string   `json:"customInputs"`
+	StartTime           time.Time           `json:"startTime"`
+	EndTime             time.Time           `json:"endTime"`
+	Organizer           Organizer           `json:"organizer"`
+	Responses           BookingResponses    `json:"responses"`
+	UserFieldsResponses UserFieldsResponses `json:"userFieldsResponses"`
+	Attendees           []Attendee          `json:"attendees"`
+	Location            string              `json:"location"`
+	DestinationCalendar []Calendar          `json:"destinationCalendar"`
+	EventTypeID         int                 `json:"eventTypeId"`
+	UID                 string              `json:"uid"`
+	BookingID           int                 `json:"bookingId"`
+	EventTitle          string              `json:"eventTitle"`
+	EventDescription    string              `json:"eventDescription"`
+	Price               int                 `json:"price"`
+	Currency            string              `json:"currency"`
+	Status              string              `json:"status"`
+	Length              int                 `json:"length"`
+	ICalUID             string              `json:"iCalUID"`
+	ICalSequence        int                 `json:"iCalSequence"`
+
+	// Fields specific to cancellation
+	CancellationReason string `json:"cancellationReason,omitempty"`
+	CancelledBy        string `json:"cancelledBy,omitempty"`
+
+	// Fields specific to rescheduling
+	RescheduleID        int       `json:"rescheduleId,omitempty"`
+	RescheduleUID       string    `json:"rescheduleUid,omitempty"`
+	RescheduleStartTime time.Time `json:"rescheduleStartTime,omitempty"`
+	RescheduleEndTime   time.Time `json:"rescheduleEndTime,omitempty"`
+	RescheduledBy       string    `json:"rescheduledBy,omitempty"`
+}
+
+type Organizer struct {
+	ID         int      `json:"id"`
+	Name       string   `json:"name"`
+	Email      string   `json:"email"`
+	Username   string   `json:"username"`
+	TimeZone   string   `json:"timeZone"`
+	Language   Language `json:"language"`
+	TimeFormat string   `json:"timeFormat"`
+	UTCOffset  int      `json:"utcOffset"`
+}
+
+type Attendee struct {
+	Email     string   `json:"email"`
+	Name      string   `json:"name"`
+	FirstName string   `json:"firstName"`
+	LastName  string   `json:"lastName"`
+	TimeZone  string   `json:"timeZone"`
+	Language  Language `json:"language"`
+	UTCOffset int      `json:"utcOffset"`
+}
+
+type Language struct {
+	Locale string `json:"locale"`
+}
+
+type Calendar struct {
+	ID           int    `json:"id"`
+	Integration  string `json:"integration"`
+	ExternalID   string `json:"externalId"`
+	PrimaryEmail string `json:"primaryEmail"`
+	UserID       int    `json:"userId"`
+	EventTypeID  *int   `json:"eventTypeId"`
+	CredentialID int    `json:"credentialId"`
+}
+
+type BookingResponses struct {
+	Name             ResponseField `json:"name"`
+	Email            ResponseField `json:"email"`
+	Notes            ResponseField `json:"notes,omitempty"`
+	Guests           GuestsField   `json:"guests"`
+	Location         LocationField `json:"location"`
+	PhoneNumber      ResponseField `json:"phone-number"`
+	RescheduleReason ResponseField `json:"rescheduleReason,omitempty"`
+}
+
+type ResponseField struct {
+	Label    string `json:"label"`
+	Value    string `json:"value"`
+	IsHidden bool   `json:"isHidden,omitempty"`
+}
+
+type LocationField struct {
+	Label    string        `json:"label"`
+	Value    LocationValue `json:"value"`
+	IsHidden bool          `json:"isHidden,omitempty"`
+}
+
+type LocationValue struct {
+	Value       string `json:"value"`
+	OptionValue string `json:"optionValue"`
+}
+
+type GuestsField struct {
+	Label    string   `json:"label"`
+	Value    []string `json:"value"`
+	IsHidden bool     `json:"isHidden,omitempty"`
+}
+
+type UserFieldsResponses struct {
+	PhoneNumber ResponseField `json:"phone-number"`
+}
