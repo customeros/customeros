8c914e6eb8e0bb792f80b715a0d9602f789b5ccf
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-11-28 16:23:45 +0100

    COS-6046: Fixed: Copy/paste variables are not identified as variables (#10032)

--- packages/apps/frontera/src/ui/form/Editor/plugins/PastePlugin.tsx
+++ packages/apps/frontera/src/ui/form/Editor/plugins/PastePlugin.tsx
@@ -8,27 +8,10 @@ import {
   PASTE_COMMAND,
   $createTextNode,
   $isRangeSelection,
-  $createParagraphNode,
 } from 'lexical';
 
 import { isValidUrl } from '@utils/urlValidation';
-
-const ALLOWED_TAGS = [
-  'ul',
-  'ol',
-  'p',
-  'div',
-  'span',
-  'a',
-  'strong',
-  'em',
-  's',
-  'blockquote',
-  'i',
-  'b',
-  'u',
-  'li', // Additional text formatting tags
-];
+import { convertPlainTextToHtml } from '@ui/form/Editor/utils/convertPlainTextToHtml';
 
 export function LinkPastePlugin() {
   const [editor] = useLexicalComposerContext();
@@ -57,58 +40,14 @@ export function LinkPastePlugin() {
             if (htmlData) {
               const parser = new DOMParser();
               const doc = parser.parseFromString(htmlData, 'text/html');
-
-              const filterNode = (node: Node): DocumentFragment => {
-                const fragment = document.createDocumentFragment();
-
-                if (node.nodeType === Node.TEXT_NODE) {
-                  fragment.appendChild(node.cloneNode(true));
-                } else if (node.nodeType === Node.ELEMENT_NODE) {
-                  const element = node as HTMLElement;
-                  const tagName = element.tagName.toLowerCase();
-
-                  if (ALLOWED_TAGS.includes(tagName)) {
-                    const newElement = document.createElement(tagName);
-
-                    if (tagName === 'a') {
-                      const href = element.getAttribute('href');
-
-                      if (href) newElement.setAttribute('href', href);
-                    }
-
-                    Array.from(element.childNodes).forEach((child) => {
-                      newElement.appendChild(filterNode(child));
-                    });
-
-                    fragment.appendChild(newElement);
-                  } else {
-                    Array.from(element.childNodes).forEach((child) => {
-                      fragment.appendChild(filterNode(child));
-                    });
-                  }
-                }
-
-                return fragment;
-              };
-
-              const filteredBody = filterNode(doc.body);
-              const newDoc = document.implementation.createHTMLDocument();
-
-              newDoc.body.appendChild(filteredBody);
-
-              const nodes = $generateNodesFromDOM(editor, newDoc);
+              const nodes = $generateNodesFromDOM(editor, doc);
 
               selection.insertNodes(nodes);
             } else {
-              const lines = pastedData.split('\n');
-              const nodes = lines.map((line) => {
-                const paragraphNode = $createParagraphNode();
-                const textNode = $createTextNode(line);
-
-                paragraphNode.append(textNode);
-
-                return paragraphNode;
-              });
+              const htmlData = convertPlainTextToHtml(pastedData);
+              const parser = new DOMParser();
+              const doc = parser.parseFromString(htmlData, 'text/html');
+              const nodes = $generateNodesFromDOM(editor, doc);
 
               selection.insertNodes(nodes);
             }
