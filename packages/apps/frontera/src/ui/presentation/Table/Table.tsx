87d6dab802b468852c6d98e3d091ec8be51394e6
Mihai <163020214+umihai29@users.noreply.github.com>
2024-11-29 14:22:16 +0200

    increase the height of the scrollbars (#10060)

--- packages/apps/frontera/src/ui/presentation/Table/Table.tsx
+++ packages/apps/frontera/src/ui/presentation/Table/Table.tsx
@@ -737,7 +737,7 @@ const TContent = forwardRef<HTMLDivElement, TContentProps>(
     const borderColorDynamic = borderColor ? borderColor : 'gray.200';
     const heightDynamic = height ? height : 'calc(100vh - 88px)';
     const scrollBarStyle =
-      '[&::-webkit-scrollbar-track]:size-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-gray-500 [&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar]:size-2 [&::-webkit-scrollbar]:bg-transparent';
+      '[&::-webkit-scrollbar-track]:size-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-gray-500 [&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar]:size-3 [&::-webkit-scrollbar]:bg-transparent';
 
     const hideCursor = () => {
       if (_ref?.current) {
