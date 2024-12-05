5fd51f91ddb4e6ee19a357c7a31ee131ebfc9bcb
kasia <kasia@openline.ai>
2024-12-05 13:24:12 +0100

    Attach generated gql files

---
+++ packages/apps/frontera/src/store/GlobalCache/__service__/updateUserOnboardingDetails.generated.ts
@@ -0,0 +1,29 @@
+import * as Types from '../../../routes/src/types/__generated__/graphql.types';
+
+export type UserUpdateOnboardingDetailsMutationVariables = Types.Exact<{
+  input: Types.UserOnboardingDetailsInput;
+}>;
+
+export type UserUpdateOnboardingDetailsMutation = {
+  __typename?: 'Mutation';
+  user_UpdateOnboardingDetails: {
+    __typename?: 'User';
+    id: string;
+    firstName: string;
+    lastName: string;
+    emails?: Array<{
+      __typename?: 'Email';
+      email?: string | null;
+      rawEmail?: string | null;
+      primary: boolean;
+    }> | null;
+    onboarding: {
+      __typename?: 'UserOnboardingDetails';
+      showOnboardingPage: boolean;
+      onboardingInboundStepCompleted: boolean;
+      onboardingOutboundStepCompleted: boolean;
+      onboardingCrmStepCompleted: boolean;
+      onboardingMailstackStepCompleted: boolean;
+    };
+  };
+};
