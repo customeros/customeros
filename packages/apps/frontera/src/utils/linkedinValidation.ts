77b0fea30232e0467c00a6194ec04af0b53dd0b1
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-12-05 14:09:06 +0100

    COS-6116: Update linkedin validation regexp (#10123)

--- packages/apps/frontera/src/utils/linkedinValidation.ts
+++ packages/apps/frontera/src/utils/linkedinValidation.ts
@@ -1,6 +1,6 @@
 export function validLinkedInProfileUrl(url: string): boolean {
   const re =
-    /(https?:\/\/(www.)|(www.))?linkedin.com\/(mwlite\/|m\/)?in\/[a-zA-Z0-9_.-]+\/?/;
+    /(https?:\/\/(www\.)|(www\.))?linkedin\.com\/(mwlite\/|m\/)?in\/[a-zA-Z0-9-]+\/?$/;
 
   return re.test(url);
 }
