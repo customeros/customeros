eef94e0dc776a35a4172af9ab7531dbdf5d09505
alexopenline <alex@openline.ai>
2024-12-07 11:39:29 +0200

    minor refactoring

--- packages/runner/customer-os-data-upkeeper/service/mailstack_service.go
+++ packages/runner/customer-os-data-upkeeper/service/mailstack_service.go
@@ -37,37 +37,25 @@ func (s *mailstackService) CheckMailstackDomainReputation() {
 	defer span.Finish()
 	tracing.TagComponentCronJob(span)
 
-	for {
-		select {
-		case <-ctx.Done():
-			s.log.Infof("Context cancelled, stopping")
-			return
-		default:
-			// continue as normal
-		}
-
-		// get mailstack domains from database
-		domains, err := s.commonServices.MailstackService.GetAllMailstackDomains(ctx)
-		if err != nil {
-			err = errors.Wrap(err, "Unable to get mailstack domains from db")
-			tracing.TraceErr(span, err)
-			return
-		}
-
-		// no record
-		if len(domains) == 0 {
-			return
-		}
-
-		for domain, tenant := range domains {
-			_, err := s.commonServices.MailboxService.ReputationScore(ctx, domain, tenant)
-			if err != nil {
-				tracing.TraceErr(span, errors.Wrap(err, "Error getting mailbox reputation score"))
-				s.log.Errorf("Error getting mailbox reputation score for %s: %s", domain, err.Error())
-			}
-		}
-
-		// force exit after single iteration
+	// get all mailstack domains from database
+	domains, err := s.commonServices.MailstackService.GetAllMailstackDomains(ctx)
+	if err != nil {
+		err = errors.Wrap(err, "Unable to get mailstack domains from db")
+		tracing.TraceErr(span, err)
 		return
 	}
+
+	// no record
+	if len(domains) == 0 {
+		return
+	}
+
+	for domain, tenant := range domains {
+		_, err := s.commonServices.MailboxService.ReputationScore(ctx, domain, tenant)
+		if err != nil {
+			tracing.TraceErr(span, errors.Wrap(err, "Error getting mailbox reputation score"))
+			s.log.Errorf("Error getting mailbox reputation score for %s: %s", domain, err.Error())
+		}
+	}
+
 }
