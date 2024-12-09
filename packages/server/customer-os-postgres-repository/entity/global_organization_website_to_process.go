72ec88b8848796d0b39e574e3b98ebd520863c30
alexopenline <alex@openline.ai>
2024-12-09 19:03:23 +0200

    Fix column name

--- packages/server/customer-os-postgres-repository/entity/global_organization_website_to_process.go
+++ packages/server/customer-os-postgres-repository/entity/global_organization_website_to_process.go
@@ -6,7 +6,7 @@ type GlobalOrganizationWebsiteToProcess struct {
 	ID          uint64    `gorm:"primary_key;autoIncrement" json:"id"`
 	Website     string    `gorm:"column:website;type:varchar(255)" json:"website"`
 	CreatedAt   time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
-	ProcessedAt time.Time `gorm:"column:updated_at;type:timestamp" json:"processedAt"`
+	ProcessedAt time.Time `gorm:"column:processed_at;type:timestamp" json:"processedAt"`
 	Processed   bool      `gorm:"column:processed;type:boolean" json:"processed"`
 	Notes       string    `gorm:"column:notes;type:text" json:"notes"`
 }
