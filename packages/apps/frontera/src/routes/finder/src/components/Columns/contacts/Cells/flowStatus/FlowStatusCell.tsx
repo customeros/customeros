46560d1248efae1fdbaec9e9c8a0fd6e778d0368
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-12-10 14:54:33 +0100

    COS-6197: Flow participant scheduling status copy and icon change (#10279)
    * COS-6197: Flow participant scheduling status copy and icon change
    
    * COS-6197: Added new flow participant status
    
    * Fix sorting

--- packages/apps/frontera/src/routes/finder/src/components/Columns/contacts/Cells/flowStatus/FlowStatusCell.tsx
+++ packages/apps/frontera/src/routes/finder/src/components/Columns/contacts/Cells/flowStatus/FlowStatusCell.tsx
@@ -9,6 +9,7 @@ import { Trophy01 } from '@ui/media/icons/Trophy01';
 import { FlowParticipantStatus } from '@graphql/types';
 import { Hourglass02 } from '@ui/media/icons/Hourglass02';
 import { CheckCircle } from '@ui/media/icons/CheckCircle';
+import { RefreshCw02 } from '@ui/media/icons/RefreshCw02';
 import { SlashCircle01 } from '@ui/media/icons/SlashCircle01';
 import { CalendarCheck01 } from '@ui/media/icons/CalendarCheck01';
 
@@ -43,6 +44,10 @@ export const FlowStatusCell = observer(({ contactID }: FlowStatusCellProps) => {
     .with(FlowParticipantStatus.Scheduled, () => [
       'Scheduled',
       <CalendarCheck01 className='size-3' />,
+    ])
+    .with(FlowParticipantStatus.Scheduling, () => [
+      'Scheduling',
+      <RefreshCw02 className='size-3' />,
     ])
     .with(FlowParticipantStatus.GoalAchieved, () => [
       'Goal achieved',
