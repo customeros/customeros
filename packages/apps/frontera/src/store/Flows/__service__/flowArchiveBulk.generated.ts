b66fa2d927cbf38dfce4de8748cfb50ec97954fe
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-12-10 16:00:00 +0100

    COS-6180: Integrated with BE mutation to bulk delete flows (#10281)

---
+++ packages/apps/frontera/src/store/Flows/__service__/flowArchiveBulk.generated.ts
@@ -0,0 +1,10 @@
+import * as Types from '../../../routes/src/types/__generated__/graphql.types';
+
+export type FlowArchiveBulkMutationVariables = Types.Exact<{
+  ids: Array<Types.Scalars['ID']['input']> | Types.Scalars['ID']['input'];
+}>;
+
+export type FlowArchiveBulkMutation = {
+  __typename?: 'Mutation';
+  flow_ArchiveBulk: { __typename?: 'Result'; result: boolean };
+};
