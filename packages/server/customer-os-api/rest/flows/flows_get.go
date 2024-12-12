055e33617b31f785d0aded53b015a4a4d3be7a81
Matt Brown <matt@openline.ai>
2024-12-12 16:35:25 +0000

    get flows api

---
+++ packages/server/customer-os-api/rest/flows/flows_get.go
@@ -0,0 +1,74 @@
+package flows
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-postgres-repository/entity"
+
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/rest"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-api/service"
+)
+
+type FlowRecord struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	Trigger     string    `json:"triggerOn,omitempty"`
+	VisibleUI   bool      `json:"visible"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"UpadatedAt,omitempty"`
+}
+
+type GetAllFlowsResponse struct {
+	rest.BaseResponse
+	Flows []FlowRecord `json:"flows"`
+}
+
+func GetFlows(s *service.Services) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, span := tracing.StartHttpServerTracerSpanWithHeader(c.Request.Context(), "Flows.GetFlows", c.Request.Header)
+		defer span.Finish()
+		tracing.TagComponentRest(span)
+
+		tenant := rest.ValidateTenant(c, ctx, span)
+		if tenant == "" {
+			rest.SendError(c, span, http.StatusUnauthorized, rest.ErrInvalidAPIKey)
+			return
+		}
+
+		// get flows for tenant from database
+		flowRecords, err := s.Repositories.PostgresRepositories.FlowRepository.GetAllFlowsForTenant(ctx)
+		if err != nil {
+			rest.SendError(c, span, http.StatusBadRequest, rest.ErrBadRequest)
+			return
+		}
+
+		response := make([]FlowRecord, len(flowRecords))
+		for i, record := range flowRecords {
+			response[i] = buildFlowRecord(ctx, record)
+		}
+
+		c.JSON(http.StatusOK, GetAllFlowsResponse{
+			BaseResponse: rest.BuildBaseResponse(rest.StatusSuccess),
+			Flows:        response,
+		})
+	}
+}
+
+func buildFlowRecord(ctx context.Context, record entity.Flow) FlowRecord {
+	return FlowRecord{
+		ID:          record.ID,
+		Name:        record.Name,
+		Description: *record.Description,
+		Trigger:     record.TriggerOn,
+		VisibleUI:   *record.VisibleInUI,
+		Status:      record.Status,
+		CreatedAt:   record.CreatedAt,
+		UpdatedAt:   record.UpdatedAt,
+	}
+}
