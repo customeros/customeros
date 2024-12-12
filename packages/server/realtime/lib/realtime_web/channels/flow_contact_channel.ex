574f887a2a308d4e47f0205216f235c256f9fb99
kasia <kasia@openline.ai>
2024-12-12 10:21:17 +0100

    COS-6235: Fix invalidating flow participant on BE events

--- packages/server/realtime/lib/realtime_web/channels/flow_contact_channel.ex
+++
@@ -1,6 +0,0 @@
-defmodule RealtimeWeb.FlowContactChannel do
-  @moduledoc """
-  This Channel broadcasts sync events to all FlowContact entity subscribers.
-  """
-  use RealtimeWeb.EntityChannelMacro, "FlowContact"
-end
