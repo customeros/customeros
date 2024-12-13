8c748810f905f305cfa8a7dd44ddd2a7d5cb5fa5
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-12-13 11:26:10 +0100

    COS-6239: When a flow is live and a user tries to remove a sender, show modal explaining why action cannot be performed (#10328)

--- packages/apps/frontera/src/routes/src/components/CommandMenu/commands/flows/ActiveFlowUpdateInfo.tsx
+++ packages/apps/frontera/src/routes/src/components/CommandMenu/commands/flows/ActiveFlowUpdateInfo.tsx
@@ -1,3 +1,4 @@
+import { match } from 'ts-pattern';
 import { observer } from 'mobx-react-lite';
 
 import { Button } from '@ui/form/Button/Button';
@@ -7,29 +8,45 @@ import { Command, CommandCancelIconButton } from '@ui/overlay/CommandMenu';
 export const ActiveFlowUpdateInfo = observer(() => {
   const store = useStore();
 
+  const type = store.ui.commandMenu.context.meta?.type;
+
   const handleClose = () => {
     store.ui.commandMenu.clearContext();
     store.ui.commandMenu.setOpen(false);
   };
+  const title = match(type)
+    .with('steps', () => 'Steps can’t be added or removed')
+    .with('senders', () => 'Senders can’t be changed in live flows')
+    .otherwise(() => '');
+
+  const descriptions = match(type)
+    .with('steps', () => (
+      <>
+        Once a flow has gone live the first time, you can only edit content and
+        basic settings.
+        <p className='mt-2'>
+          To add or remove a step, you’d have to duplicate the flow or create a
+          new one.{' '}
+        </p>
+      </>
+    ))
+    .with('senders', () => (
+      <>
+        This sender has scheduled actions. To change or remove the senders,
+        you’ll first have to stop the flow.
+      </>
+    ))
+    .otherwise(() => '');
 
   return (
     <Command>
       <article className='relative w-full p-6 flex flex-col border-b border-b-gray-100'>
         <div className='flex items-center justify-between'>
-          <h1 className='text-base font-semibold'>
-            Steps can’t be added or removed
-          </h1>
+          <h1 className='text-base font-semibold'>{title}</h1>
           <CommandCancelIconButton onClose={handleClose} />
         </div>
 
-        <p className='text-sm mt-2'>
-          Once a flow has gone live the first time, you can only edit content
-          and basic settings.
-          <p className='mt-2'>
-            To add or remove a step, you’d have to duplicate the flow or create
-            a new one.{' '}
-          </p>
-        </p>
+        <p className='text-sm mt-2'>{descriptions}</p>
         <div className='flex justify-between gap-3 mt-6'>
           <Button
             size='sm'
