3fb55088815b464399dfa2c8e21190189e033d31
Kasia <121807793+Kasia9090@users.noreply.github.com>
2024-12-20 15:33:13 +0100

    COS-6318: Show customer map only for selected tenants (#10469)

--- packages/apps/frontera/src/routes/src/components/RootSidenav/components/sections/NavigationSections.tsx
+++ packages/apps/frontera/src/routes/src/components/RootSidenav/components/sections/NavigationSections.tsx
@@ -1,8 +1,10 @@
-import { cn } from '@ui/utils/cn.ts';
+import { useFeatureIsOn } from '@growthbook/growthbook-react';
+
+import { cn } from '@ui/utils/cn';
 import { Bubbles } from '@ui/media/icons/Bubbles';
 import { Preferences } from '@shared/components/RootSidenav/hooks';
 import { SidenavItem } from '@shared/components/RootSidenav/components/SidenavItem';
-import { TeamViewsSectionSection } from '@shared/components/RootSidenav/components/sections/TeamViewsSection.tsx';
+import { TeamViewsSectionSection } from '@shared/components/RootSidenav/components/sections/TeamViewsSection';
 
 import { FavoritesSection } from './FavoritesSection';
 import { GeneralViewsSection } from './GeneralViewsSection';
@@ -24,22 +26,27 @@ export const NavigationSections = ({
   handleItemClick,
   checkIsActive,
 }: NavigationSectionsProps) => {
+  const showCustomerMap = useFeatureIsOn('show-customer-map');
+
   return (
     <div className='px-2 pt-2.5 gap-4 overflow-y-auto overflow-hidden flex flex-col flex-1'>
-      <SidenavItem
-        label='Customer map'
-        dataTest={`side-nav-item-customer-map`}
-        isActive={checkIsActive('customer-map')}
-        onClick={() => handleItemClick('customer-map')}
-        icon={(isActive) => (
-          <Bubbles
-            className={cn(
-              'size-4 min-w-4 text-gray-500',
-              isActive && 'text-gray-700',
-            )}
-          />
-        )}
-      />
+      {showCustomerMap && (
+        <SidenavItem
+          label='Customer map'
+          dataTest={`side-nav-item-customer-map`}
+          isActive={checkIsActive('customer-map')}
+          onClick={() => handleItemClick('customer-map')}
+          icon={(isActive) => (
+            <Bubbles
+              className={cn(
+                'size-4 min-w-4 text-gray-500',
+                isActive && 'text-gray-700',
+              )}
+            />
+          )}
+        />
+      )}
+
       <LifecycleStagesSection
         preferences={preferences}
         checkIsActive={checkIsActive}
