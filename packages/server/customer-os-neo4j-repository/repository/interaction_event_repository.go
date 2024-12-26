b45ae7b42b8c99864a78509fa2dd15bc6954dde0
alexopenline <alex@openline.ai>
2024-12-26 11:32:16 +0200

    start rework interaction event

--- packages/server/customer-os-neo4j-repository/repository/interaction_event_repository.go
+++ packages/server/customer-os-neo4j-repository/repository/interaction_event_repository.go
@@ -4,19 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j/db"
-	commonmodel "github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/model"
-	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
-	"time"
 )
 
 // TODO delete me
 type InteractionEventRepository interface {
-	GetInteractionEventIdByExternalId(ctx context.Context, tenant, externalSystemId, externalId string) (string, error)
-
-	MergeEmailInteractionEvent(ctx context.Context, tx neo4j.ManagedTransaction, tenant string, syncDate time.Time, message commonmodel.SaveEmailMessage, source, appSource string) (string, error)
 	LinkInteractionEventToSession(ctx context.Context, tx neo4j.ManagedTransaction, tenant, interactionEventId, interactionSessionId string) error
-
 	InteractionEventSentByEmail(ctx context.Context, tx neo4j.ManagedTransaction, tenant, interactionEventId, emailId string) error
 	InteractionEventSentToEmails(ctx context.Context, tx neo4j.ManagedTransaction, tenant, interactionEventId, sentType string, emailsId []string) error
 }
@@ -29,92 +21,6 @@ func NewInteractionEventRepository(driver *neo4j.DriverWithContext) InteractionE
 	return &interactionEventRepository{
 		driver: driver,
 	}
-}
-
-func (r *interactionEventRepository) GetInteractionEventIdByExternalId(ctx context.Context, tenant, externalSystemId, externalId string) (string, error) {
-	session := utils.NewNeo4jReadSession(ctx, *r.driver)
-	defer session.Close(ctx)
-
-	query := "MATCH (ie:InteractionEvent_%s)-[IS_LINKED_WITH{externalId:$externalId}]-(e:ExternalSystem{id:$externalSystemId})" +
-		" RETURN ie.id"
-
-	dbRecord, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-		queryResult, err := tx.Run(ctx, fmt.Sprintf(query, tenant),
-			map[string]interface{}{
-				"externalId":       externalId,
-				"externalSystemId": externalSystemId,
-			})
-		if err != nil {
-			return nil, err
-		}
-		record, err := queryResult.Single(ctx)
-		if err != nil {
-			return nil, err
-		}
-		return record, nil
-	})
-	if err != nil && err.Error() == "Result contains no more records" {
-		return "", nil
-	}
-	if err != nil {
-		return "", err
-	}
-	return dbRecord.(*db.Record).Values[0].(string), nil
-}
-
-func (r *interactionEventRepository) MergeEmailInteractionEvent(ctx context.Context, tx neo4j.ManagedTransaction, tenant string, syncDate time.Time, message commonmodel.SaveEmailMessage, source, appSource string) (string, error) {
-	query := "MATCH (:Tenant {name:$tenant})<-[:EXTERNAL_SYSTEM_BELONGS_TO_TENANT]-(e:ExternalSystem {id:$externalSystemId}) " +
-		" MERGE (ie:InteractionEvent_%s {source:$source, channel:$channel})-[rel:IS_LINKED_WITH {externalId:$externalId}]->(e) " +
-		" ON CREATE SET " +
-		"  ie:InteractionEvent, " +
-		"  ie:TimelineEvent, " +
-		"  ie:TimelineEvent_%s, " +
-		"  rel.syncDate=$syncDate, " +
-		"  ie.createdAt=$createdAt, " +
-		"  ie.id=randomUUID(), " +
-		"  ie.identifier=$identifier, " +
-		"  ie.channel=$channel, " +
-		"  ie.channelData=$channelData, " +
-		"  ie.content=$content, " +
-		"  ie.contentType=$contentType, " +
-		"  ie.sourceOfTruth=$sourceOfTruth, " +
-		"  ie.appSource=$appSource " +
-		" WITH ie " +
-		" RETURN ie.id"
-
-	params := map[string]interface{}{
-		"tenant":           tenant,
-		"identifier":       message.ExternalId,
-		"source":           source,
-		"sourceOfTruth":    "openline",
-		"appSource":        appSource,
-		"externalId":       message.ExternalId,
-		"externalSystemId": message.ExternalSystem,
-		"syncDate":         syncDate,
-		"createdAt":        message.CreatedAt,
-		"channel":          message.Channel,
-		"channelData":      message.ChannelData,
-	}
-
-	if message.Html != "" {
-		params["content"] = message.Html
-		params["contentType"] = "text/html"
-	} else {
-		params["content"] = message.Text
-		params["contentType"] = "text/plain"
-	}
-
-	queryResult, err := tx.Run(ctx, fmt.Sprintf(query, tenant, tenant),
-		params)
-	if err != nil {
-		return "", err
-	}
-	record, err := queryResult.Single(ctx)
-	if err != nil {
-		return "", err
-	}
-
-	return record.Values[0].(string), nil
 }
 
 func (r *interactionEventRepository) LinkInteractionEventToSession(ctx context.Context, tx neo4j.ManagedTransaction, tenant, interactionEventId, interactionSessionId string) error {
