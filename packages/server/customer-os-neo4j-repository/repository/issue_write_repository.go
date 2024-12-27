679b0110288fb2907f14bed3645a9da8cda5673e
alexopenline <alex@openline.ai>
2024-12-27 16:12:15 +0200

    Save issue service

--- packages/server/customer-os-neo4j-repository/repository/issue_write_repository.go
+++ packages/server/customer-os-neo4j-repository/repository/issue_write_repository.go
@@ -4,40 +4,16 @@ import (
 	"context"
 	"fmt"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/data_fields"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
-	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/constants"
-	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/model"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
-	"time"
 )
 
-type IssueCreateFields struct {
-	CreatedAt                 time.Time          `json:"createdAt"`
-	SourceFields              model.SourceFields `json:"sourceFields"`
-	GroupId                   string             `json:"groupId"`
-	Subject                   string             `json:"subject"`
-	Description               string             `json:"description"`
-	Status                    string             `json:"status"`
-	Priority                  string             `json:"priority"`
-	ReportedByOrganizationId  string             `json:"reportedByOrganizationId"`
-	SubmittedByOrganizationId string             `json:"submittedByOrganizationId"`
-	SubmittedByUserId         string             `json:"submittedByUserId"`
-}
-
-type IssueUpdateFields struct {
-	GroupId     string `json:"groupId"`
-	Subject     string `json:"subject"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
-	Priority    string `json:"priority"`
-	Source      string `json:"source"`
-}
-
 type IssueWriteRepository interface {
-	Create(ctx context.Context, tenant, issueId string, data IssueCreateFields) error
-	Update(ctx context.Context, tenant, issueId string, data IssueUpdateFields) error
+	Create(ctx context.Context, tx *neo4j.ManagedTransaction, tenant, issueId string, data data_fields.IssueFields) error
+	Update(ctx context.Context, tx *neo4j.ManagedTransaction, tenant, issueId string, data data_fields.IssueFields) error
 	AddUserAssignee(ctx context.Context, tenant, issueId, userId string) error
 	RemoveUserAssignee(ctx context.Context, tenant, issueId, userId string) error
 	AddUserFollower(ctx context.Context, tenant, issueId, userId string) error
@@ -61,12 +37,12 @@ func NewIssueWriteRepository(driver *neo4j.DriverWithContext, database string) I
 	}
 }
 
-func (r *issueWriteRepository) Create(ctx context.Context, tenant, issueId string, data IssueCreateFields) error {
+func (r *issueWriteRepository) Create(ctx context.Context, tx *neo4j.ManagedTransaction, tenant, issueId string, data data_fields.IssueFields) error {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "IssueWriteRepository.Create")
 	defer span.Finish()
 	tracing.TagComponentNeo4jRepository(span)
 	tracing.TagTenant(span, tenant)
-	span.SetTag(tracing.SpanTagEntityId, issueId)
+	tracing.TagEntity(span, issueId)
 	tracing.LogObjectAsJson(span, "data", data)
 
 	cypher := fmt.Sprintf(`MATCH (t:Tenant {name:$tenant})
@@ -78,21 +54,12 @@ func (r *issueWriteRepository) Create(ctx context.Context, tenant, issueId strin
 								i.createdAt=$createdAt,
 								i.updatedAt=datetime(),
 								i.source=$source,
-								i.sourceOfTruth=$sourceOfTruth,
 								i.appSource=$appSource,
 								i.groupId=$groupId,
 								i.subject=$subject,
 								i.description=$description,	
 								i.status=$status,	
 								i.priority=$priority
-							ON MATCH SET 	
-								i.groupId = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.groupId is null OR i.groupId = '' THEN $groupId ELSE i.groupId END,
-								i.subject = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.subject is null OR i.subject = '' THEN $subject ELSE i.subject END,
-								i.description = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.description is null OR i.description = '' THEN $description ELSE i.description END,
-								i.status = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.status is null OR i.status = '' THEN $status ELSE i.status END,
-								i.priority = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.priority is null OR i.priority = '' THEN $priority ELSE i.priority END,
-								i.updatedAt = datetime(),
-								i.sourceOfTruth = case WHEN $overwrite=true THEN $sourceOfTruth ELSE i.sourceOfTruth END
 							WITH i, t
 							OPTIONAL MATCH (t)<-[:ORGANIZATION_BELONGS_TO_TENANT]-(o:Organization {id:$reportedByOrganizationId}) 
 							WHERE $reportedByOrganizationId <> ""
@@ -112,65 +79,86 @@ func (r *issueWriteRepository) Create(ctx context.Context, tenant, issueId strin
 	params := map[string]any{
 		"tenant":                    tenant,
 		"issueId":                   issueId,
-		"createdAt":                 data.CreatedAt,
-		"source":                    data.SourceFields.Source,
-		"sourceOfTruth":             data.SourceFields.SourceOfTruth,
-		"appSource":                 data.SourceFields.AppSource,
-		"groupId":                   data.GroupId,
-		"subject":                   data.Subject,
-		"description":               data.Description,
-		"status":                    data.Status,
-		"priority":                  data.Priority,
-		"reportedByOrganizationId":  data.ReportedByOrganizationId,
-		"submittedByOrganizationId": data.SubmittedByOrganizationId,
-		"submittedByUserId":         data.SubmittedByUserId,
-		"overwrite":                 data.SourceFields.Source == constants.SourceOpenline,
+		"createdAt":                 utils.IfNotNilTimeWithDefault(data.CreatedAt, utils.Now()),
+		"source":                    utils.IfNotNilString(data.Source),
+		"appSource":                 utils.IfNotNilString(data.AppSource),
+		"groupId":                   utils.IfNotNilString(data.GroupId),
+		"subject":                   utils.IfNotNilString(data.Subject),
+		"description":               utils.IfNotNilString(data.Description),
+		"status":                    utils.IfNotNilString(data.Status),
+		"priority":                  utils.IfNotNilString(data.Priority),
+		"reportedByOrganizationId":  utils.IfNotNilString(data.ReportedByOrganizationId),
+		"submittedByOrganizationId": utils.IfNotNilString(data.SubmittedByOrganizationId),
+		"submittedByUserId":         utils.IfNotNilString(data.SubmittedByUserId),
 	}
 	span.LogFields(log.String("cypher", cypher))
 	tracing.LogObjectAsJson(span, "params", params)
 
-	err := utils.ExecuteWriteQuery(ctx, *r.driver, cypher, params)
+	_, err := utils.ExecuteWriteInTransaction(ctx, r.driver, r.database, tx, func(tx neo4j.ManagedTransaction) (any, error) {
+		_, err := tx.Run(ctx, cypher, params)
+		if err != nil {
+			return nil, err
+		}
+		return nil, nil
+	})
+
 	if err != nil {
 		tracing.TraceErr(span, err)
 	}
+
 	return err
 }
 
-func (r *issueWriteRepository) Update(ctx context.Context, tenant, issueId string, data IssueUpdateFields) error {
-	span, ctx := opentracing.StartSpanFromContext(ctx, "IssueWriteRepository.Update")
+func (r *issueWriteRepository) Update(ctx context.Context, tx *neo4j.ManagedTransaction, tenant, issueId string, data data_fields.IssueFields) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "IssueWriteRepository.Ureate")
 	defer span.Finish()
 	tracing.TagComponentNeo4jRepository(span)
 	tracing.TagTenant(span, tenant)
-	span.SetTag(tracing.SpanTagEntityId, issueId)
+	tracing.TagEntity(span, issueId)
 	tracing.LogObjectAsJson(span, "data", data)
 
 	cypher := `MATCH (t:Tenant {name:$tenant})<-[:ISSUE_BELONGS_TO_TENANT]-(i:Issue {id:$issueId})
-		 	SET	
-				i.groupId = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.groupId is null OR i.groupId = '' THEN $groupId ELSE i.groupId END,
-				i.subject = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.subject is null OR i.subject = '' THEN $subject ELSE i.subject END,
-				i.description = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.description is null OR i.description = '' THEN $description ELSE i.description END,
-				i.status = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.status is null OR i.status = '' THEN $status ELSE i.status END,
-				i.priority = CASE WHEN i.sourceOfTruth=$sourceOfTruth OR $overwrite=true OR i.priority is null OR i.priority = '' THEN $priority ELSE i.priority END,
-				i.updatedAt = datetime(),
-				i.sourceOfTruth = case WHEN $overwrite=true THEN $sourceOfTruth ELSE i.sourceOfTruth END`
+		 	SET	i.updatedAt = datetime() `
 	params := map[string]any{
-		"tenant":        tenant,
-		"issueId":       issueId,
-		"groupId":       data.GroupId,
-		"subject":       data.Subject,
-		"description":   data.Description,
-		"status":        data.Status,
-		"priority":      data.Priority,
-		"sourceOfTruth": data.Source,
-		"overwrite":     data.Source == constants.SourceOpenline,
+		"tenant": tenant,
 	}
+
+	if data.GroupId != nil {
+		cypher += `, i.groupId = $groupId`
+		params["groupId"] = *data.GroupId
+	}
+	if data.Subject != nil {
+		cypher += `, i.subject = $subject`
+		params["subject"] = *data.Subject
+	}
+	if data.Description != nil {
+		cypher += `, i.description = $description`
+		params["description"] = *data.Description
+	}
+	if data.Status != nil {
+		cypher += `, i.status = $status`
+		params["status"] = *data.Status
+	}
+	if data.Priority != nil {
+		cypher += `, i.priority = $priority`
+		params["priority"] = *data.Priority
+	}
+
 	span.LogFields(log.String("cypher", cypher))
 	tracing.LogObjectAsJson(span, "params", params)
 
-	err := utils.ExecuteWriteQuery(ctx, *r.driver, cypher, params)
+	_, err := utils.ExecuteWriteInTransaction(ctx, r.driver, r.database, tx, func(tx neo4j.ManagedTransaction) (any, error) {
+		_, err := tx.Run(ctx, cypher, params)
+		if err != nil {
+			return nil, err
+		}
+		return nil, nil
+	})
+
 	if err != nil {
 		tracing.TraceErr(span, err)
 	}
+
 	return err
 }
 
