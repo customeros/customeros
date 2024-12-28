5a7d35ed3c133c222f2437baf1dffb9892e83d3d
alexopenline <alex@openline.ai>
2024-12-28 13:47:01 +0200

    fix tests

--- packages/server/customer-os-api/test/grpc/events_platform/mock_contract.go
+++ packages/server/customer-os-api/test/grpc/events_platform/mock_contract.go
@@ -3,11 +3,9 @@ package events_platform
 import (
 	"context"
 	contractpb "github.com/openline-ai/openline-customer-os/packages/server/events-processing-proto/gen/proto/go/api/grpc/v1/contract"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type MockContractServiceCallbacks struct {
-	SoftDeleteContract                    func(context.Context, *contractpb.SoftDeleteContractGrpcRequest) (*emptypb.Empty, error)
 	RolloutRenewalOpportunityOnExpiration func(context.Context, *contractpb.RolloutRenewalOpportunityOnExpirationGrpcRequest) (*contractpb.ContractIdGrpcResponse, error)
 }
 
@@ -19,13 +17,6 @@ func SetContractCallbacks(callbacks *MockContractServiceCallbacks) {
 
 type MockContractService struct {
 	contractpb.UnimplementedContractGrpcServiceServer
-}
-
-func (MockContractService) SoftDeleteContract(context context.Context, proto *contractpb.SoftDeleteContractGrpcRequest) (*emptypb.Empty, error) {
-	if contractCallbacks.SoftDeleteContract == nil {
-		panic("contractCallbacks.SoftDeleteContract is not set")
-	}
-	return contractCallbacks.SoftDeleteContract(context, proto)
 }
 
 func (MockContractService) RolloutRenewalOpportunityOnExpiration(context context.Context, proto *contractpb.RolloutRenewalOpportunityOnExpirationGrpcRequest) (*contractpb.ContractIdGrpcResponse, error) {
