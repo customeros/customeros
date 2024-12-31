6f46044c40b02b804f270350de5c0dc005df4c86
alexopenline <alex@openline.ai>
2024-12-31 16:41:08 +0200

    remove update sli proto

--- packages/server/customer-os-api/test/grpc/events_platform/mock_service_line_item.go
+++ packages/server/customer-os-api/test/grpc/events_platform/mock_service_line_item.go
@@ -6,8 +6,6 @@ import (
 )
 
 type MockServiceLineItemServiceCallbacks struct {
-	CreateServiceLineItem func(context.Context, *servicelineitempb.CreateServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error)
-	UpdateServiceLineItem func(context.Context, *servicelineitempb.UpdateServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error)
 	DeleteServiceLineItem func(context.Context, *servicelineitempb.DeleteServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error)
 	CloseServiceLineItem  func(context.Context, *servicelineitempb.CloseServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error)
 }
@@ -20,20 +18,6 @@ func SetServiceLineItemCallbacks(callbacks *MockServiceLineItemServiceCallbacks)
 
 type MockServiceLineItemService struct {
 	servicelineitempb.UnimplementedServiceLineItemGrpcServiceServer
-}
-
-func (MockServiceLineItemService) CreateServiceLineItem(context context.Context, proto *servicelineitempb.CreateServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error) {
-	if serviceLineItemCallbacks.CreateServiceLineItem == nil {
-		panic("serviceLineItemCallbacks.CreateServiceLineItem is not set")
-	}
-	return serviceLineItemCallbacks.CreateServiceLineItem(context, proto)
-}
-
-func (MockServiceLineItemService) UpdateServiceLineItem(context context.Context, proto *servicelineitempb.UpdateServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error) {
-	if serviceLineItemCallbacks.UpdateServiceLineItem == nil {
-		panic("serviceLineItemCallbacks.UpdateServiceLineItem is not set")
-	}
-	return serviceLineItemCallbacks.UpdateServiceLineItem(context, proto)
 }
 
 func (MockServiceLineItemService) DeleteServiceLineItem(context context.Context, proto *servicelineitempb.DeleteServiceLineItemGrpcRequest) (*servicelineitempb.ServiceLineItemIdGrpcResponse, error) {
