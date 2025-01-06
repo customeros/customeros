afeb16501b9e8f40c02fd79af29195acb1e9ad43
alexopenline <alex@openline.ai>
2025-01-06 15:31:55 +0200

    fix

--- packages/server/events-subscribers/listeners/flow_engine_webhooks.go
+++ packages/server/events-subscribers/listeners/flow_engine_webhooks.go
@@ -18,10 +18,11 @@ import (
 )
 
 var eventDataTypes = map[string]reflect.Type{
-	data_fields.MeetingSummaryEvent{}.Type(): reflect.TypeOf(data_fields.MeetingSummaryEvent{}),
-	data_fields.ContactCreateEvent{}.Type():  reflect.TypeOf(data_fields.ContactCreateEvent{}),
-	data_fields.MarkdownEventFields{}.Type(): reflect.TypeOf(data_fields.MarkdownEventFields{}),
-	data_fields.WebsiteVisitEvent{}.Type():   reflect.TypeOf(data_fields.WebsiteVisitEvent{}),
+	data_fields.MeetingSummaryEvent{}.Type():    reflect.TypeOf(data_fields.MeetingSummaryEvent{}),
+	data_fields.ContactCreateEvent{}.Type():     reflect.TypeOf(data_fields.ContactCreateEvent{}),
+	data_fields.MarkdownEventFields{}.Type():    reflect.TypeOf(data_fields.MarkdownEventFields{}),
+	data_fields.WebsiteVisitEvent{}.Type():      reflect.TypeOf(data_fields.WebsiteVisitEvent{}),
+	data_fields.SlackNotifyEventFields{}.Type(): reflect.TypeOf(data_fields.SlackNotifyEventFields{}),
 }
 
 func OnWebhookEventCreated(ctx context.Context, s *service.Services, input any) error {
