5788a148eff2b724b25cef498b51559ff30e5509
alexopenline <alex@openline.ai>
2025-01-08 12:28:49 +0200

    fix

--- packages/server/events-processing-platform-subscribers/subscriptions/graph/invoice_event_handler.go
+++ packages/server/events-processing-platform-subscribers/subscriptions/graph/invoice_event_handler.go
@@ -2,8 +2,8 @@ package graph
 
 import (
 	"context"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/clients/grpc_client"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/enum"
-	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/grpc_client"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/model"
 	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/utils"
 	neo4jentity "github.com/openline-ai/openline-customer-os/packages/server/customer-os-neo4j-repository/entity"
