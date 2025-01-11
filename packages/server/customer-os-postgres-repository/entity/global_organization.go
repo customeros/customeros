a950e35aef4c0c6410ae440640ec6a04a33d6a54
alexopenline <alex@openline.ai>
2025-01-11 18:37:56 +0200

    New columns in global orgs

--- packages/server/customer-os-postgres-repository/entity/global_organization.go
+++ packages/server/customer-os-postgres-repository/entity/global_organization.go
@@ -3,29 +3,36 @@ package entity
 import "time"
 
 type GlobalOrganization struct {
-	ID                          uint64    `gorm:"primary_key;autoIncrement" json:"id"`
-	Name                        string    `gorm:"column:name;type:varchar(255)" json:"name"`
-	PrimaryDomain               string    `gorm:"column:primary_domain;type:varchar(255);NOT NULL;index:idx_global_organization_primary_domain,unique" json:"primaryDomain"`
-	OtherDomains                string    `gorm:"column:other_domains;type:text" json:"otherDomains"`
-	CreatedAt                   time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
-	UpdatedAt                   time.Time `gorm:"column:updated_at;type:timestamp;DEFAULT:current_timestamp" json:"updatedAt"`
-	Description                 string    `gorm:"column:description;type:text" json:"description"`
-	IconUrl                     string    `gorm:"column:icon_url;type:varchar(2000)" json:"iconUrl"`
-	LogoUrl                     string    `gorm:"column:logo_url;type:varchar(2000)" json:"logoUrl"`
-	Website                     string    `gorm:"column:website;type:varchar(255)" json:"website"`
-	LinkedInUrl                 string    `gorm:"column:linkedin;type:varchar(255)" json:"linkedin"`
-	LinkedInAlias               string    `gorm:"column:linkedin_alias;type:varchar(255)" json:"linkedinAlias"`
-	OtherSocials                []string  `gorm:"column:other_socials;type:text[]" json:"otherSocials"`
-	ValueProposition            string    `gorm:"column:value_proposition;type:text" json:"valueProposition"`
-	TargetAudience              string    `gorm:"column:target_audience;type:text" json:"targetAudience"`
-	IndustryGicsSectorId        string    `gorm:"column:industry_gics_sector_id;type:varchar(255)" json:"industryGicsSectorId"`
-	IndustryGicsIndustryGroupId string    `gorm:"column:industry_gics_industry_group_id;type:varchar(255)" json:"industryGicsIndustryGroupId"`
-	IndustryGicsIndustryId      string    `gorm:"column:industry_gics_industry_id;type:varchar(255)" json:"industryGicsIndustryId"`
-	IndustryGicsSubIndustryId   string    `gorm:"column:industry_gics_sub_industry_id;type:varchar(255)" json:"industryGicsSubIndustryId"`
-	YearFounded                 int       `gorm:"column:year_founded" json:"yearFounded"`
-	EmployeeCount               int64     `gorm:"column:employee_count" json:"employeeCount"`
-	City                        string    `gorm:"column:city;type:varchar(255)" json:"city"`
-	CountryA2                   string    `gorm:"column:country_a2;type:varchar(255)" json:"countryA2"`
+	ID                 uint64    `gorm:"primary_key;autoIncrement" json:"id"`
+	Name               string    `gorm:"column:name;type:varchar(255)" json:"name"`
+	PrimaryDomain      string    `gorm:"column:primary_domain;type:varchar(255);NOT NULL;index:idx_global_organization_primary_domain,unique" json:"primaryDomain"`
+	OtherDomains       string    `gorm:"column:other_domains;type:text" json:"otherDomains"`
+	CreatedAt          time.Time `gorm:"column:created_at;type:timestamp;DEFAULT:current_timestamp" json:"createdAt"`
+	UpdatedAt          time.Time `gorm:"column:updated_at;type:timestamp;DEFAULT:current_timestamp" json:"updatedAt"`
+	Description        string    `gorm:"column:description;type:text" json:"description"`
+	IconUrl            string    `gorm:"column:icon_url;type:varchar(2000)" json:"iconUrl"`
+	LogoUrl            string    `gorm:"column:logo_url;type:varchar(2000)" json:"logoUrl"`
+	Website            string    `gorm:"column:website;type:varchar(255)" json:"website"`
+	LinkedInUrl        string    `gorm:"column:linkedin;type:varchar(255)" json:"linkedin"`
+	LinkedInAlias      string    `gorm:"column:linkedin_alias;type:varchar(255)" json:"linkedinAlias"`
+	OtherSocials       []string  `gorm:"column:other_socials;type:text[]" json:"otherSocials"`
+	ValueProposition   string    `gorm:"column:value_proposition;type:text" json:"valueProposition"`
+	IndustryNaicsCode  string    `gorm:"column:industry_naics_code;type:varchar(255)" json:"industryNaicsCode"`
+	IndustryNaicsName  string    `gorm:"column:industry_naics_name;type:varchar(255)" json:"industryNaicsName"`
+	Market             string    `gorm:"column:market;type:text" json:"market"`
+	YearFounded        int       `gorm:"column:year_founded" json:"yearFounded"`
+	EmployeeCount      int64     `gorm:"column:employee_count" json:"employeeCount"`
+	City               string    `gorm:"column:city;type:varchar(255)" json:"city"`
+	Region             string    `gorm:"column:region;type:varchar(255)" json:"region"`
+	CountryA2          string    `gorm:"column:country_a2;type:varchar(255)" json:"countryA2"`
+	SyncedToNeoAt      time.Time `gorm:"column:created_at;type:timestamp" json:"syncedToNeo"`
+	IndustrySetAt      time.Time `gorm:"column:industry_identified_at;type:timestamp" json:"industryIdentifiedAt"`
+	DescriptionSetAt   time.Time `gorm:"column:description_identified_at;type:timestamp" json:"descriptionIdentifiedAt"`
+	SourceDescription1 string    `gorm:"column:source_description_1;type:text" json:"sourceDescription1"`
+	SourceDescription2 string    `gorm:"column:source_description_2;type:text" json:"sourceDescription2"`
+	SourceDescription3 string    `gorm:"column:source_description_3;type:text" json:"sourceDescription3"`
+	SourceDescription4 string    `gorm:"column:source_description_3;type:text" json:"sourceDescription4"`
+	SourceDescription5 string    `gorm:"column:source_description_3;type:text" json:"sourceDescription5"`
 }
 
 // TableName sets the name of the table for GORM
