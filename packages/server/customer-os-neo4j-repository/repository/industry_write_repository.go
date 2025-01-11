854816d86e356812bd528af6badbddc61517f4a7
alexopenline <alex@openline.ai>
2025-01-11 11:34:25 +0200

    query to set organization

--- packages/server/customer-os-neo4j-repository/repository/industry_write_repository.go
+++ packages/server/customer-os-neo4j-repository/repository/industry_write_repository.go
@@ -2,11 +2,13 @@ package repository
 
 import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/openline-ai/openline-customer-os/packages/server/customer-os-common-module/tracing"
+	"github.com/opentracing/opentracing-go"
 	"golang.org/x/net/context"
 )
 
 type IndustryWriteRepository interface {
-	ReplaceForOrganization(ctx context.Context, tenant string, industryCode, organizationId string) error
+	ReplaceForOrganization(ctx context.Context, tx *neo4j.ManagedTransaction, tenant, industryCode, organizationId string) error
 }
 
 type industryWriteRepository struct {
@@ -21,7 +23,24 @@ func NewIndustryWriteRepository(driver *neo4j.DriverWithContext, database string
 	}
 }
 
-func (r *industryWriteRepository) ReplaceForOrganization(ctx context.Context, tenant string, industryCode, organizationId string) error {
-	//TODO implement me
-	panic("implement me")
+func (r *industryWriteRepository) ReplaceForOrganization(ctx context.Context, tx *neo4j.ManagedTransaction, tenant, industryCode, organizationId string) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "EmailWriteRepository.LinkWithOrganization")
+	defer span.Finish()
+	tracing.TagComponentNeo4jRepository(span)
+	tracing.TagTenant(span, tenant)
+
+	cypher := `MATCH (o:Organization {id: $organizationId})-[:ORGANIZATION_BELONGS_TO_TENANT]->(t:Tenant {name: $tenant})
+				MATCH (i:Industry {code: $industryCode})
+				OPTIONAL MATCH (o)-[oldRel:HAS_INDUSTRY]->(other:Industry)
+					WHERE other <> i
+					DELETE oldRel
+				MERGE (o)-[newRel:HAS_INDUSTRY]->(i)
+					SET o.updatedAt = datetime()`
+	params := map[string]any{
+		"tenant":         tenant,
+		"organizationId": organizationId,
+		"industryCode":   industryCode,
+	}
+
+	return LogAndExecuteWriteQueryInTx(ctx, tx, r.driver, r.database, cypher, params, span)
 }
