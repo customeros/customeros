48eff1ee009e2218829d2c0cf969b8e827193aba
alexopenline <alex@openline.ai>
2025-01-11 14:26:17 +0200

    Set industry

--- packages/server/customer-os-common-module/data_fields/organization_fields.go
+++ packages/server/customer-os-common-module/data_fields/organization_fields.go
@@ -21,6 +21,7 @@ type OrganizationFields struct {
 	Description        *string                             `json:"description,omitempty"`
 	Industry           *string                             `json:"industry,omitempty"`
 	IndustryCode       *string                             `json:"industryCode,omitempty"`
+	IndustryName       *string                             `json:"industryName,omitempty"` // industry name used only for logging, do not set manually
 	TargetAudience     *string                             `json:"targetAudience,omitempty"`
 	ValueProposition   *string                             `json:"valueProposition,omitempty"`
 	LastFundingRound   *string                             `json:"lastFundingRound,omitempty"`
