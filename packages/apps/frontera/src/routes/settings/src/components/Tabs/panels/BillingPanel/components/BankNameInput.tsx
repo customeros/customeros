dc868dd691fde624d1c39b98ddd9b27323134eb3
kasia <kasia@openline.ai>
2025-01-13 12:08:57 +0100

    COS-6464: Fix laggy behaviout of bank account name input

--- packages/apps/frontera/src/routes/settings/src/components/Tabs/panels/BillingPanel/components/BankNameInput.tsx
+++ packages/apps/frontera/src/routes/settings/src/components/Tabs/panels/BillingPanel/components/BankNameInput.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import { Store } from '@store/store.ts';
 
@@ -7,18 +7,20 @@ import { BankAccount } from '@graphql/types';
 
 export const BankNameInput = ({ account }: { account: Store<BankAccount> }) => {
   const nameRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState(() => account?.value?.bankName ?? '');
 
   return (
     <Input
       ref={nameRef}
       autoComplete='off'
+      value={name ?? ''}
       variant={'unstyled'}
       aria-label='Bank Name'
       placeholder='Bank name'
       className='text-md font-semibold'
       onFocus={(e) => e?.target?.select()}
-      value={account?.value?.bankName ?? ''}
-      onChange={(e) =>
+      onChange={(e) => setName(e.target.value)}
+      onBlur={(e) =>
         account?.update((acc) => {
           acc.bankName = e.target.value;
 
