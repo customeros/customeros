e14a8c6266598ac8c52f75a3142ca51af0474506
Eduard Firut <108661145+edifirut@users.noreply.github.com>
2025-01-14 13:03:25 +0200

    Fix (#10683)

--- packages/server/customer-os-neo4j-repository/mapper/node_to_entity_mapper.go
+++ packages/server/customer-os-neo4j-repository/mapper/node_to_entity_mapper.go
@@ -1062,6 +1062,7 @@ func MapDbNodeToFlowEntity(node *dbtype.Node) *entity.FlowEntity {
 		Id:             utils.GetStringPropOrEmpty(props, "id"),
 		CreatedAt:      utils.GetTimePropOrEpochStart(props, "createdAt"),
 		UpdatedAt:      utils.GetTimePropOrEpochStart(props, "updatedAt"),
+		TableViewDefId: utils.GetStringPropOrEmpty(props, "tableViewDefId"),
 		DefaultName:    utils.GetStringPropOrEmpty(props, "defaultName"),
 		Name:           utils.GetStringPropOrEmpty(props, "name"),
 		Nodes:          utils.GetStringPropOrEmpty(props, "nodes"),
