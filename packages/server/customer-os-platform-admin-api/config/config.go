970b732e55ad9a25825303eb145b0c2aed4396cb
alexopenline <alex@openline.ai>
2025-01-18 17:16:35 +0200

    fix platform admin

--- packages/server/customer-os-platform-admin-api/config/config.go
+++ packages/server/customer-os-platform-admin-api/config/config.go
@@ -2,17 +2,27 @@ package config
 
 import (
 	"github.com/caarlos0/env/v6"
-	"github.com/joho/godotenv"
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/config"
+	commconf "github.com/customeros/customeros/packages/server/customer-os-common-module/config"
 	"github.com/customeros/customeros/packages/server/customer-os-common-module/logger"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/tracing"
 	"github.com/customeros/customeros/packages/server/customer-os-common-module/validator"
 	"github.com/customeros/customeros/packages/server/customer-os-platform-admin-api/metrics"
+	"github.com/joho/godotenv"
 	"log"
 )
 
 type Config struct {
 	App    App
-	Common config.CommonConfig
+	Common *commconf.CommonConfig
+}
+
+type CommonConfig struct {
+	Logger        logger.Config
+	Jaeger        tracing.JaegerConfig
+	Postgres      commconf.PostgresConfig
+	PostgresAsync commconf.PostgresAsyncConfig
+	Neo4j         commconf.Neo4jConfig
+	GrpcClient    commconf.GrpcClientConfig
 }
 
 type App struct {
@@ -31,10 +41,29 @@ func InitConfig() (*Config, error) {
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("%+v", err)
 	}
-
-	err := validator.GetValidator().Struct(cfg)
+	err := validator.GetValidator().Struct(cfg.App)
 	if err != nil {
 		return nil, err
+	}
+
+	cmnCfg := CommonConfig{}
+	if err := env.Parse(&cmnCfg); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	err = validator.GetValidator().Struct(cmnCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.Common = &commconf.CommonConfig{
+		Infrastructure: commconf.InfrastructureConfig{
+			LoggerConfig:        cmnCfg.Logger,
+			JaegerConfig:        cmnCfg.Jaeger,
+			PostgresConfig:      cmnCfg.Postgres,
+			PostgresAsyncConfig: cmnCfg.PostgresAsync,
+			Neo4jConfig:         cmnCfg.Neo4j,
+			GrpcClientConfig:    cmnCfg.GrpcClient,
+		},
 	}
 
 	return &cfg, nil
