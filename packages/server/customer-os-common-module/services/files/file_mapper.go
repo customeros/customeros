7641c1818f0c93788946fba082e4247ee60e6a5c
alexopenline <alex@openline.ai>
2025-01-19 11:04:20 +0200

    fix

--- packages/server/customer-os-common-module/services/files/file_mapper.go
+++ packages/server/customer-os-common-module/services/files/file_mapper.go
@@ -1,27 +1,9 @@
 package files
 
 import (
-	"fmt"
-
 	"github.com/customeros/customeros/packages/server/customer-os-common-module/interfaces"
 	neo4jEntity "github.com/customeros/customeros/packages/server/customer-os-neo4j-repository/entity"
 )
-
-func MapFileEntityToDTO(input *interfaces.File, serviceUrl string) *interfaces.FileDTO {
-	if input == nil {
-		return nil
-	}
-	file := interfaces.FileDTO{
-		Id:          input.ID,
-		FileName:    input.FileName,
-		MimeType:    input.MimeType,
-		Size:        input.Size,
-		MetadataUrl: fmt.Sprintf("%s/files/%s", serviceUrl, input.ID),
-		DownloadUrl: fmt.Sprintf("%s/files/%s/download", serviceUrl, input.ID),
-		CdnUrl:      input.CdnUrl,
-	}
-	return &file
-}
 
 func MapAttachmentResponseToFileEntity(input *neo4jEntity.AttachmentEntity) *interfaces.File {
 	if input == nil {
