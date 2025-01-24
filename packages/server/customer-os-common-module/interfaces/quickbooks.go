f6c6850c522b753ca5d7c693f4ac6e31a371cdaa
Eduard Firut <108661145+edifirut@users.noreply.github.com>
2025-01-24 14:14:38 +0200

    QB work (#10946)

--- packages/server/customer-os-common-module/interfaces/quickbooks.go
+++ packages/server/customer-os-common-module/interfaces/quickbooks.go
@@ -9,7 +9,18 @@ import (
 
 type QuickbooksService interface {
 	GetAndStoreAccessToken(ctx context.Context, realmId string, requestData url.Values) (*postgres_entity.QuickbooksSettingsEntity, error)
+	SaveProduct(ctx context.Context, id, productName string) (*QuickbooksSaveProductResponse, error)
 	SaveCustomer(ctx context.Context, id, customerName string) (*QuickbooksSaveCustomerResponse, error)
+}
+
+type QuickbooksInvoiceLine struct {
+	DetailType          string  `json:"DetailType"`
+	Amount              float64 `json:"Amount"`
+	SalesItemLineDetail struct {
+		ItemRef struct {
+			Value string `json:"value"`
+		} `json:"ItemRef"`
+	} `json:"SalesItemLineDetail"`
 }
 
 type OauthQuickbooksResponse struct {
@@ -23,33 +34,22 @@ type OauthQuickbooksResponse struct {
 
 type QuickbooksSaveCustomerResponse struct {
 	Customer *struct {
-		Taxable         bool `json:"Taxable"`
-		Job             bool `json:"Job"`
-		BillWithParent  bool `json:"BillWithParent"`
-		Balance         int  `json:"Balance"`
-		BalanceWithJobs int  `json:"BalanceWithJobs"`
-		CurrencyRef     struct {
-			Value string `json:"value"`
-			Name  string `json:"name"`
-		} `json:"CurrencyRef"`
-		PreferredDeliveryMethod string `json:"PreferredDeliveryMethod"`
-		IsProject               bool   `json:"IsProject"`
-		Domain                  string `json:"domain"`
-		Sparse                  bool   `json:"sparse"`
-		Id                      string `json:"Id"`
-		SyncToken               string `json:"SyncToken"`
-		MetaData                struct {
-			CreateTime      time.Time `json:"CreateTime"`
-			LastUpdatedTime time.Time `json:"LastUpdatedTime"`
-		} `json:"MetaData"`
-		GivenName          string `json:"GivenName"`
-		FullyQualifiedName string `json:"FullyQualifiedName"`
-		DisplayName        string `json:"DisplayName"`
-		PrintOnCheckName   string `json:"PrintOnCheckName"`
-		Active             bool   `json:"Active"`
-		DefaultTaxCodeRef  struct {
-			Value string `json:"value"`
-		} `json:"DefaultTaxCodeRef"`
+		Id string `json:"Id"`
+	} `json:"Customer"`
+	Fault *struct {
+		Error []struct {
+			Message string `json:"Message"`
+			Detail  string `json:"Detail"`
+			Code    string `json:"code"`
+		} `json:"Error"`
+		Type string `json:"type"`
+	} `json:"Fault"`
+	Time time.Time `json:"time"`
+}
+
+type QuickbooksSaveProductResponse struct {
+	Customer *struct {
+		Id string `json:"Id"`
 	} `json:"Customer"`
 	Fault *struct {
 		Error []struct {
