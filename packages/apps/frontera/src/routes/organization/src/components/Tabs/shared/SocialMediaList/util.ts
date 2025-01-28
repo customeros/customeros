437f70483af7f39225fb2500b49363f07aeeed16
mihai@customeros.ai <mihai@customeros.ai>
2025-01-28 13:18:34 +0200

    fix small issues on socials from aboutPanel

--- packages/apps/frontera/src/routes/organization/src/components/Tabs/shared/SocialMediaList/util.ts
+++ packages/apps/frontera/src/routes/organization/src/components/Tabs/shared/SocialMediaList/util.ts
@@ -1,21 +1,21 @@
 export function isKnownUrl(input = '') {
   const url = input.trim().toLowerCase();
 
-  if (url.includes('google')) return 'google';
-  if (url.includes('instagram')) return 'instagram';
-  if (url.includes('reddit')) return 'reddit';
-  if (url.includes('snapchat')) return 'snapchat';
-  if (url.includes('twitter')) return 'twitter';
-  if (url.includes('discord')) return 'discord';
-  if (url.includes('linkedin')) return 'linkedin';
-  if (url.includes('tiktok')) return 'tiktok';
-  if (url.includes('telegram')) return 'telegram';
-  if (url.includes('clubhouse')) return 'clubhouse';
-  if (url.includes('youtube')) return 'youtube';
-  if (url.includes('pinterest')) return 'pinterest';
-  if (url.includes('angellist')) return 'angellist';
-  if (url.includes('github')) return 'github';
-  if (url.includes('facebook')) return 'facebook';
-  if (url.includes('slack')) return 'slack';
-  if (url.includes('notion')) return 'notion';
+  if (url.includes('google.com')) return 'google';
+  if (url.includes('instagram.com')) return 'instagram';
+  if (url.includes('reddit.com')) return 'reddit';
+  if (url.includes('snapchat.com')) return 'snapchat';
+  if (url.includes('twitter.com')) return 'twitter';
+  if (url.includes('discord.com')) return 'discord';
+  if (url.includes('linkedin.com')) return 'linkedin';
+  if (url.includes('tiktok.com')) return 'tiktok';
+  if (url.includes('telegram.com')) return 'telegram';
+  if (url.includes('clubhouse.com')) return 'clubhouse';
+  if (url.includes('youtube.com')) return 'youtube';
+  if (url.includes('pinterest.com')) return 'pinterest';
+  if (url.includes('angellist.com')) return 'angellist';
+  if (url.includes('github.com')) return 'github';
+  if (url.includes('facebook.com')) return 'facebook';
+  if (url.includes('slack.com')) return 'slack';
+  if (url.includes('notion.com')) return 'notion';
 }
