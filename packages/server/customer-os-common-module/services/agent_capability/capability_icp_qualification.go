0352e2f144eab57005e370f846c925c74ad9f104
mattbr0wn <matt@openline.ai>
2025-01-28 20:04:00 +0000

    save

--- packages/server/customer-os-common-module/services/agent_capability/capability_icp_qualification.go
+++ packages/server/customer-os-common-module/services/agent_capability/capability_icp_qualification.go
@@ -3,13 +3,17 @@ package agent_capability
 import (
 	"context"
 	"fmt"
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/common"
-	"github.com/pkg/errors"
+	"strings"
 
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/interfaces"
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/tracing"
 	postgres_repository "github.com/customeros/customeros/packages/server/customer-os-postgres-repository/repository"
 	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/common"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/enum"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/interfaces"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/tracing"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/utils"
 )
 
 type ICPQualificationCapability struct {
@@ -18,22 +22,32 @@ type ICPQualificationCapability struct {
 }
 
 type ICPQualificationInput struct {
-	QualificationCriteria    string `json:"qualificationCriteria"`
-	DisqualificationCriteria string `json:"disqualificationCriteria"`
-	PrimaryDomain            string `json:"primaryDomain"`
+	QualificationCriteria    string `json:"organizationQualificationCriteria"`
+	DisqualificationCriteria string `json:"organizationDisqualificationCriteria"`
+	PrimaryDomain            string `json:"organizationPrimaryDomain"`
 }
 
 type ICPQualificationResult struct {
+	isICPFit        string `json:"isIcpFit"`
+	icpFitRationale string `json:"icpFitRationale"`
 }
 
 func (c *ICPQualificationCapability) ValidateConfig(config NoConfig) error {
-	//TODO implement me
+	// TODO implement me
 	panic("implement me")
 }
 
 func (c *ICPQualificationCapability) ValidateInput(input ICPQualificationInput) error {
-	//TODO implement me
-	panic("implement me")
+	if input.PrimaryDomain == "" {
+		return errors.New("missing required input: PromaryDomain")
+	}
+	if input.QualificationCriteria == "" {
+		return errors.New("missing required input: QualificationCriteria")
+	}
+	if input.DisqualificationCriteria == "" {
+		return errors.New("missing required input: DisqualificationCriteria")
+	}
+	return nil
 }
 
 func (c *ICPQualificationCapability) GetInput() any {
@@ -79,21 +93,97 @@ func (c *ICPQualificationCapability) Execute(ctx context.Context, data ICPQualif
 	// get ICP definition
 
 	// get all company context
-	_, err := c.postgresRepositories.GlobalOrganizationRepository.GetByPrimaryDomain(ctx, data.PrimaryDomain)
+	company, err := c.postgresRepositories.GlobalOrganizationRepository.GetByPrimaryDomain(ctx, data.PrimaryDomain)
 	if err != nil {
+		tracing.TraceErr(span, err)
+		return ICPQualificationResult{}, err
+	}
+	if company == nil {
+		err := errors.New("Company not set in global orgs, cannot run ICP qualification")
 		tracing.TraceErr(span, err)
 		return ICPQualificationResult{}, err
 	}
 
 	// build prompt
+	systemPrompt := `You are a world class company analyst.  Your objective is to determine whether the company I provide you fits our ideal customer profile or not.  I will provide you with three datasets: 1. a description of our ideal customer, 2. criteria that automatically disqualifies companies, and 3. all the context I have about the company, including their name, location, and several descriptions taken from their website and linkedin pages.  I need you to respond with two things: 1. true/false indicator on whether the company is an ICP fit or not, and 2. a short (less than 100 characters) description of your decision rationale.  Please output this as a pipe delimited string like this:  ICP_Fit: false | Rationale: this is my rationale.`
+
+	content := fmt.Sprintf(`
+    Company Name: %s
+    Company Domain: %s
+    Year Founded: %s
+    Employee Count: %s
+    Location: %s, %s, %s
+    Industry NAICS Code: %s
+    Industry Name: %s
+    Company Description 1: %s
+    Company Description 2: %s
+    Company Description 3: %s
+    Company Description 4: %s
+    Company Description 5: %s
+    `, company.Name, company.PrimaryDomain, company.YearFounded, company.EmployeeCount,
+		company.City, company.Region, company.CountryA2, company.IndustryNaicsCode,
+		company.IndustryNaicsName, company.Description, company.SourceDescription1, company.SourceDescription2, company.SourceDescription3, company.SourceDescription4)
 
 	// askAI
+	answer, err := c.aiService.AskAI(ctx, enum.AIModelAnthropicHaiku, systemPrompt, content)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return ICPQualificationResult{}, err
+	}
+
+	// parse answer
+	err = c.validateAnswer(ctx, answer)
+	if err != nil {
+		tracing.TraceErr(span, err)
+		return ICPQualificationResult{}, err
+	}
+
+	icpFit, rationale := c.parseAnswer(ctx, *answer)
 
 	// save to timeline
 
 	// disqualify as lead if not a fit
 
 	return ICPQualificationResult{}, nil
+}
+
+func (c *ICPQualificationCapability) validateAnswer(ctx context.Context, answer *string) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ICPQualificationCapability.parseAnswer")
+	defer span.Finish()
+	tracing.TagComponentService(span)
+	span.LogKV("answer", utils.IfNotNilString(answer))
+
+	if answer == nil {
+		return errors.New("answer is empty")
+	}
+
+	if !strings.Contains(*answer, "ICP_Fit:") {
+		return fmt.Errorf("missing ICP_Fit in input string")
+	}
+	if !strings.Contains(*answer, "| Rationale:") {
+		return fmt.Errorf("missing Rationale in input string")
+	}
+	return nil
+}
+
+func (c *ICPQualificationCapability) parseAnswer(ctx context.Context, answer string) (bool, string) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ICPQualificationCapability.parseAnswer")
+	defer span.Finish()
+	tracing.TagComponentService(span)
+
+	parts := strings.Split(answer, " | Rationale: ")
+	if len(parts) != 2 {
+		return false, ""
+	}
+
+	// Get ICP_Fit value by trimming prefix and converting to bool
+	icpFitStr := strings.TrimPrefix(parts[0], "ICP_Fit: ")
+	icpFit := strings.ToLower(icpFitStr) == "true"
+
+	// Get rationale
+	rationale := parts[1]
+
+	return icpFit, rationale
 }
 
 // ExecuteUntyped implements the AgentCapabilityUntyped interface.
