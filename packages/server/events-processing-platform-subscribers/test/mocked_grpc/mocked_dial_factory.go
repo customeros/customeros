4efbbebd85a9bfc057fa17a5283d0cc9aee11dc7
Eduard Firut <edi@customeros.ai>
2025-01-28 15:41:41 +0200

    fix

--- packages/server/events-processing-platform-subscribers/test/mocked_grpc/mocked_dial_factory.go
+++ packages/server/events-processing-platform-subscribers/test/mocked_grpc/mocked_dial_factory.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/customeros/customeros/packages/server/customer-os-common-module/clients/grpc_client"
 	invoicepb "github.com/customeros/customeros/packages/server/events-processing-proto/gen/proto/go/api/grpc/v1/invoice"
-	organizationpb "github.com/customeros/customeros/packages/server/events-processing-proto/gen/proto/go/api/grpc/v1/organization"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
@@ -31,7 +30,6 @@ func (dfi MockedTestDialFactoryImpl) GetEventsProcessingPlatformConn() (*grpc.Cl
 
 	server := grpc.NewServer()
 
-	organizationpb.RegisterOrganizationGrpcServiceServer(server, &MockOrganizationService{})
 	invoicepb.RegisterInvoiceGrpcServiceServer(server, &MockInvoiceService{})
 
 	go func() {
