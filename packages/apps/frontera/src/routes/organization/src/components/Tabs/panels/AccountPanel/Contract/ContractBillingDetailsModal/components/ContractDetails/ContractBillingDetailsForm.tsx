bdba6f93574e9c5ac82d6e2aa12884477a8b0d00
Kasia <121807793+Kasia9090@users.noreply.github.com>
2025-01-29 17:21:53 +0100

    COS-6647: Fixed the issue causing the contract start date to shift (#11045)

--- packages/apps/frontera/src/routes/organization/src/components/Tabs/panels/AccountPanel/Contract/ContractBillingDetailsModal/components/ContractDetails/ContractBillingDetailsForm.tsx
+++ packages/apps/frontera/src/routes/organization/src/components/Tabs/panels/AccountPanel/Contract/ContractBillingDetailsModal/components/ContractDetails/ContractBillingDetailsForm.tsx
@@ -115,14 +115,11 @@ export const ContractBillingDetailsForm = observer(
               </span>
 
               <DatePickerUnderline
-                value={toZonedTime(
-                  contractStore?.tempValue?.serviceStarted,
-                  'UTC',
-                )}
+                value={contractStore?.tempValue?.serviceStarted}
                 onChange={(date) =>
                   contractStore?.updateTemp((prev) => ({
                     ...prev,
-                    serviceStarted: date,
+                    serviceStarted: date ? date.toISOString() : null,
                   }))
                 }
               />
