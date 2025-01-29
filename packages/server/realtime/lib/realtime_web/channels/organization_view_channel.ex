f24b603abf1a6366f9d9caf3bd3485b133f78ac3
Kasia <121807793+Kasia9090@users.noreply.github.com>
2025-01-29 17:46:05 +0100

    COS-6588: Fixed user avatars not displaying profile pictures in the organization profile view (#11048)

--- packages/server/realtime/lib/realtime_web/channels/organization_view_channel.ex
+++ packages/server/realtime/lib/realtime_web/channels/organization_view_channel.ex
@@ -33,6 +33,7 @@ defmodule RealtimeWeb.OrganizationViewChannel do
         online_at: inspect(System.system_time(:second)),
         metadata: %{"source" => "customerOS"},
         username: socket.assigns.username,
+        user_id: socket.assigns.user_id,
         color: Map.get(socket.assigns.user_color, socket.assigns.user_id)
       })
 
