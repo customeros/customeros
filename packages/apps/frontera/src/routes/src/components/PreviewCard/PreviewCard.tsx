f62724b8a863258c4c243a73e50826b0a91cfc06
mihai@customeros.ai <mihai@customeros.ai>
2025-01-30 17:22:39 +0200

    fix: preview card width

--- packages/apps/frontera/src/routes/src/components/PreviewCard/PreviewCard.tsx
+++ packages/apps/frontera/src/routes/src/components/PreviewCard/PreviewCard.tsx
@@ -10,7 +10,7 @@ export const PreviewCard = ({ children }: PreviewCardProps) => {
   return (
     <div
       data-state={previewCard ? 'open' : 'closed'}
-      className='data-[state=open]:animate-slideLeftAndFade data-[state=closed]:animate-slideRightAndFade flex flex-col border border-r-0 border-t-0 border-gray-200 bg-white w-[450px]'
+      className='data-[state=open]:animate-slideLeftAndFade data-[state=closed]:animate-slideRightAndFade flex flex-col border border-r-0 border-t-0 border-gray-200 bg-white max-w-[450px] min-w-[450px]'
     >
       {children}
     </div>
