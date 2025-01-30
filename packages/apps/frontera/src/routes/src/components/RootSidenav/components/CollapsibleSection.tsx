e1a0f1b8e6be98e536828d5925586f38a7cd5413
Kasia <121807793+Kasia9090@users.noreply.github.com>
2025-01-30 12:20:30 +0100

    COS-6659: Change Team and My view sections dropdown to chevron-down (#11059)

--- packages/apps/frontera/src/routes/src/components/RootSidenav/components/CollapsibleSection.tsx
+++ packages/apps/frontera/src/routes/src/components/RootSidenav/components/CollapsibleSection.tsx
@@ -20,14 +20,13 @@ export const CollapsibleSection = ({
     <div>
       <button
         onClick={onToggle}
-        className='w-full gap-1 flex justify-flex-start pl-3.5 cursor-pointer text-gray-500 hover:text-gray-700 transition-colors'
+        className='w-full gap-1 flex justify-flex-start items-center pl-3.5 cursor-pointer text-gray-500 hover:text-gray-700 transition-colors'
       >
         <span className='text-sm  text-gray-500'>{title}</span>
 
         <Icon
-          fill='currentColor'
-          name='arrow-dropdown'
-          className={cn('w-5 h-5', {
+          name='chevron-down'
+          className={cn('w-3 h-3', {
             'transform -rotate-90': !isOpen,
           })}
         />
