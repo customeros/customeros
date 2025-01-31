f4a419e77e53594fb8962ba629f31c767964fe79
Mihai <163020214+umihai29@users.noreply.github.com>
2025-01-31 16:23:11 +0200

    change: display only firstType of social (#11083)
    * change: display only firstType of social
    
    * use the isKnownUrl function

--- packages/apps/frontera/src/routes/organization/src/components/Tabs/shared/SocialMediaList/SocialMediaList.tsx
+++ packages/apps/frontera/src/routes/organization/src/components/Tabs/shared/SocialMediaList/SocialMediaList.tsx
@@ -3,27 +3,38 @@ import { useParams } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 
 import { useStore } from '@shared/hooks/useStore';
+import { SelectOption } from '@shared/types/SelectOptions.ts';
+import { Social } from '@shared/types/__generated__/graphql.types.ts';
 
+import { isKnownUrl } from './util.ts';
 import { SocialMediaItem } from './SocialMediaItem.tsx';
 
 interface SocialMediaListProps {
   dataTest?: string;
   isReadOnly?: boolean;
   leftElement?: React.ReactNode;
-  value?: { label: string; value: string }[];
 }
 
 export const SocialMediaList = observer(
-  ({ value, isReadOnly, dataTest, leftElement }: SocialMediaListProps) => {
+  ({ isReadOnly, dataTest, leftElement }: SocialMediaListProps) => {
     const store = useStore();
     const id = useParams()?.id as string;
     const organization = store.organizations.getById(store.ui.focusRow ?? id);
 
     if (!organization || !organization?.value) return null;
 
+    const filteredSocials = filterUniqueSocials(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      organization.value.socialMedia as any,
+    );
+    const socialOptions = filteredSocials.map((social) => ({
+      value: social.id,
+      label: social.url,
+    }));
+
     return (
       <div className='flex flex-wrap gap-2'>
-        {value?.map(({ value: v, label: l }) => (
+        {socialOptions.map(({ value: v, label: l }: SelectOption) => (
           <div key={v} className='w-auto '>
             <SocialMediaItem
               id={v}
@@ -39,3 +50,26 @@ export const SocialMediaList = observer(
     );
   },
 );
+
+//this function will be moved in the BE in the future
+
+const getSocialDomain = (url: string) => {
+  if (!url || typeof url !== 'string') return null;
+
+  return isKnownUrl(url.trim().toLowerCase()) || null;
+};
+
+const filterUniqueSocials = (socialArray = []) => {
+  const uniqueSocials = new Map();
+
+  socialArray.forEach((social: Social) => {
+    if (!social || typeof social !== 'object' || !social.url) return;
+    const domain = getSocialDomain(social.url);
+
+    if (domain && !uniqueSocials.has(domain)) {
+      uniqueSocials.set(domain, social);
+    }
+  });
+
+  return Array.from(uniqueSocials.values());
+};
