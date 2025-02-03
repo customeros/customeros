fc1465f70195c169fd3c680ccaa65b2105ae801d
kasia <kasia@openline.ai>
2025-02-03 17:35:30 +0100

    COS-6682: Posthog Identify user

--- packages/apps/frontera/src/routes/src/components/Providers/AnalyticsProvider.tsx
+++ packages/apps/frontera/src/routes/src/components/Providers/AnalyticsProvider.tsx
@@ -65,6 +65,15 @@ export const AnalyticsProvider = observer(
             });
           }
         }
+
+        if (import.meta.env.PROD) {
+          if (window.posthog) {
+            window.posthog.identify(id, { email, name });
+            window.posthog.group('tenant', store.session.value.tenant, {
+              campaign: store.session.value.campaign,
+            });
+          }
+        }
       });
     }, []);
 
