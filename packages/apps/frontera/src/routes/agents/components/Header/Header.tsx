c997e819e50bf631dcaabf159e17cd204d38e2df
Alex Calinica <acalinica@openline.ai>
2025-02-03 14:17:40 +0200

    Refactor agent components for improved styling and structure

--- packages/apps/frontera/src/routes/agents/components/Header/Header.tsx
+++ packages/apps/frontera/src/routes/agents/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className='w-full border-b border-b-gray-200 px-3 py-2 flex justify-between items-center'>
+    <div className='w-full border-b border-b-gray-200 px-3 py-[11px] flex justify-between items-center'>
       <p className='text-md font-medium'>Agents</p>
     </div>
   );
--- packages/apps/frontera/src/routes/src/components/RootSidenav/components/sections/NavigationSections.tsx
+++ packages/apps/frontera/src/routes/src/components/RootSidenav/components/sections/NavigationSections.tsx
@@ -1,11 +1,7 @@
-import { useLocation } from 'react-router-dom';
-
-import { observer } from 'mobx-react-lite';
 import { useFeatureIsOn } from '@growthbook/growthbook-react';
 
 import { cn } from '@ui/utils/cn';
 import { Icon } from '@ui/media/Icon';
-import { useStore } from '@shared/hooks/useStore';
 import { Preferences } from '@shared/components/RootSidenav/hooks';
 import { SidenavItem } from '@shared/components/RootSidenav/components/SidenavItem';
 import { TeamViewsSectionSection } from '@shared/components/RootSidenav/components/sections/TeamViewsSection';
@@ -23,81 +19,67 @@ interface NavigationSectionsProps {
   ) => boolean;
 }
 
-export const NavigationSections = observer(
-  ({
-    preferences,
-    togglePreference,
-    handleItemClick,
-    checkIsActive,
-  }: NavigationSectionsProps) => {
-    const store = useStore();
-    const { pathname } = useLocation();
-    const showCustomerMap = useFeatureIsOn('show-customer-map');
-    const flowSequencesView = store.tableViewDefs.getById(
-      store.tableViewDefs.flowsPreset ?? '',
-    );
-    const isFlowEditorActive = pathname.includes('flow-editor');
+export const NavigationSections = ({
+  preferences,
+  togglePreference,
+  handleItemClick,
+  checkIsActive,
+}: NavigationSectionsProps) => {
+  const showCustomerMap = useFeatureIsOn('show-customer-map');
 
-    return (
-      <div className='px-2 pt-2.5 gap-4 overflow-y-auto overflow-hidden flex flex-col flex-1'>
-        <div className='flex flex-col'>
-          {showCustomerMap && (
-            <SidenavItem
-              label='Customer map'
-              dataTest={`side-nav-item-customer-map`}
-              isActive={checkIsActive('customer-map')}
-              onClick={() => handleItemClick('customer-map')}
-              icon={(isActive) => (
-                <Icon
-                  name='bubbles'
-                  className={cn('text-gray-500', isActive && 'text-gray-700')}
-                />
-              )}
-            />
-          )}
-
+  return (
+    <div className='px-2 pt-2.5 gap-4 overflow-y-auto overflow-hidden flex flex-col flex-1'>
+      <div className='flex flex-col'>
+        {showCustomerMap && (
           <SidenavItem
-            label='Flows'
-            dataTest={`side-nav-item-all-flows`}
-            onClick={() =>
-              handleItemClick(`finder?preset=${flowSequencesView?.value?.id}`)
-            }
-            isActive={
-              checkIsActive('finder', {
-                preset: flowSequencesView?.value?.id ?? '',
-              }) || isFlowEditorActive
-            }
+            label='Customer map'
+            dataTest={`side-nav-item-customer-map`}
+            isActive={checkIsActive('customer-map')}
+            onClick={() => handleItemClick('customer-map')}
             icon={(isActive) => (
               <Icon
-                name='atom-01'
-                className={cn(
-                  'size-4 min-w-4 text-gray-500',
-                  isActive && 'text-gray-700',
-                )}
+                name='bubbles'
+                className={cn('text-gray-500', isActive && 'text-gray-700')}
               />
             )}
           />
-        </div>
+        )}
 
-        <TeamViewsSectionSection
-          preferences={preferences}
-          checkIsActive={checkIsActive}
-          handleItemClick={handleItemClick}
-          togglePreference={togglePreference}
-        />
-        <FavoritesSection
-          preferences={preferences}
-          checkIsActive={checkIsActive}
-          handleItemClick={handleItemClick}
-          togglePreference={togglePreference}
-        />
-        <GeneralViewsSection
-          preferences={preferences}
-          checkIsActive={checkIsActive}
-          handleItemClick={handleItemClick}
-          togglePreference={togglePreference}
+        <SidenavItem
+          label='Agents'
+          isActive={checkIsActive('agents')}
+          dataTest={`side-nav-item-all-agents`}
+          onClick={() => handleItemClick('agents')}
+          icon={(isActive) => (
+            <Icon
+              name='atom-01'
+              className={cn(
+                'size-4 min-w-4 text-gray-500',
+                isActive && 'text-gray-700',
+              )}
+            />
+          )}
         />
       </div>
-    );
-  },
-);
+
+      <TeamViewsSectionSection
+        preferences={preferences}
+        checkIsActive={checkIsActive}
+        handleItemClick={handleItemClick}
+        togglePreference={togglePreference}
+      />
+      <FavoritesSection
+        preferences={preferences}
+        checkIsActive={checkIsActive}
+        handleItemClick={handleItemClick}
+        togglePreference={togglePreference}
+      />
+      <GeneralViewsSection
+        preferences={preferences}
+        checkIsActive={checkIsActive}
+        handleItemClick={handleItemClick}
+        togglePreference={togglePreference}
+      />
+    </div>
+  );
+};
