dbe90021368d751b0407e54d7e5ed08e4cd07c23
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-02-05 10:44:55 +0000

    fix api parsing (#11161)

--- packages/server/customer-os-api/graphql/resolver/agent.resolvers.go
+++ packages/server/customer-os-api/graphql/resolver/agent.resolvers.go
@@ -8,15 +8,14 @@ import (
 	"context"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/common"
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/data_fields"
-	"github.com/customeros/customeros/packages/server/customer-os-common-module/utils"
-	postgresentity "github.com/customeros/customeros/packages/server/customer-os-postgres-repository/entity"
-
 	"github.com/customeros/customeros/packages/server/customer-os-api/graphql/model"
 	"github.com/customeros/customeros/packages/server/customer-os-api/mapper"
 	enummapper "github.com/customeros/customeros/packages/server/customer-os-api/mapper/enum"
 	"github.com/customeros/customeros/packages/server/customer-os-api/tracing"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/common"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/data_fields"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/utils"
+	postgresentity "github.com/customeros/customeros/packages/server/customer-os-postgres-repository/entity"
 )
 
 // AgentSave is the resolver for the agent_Save field.
