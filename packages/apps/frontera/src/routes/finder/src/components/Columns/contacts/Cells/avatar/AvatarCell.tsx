74178a08d4ddbc6f61d8710583b9a629539f24fc
Kasia <121807793+Kasia9090@users.noreply.github.com>
2025-02-05 15:10:30 +0100

    COS-6679: Handle fallback for avatars in the table in case images don't load (#11134)
    * COS-6679: Handle fallback for avatars in the table in case images don't load
    
    * Ensure contact avatar is a circle

--- packages/apps/frontera/src/routes/finder/src/components/Columns/contacts/Cells/avatar/AvatarCell.tsx
+++ packages/apps/frontera/src/routes/finder/src/components/Columns/contacts/Cells/avatar/AvatarCell.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { observer } from 'mobx-react-lite';
 import { useLocalStorage } from 'usehooks-ts';
 
@@ -21,6 +23,7 @@ export const AvatarCell = observer(
     const fullName = name || 'Unnamed';
     const contactStore = store.contacts.value.get(id);
     const [previewCard, setPreviewCard] = useLocalStorage('previewCard', false);
+    const [status, setStatus] = useState('loading');
 
     const isEnriching = contactStore?.isEnriching;
 
@@ -36,23 +39,31 @@ export const AvatarCell = observer(
             }
           }}
           className={cn(
-            'w-6 h-6 flex items-center justify-center rounded border border-gray-200 cursor-pointer focus:outline-none',
+            'w-6 h-6 flex items-center justify-center  border border-gray-200 cursor-pointer focus:outline-none rounded-full',
             {
               'animate-pulse': isEnriching,
               'cursor-default': !canNavigate,
             },
           )}
         >
-          {src ? (
-            <Image
-              src={src}
-              alt={fullName}
-              loading='lazy'
-              decoding='async'
-              fetchPriority='low'
-              className='w-full h-full object-contain'
-            />
-          ) : (
+          {src && status !== 'error' && (
+            <>
+              <Image
+                src={src}
+                alt={fullName}
+                loading='lazy'
+                decoding='async'
+                fetchPriority='low'
+                onError={() => setStatus('error')}
+                onLoad={() => setStatus('loaded')}
+                className={cn('w-full h-full object-contain rounded-full', {
+                  'opacity-0 size-0': status === 'loading',
+                  'opacity-100': status === 'loaded',
+                })}
+              />
+            </>
+          )}
+          {(!src || status === 'error' || status === 'loading') && (
             <User02 className='w-4 h-4 text-gray-700' />
           )}
         </div>
