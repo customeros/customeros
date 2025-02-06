cfecd4d090a44d5ec70a8d2959155b98e3edf97a
Eduard Firut <edi@customeros.ai>
2025-02-06 12:05:00 +0200

    fix

--- packages/server/customer-os-api/graphql/resolver/cache.resolvers_it_test.go
+++ packages/server/customer-os-api/graphql/resolver/cache.resolvers_it_test.go
@@ -8,46 +8,9 @@ import (
 	neo4jtest "github.com/customeros/customeros/packages/server/customer-os-neo4j-repository/test"
 	"github.com/stretchr/testify/require"
 
-	"github.com/customeros/customeros/packages/server/customer-os-api/graphql/model"
 	neo4jt "github.com/customeros/customeros/packages/server/customer-os-api/test/neo4j"
 	"github.com/customeros/customeros/packages/server/customer-os-api/utils/decode"
 )
-
-func TestQueryGlobalCache_GCliCache_IsOwnerTrue(t *testing.T) {
-	ctx := context.Background()
-	defer tearDownTestCase(ctx)(t)
-
-	neo4jtest.CreateTenant(ctx, driver, tenantName)
-	neo4jtest.CreateUser(ctx, driver, tenantName, neo4jentity.UserEntity{
-		Id:        testUserId,
-		FirstName: "a",
-		LastName:  "b",
-	})
-	organizationId := neo4jtest.CreateOrganization(ctx, driver, tenantName, neo4jentity.OrganizationEntity{Name: "org1"})
-
-	neo4jtest.UserOwnsOrganization(ctx, driver, testUserId, organizationId)
-
-	require.Equal(t, 1, neo4jtest.GetCountOfNodes(ctx, driver, "Tenant"))
-	require.Equal(t, 1, neo4jtest.GetCountOfNodes(ctx, driver, "User"))
-	require.Equal(t, 1, neo4jtest.GetCountOfNodes(ctx, driver, "Organization"))
-	require.Equal(t, 1, neo4jtest.GetCountOfRelationships(ctx, driver, "OWNS"))
-
-	rawResponse, err := c.RawPost(getQuery("cache/global_Cache"))
-	assertRawResponseSuccess(t, rawResponse, err)
-
-	var gcliCacheResponse struct {
-		Global_Cache struct {
-			User    model.User `json:"user"`
-			IsOwner bool       `json:"isOwner"`
-		}
-	}
-
-	err = decode.Decode(rawResponse.Data.(map[string]any), &gcliCacheResponse)
-	require.Nil(t, err)
-	require.NotNil(t, gcliCacheResponse)
-
-	require.Equal(t, true, gcliCacheResponse.Global_Cache.IsOwner)
-}
 
 func TestQueryGlobalCache_GCliCache_HasContracts_False(t *testing.T) {
 	ctx := context.Background()
