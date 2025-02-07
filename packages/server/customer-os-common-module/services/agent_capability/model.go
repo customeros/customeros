f522772339f2c03f25447ca029bf318120223f23
mattbr0wn <matt@openline.ai>
2025-02-07 10:21:03 +0000

    add company picker config

--- packages/server/customer-os-common-module/services/agent_capability/model.go
+++ packages/server/customer-os-common-module/services/agent_capability/model.go
@@ -1,3 +1,13 @@
 package agent_capability
 
 type NoOutput struct{}
+
+type ConfigSingleValue struct {
+	Value string `json:"value"`
+	Error string `json:"error"`
+}
+
+type ConfigMultipleValues struct {
+	Value []string `json:"value"`
+	Error string   `json:"error"`
+}
