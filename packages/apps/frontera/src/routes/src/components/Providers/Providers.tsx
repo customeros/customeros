76724e9b036867d785b7f46965948295bf91e1b7
Alex Calinica <acalinica@openline.ai>
2025-02-07 13:09:28 +0200

    Remove Devtools component from Providers

--- packages/apps/frontera/src/routes/src/components/Providers/Providers.tsx
+++ packages/apps/frontera/src/routes/src/components/Providers/Providers.tsx
@@ -5,7 +5,6 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { StoreProvider } from './StoreProvider';
-import { Devtools } from '../Devtools/Devtools';
 import { AnalyticsProvider } from './AnalyticsProvider';
 import { PhoenixSocketProvider } from './SocketProvider';
 import { GrowthbookProvider } from './GrowthbookProvider';
@@ -35,7 +34,6 @@ export const Providers = ({ children, isProduction }: ProvidersProps) => {
           <RecoilRoot>
             <IntegrationsProvider>
               <GrowthbookProvider>
-                <Devtools />
                 <AnalyticsProvider isProduction={isProduction}>
                   {children}
                   <ToastContainer
