c15ae5d6bb5a815ead884ad9c7fc848619e3a8c7
alexopenline <95470380+alexopenline@users.noreply.github.com>
2025-02-07 10:36:55 +0200

    Fix slack sync (#11212)

--- packages/runner/sync-slack/repository/repositories.go
+++ packages/runner/sync-slack/repository/repositories.go
@@ -1,17 +1,16 @@
 package repository
 
 import (
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	neo4jrepo "github.com/customeros/customeros/packages/runner/sync-slack/repository/neo4j"
 	postgresrepo "github.com/customeros/customeros/packages/runner/sync-slack/repository/postgres"
 	"github.com/customeros/customeros/packages/server/customer-os-common-module/config"
 	postgresRepository "github.com/customeros/customeros/packages/server/customer-os-postgres-repository/repository"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 type Repositories struct {
 	PostgresRepositories *postgresRepository.Repositories
 
-	TenantSettingsRepository    postgresrepo.TenantSettingsRepository
 	SlackSyncSettingsRepository postgresrepo.SlackSyncSettingsRepository
 	SlackSyncRunRepository      postgresrepo.SlackSyncRunRepository
 
@@ -23,7 +22,6 @@ func InitRepositories(driver *neo4j.DriverWithContext, postgresDB *config.Postgr
 	repositories := Repositories{
 		PostgresRepositories: postgresRepository.InitRepositories(postgresDB),
 
-		TenantSettingsRepository:    postgresrepo.NewTenantSettingsRepository(postgresDB.GormDB),
 		SlackSyncSettingsRepository: postgresrepo.NewSlackSyncSettingsRepository(postgresDB.GormDB),
 		SlackSyncRunRepository:      postgresrepo.NewSlackSyncRunRepository(postgresDB.GormDB),
 
