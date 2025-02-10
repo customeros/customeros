cf45cd3cab9ee98f256e772aefc7487225016a39
Eduard Firut <edi@customeros.ai>
2025-02-10 14:28:45 +0200

    Emails work

--- packages/server/customer-os-postgres-repository/entity/oauth_token.go
+++ packages/server/customer-os-postgres-repository/entity/oauth_token.go
@@ -1,6 +1,11 @@
 package postgres_entity
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/pkg/errors"
 	"time"
 )
 
@@ -22,4 +27,66 @@ type OAuthTokenEntity struct {
 
 func (OAuthTokenEntity) TableName() string {
 	return "oauth_token"
+}
+
+// Encrypt a token using AES-GCM
+func EncryptToken(key string, token string) (string, error) {
+	decodedKey, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher([]byte(decodedKey))
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte(token), nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+// Decrypt a token using AES-GCM
+func DecryptToken(key string, encryptedToken string) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(encryptedToken)
+	if err != nil {
+		return "", err
+	}
+
+	decodedKey, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher([]byte(decodedKey))
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
 }
