5703722591d4cd6c4d17afcd86b096c29eb1ccf8
alexopenline <alex@openline.ai>
2025-02-12 18:23:56 +0200

    fix agent scope query

--- packages/server/customer-os-common-module/services/agent_capability/model.go
+++ packages/server/customer-os-common-module/services/agent_capability/model.go
@@ -7,6 +7,11 @@ type ConfigSingleValue struct {
 	Error string `json:"error"`
 }
 
+type ConfigSingleBoolValue struct {
+	Value bool   `json:"value"`
+	Error string `json:"error"`
+}
+
 type ConfigMultipleValues struct {
 	Value []string `json:"value"`
 	Error string   `json:"error"`
