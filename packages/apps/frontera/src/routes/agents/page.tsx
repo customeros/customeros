294f67c547e3dcffa3e757ded10c76691e2f3816
mihai@customeros.ai <mihai@customeros.ai>
2025-02-17 17:23:00 +0200

    fix

--- packages/apps/frontera/src/routes/agents/page.tsx
+++ packages/apps/frontera/src/routes/agents/page.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 
 import { observer } from 'mobx-react-lite';
-import { useLocalStorage } from 'usehooks-ts';
+import { useUnmount, useLocalStorage } from 'usehooks-ts';
 
 import { useStore } from '@shared/hooks/useStore';
+import { PreviewCard } from '@shared/components/PreviewCard';
+import { ShortcutsPanel } from '@shared/components/PreviewCard/components/ShortcutsPanel';
 import {
   ScrollAreaRoot,
   ScrollAreaThumb,
@@ -45,35 +47,55 @@ export const AgentsPage = observer(() => {
     store.ui.commandMenu.setType('AgentsCommands');
   }, []);
 
+  useUnmount(() => {
+    store.ui.setShortcutsPanel(false);
+  });
+
   return (
-    <div className='relative h-full'>
+    <div>
       <Header />
-      <ScrollAreaRoot className='h-full'>
-        <ScrollAreaViewport>
-          <div className='flex flex-wrap p-4 gap-4'>
-            {agents
-              .filter((agent) => agent.value.visible)
-              .map((agent) => (
-                <AgentCard
-                  id={agent.id}
-                  key={agent.id}
-                  icon={agent.value.icon}
-                  name={agent.value.name}
-                  colorMap={agent.colorMap}
-                  defaultName={agent.defaultName}
-                  status={agent.value.isActive ? 'ON' : 'OFF'}
-                  hasError={!!agent.value.error || !agent.value.isConfigured}
-                />
-              ))}
-            <div className='min-w-[372px] flex-1 p-3'></div>
-            <div className='min-w-[372px] flex-1 p-3'></div>
-            <div className='min-w-[372px] flex-1 p-3'></div>
+      <div className='flex h-full'>
+        <div className='relative h-full w-full'>
+          <div className='h-full'>
+            <ScrollAreaRoot className='h-full'>
+              <ScrollAreaViewport>
+                <div className='flex flex-wrap p-4 gap-4'>
+                  {agents
+                    .filter((agent) => agent.value.visible)
+                    .map((agent) => (
+                      <AgentCard
+                        id={agent.id}
+                        key={agent.id}
+                        icon={agent.value.icon}
+                        name={agent.value.name}
+                        colorMap={agent.colorMap}
+                        defaultName={agent.defaultName}
+                        status={agent.value.isActive ? 'ON' : 'OFF'}
+                        hasError={
+                          !!agent.value.error || !agent.value.isConfigured
+                        }
+                      />
+                    ))}
+                  <div className='min-w-[372px] flex-1 p-3'></div>
+                  <div className='min-w-[372px] flex-1 p-3'></div>
+                  <div className='min-w-[372px] flex-1 p-3'></div>
+                </div>
+              </ScrollAreaViewport>
+              <ScrollAreaScrollbar orientation='vertical'>
+                <ScrollAreaThumb />
+              </ScrollAreaScrollbar>
+            </ScrollAreaRoot>
           </div>
-        </ScrollAreaViewport>
-        <ScrollAreaScrollbar orientation='vertical'>
-          <ScrollAreaThumb />
-        </ScrollAreaScrollbar>
-      </ScrollAreaRoot>
+        </div>
+
+        <div className='h-full'>
+          {store.ui.showShortcutsPanel && (
+            <PreviewCard>
+              <ShortcutsPanel />
+            </PreviewCard>
+          )}
+        </div>
+      </div>
     </div>
   );
 });
--- packages/apps/frontera/src/routes/src/components/PreviewCard/PreviewCard.tsx
+++ packages/apps/frontera/src/routes/src/components/PreviewCard/PreviewCard.tsx
@@ -10,7 +10,7 @@ export const PreviewCard = ({ children }: PreviewCardProps) => {
   return (
     <div
       data-state={previewCard ? 'open' : 'closed'}
-      className='data-[state=open]:animate-slideLeftAndFade data-[state=closed]:animate-slideRightAndFade flex flex-col border border-r-0 border-t-0 border-gray-200 bg-white max-w-[400px] min-w-[400px]'
+      className='data-[state=open]:animate-slideLeftAndFade data-[state=closed]:animate-slideRightAndFade flex flex-col border border-r-0 border-t-0 border-b-0 border-gray-200  max-w-[400px] min-w-[400px] overflow-y-auto h-screen'
     >
       {children}
     </div>
