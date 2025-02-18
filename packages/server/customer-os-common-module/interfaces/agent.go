630a5bb2634ada15860d42a7c697a0c843466670
mattbr0wn <matt@openline.ai>
2025-02-18 15:57:48 +0000

    stats in api

--- packages/server/customer-os-common-module/interfaces/agent.go
+++ packages/server/customer-os-common-module/interfaces/agent.go
@@ -20,6 +20,7 @@ type AgentService interface {
 
 	CreateAgentExecutionRecord(ctx context.Context, agent postgres_entity.Agent, triggerEvent, traceId string) (string, error)
 	GetAgentInfo(ctx context.Context) (*map[enum.AgentType]AgentInfo, error)
+	GetNorthStarMetricById(ctx context.Context, agentID string, agentType enum.AgentType) (string, error)
 }
 
 type AgentRegistry interface {
