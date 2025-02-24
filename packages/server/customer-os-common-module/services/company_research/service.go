dab0e1faec0a966ea5d631692ee6c05bb03b57a7
mattbr0wn <matt@openline.ai>
2025-02-24 12:24:36 +0000

    save

---
+++ packages/server/customer-os-common-module/services/company_research/service.go
@@ -0,0 +1,38 @@
+package company_research
+
+import (
+	"context"
+
+	postgres_repository "github.com/customeros/customeros/packages/server/customer-os-postgres-repository/repository"
+	"github.com/opentracing/opentracing-go"
+
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/interfaces"
+	"github.com/customeros/customeros/packages/server/customer-os-common-module/tracing"
+)
+
+type companyResearchService struct {
+	postgresRepositories *postgres_repository.Repositories
+	aiService            interfaces.AIService
+}
+
+func NewCompanyResearchService(
+	postgresRepositories *postgres_repository.Repositories,
+	aiService interfaces.AIService,
+) interfaces.CompanyResearch {
+	return &companyResearchService{
+		postgresRepositories: postgresRepositories,
+		aiService:            aiService,
+	}
+}
+
+func (s *companyResearchService) GenerateIdealCustomerProfile(ctx context.Context, tenantDomain string, trainingWebsites []string) (string, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "icpService.GenerateIdealCustomerProfile")
+	defer span.Finish()
+	tracing.SetDefaultServiceSpanTags(ctx, span)
+}
+
+func (s *companyResearchService) GenerateCompanyBrief(ctx context.Context, domains []string) (string, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "companyResearchService.GenerateCompanyBrief")
+	defer span.Finish()
+	tracing.SetDefaultServiceSpanTags(ctx, span)
+}
