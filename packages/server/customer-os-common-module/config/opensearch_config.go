75ccdefc221d0595e0a8ec1193be72452dca6d1c
mattbr0wn <matt@openline.ai>
2025-03-05 16:16:25 +0000

    fix opensearch config

--- packages/server/customer-os-common-module/config/opensearch_config.go
+++ packages/server/customer-os-common-module/config/opensearch_config.go
@@ -1,25 +1,15 @@
 package config
 
 type OpensearchConfig struct {
-	Tracing *OpensearchTracingConfig
-	Events  *OpensearchEventsConfig
-	AI      *OpensearchAIConfig
-}
+	TracingUrl      string `env:"OPENSEARCH_TRACING_URL"`
+	TracingUsername string `env:"OPENSEARCH_TRACING_USERNAME"`
+	TracingPassword string `env:"OPENSEARCH_TRACING_PASSWORD"`
 
-type OpensearchTracingConfig struct {
-	Url      string `env:"OPENSEARCH_TRACING_URL"`
-	Username string `env:"OPENSEARCH_TRACING_USERNAME"`
-	Password string `env:"OPENSEARCH_TRACING_PASSWORD"`
-}
+	EventsUrl      string `env:"OPENSEARCH_EVENTS_URL"`
+	EventsUsername string `env:"OPENSEARCH_EVENTS_USERNAME"`
+	EventsPassword string `env:"OPENSEARCH_EVENTS_PASSWORD"`
 
-type OpensearchEventsConfig struct {
-	Url      string `env:"OPENSEARCH_EVENTS_URL"`
-	Username string `env:"OPENSEARCH_EVENTS_USERNAME"`
-	Password string `env:"OPENSEARCH_EVENTS_PASSWORD"`
-}
-
-type OpensearchAIConfig struct {
-	Url      string `env:"OPENSEARCH_AI_URL"`
-	Username string `env:"OPENSEARCH_AI_USERNAME"`
-	Password string `env:"OPENSEARCH_AI_PASSWORD"`
+	AIUrl      string `env:"OPENSEARCH_AI_URL"`
+	AIUsername string `env:"OPENSEARCH_AI_USERNAME"`
+	AIPassword string `env:"OPENSEARCH_AI_PASSWORD"`
 }
