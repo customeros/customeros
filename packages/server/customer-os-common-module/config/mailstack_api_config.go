3ba9f4bcbb34cb0ac5b5e1e5ce36edd6e39d6e6c
alexopenline <alex@openline.ai>
2025-03-14 20:37:19 +0200

    remove default api url

--- packages/server/customer-os-common-module/config/mailstack_api_config.go
+++ packages/server/customer-os-common-module/config/mailstack_api_config.go
@@ -1,7 +1,7 @@
 package config
 
 type MailstackApiConfig struct {
-	ApiUrl string `env:"MAILSTACK_API_URL" envDefault:"https://api.mailstack.customeros.ai"`
+	ApiUrl string `env:"MAILSTACK_API_URL"`
 	ApiKey string `env:"MAILSTACK_API_KEY"`
 	// Deprecated, to be removed
 	SupportedTlds []string `env:"MAILSTACK_SUPPORTED_TLD" envDefault:"com"`
