032e5e041e482f57e0ade148929d813bdc6b92f8
alexopenline <alex@openline.ai>
2025-03-15 15:20:00 +0200

    remove open srs unused method

--- packages/server/customer-os-common-module/interfaces/opensrs.go
+++ packages/server/customer-os-common-module/interfaces/opensrs.go
@@ -9,7 +9,6 @@ import (
 type OpenSrsService interface {
 	SendEmail(ctx context.Context, request *postgres_entity.EmailMessage) error
 	SetupMailbox(ctx context.Context, tenant, username, password string, forwardingTo []string, webmailEnabled bool) error
-	GetMailboxDetails(ctx context.Context, email string) (MailboxDetails, error)
 }
 
 type MailboxDetails struct {
