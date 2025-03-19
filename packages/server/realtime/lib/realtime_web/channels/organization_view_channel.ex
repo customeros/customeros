41def5733f3ada85d70df5ac1223d91e41c5aedf
Alex Calinica <acalinica@openline.ai>
2025-03-19 17:12:27 +0200

    Add join function without username to channels
    Remove unused handle_in functions and comments

--- packages/server/realtime/lib/realtime_web/channels/organization_view_channel.ex
+++ packages/server/realtime/lib/realtime_web/channels/organization_view_channel.ex
@@ -11,6 +11,23 @@ defmodule RealtimeWeb.OrganizationViewChannel do
   @impl true
   def join(
         "organization_presence:" <> _organization_id,
+        %{"user_id" => user_id},
+        socket
+      ) do
+    {:ok, color} = ColorManager.assign_color(user_id)
+
+    socket =
+      socket
+      |> assign(:user_id, user_id)
+      |> assign(user_color: %{user_id => color})
+
+    send(self(), :after_join)
+    {:ok, socket}
+  end
+
+  @impl true
+  def join(
+        "organization_presence:" <> _organization_id,
         %{"user_id" => user_id, "username" => username},
         socket
       ) do
@@ -32,53 +49,12 @@ defmodule RealtimeWeb.OrganizationViewChannel do
       Presence.track(socket, socket.assigns.user_id, %{
         online_at: inspect(System.system_time(:second)),
         metadata: %{"source" => "customerOS"},
-        username: socket.assigns.username,
+        username: Map.get(socket.assigns, :username, "Anonymous"),
         user_id: socket.assigns.user_id,
         color: Map.get(socket.assigns.user_color, socket.assigns.user_id)
       })
 
     push(socket, "presence_state", Presence.list(socket))
-    # push(socket, "feed", %{list: feed_items(socket)}) # push CRDT state to the client
-    {:noreply, socket}
-  end
-
-  # # Helper functions
-  # defp merge_states(crdt_state, client_state) do
-  #   Map.merge(client_state, crdt_state)
-  # end
-  # defp feed_items(socket) do
-  #   # Use the socket to fetch the feed items
-  #   # if client has data already in the socket, use that
-  #   # client_state = if Map.has_key?(payload, "body") do
-  #   #   Map.fetch!(payload, "body")
-  #   # else
-  #   #   %{"body" => ""}
-  #   # end
-  #   client_state = %{"body" => ""}
-  #   # Fetch current CRDT state
-  #   crdt_state = RealtimeWeb.GlobalConfig.get(:payload)
-  #   Logger.debug("CRDT state: #{inspect(crdt_state)}")
-
-  #   # Merge CRDT state with client state
-  #   merged_state = merge_states(crdt_state, client_state)
-  #   Logger.debug("Merged state: #{inspect(merged_state)}")
-
-  #   # Push merged state back to the client
-  #   push(socket, "shout", merged_state)
-  # end
-
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (chat:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast!(socket, "shout", payload)
     {:noreply, socket}
   end
 
