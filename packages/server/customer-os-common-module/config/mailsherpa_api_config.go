9eafaae75ff46a7adf9d0f5fb568b0bda793e131
mattbr0wn <matt@openline.ai>
2025-03-29 10:19:34 +0000

    update dns

--- packages/server/customer-os-common-module/config/mailsherpa_api_config.go
+++ packages/server/customer-os-common-module/config/mailsherpa_api_config.go
@@ -1,6 +1,6 @@
 package config
 
 type MailSherpaApiConfig struct {
-	MailsherpaApiUrl string `env:"MAILSHERPA_API_URL" envDefault:"https://mailsherpa.customeros.ai"`
+	MailsherpaApiUrl string `env:"MAILSHERPA_API_URL" envDefault:"https://mailsherpa.customeros.cloud"`
 	MailsherpaApiKey string `env:"MAILSHERPA_API_KEY" envDefault:""`
 }
