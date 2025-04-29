2b42e3beac102771c45c904b775ffa218be94ab7
matt brown <matt@customeros.ai>
2025-04-29 17:30:24 +0100

    patch: distroless image

--- packages/server/leads/main.go
+++ packages/server/leads/main.go
@@ -60,43 +60,26 @@ func main() {
 		log.Fatalf("Openline database initialization failed: %v", err)
 	}
 
-	switch os.Args[1] {
-	case "migrate":
-		// Run Mailstack database migrations
-		// err := repository.MigrateInboxDB(cfg.InboxDatabaseConfig, inboxDB)
-		// if err != nil {
-		// 	log.Fatalf("Inbox database migration failed: %v", err)
-		// }
-		// log.Println("Inbox database migration completed successfully")
-		//
-		// // Run DataWarehouse migrations
-		// err = repository.MigrateDataWarehouse(cfg.DataWarehouseConfig, warehouseDB)
-		// if err != nil {
-		// 	log.Fatalf("Warehouse database migration failed: %v", err)
-		// }
-		// log.Println("Warehouse database migration completed successfully")
-
-	case "server":
-		log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-		log.Println("ActivityStream starting up...")
-
-		srv, err := server.NewServer(cfg, leadsDB, warehouseDB)
-		if err != nil {
-			log.Fatalf("Server setup failed: %v", err)
-		}
-
-		// Start the server
-		err = srv.Run()
-		if err != nil {
-			log.Fatalf("Server startup failed: %v", err)
-		}
-
-	default:
-		fmt.Printf("Unknown command: %s\n", os.Args[1])
-		fmt.Println("Usage: go run <command>")
-		fmt.Println("Commands:")
-		fmt.Println("  migrate   Run database migrations")
-		fmt.Println("  server    Start the application server")
-		os.Exit(1)
+	err = runDBMigration()
+	if err != nil {
+		log.Fatalf("Database migration failed...")
 	}
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.Println("Leads starting up...")
+
+	srv, err := server.NewServer(cfg, leadsDB, warehouseDB)
+	if err != nil {
+		log.Fatalf("Server setup failed: %v", err)
+	}
+
+	// Start the server
+	err = srv.Run()
+	if err != nil {
+		log.Fatalf("Server startup failed: %v", err)
+	}
+}
+
+func runDBMigration() error {
+	return nil
 }
