5243040fb7847939d8a67c907a010f7c7e63cd30
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-05-05 20:55:25 +0100

    patch: update build dependencies (#12095)

--- packages/server/core-crm/internal/models/company.go
+++ packages/server/core-crm/internal/models/company.go
@@ -5,8 +5,8 @@ import "github.com/customeros/customeros/packages/server/core-crm/internal/enum"
 type Company struct {
 	ID               string                    `gorm:"column:id;primaryKey;type:varchar(30);"`
 	Tenant           string                    `gorm:"column:tenant;type:varchar(255);index;not null"`
-	CustomID         string                    `gorm:"column:tenant;type:varchar(255);index;not null"`
-	GlobalOrgID      string                    `gorm:"column:tenant;type:varchar(255);index;not null"`
+	CustomID         string                    `gorm:"column:custom_id;type:varchar(255);index;not null"`
+	GlobalOrgID      string                    `gorm:"column:global_org_id;type:varchar(255);index;not null"`
 	ICPFit           string                    `gorm:"column:country;type:varchar(255)"`
 	ICPFitUpdatedAt  string                    `gorm:"column:tenant;type:varchar(255);index;not null"`
 	Stage            enum.CustomerJourneyStage `gorm:"column:stage;type:varchar(50);index;default:'target'"`
