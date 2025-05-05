e692d4a32cffd0bec9e24d004a77abf95137751b
matt brown <matt@customeros.ai>
2025-05-05 20:20:58 +0100

    patch: update build dependencies

--- deployment/runners/build-and-push-image.sh
+++ deployment/runners/build-and-push-image.sh
@@ -10,7 +10,9 @@ ARCH=${5:-"arm64"}
 REPO=$6
 APP_PATH=$7
 
-CONTAINERFILE_PATH="./${APP_PATH}/deployments/build/Containerfile"
+cd ${APP_PATH}
+
+CONTAINERFILE_PATH="./deployments/build/Containerfile"
 
 # Ensure required variables are set
 if [ -z "$REGISTRY" ] || [ -z "$IMAGE_NAME" ] || [ -z "$VERSION" ]; then
