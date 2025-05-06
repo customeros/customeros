e9c0efa1f8cc20b456976d6dfd76c4da13f6ef78
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-05-06 18:09:03 +0100

    patch: fix nats startup (#12125)

--- deployment/up.sh
+++ deployment/up.sh
@@ -1,5 +1,5 @@
 #docker build -t ghcr.io/customeros/customeros/neo4j-sidecar:latest -f neo4j-sidecar/Dockerfile .
 #docker push ghcr.io/customeros/customeros/neo4j-sidecar:latest
 
-docker compose -f docker-compose.yaml pull
-docker compose -f docker-compose.yaml up -d
+docker compose -f compose.yaml pull
+docker compose -f compose.yaml up -d
