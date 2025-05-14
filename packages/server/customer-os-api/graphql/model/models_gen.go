2a3cb2bdb44cbd2fe428caadd9abbfbdd2160cb9
alexopenline <alex@openline.ai>
2025-05-14 11:47:48 +0300

    remove deprecated stages

--- packages/server/customer-os-api/graphql/model/models_gen.go
+++ packages/server/customer-os-api/graphql/model/models_gen.go
@@ -5978,23 +5978,14 @@ func (e OrganizationRelationship) MarshalJSON() ([]byte, error) {
 type OrganizationStage string
 
 const (
-	OrganizationStageTarget         OrganizationStage = "TARGET"
-	OrganizationStageEducation      OrganizationStage = "EDUCATION"
-	OrganizationStageSolution       OrganizationStage = "SOLUTION"
-	OrganizationStageEvaluation     OrganizationStage = "EVALUATION"
-	OrganizationStageReadyToBuy     OrganizationStage = "READY_TO_BUY"
-	OrganizationStageOpportunity    OrganizationStage = "OPPORTUNITY"
-	OrganizationStageCustomer       OrganizationStage = "CUSTOMER"
-	OrganizationStageNotAFit        OrganizationStage = "NOT_A_FIT"
-	OrganizationStageEngaged        OrganizationStage = "ENGAGED"
-	OrganizationStageInitialValue   OrganizationStage = "INITIAL_VALUE"
-	OrganizationStageLead           OrganizationStage = "LEAD"
-	OrganizationStageMaxValue       OrganizationStage = "MAX_VALUE"
-	OrganizationStageOnboarding     OrganizationStage = "ONBOARDING"
-	OrganizationStagePendingChurn   OrganizationStage = "PENDING_CHURN"
-	OrganizationStageRecurringValue OrganizationStage = "RECURRING_VALUE"
-	OrganizationStageTrial          OrganizationStage = "TRIAL"
-	OrganizationStageUnqualified    OrganizationStage = "UNQUALIFIED"
+	OrganizationStageTarget      OrganizationStage = "TARGET"
+	OrganizationStageEducation   OrganizationStage = "EDUCATION"
+	OrganizationStageSolution    OrganizationStage = "SOLUTION"
+	OrganizationStageEvaluation  OrganizationStage = "EVALUATION"
+	OrganizationStageReadyToBuy  OrganizationStage = "READY_TO_BUY"
+	OrganizationStageOpportunity OrganizationStage = "OPPORTUNITY"
+	OrganizationStageCustomer    OrganizationStage = "CUSTOMER"
+	OrganizationStageNotAFit     OrganizationStage = "NOT_A_FIT"
 )
 
 var AllOrganizationStage = []OrganizationStage{
@@ -6006,20 +5997,11 @@ var AllOrganizationStage = []OrganizationStage{
 	OrganizationStageOpportunity,
 	OrganizationStageCustomer,
 	OrganizationStageNotAFit,
-	OrganizationStageEngaged,
-	OrganizationStageInitialValue,
-	OrganizationStageLead,
-	OrganizationStageMaxValue,
-	OrganizationStageOnboarding,
-	OrganizationStagePendingChurn,
-	OrganizationStageRecurringValue,
-	OrganizationStageTrial,
-	OrganizationStageUnqualified,
 }
 
 func (e OrganizationStage) IsValid() bool {
 	switch e {
-	case OrganizationStageTarget, OrganizationStageEducation, OrganizationStageSolution, OrganizationStageEvaluation, OrganizationStageReadyToBuy, OrganizationStageOpportunity, OrganizationStageCustomer, OrganizationStageNotAFit, OrganizationStageEngaged, OrganizationStageInitialValue, OrganizationStageLead, OrganizationStageMaxValue, OrganizationStageOnboarding, OrganizationStagePendingChurn, OrganizationStageRecurringValue, OrganizationStageTrial, OrganizationStageUnqualified:
+	case OrganizationStageTarget, OrganizationStageEducation, OrganizationStageSolution, OrganizationStageEvaluation, OrganizationStageReadyToBuy, OrganizationStageOpportunity, OrganizationStageCustomer, OrganizationStageNotAFit:
 		return true
 	}
 	return false
--- packages/server/customer-os-common-module/enum/organization_stage.go
+++ packages/server/customer-os-common-module/enum/organization_stage.go
@@ -11,25 +11,6 @@ const (
 	Opportunity OrganizationStage = "OPPORTUNITY"
 	Customer    OrganizationStage = "CUSTOMER"
 	NotAFit     OrganizationStage = "NOT_A_FIT"
-
-	// Deprecated
-	Lead OrganizationStage = "LEAD"
-	// Deprecated
-	Engaged OrganizationStage = "ENGAGED"
-	// Deprecated
-	Unqualified OrganizationStage = "UNQUALIFIED"
-	// Deprecated
-	Onboarding OrganizationStage = "ONBOARDING"
-	// Deprecated
-	InitialValue OrganizationStage = "INITIAL_VALUE"
-	// Deprecated
-	RecurringValue OrganizationStage = "RECURRING_VALUE"
-	// Deprecated
-	MaxValue OrganizationStage = "MAX_VALUE"
-	// Deprecated
-	PendingChurn OrganizationStage = "PENDING_CHURN"
-	// Deprecated
-	Trial OrganizationStage = "TRIAL"
 )
 
 func (e OrganizationStage) String() string {
@@ -54,25 +35,6 @@ func DecodeOrganizationStage(str string) OrganizationStage {
 		return Customer
 	case NotAFit.String():
 		return NotAFit
-
-	case Lead.String():
-		return Lead
-	case Engaged.String():
-		return Engaged
-	case Unqualified.String():
-		return Unqualified
-	case Onboarding.String():
-		return Onboarding
-	case InitialValue.String():
-		return InitialValue
-	case RecurringValue.String():
-		return RecurringValue
-	case MaxValue.String():
-		return MaxValue
-	case PendingChurn.String():
-		return PendingChurn
-	case Trial.String():
-		return Trial
 	default:
 		return ""
 	}
