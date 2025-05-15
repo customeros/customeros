cf90013263f3c4f12136c22ecf7d2a09c6860723
alexopenline <alex@openline.ai>
2025-05-15 12:05:21 +0300

    patch: process tenant created event

--- packages/server/core-crm/go.mod
+++ packages/server/core-crm/go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/customeros/customeros/packages/server/customer-os-common-module v0.0.0-20250512171648-ebf2777022e6
 	github.com/customeros/customeros/packages/server/customer-os-neo4j-repository v0.0.0-20250512171648-ebf2777022e6
 	github.com/customeros/customeros/packages/server/customer-os-postgres-repository v0.0.0-20250512171648-ebf2777022e6
-	github.com/customeros/mailstack v0.2.23
+	github.com/customeros/customeros/packages/server/enums v0.0.0-20250514170720-11697afc2afb
 	github.com/gin-gonic/gin v1.10.0
 	github.com/joho/godotenv v1.5.1
 	github.com/nats-io/nats.go v1.42.0
@@ -28,7 +28,6 @@ require (
 	go.opentelemetry.io/otel/sdk v1.35.0
 	go.opentelemetry.io/otel/trace v1.35.0
 	go.uber.org/zap v1.27.0
-	google.golang.org/protobuf v1.36.6
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.26.1
 	k8s.io/apimachinery v0.33.0
@@ -60,7 +59,6 @@ require (
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/coocood/freecache v1.2.4 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
-	github.com/customeros/customeros/packages/server/enums v0.0.0-20250514170720-11697afc2afb // indirect
 	github.com/customeros/mailsherpa v0.3.9 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
@@ -178,7 +176,8 @@ require (
 	google.golang.org/api v0.210.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250218202821-56aae31c358a // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/grpc v1.72.0 // indirect
+	google.golang.org/grpc v1.72.1 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- packages/server/core-crm/service/outbox_processor/process_event.go
+++ packages/server/core-crm/service/outbox_processor/process_event.go
@@ -3,8 +3,12 @@ package outbox_processor
 import (
 	"context"
 	"errors"
+	"fmt"
+	nats_common "github.com/customeros/customeros/packages/server/customer-os-common-module/nats"
 	"github.com/customeros/customeros/packages/server/customer-os-common-module/telemetry"
 	postgres_entity "github.com/customeros/customeros/packages/server/customer-os-postgres-repository/entity"
+	"github.com/customeros/customeros/packages/server/enums"
+	"github.com/nats-io/nats.go"
 )
 
 func (s *OutboxProcessor) processEvent(ctx context.Context, outboxEvent *postgres_entity.OutboxEvent) error {
@@ -14,8 +18,8 @@ func (s *OutboxProcessor) processEvent(ctx context.Context, outboxEvent *postgre
 	span.TagEntity(outboxEvent.ID)
 
 	switch {
-	//case strings.HasPrefix(event.EventType.String(), "webtracker"):
-	//	return s.processWebTrackerEvent(ctx, event)
+	case outboxEvent.EventType == enums.EventTenantCreated:
+		return s.processTenantCreatedEvent(ctx, outboxEvent)
 
 	default:
 		err := errors.New("event type not implemented")
@@ -24,95 +28,34 @@ func (s *OutboxProcessor) processEvent(ctx context.Context, outboxEvent *postgre
 	}
 }
 
-//
-//func (s *OutboxProcessor) processWebscraperEvent(ctx context.Context, event *models.OutboxEvent) error {
-//	span, ctx := telemetry.StartServiceSpan(ctx, "OutboxProcessor.processWebscraperEvent")
-//	defer span.Finish()
-//
-//	scrapedEvent := &pb.WebpageScraped{}
-//	err := proto.Unmarshal(event.Payload, scrapedEvent)
-//	if err != nil {
-//		span.TraceError(err)
-//		return err
-//	}
-//
-//	eventLog := &models.ScraperEvent{
-//		ID:        event.ID,
-//		Timestamp: event.CreatedAt,
-//		Event:     event.EventType,
-//		Publisher: event.Publisher,
-//		Domain:    scrapedEvent.Url,
-//		Url:       scrapedEvent.Url,
-//		Payload:   event.Payload,
-//	}
-//
-//	err = s.repositories.ScraperEvent.Create(ctx, eventLog)
-//	if err != nil {
-//		span.TraceError(err)
-//		return err
-//	}
-//
-//	err = s.publishEvent(ctx, event)
-//	if err != nil {
-//		span.TraceError(err)
-//		return err
-//	}
-//	return nil
-//}
-//
-//func (s *OutboxProcessor) processWebTrackerEvent(ctx context.Context, event *models.OutboxEvent) error {
-//	span, ctx := telemetry.StartServiceSpan(ctx, "OutboxProcessor.processWebTrackerEvent")
-//	defer span.Finish()
-//	span.TagEventType(event.EventType.String())
-//	span.TagEntity(event.ID)
-//
-//	// write event to data warehouse
-//	eventLog := &models.WebTrackerEvent{
-//		ID:        event.ID,
-//		Event:     event.EventType,
-//		Publisher: event.Publisher,
-//		Timestamp: event.CreatedAt,
-//		Tenant:    event.Tenant,
-//		TrackerID: event.EntityID,
-//		SessionID: event.SessionID,
-//		Payload:   event.Payload,
-//	}
-//
-//	err := s.repositories.WebTrackerEvent.Create(ctx, eventLog)
-//	if err != nil {
-//		span.TraceError(err)
-//		return err
-//	}
-//
-//	// determine if event needs to be published
-//	if !strings.HasPrefix(event.EventType.String(), "webtracker.event") {
-//		err = s.publishEvent(ctx, event)
-//		if err != nil {
-//			span.TraceError(err)
-//			return err
-//		}
-//	}
-//
-//	return nil
-//}
-//
-//func (s *OutboxProcessor) publishEvent(ctx context.Context, event *models.OutboxEvent) error {
-//	span, ctx := telemetry.StartServiceSpan(ctx, "OutboxProcessor.publishEvent")
-//	defer span.Finish()
-//	span.TagEventType(event.EventType.String())
-//	span.TagEntity(event.ID)
-//
-//	// Create message with headers
-//	msg := nats.NewMsg(event.EventType.String())
-//	msg.Data = event.Payload
-//	msg.Header.Set(nats_internal.HEADER_TENANT, event.Tenant)
-//
-//	// Publish to the stored subject
-//	_, err := s.natsConn.JS.PublishMsg(msg)
-//	if err != nil {
-//		span.TraceError(err)
-//		return fmt.Errorf("failed to publish outbox event: %w", err)
-//	}
-//
-//	return nil
-//}
+func (s *OutboxProcessor) processTenantCreatedEvent(ctx context.Context, event *postgres_entity.OutboxEvent) error {
+	span, ctx := telemetry.StartServiceSpan(ctx, "OutboxProcessor.processTenantCreatedEvent")
+	defer span.Finish()
+	span.TagEventType(event.EventType.String())
+	span.TagEntity(event.ID)
+
+	// TODO write event to data warehouse
+
+	return s.publishEvent(ctx, event)
+}
+
+func (s *OutboxProcessor) publishEvent(ctx context.Context, event *postgres_entity.OutboxEvent) error {
+	span, ctx := telemetry.StartServiceSpan(ctx, "OutboxProcessor.publishEvent")
+	defer span.Finish()
+	span.TagEventType(event.EventType.String())
+	span.TagEntity(event.ID)
+
+	// Create message with headers
+	msg := nats.NewMsg(event.EventType.String())
+	msg.Data = event.Payload
+	msg.Header.Set(string(nats_common.NATS_HEADER_TENANT), event.Tenant)
+
+	// Publish to the stored subject
+	_, err := s.natsConn.JS.PublishMsg(msg)
+	if err != nil {
+		span.TraceError(err)
+		return fmt.Errorf("failed to publish outbox event: %w", err)
+	}
+
+	return nil
+}
