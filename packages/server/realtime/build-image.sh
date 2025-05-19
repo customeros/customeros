cc1debdcffb9bdc8d3f89d60994afd16a198a367
Alex Calinica <acalinica@customeros.ai>
2025-05-19 16:43:14 +0300

    patch: fix docker path

--- packages/server/realtime/build-image.sh
+++ packages/server/realtime/build-image.sh
@@ -10,7 +10,7 @@ ARCH=${5:-"arm64"}
 REPO=$6
 APP_PATH=$7
 
-CONTAINERFILE_PATH="./Dockerfile"
+CONTAINERFILE_PATH="${APP_PATH}/Dockerfile"
 
 # Ensure required variables are set
 if [ -z "$REGISTRY" ] || [ -z "$IMAGE_NAME" ] || [ -z "$VERSION" ]; then
