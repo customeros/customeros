03adc9c9b8f66742f3808e701020a3da4bda63f5
alexopenline <alex@openline.ai>
2025-05-19 16:06:01 +0300

    patch: search and sort qualification status (#12225)

--- packages/server/customer-os-common-module/enum/qualification_status.go
+++ packages/server/customer-os-common-module/enum/qualification_status.go
@@ -3,6 +3,7 @@ package enum
 type QualificationStatus string
 
 const (
+	QualificationStatusPending      QualificationStatus = "PENDING"
 	QualificationStatusQualifying   QualificationStatus = "QUALIFYING"
 	QualificationStatusQualified    QualificationStatus = "QUALIFIED"
 	QualificationStatusNotQualified QualificationStatus = "UNQUALIFIED"
@@ -12,8 +13,25 @@ func (e QualificationStatus) String() string {
 	return string(e)
 }
 
+func (e QualificationStatus) Order() int64 {
+	switch e {
+	case QualificationStatusPending:
+		return 0
+	case QualificationStatusQualifying:
+		return 1
+	case QualificationStatusQualified:
+		return 2
+	case QualificationStatusNotQualified:
+		return 3
+	default:
+		return 0
+	}
+}
+
 func DecodeQualificationStatus(str string) QualificationStatus {
 	switch str {
+	case QualificationStatusPending.String():
+		return QualificationStatusPending
 	case QualificationStatusQualifying.String():
 		return QualificationStatusQualifying
 	case QualificationStatusQualified.String():
